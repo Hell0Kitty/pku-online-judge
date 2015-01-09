b0cf866d86c1cb7d9ae1a4b3b3d037543196d2ad
sdecoder <sde.work.inbox@gmail.com>
2015-01-09 06:54:53 -0500

    2015-01-09 06:54:53

--- 1067.cc
+++ 1067.cc
@@ -3,20 +3,22 @@
 #include <iostream>
 //#include <cmath>
 using namespace std;
-double p=(sqrt((double)5)+1)/double(2);
+double p = (sqrt((double)5) + 1) / double(2);
 
-inline int myabs(int para){
-    if( para < 0 ) return -1 * para;
-    return para;
+inline int myabs(int para) {
+  if (para < 0) return -1 * para;
+  return para;
 }
 
-int main (){
-    int a,b,c;
-        while(scanf("%d%d",&a,&b)!=EOF){
-        c = myabs(a-b);
-        a=a>b?b:a;
-        if(a==(int)(p*c)) printf("0\n");
-        else printf("1\n");
-    }
-    return 0;
+int main() {
+  int a, b, c;
+  while (scanf("%d%d", &a, &b) != EOF) {
+    c = myabs(a - b);
+    a = a > b ? b : a;
+    if (a == (int)(p * c))
+      printf("0\n");
+    else
+      printf("1\n");
+  }
+  return 0;
 }
