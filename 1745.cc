29cf904dae24699af76267a54d8862ce85751bf8
sdecoder <sde.work.inbox@gmail.com>
2015-01-25 06:48:03 -0500

    2015-01-25 06:48:03

--- 1745.cc
+++ 1745.cc
@@ -9,58 +9,51 @@ int array[maxBound];
 int ncase, k;
 bool dp[maxBound][kBound];
 
-int modfunc(int i, int j){
-  if (i >=0 )
-    return i%j;
+int modfunc(int i, int j) {
+  if (i >= 0) return i % j;
 
-  return abs(i)%j;
+  return abs(i) % j;
 }
 
-void answer(){
+void answer() {
   memset(dp, 0, sizeof(dp));
   dp[1][array[1]] = true;
-  for (int i =1 ;i <= ncase -1 ;i++)
-  {
-    for (int j =0 ;j <= k-1;j++)
-    {
-      if (dp[i][j])
-      {
-        int temp = modfunc(j + array[i+1], k);
-        dp[i+1][temp] = true;
-        dp[i+1][modfunc(j-array[i+1],k)] = true;
+  for (int i = 1; i <= ncase - 1; i++) {
+    for (int j = 0; j <= k - 1; j++) {
+      if (dp[i][j]) {
+        int temp = modfunc(j + array[i + 1], k);
+        dp[i + 1][temp] = true;
+        dp[i + 1][modfunc(j - array[i + 1], k)] = true;
       }
     }
   }
 
-  if (dp[ncase][0])
-  {
+  if (dp[ncase][0]) {
     printf("Divisible\n");
-  }else{
+  } else {
     printf("Not divisible\n");
   }
-//  for (int i = 1 ;i <= ncase;i++)
-//  {
-//    for (int j = 1; j<= k;j++)
-//    {
-//      printf("%d ", dp[i][j]);
-//    }
-//    puts("");
-//  }
-
+  //  for (int i = 1 ;i <= ncase;i++)
+  //  {
+  //    for (int j = 1; j<= k;j++)
+  //    {
+  //      printf("%d ", dp[i][j]);
+  //    }
+  //    puts("");
+  //  }
 }
-int main(){
-  scanf("%d%d",&ncase, &k);
-  for (int i = 1;i<= ncase;i++)
-  {
-    int temp ;
-    scanf("%d",&temp);
-    array[i] = modfunc(temp,k);
+int main() {
+  scanf("%d%d", &ncase, &k);
+  for (int i = 1; i <= ncase; i++) {
+    int temp;
+    scanf("%d", &temp);
+    array[i] = modfunc(temp, k);
   }
-//  for (int i = 1; i<= ncase;i++)
-//  {
-//    printf("%d ", array[i]);
-//  }
-//  puts("");
+  //  for (int i = 1; i<= ncase;i++)
+  //  {
+  //    printf("%d ", array[i]);
+  //  }
+  //  puts("");
   answer();
 
   return 0;
