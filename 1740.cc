b4777418b91aa916d2a3cc06ed6b391a7ff4930f
sdecoder <sde.work.inbox@gmail.com>
2015-02-04 15:48:59 -0500

    2015-02-04 15:48:59

--- 1740.cc
+++ 1740.cc
@@ -11,19 +11,16 @@ long n;
 bool Init() {
   long k;
   cin >> n;
-  for (k = 0; k < n; k++)
-    cin >> h[k];
+  for (k = 0; k < n; k++) cin >> h[k];
   return n != 0;
 }
 
 bool GirlWin() {
   long k;
-  if (n % 2 == 1)
-    return true;
+  if (n % 2 == 1) return true;
   std::sort(h, h + n);
   for (k = 0; k < n; k += 2)
-    if (h[k] != h[k + 1])
-      return true;
+    if (h[k] != h[k + 1]) return true;
   return false;
 }
 
