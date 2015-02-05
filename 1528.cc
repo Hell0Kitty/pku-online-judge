36c97bc4ab9a42ad079d7217a03785032981d090
sdecoder <sde.work.inbox@gmail.com>
2015-02-05 07:15:13 -0500

    2015-02-05 07:15:13

--- 1528.cc
+++ 1528.cc
@@ -4,40 +4,51 @@
 #include <sstream>
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
-int func(int number){
+int func(int number) {
   int sum = 1;
-  for(int i = 2; i < sqrt((double)number); i++){
-    if(number %i ==0 ) {sum += i; sum += number /i;}
+  for (int i = 2; i < sqrt((double)number); i++) {
+    if (number % i == 0) {
+      sum += i;
+      sum += number / i;
+    }
   }
   int reg = sqrt((double)number);
-  if( reg * reg == number){
-    sum += reg; 
+  if (reg * reg == number) {
+    sum += reg;
   }
-  if( sum == number) return 0 ;
-  if (sum > number) return 2;   
- 
+  if (sum == number) return 0;
+  if (sum > number) return 2;
+
   return 1;
-  
 }
 
-int main(){
-  //15 28 6 56 60000 22 496 0
+int main() {
+  // 15 28 6 56 60000 22 496 0
   puts("PERFECTION OUTPUT");
-  while(true){
-    int number; 
+  while (true) {
+    int number;
     scanf("%d", &number);
-    if(number == 0) break;
-    if(number == 1 ) {printf( "%5d  ", number); puts("DEFICIENT"); continue;}
+    if (number == 0) break;
+    if (number == 1) {
+      printf("%5d  ", number);
+      puts("DEFICIENT");
+      continue;
+    }
     int result = func(number);
-    if(result == 0 ) {printf( "%5d  ", number); puts("PERFECT");}
-    else if (result == 1) {printf( "%5d  ", number); puts("DEFICIENT");}
-   else if (result == 2) {printf( "%5d  ", number); puts("ABUNDANT");}
+    if (result == 0) {
+      printf("%5d  ", number);
+      puts("PERFECT");
+    } else if (result == 1) {
+      printf("%5d  ", number);
+      puts("DEFICIENT");
+    } else if (result == 2) {
+      printf("%5d  ", number);
+      puts("ABUNDANT");
+    }
   }
   puts("END OF OUTPUT");
 
-  
-  
   return 0;
 }
