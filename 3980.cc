e68c4a4887bb265affc9dfc774fe9d5ecb4a3b76
sdecoder <sde.work.inbox@gmail.com>
2015-02-15 02:09:28 -0500

    2015-02-15 02:09:28

--- 3980.cc
+++ 3980.cc
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
 
-
-int main()
-{
-	int a,b;
-	while(cin>>a>>b)
-	{
-		cout<<a%b<<endl;
-	}
+int main() {
+  int a, b;
+  while (cin >> a >> b) {
+    cout << a % b << endl;
+  }
 }
