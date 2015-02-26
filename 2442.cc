ef85aac25134f94b41293bb6e6e46de87d7f1180
sdecoder <sde.work.inbox@gmail.com>
2015-02-25 23:16:06 -0500

    2015-02-25 23:16:06

--- 2442.cc
+++ 2442.cc
@@ -23,61 +23,51 @@ using namespace std;
  * 读入第一行只data数组中,排序
  * a.读入下一行至data2数组,排序
  * b.data2[0]与data中各项相加 ,加入最大值堆
- * c.data2[i]加上data中的第0项,所得值如果比堆头元素大,则continue;否则,堆pop后加入所得值,继续加data中的第1,2,3,4...项.(i >= 1)
+ * c.data2[i]加上data中的第0项,所得值如果比堆头元素大,则continue;否则,堆pop后加入所得值,继续加data中的第1,2,3,4...项.(i
+ * >= 1)
  * d.将最大值堆导入到data数组
  * 重复abcd
  * 最后答案就在data中
  */
 
-int main()
-{
-	int t = 0;
-	int m = 0, n = 0;
-	scanf("%d", &t);
-	for (int i = 0; i < t; ++i)
-	{
-		scanf("%d%d", &m, &n);
-		int data[maxm] = {0};
-		int data2[maxm] = {0};
+int main() {
+  int t = 0;
+  int m = 0, n = 0;
+  scanf("%d", &t);
+  for (int i = 0; i < t; ++i) {
+    scanf("%d%d", &m, &n);
+    int data[maxm] = {0};
+    int data2[maxm] = {0};
 
-		for (int k = 0; k < n; ++k)
-		{
-			scanf("%d", &data[k]);
-		}
-		sort(data, data + n);
+    for (int k = 0; k < n; ++k) {
+      scanf("%d", &data[k]);
+    }
+    sort(data, data + n);
 
-		for (int j = 1; j < m; ++j)
-		{
-			for (int k = 0; k < n; ++k)
-				scanf("%d", &data2[k]);
-			sort(data2, data2 + n);
-			priority_queue <int, vector<int>, less<int> > pq;
+    for (int j = 1; j < m; ++j) {
+      for (int k = 0; k < n; ++k) scanf("%d", &data2[k]);
+      sort(data2, data2 + n);
+      priority_queue<int, vector<int>, less<int> > pq;
 
-			for (int k = 0; k < n; ++k)
-			{
-				pq.push(data2[0] + data[k]);
-			}
-			for (int k = 1; k < n; ++k)
-			{
-				for (int l = 0; l < n; ++l)
-				{
-					int tmp = data2[k] + data[l];
-					if (tmp >= pq.top())
-						break;
-					pq.pop();
-					pq.push(tmp);
-				}
-			}
-			for (int k = n - 1; k >= 0; --k)
-			{
-				data[k] = pq.top();
-				pq.pop();
-			}
-		}
+      for (int k = 0; k < n; ++k) {
+        pq.push(data2[0] + data[k]);
+      }
+      for (int k = 1; k < n; ++k) {
+        for (int l = 0; l < n; ++l) {
+          int tmp = data2[k] + data[l];
+          if (tmp >= pq.top()) break;
+          pq.pop();
+          pq.push(tmp);
+        }
+      }
+      for (int k = n - 1; k >= 0; --k) {
+        data[k] = pq.top();
+        pq.pop();
+      }
+    }
 
-		for (int j = 0; j < n; ++j)
-			printf("%d ", data[j]);
-		printf("\n");
-	}
-	return 0;
+    for (int j = 0; j < n; ++j) printf("%d ", data[j]);
+    printf("\n");
+  }
+  return 0;
 }
