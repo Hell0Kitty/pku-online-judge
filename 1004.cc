bb38e3836473621bc66a9e6d20c9cc7a9ff3a495
sdecoder <sde.work.inbox@gmail.com>
2015-03-24 07:25:30 -0400

    2015-03-24 07:25:30

--- 1004.cc
+++ 1004.cc
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    int i;
-    float sum = 0;
-    float input;
-    for (i = 0;i< 12;i++)
-    {
-        scanf("%f",&input);
-        sum = input+sum;
-    }
-    printf("$%.2f\n",sum/12.0);
-    return 0;
+int main() {
+  int i;
+  float sum = 0;
+  float input;
+  for (i = 0; i < 12; i++) {
+    scanf("%f", &input);
+    sum = input + sum;
+  }
+  printf("$%.2f\n", sum / 12.0);
+  return 0;
 }
