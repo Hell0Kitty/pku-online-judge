2eb6bba9e25f4ee5ab9789b19ff2af65e8f005df
sdecoder <sde.work.inbox@gmail.com>
2015-04-16 03:09:24 -0400

    2015-04-16 03:09:24

--- 2470.cc
+++ 2470.cc
@@ -7,12 +7,10 @@ int main() {
 
   int i;
   while (scanf("%d", &n) && n != 0) {
-    for (i = 1; i <= n; i++)
-      scanf("%d", &a[i]);
+    for (i = 1; i <= n; i++) scanf("%d", &a[i]);
 
     for (i = 1; i <= n; i++)
-      if (a[a[i]] != i)
-        break;
+      if (a[a[i]] != i) break;
 
     if (i >= n + 1)
       printf("ambiguous\n");
