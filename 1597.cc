4f352aa855448e546da5c8405c0c8c2fc6be99a4
sdecoder <sde.work.inbox@gmail.com>
2015-04-20 18:59:05 -0400

    2015-04-20 18:59:05

--- 1597.cc
+++ 1597.cc
@@ -1,31 +1,29 @@
-#include"stdio.h"
-#include"string.h"
+#include "stdio.h"
+#include "string.h"
 #define N 100001
-int n,m;
+int n, m;
 int mark[N];
-void fun()
-{
-	int i;
-	int ans;
-	memset(mark,0,sizeof(mark));
-	ans=0;
-	mark[0]=1;
-	for(i=0;i<m;i++)
-	{
-		ans=(ans+n)%m;
-		if(mark[ans]==1)break;
-		mark[ans]=1;
-	}
-	for(i=0;i<m;i++)
-		if(mark[i]!=1)break;
-	if(i>=m)printf("%10d%10d%s\n",n,m,"    Good Choice");
-	else printf("%10d%10d%s\n",n,m,"    Bad Choice");
-	printf("\n");
+void fun() {
+  int i;
+  int ans;
+  memset(mark, 0, sizeof(mark));
+  ans = 0;
+  mark[0] = 1;
+  for (i = 0; i < m; i++) {
+    ans = (ans + n) % m;
+    if (mark[ans] == 1) break;
+    mark[ans] = 1;
+  }
+  for (i = 0; i < m; i++)
+    if (mark[i] != 1) break;
+  if (i >= m)
+    printf("%10d%10d%s\n", n, m, "    Good Choice");
+  else
+    printf("%10d%10d%s\n", n, m, "    Bad Choice");
+  printf("\n");
 }
 
-int main()
-{
-	while(scanf("%d%d",&n,&m)!=-1)
-		fun();
-	return 0;
+int main() {
+  while (scanf("%d%d", &n, &m) != -1) fun();
+  return 0;
 }
