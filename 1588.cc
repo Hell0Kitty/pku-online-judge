5f40ae95bc4523032a7b4f2974f13bb551324a86
sdecoder <sde.work.inbox@gmail.com>
2015-05-11 10:49:21 -0400

    2015-05-11 10:49:21

--- 1588.cc
+++ 1588.cc
@@ -7,41 +7,31 @@
 #include <stack>
 #include <queue>
 
-using namespace std; 
+using namespace std;
 
+int roman_to_int(string s) { return 0; }
 
-int roman_to_int(string s){
-  
-  return 0; 
-}
-
-
-    
-    
-    
-int main(){
+int main() {
   std::ios::sync_with_stdio(false);
 
   string inputbuff;
-  while(cin >> inputbuff){
-    if( inputbuff[0] == '='){
-      cout<< "[dbg]print the result; " << endl;
+  while (cin >> inputbuff) {
+    if (inputbuff[0] == '=') {
+      cout << "[dbg]print the result; " << endl;
 
-    }else if (inputbuff[0] == '+'){
-      cout<< "[dbg]add operator ; " << endl;
-      
-    }else if (inputbuff[0] == '-'){
-      cout<< "[dbg]minus operator ; " << endl;
-      
-    }else if (inputbuff[0] == '*'){
-      cout<< "[dbg]multiple operator ; " << endl;
-      
-    }else if (inputbuff[0] == '/'){
-      cout<< "[dbg]divide operator ; " << endl;
-      
+    } else if (inputbuff[0] == '+') {
+      cout << "[dbg]add operator ; " << endl;
+
+    } else if (inputbuff[0] == '-') {
+      cout << "[dbg]minus operator ; " << endl;
+
+    } else if (inputbuff[0] == '*') {
+      cout << "[dbg]multiple operator ; " << endl;
+
+    } else if (inputbuff[0] == '/') {
+      cout << "[dbg]divide operator ; " << endl;
     }
-    
   }
 
-  return 0; 
+  return 0;
 }
