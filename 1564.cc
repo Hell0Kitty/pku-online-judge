30955f80e15734d26c7e68427ab8a0fda86aa6c8
sdecoder <sde.work.inbox@gmail.com>
2015-05-16 14:27:02 -0400

    2015-05-16 14:27:02

--- 1564.cc
+++ 1564.cc
@@ -9,17 +9,14 @@ void dfs(int now, int now_sum, int ll) {
   int i;
   if (now_sum == sum) {
     flag = 1;
-    for (i = 1; i < ll - 1; i++)
-      cout << ways[i] << '+';
+    for (i = 1; i < ll - 1; i++) cout << ways[i] << '+';
     cout << ways[i] << endl;
     return;
   }
   int last = -1;
   for (i = now + 1; i <= n; i++) {
-    if (last == numb[i])
-      continue;
-    if (now_sum + numb[i] > sum)
-      continue;
+    if (last == numb[i]) continue;
+    if (now_sum + numb[i] > sum) continue;
     ways[ll] = numb[i];
     last = numb[i];
     dfs(i, now_sum + numb[i], ll + 1);
@@ -29,12 +26,10 @@ int main() {
   int i;
   while (cin >> sum >> n, sum, n) {
     flag = 0;
-    for (i = 1; i <= n; i++)
-      cin >> numb[i];
+    for (i = 1; i <= n; i++) cin >> numb[i];
     cout << "Sums of " << sum << ':' << endl;
     dfs(0, 0, 1);
-    if (!flag)
-      cout << "NONE\n";
+    if (!flag) cout << "NONE\n";
   }
   return 0;
 }
