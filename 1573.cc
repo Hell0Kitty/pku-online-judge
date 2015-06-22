553d1b91b7a791cf051c044f0aa7ab07cdd561cc
sdecoder <sde.work.inbox@gmail.com>
2015-06-22 17:26:37 -0400

    2015-06-22 17:26:37

--- 1573.cc
+++ 1573.cc
@@ -8,103 +8,92 @@
 
 using namespace std;
 
-struct point{
-  int x; 
-  int y; 
+struct point {
+  int x;
+  int y;
 };
 
 std::vector<struct point> vp;
 char matrix[20][20];
-int row, column, enter; 
+int row, column, enter;
 
-bool out_of_bound(int x, int y){
-  if(x<0 || y <0) return true; 
-  if(x >= row || y >=column ) return true; 
-  return false; 
+bool out_of_bound(int x, int y) {
+  if (x < 0 || y < 0) return true;
+  if (x >= row || y >= column) return true;
+  return false;
 }
 
-
-bool repeat(int newx, int newy){
-  for (int i = 0; i < vp.size(); ++i)
-  {
-    if( newx == vp[i].x && newy == vp[i].y ){
-      cout << i <<" step(s) before a loop of " << vp.size() -i <<" step(s)" << endl;
-      return true; 
+bool repeat(int newx, int newy) {
+  for (int i = 0; i < vp.size(); ++i) {
+    if (newx == vp[i].x && newy == vp[i].y) {
+      cout << i << " step(s) before a loop of " << vp.size() - i << " step(s)"
+           << endl;
+      return true;
     }
   }
   return false;
 }
-void run(int enter){
-  int cur = 0; 
-  int startx = 0; 
-  int starty = enter -1 ;
+void run(int enter) {
+  int cur = 0;
+  int startx = 0;
+  int starty = enter - 1;
   struct point p;
-  p.x = startx; p.y = starty; 
+  p.x = startx;
+  p.y = starty;
   cout << "[dbg] the startx " << startx << " starty: " << starty << endl;
   vp.clear();
   vp.push_back(p);
 
-  while(true){
-    cur ++;
+  while (true) {
+    cur++;
     char inst = matrix[p.x][p.y];
-    if( inst == 'E') {
+    if (inst == 'E') {
       p.y++;
       struct point newpoint = p;
       vp.push_back(newpoint);
-      if(out_of_bound(p.x, p.y )) break;  
-      if(repeat(p.x, p.y )) {
-
-        return; 
+      if (out_of_bound(p.x, p.y)) break;
+      if (repeat(p.x, p.y)) {
+        return;
       }
-    } else if( inst == 'W') {
-      
-      p.y --; 
+    } else if (inst == 'W') {
+      p.y--;
       vp.push_back(newpoint);
-      cout << "[dbg] now at " << newx << " " << newy << " W" << endl ;
-      if(out_of_bound(p.x, p.y )) break;  
-      if(repeat(p.x, p.y )) {
-        return; 
+      cout << "[dbg] now at " << newx << " " << newy << " W" << endl;
+      if (out_of_bound(p.x, p.y)) break;
+      if (repeat(p.x, p.y)) {
+        return;
       }
 
-    } else if( inst == 'N') {
-      p.x ++; 
+    } else if (inst == 'N') {
+      p.x++;
       vp.push_back(newpoint);
-      if(out_of_bound(p.x, p.y )) break;  
-      if(repeat(p.x, p.y )) {
-
-        return; 
+      if (out_of_bound(p.x, p.y)) break;
+      if (repeat(p.x, p.y)) {
+        return;
       }
 
     } else {
       // inst == 's'
-      p.x --;
+      p.x--;
       vp.push_back(newpoint);
-      if(out_of_bound(p.x, p.y  )) break;  
-      if(repeat(p.x, p.y )) {
-
-        return; 
+      if (out_of_bound(p.x, p.y)) break;
+      if (repeat(p.x, p.y)) {
+        return;
       }
-
     }
-  } 
-  cout << cur << " step(s) to exit" << endl; 
-
-
+  }
+  cout << cur << " step(s) to exit" << endl;
 }
 
-
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   std::ios::sync_with_stdio(false);
-  while(true){
-    cin >> row >> column >> enter; 
-    if(!row && !column && !enter) break; 
-    for (int i = 0; i < row; ++i)
-    {
-      string tmp ;
-      cin >> tmp ; 
-      for (int j = 0; j < column; ++j)
-      {
+  while (true) {
+    cin >> row >> column >> enter;
+    if (!row && !column && !enter) break;
+    for (int i = 0; i < row; ++i) {
+      string tmp;
+      cin >> tmp;
+      for (int j = 0; j < column; ++j) {
         matrix[i][j] = tmp[j];
       }
     }
