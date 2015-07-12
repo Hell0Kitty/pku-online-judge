1fa7ff662110d11a97b56ae4482425f0b8a5a437
sdecoder <sde.work.inbox@gmail.com>
2015-07-12 02:07:46 -0400

    2015-07-12 02:07:46

--- 1922.cc
+++ 1922.cc
@@ -5,26 +5,24 @@
 #include <algorithm>
 
 using namespace std;
-const int INF=0x3f3f3f3f;
+const int INF = 0x3f3f3f3f;
 int n;
 double ans;
-int main()
-{
-    int v,t;
-    double time;
-    while(scanf("%d",&n),n){
-        ans=INF;
-        for(int i=1;i<=n;i++){
-            scanf("%d%d",&v,&t);
-            if(t<0)
-                continue;
-            time=t+4.5/v*3600;
-            ans=min(time,ans);
-            //if(ans>time)
-               // ans=time;
-        }
-        int res=ceil(ans);
-        printf("%d\n",res);
+int main() {
+  int v, t;
+  double time;
+  while (scanf("%d", &n), n) {
+    ans = INF;
+    for (int i = 1; i <= n; i++) {
+      scanf("%d%d", &v, &t);
+      if (t < 0) continue;
+      time = t + 4.5 / v * 3600;
+      ans = min(time, ans);
+      // if(ans>time)
+      // ans=time;
     }
-    return 0;
+    int res = ceil(ans);
+    printf("%d\n", res);
+  }
+  return 0;
 }
