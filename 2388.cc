aed0dca6f7d503d316b295101b5a595baa5c3a26
sdecoder <sde.work.inbox@gmail.com>
2015-07-17 15:06:29 -0400

    2015-07-17 15:06:29

--- 2388.cc
+++ 2388.cc
@@ -3,27 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
   std::ios::sync_with_stdio(false);
 
-  int ncase = 0; 
+  int ncase = 0;
   cin >> ncase;
-  if(ncase == 0) {
+  if (ncase == 0) {
     cout << 0 << endl;
     return 0;
   }
 
   int* nums = new int[ncase];
-  for (int i = 0; i < ncase; ++i)
-  {
-    cin >> nums[i]; 
+  for (int i = 0; i < ncase; ++i) {
+    cin >> nums[i];
   }
 
-   sort(nums, nums + ncase);
-   
-  if( ncase & 0x1 ) cout<< nums[ncase >> 1] << endl;
-  else cout<< (nums[ncase>>1] + nums[(ncase>>1) +1] ) /2 << endl;
-  
+  sort(nums, nums + ncase);
+
+  if (ncase & 0x1)
+    cout << nums[ncase >> 1] << endl;
+  else
+    cout << (nums[ncase >> 1] + nums[(ncase >> 1) + 1]) / 2 << endl;
 
   delete nums;
   return 0;
