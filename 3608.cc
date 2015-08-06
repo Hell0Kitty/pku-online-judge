bf47a1e287a3b5af182fcb80db0e76fd7fdce2da
sdecoder <sde.work.inbox@gmail.com>
2015-08-06 01:21:49 -0400

    2015-08-06 01:21:49

--- 3608.cc
+++ 3608.cc
@@ -16,14 +16,12 @@ typedef struct TPolygon {
 typedef struct TLine { double a, b, c; } TLine;
 
 double max(double a, double b) {
-  if (a > b)
-    return a;
+  if (a > b) return a;
   return b;
 }
 
 double min(double a, double b) {
-  if (a < b)
-    return a;
+  if (a < b) return a;
   return b;
 }
 
@@ -64,14 +62,11 @@ double disPointToSeg(TPoint p1, TPoint p2, TPoint p3) {
   double a = distance(p1, p2);
   double b = distance(p1, p3);
   double c = distance(p2, p3);
-  if (fabs(a + b - c) < eps)
-    return 0;
-  if (fabs(a + c - b) < eps || fabs(b + c - a) < eps)
-    return min(a, b);
+  if (fabs(a + b - c) < eps) return 0;
+  if (fabs(a + c - b) < eps || fabs(b + c - a) < eps) return min(a, b);
   double t1 = -a * a + b * b + c * c;
   double t2 = a * a - b * b + c * c;
-  if (t1 <= 0 || t2 <= 0)
-    return min(a, b);
+  if (t1 <= 0 || t2 <= 0) return min(a, b);
 
   TLine l1 = lineFromSegment(p2, p3);
   return fabs(l1.a * p1.x + l1.b * p1.y + l1.c) /
@@ -95,21 +90,16 @@ double angle(TPoint p1, TPoint p2, double SlewRate) {
       ang = 3 * pi / 2;
   } else {
     ang = atan(p.y / p.x);
-    if (p.x < 0)
-      ang += pi;
+    if (p.x < 0) ang += pi;
   }
-  while (ang < 0)
-    ang += 2 * pi;
-  if (ang >= pi)
-    SlewRate += pi;
+  while (ang < 0) ang += 2 * pi;
+  if (ang >= pi) SlewRate += pi;
   if (ang > SlewRate)
     tmp = ang - SlewRate;
   else
     tmp = pi - (SlewRate - ang);
-  while (tmp >= pi)
-    tmp -= pi;
-  if (fabs(tmp - pi) < eps)
-    tmp = 0;
+  while (tmp >= pi) tmp -= pi;
+  if (fabs(tmp - pi) < eps) tmp = 0;
   return tmp;
 }
 
@@ -121,37 +111,28 @@ int main() {
   while (scanf("%d%d", &n, &m) && n) {
     polygon1.n = n;
     polygon2.n = m;
-    for (i = 0; i < n; i++)
-      scanf("%lf%lf", &polygon1.p[i].x, &polygon1.p[i].y);
-    for (i = 0; i < m; i++)
-      scanf("%lf%lf", &polygon2.p[i].x, &polygon2.p[i].y);
-    if (polygonArea(polygon1) < 0)
-      ChangeClockwise(polygon1);
-    if (polygonArea(polygon2) < 0)
-      ChangeClockwise(polygon2);
+    for (i = 0; i < n; i++) scanf("%lf%lf", &polygon1.p[i].x, &polygon1.p[i].y);
+    for (i = 0; i < m; i++) scanf("%lf%lf", &polygon2.p[i].x, &polygon2.p[i].y);
+    if (polygonArea(polygon1) < 0) ChangeClockwise(polygon1);
+    if (polygonArea(polygon2) < 0) ChangeClockwise(polygon2);
     ymin1 = inf, ymax2 = -inf;
     for (i = 0; i < n; i++)
-      if (polygon1.p[i].y < ymin1)
-        ymin1 = polygon1.p[i].y, k1 = i;
+      if (polygon1.p[i].y < ymin1) ymin1 = polygon1.p[i].y, k1 = i;
     for (i = 0; i < m; i++)
-      if (polygon2.p[i].y > ymax2)
-        ymax2 = polygon2.p[i].y, k2 = i;
+      if (polygon2.p[i].y > ymax2) ymax2 = polygon2.p[i].y, k2 = i;
     double SlewRate = 0;
     double angle1, angle2;
     ans = inf;
     double Slope = 0;
     while (Slope <= 360) {
-      while (SlewRate >= pi)
-        SlewRate -= pi;
-      if (fabs(pi - SlewRate) < eps)
-        SlewRate = 0;
+      while (SlewRate >= pi) SlewRate -= pi;
+      if (fabs(pi - SlewRate) < eps) SlewRate = 0;
       angle1 = angle(polygon1.p[k1], polygon1.p[(k1 + 1) % n], SlewRate);
       angle2 = angle(polygon2.p[k2], polygon2.p[(k2 + 1) % m], SlewRate);
       if (fabs(angle1 - angle2) < eps) {
         d = disPallSeg(polygon1.p[k1], polygon1.p[(k1 + 1) % n], polygon2.p[k2],
                        polygon2.p[(k2 + 1) % m]);
-        if (d < ans)
-          ans = d;
+        if (d < ans) ans = d;
         k1++;
         k1 %= n;
         k2++;
@@ -161,8 +142,7 @@ int main() {
       } else if (angle1 < angle2) {
         d = disPointToSeg(polygon2.p[k2], polygon1.p[k1],
                           polygon1.p[(k1 + 1) % n]);
-        if (d < ans)
-          ans = d;
+        if (d < ans) ans = d;
         k1++;
         k1 %= n;
         SlewRate += angle1;
@@ -170,8 +150,7 @@ int main() {
       } else {
         d = disPointToSeg(polygon1.p[k1], polygon2.p[k2],
                           polygon2.p[(k2 + 1) % m]);
-        if (d < ans)
-          ans = d;
+        if (d < ans) ans = d;
         k2++;
         k2 %= m;
         SlewRate += angle2;
