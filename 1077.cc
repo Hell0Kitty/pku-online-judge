44acc90cab5ede36227e3fa6c78921e1bf130007
sdecoder <sde.work.inbox@gmail.com>
2015-08-08 20:25:14 -0400

    2015-08-08 20:25:14

--- 1077.cc
+++ 1077.cc
@@ -10,31 +10,27 @@
 #include <iostream>
 //#include <cstring>
 #include <cassert>
-#define DEST 46233 // The sequence num of "123456780",the destination
+#define DEST 46233  // The sequence num of "123456780",the destination
 using namespace std;
 struct Result {
   char status[10];
-  short xPos; // Current position of x
-  char move[50]; // record the current searching record! Important
+  short xPos;     // Current position of x
+  char move[50];  // record the current searching record! Important
 };
 Result queue1[25000] = {0}, *p1 = queue1, *q1 = queue1 + 1;
 Result queue2[25000] = {{"123456780", 8, "\0"}}, *p2 = queue2, *q2 = queue2 + 1;
 
-
-
-void strrev(char *s)
-{
- char temp;
-    for(char *end = s + strlen(s) - 1; end > s ; --end, ++s)
-    {
-        temp = *s;
-        *s = *end;
-        *end = temp;
-    }
+void strrev(char *s) {
+  char temp;
+  for (char *end = s + strlen(s) - 1; end > s; --end, ++s) {
+    temp = *s;
+    *s = *end;
+    *end = temp;
   }
+}
 
 char Forward[50] = {0}, Backward[50] = {0};
-static short used[362881] = {false}; // to mark the searching sequence
+static short used[362881] = {false};  // to mark the searching sequence
 // now: the position of q (q1 == queue1 + now1)
 int count1 = 1, count2 = 1, now1 = 1, now2 = 1;
 
@@ -42,8 +38,7 @@ int SequenceNum(const Result *m) {
   int big[9] = {0}, sum = 0;
   for (int i = 0; i < 8; ++i) {
     for (int j = i + 1; j < 9; ++j) {
-      if (m->status[i] > m->status[j])
-        ++big[8 - i];
+      if (m->status[i] > m->status[j]) ++big[8 - i];
     }
   }
   int fac = 1;
@@ -58,7 +53,6 @@ void BFS() {
   int step, pos, Snum;
   char temp;
   while (true) {
-
     /* queue1*/
 
     assert(count1 < 24999);
@@ -81,10 +75,9 @@ void BFS() {
           strcpy(Backward, (queue2 + (-used[Snum]))->move);
           return;
         } else {
-          used[Snum] = now1; // mark a positive number
+          used[Snum] = now1;  // mark a positive number
           ++q1, ++now1, ++count1;
-          if (q1 == queue1 + 25000)
-            q1 = queue1 + 1, now1 = 1;
+          if (q1 == queue1 + 25000) q1 = queue1 + 1, now1 = 1;
         }
       }
     }
@@ -106,10 +99,9 @@ void BFS() {
           strcpy(Backward, (queue2 + (-used[Snum]))->move);
           return;
         } else {
-          used[Snum] = now1; // mark a positive number
+          used[Snum] = now1;  // mark a positive number
           ++q1, ++now1, ++count1;
-          if (q1 == queue1 + 25000)
-            q1 = queue1 + 1, now1 = 1;
+          if (q1 == queue1 + 25000) q1 = queue1 + 1, now1 = 1;
         }
       }
     }
@@ -131,10 +123,9 @@ void BFS() {
           strcpy(Backward, (queue2 + (-used[Snum]))->move);
           return;
         } else {
-          used[Snum] = now1; // mark a positive number
+          used[Snum] = now1;  // mark a positive number
           ++q1, ++now1, ++count1;
-          if (q1 == queue1 + 25000)
-            q1 = queue1 + 1, now1 = 1;
+          if (q1 == queue1 + 25000) q1 = queue1 + 1, now1 = 1;
         }
       }
     }
@@ -156,16 +147,14 @@ void BFS() {
           strcpy(Backward, (queue2 + (-used[Snum]))->move);
           return;
         } else {
-          used[Snum] = now1; // mark a positive number
+          used[Snum] = now1;  // mark a positive number
           ++q1, ++now1, ++count1;
-          if (q1 == queue1 + 25000)
-            q1 = queue1 + 1, now1 = 1;
+          if (q1 == queue1 + 25000) q1 = queue1 + 1, now1 = 1;
         }
       }
     }
     // p1 is over
-    if (++p1 == queue1 + 25000)
-      p1 = queue1 + 1;
+    if (++p1 == queue1 + 25000) p1 = queue1 + 1;
     --count1;
 
     /* queue2 */
@@ -184,16 +173,15 @@ void BFS() {
       // queue1 is positive queue2 is negative 0 means unsearched
       if (used[Snum] >= 0) {
         strcpy(q2->move, p2->move);
-        q2->move[step] = 'd'; // searching backward,the move is the opposite
+        q2->move[step] = 'd';  // searching backward,the move is the opposite
         if (used[Snum] > 0) {
           strcpy(Forward, (queue1 + used[Snum])->move);
           strcpy(Backward, q2->move);
           return;
         } else {
-          used[Snum] = -now2; // mark a negative number
+          used[Snum] = -now2;  // mark a negative number
           ++q2, ++now2, ++count2;
-          if (q2 == queue2 + 25000)
-            q2 = queue2 + 1, now2 = 1;
+          if (q2 == queue2 + 25000) q2 = queue2 + 1, now2 = 1;
         }
       }
     }
@@ -215,10 +203,9 @@ void BFS() {
           strcpy(Backward, q2->move);
           return;
         } else {
-          used[Snum] = -now2; // mark a negative number
+          used[Snum] = -now2;  // mark a negative number
           ++q2, ++now2, ++count2;
-          if (q2 == queue2 + 25000)
-            q2 = queue2 + 1, now2 = 1;
+          if (q2 == queue2 + 25000) q2 = queue2 + 1, now2 = 1;
         }
       }
     }
@@ -237,14 +224,13 @@ void BFS() {
         q2->move[step] = 'r';
         if (used[Snum] > 0) {
           strcpy(Forward,
-                 (queue1 + used[Snum])->move); // bug1 detected:q1->queue1
+                 (queue1 + used[Snum])->move);  // bug1 detected:q1->queue1
           strcpy(Backward, q2->move);
           return;
         } else {
-          used[Snum] = -now2; // mark a negative number
+          used[Snum] = -now2;  // mark a negative number
           ++q2, ++now2, ++count2;
-          if (q2 == queue2 + 25000)
-            q2 = queue2 + 1, now2 = 1;
+          if (q2 == queue2 + 25000) q2 = queue2 + 1, now2 = 1;
         }
       }
     }
@@ -266,16 +252,14 @@ void BFS() {
           strcpy(Backward, q2->move);
           return;
         } else {
-          used[Snum] = -now2; // bug2 detected: negative number
+          used[Snum] = -now2;  // bug2 detected: negative number
           ++q2, ++now2, ++count2;
-          if (q2 == queue2 + 25000)
-            q2 = queue2 + 1, now2 = 1;
+          if (q2 == queue2 + 25000) q2 = queue2 + 1, now2 = 1;
         }
       }
     }
     // p1 is over
-    if (++p2 == queue2 + 25000)
-      p2 = queue2 + 1;
+    if (++p2 == queue2 + 25000) p2 = queue2 + 1;
     --count2;
   }
 }
@@ -292,10 +276,8 @@ int main() {
   int reversepair = 0;
   for (int i = 0; i < 8; ++i) {
     for (int j = i + 1; j < 9; ++j) {
-      if (p1->status[i] == '0' || p1->status[j] == '0')
-        continue;
-      if (p1->status[i] < p1->status[j])
-        ++reversepair;
+      if (p1->status[i] == '0' || p1->status[j] == '0') continue;
+      if (p1->status[i] < p1->status[j]) ++reversepair;
     }
   }
   if (reversepair % 2) {
@@ -307,12 +289,12 @@ int main() {
     printf("0\n");
     return 0;
   }
-  used[SequenceNum(p1)] = 30000; // mark first
+  used[SequenceNum(p1)] = 30000;  // mark first
   used[DEST] = -30000;
   memset(p1->move, 0, 100 * sizeof(char));
   /* Search and print result */
   BFS();
-  //strrev(Backward);
+  // strrev(Backward);
 
   strrev(Backward);
   printf("%s%s\n", Forward, Backward);
