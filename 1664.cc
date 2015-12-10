429bec6df16007726405d86e65674a69ebc93ad2
sdecoder <sde.work.inbox@gmail.com>
2015-12-09 19:52:58 -0500

    2015-12-09 19:52:58

--- 1664.cc
+++ 1664.cc
@@ -4,13 +4,11 @@ using namespace std;
 
 int f(int i, int j, int maxVal) {
   int sum = 0;
-  if (j == 1)
-    return 1;
-  if (i == 1 || i == 0)
-    return 1;
+  if (j == 1) return 1;
+  if (i == 1 || i == 0) return 1;
   int k;
   for (k = min(maxVal, i); k * j >= i;
-       k--) // k值不能小于当前平均每个盘子中的苹果数
+       k--)  // k值不能小于当前平均每个盘子中的苹果数
   {
     sum = sum + f(i - k, j - 1, k);
   }
