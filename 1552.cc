304c460b753f7c665c5fe2452396a738c2fdb85e
sdecoder <sde.work.inbox@gmail.com>
2015-12-12 01:20:13 -0500

    2015-12-12 01:20:13

--- 1552.cc
+++ 1552.cc
@@ -5,20 +5,17 @@ using namespace std;
 int main() {
   int i, j, x[20], leight, num;
   while (cin >> x[0]) {
-    if (x[0] == -1)
-      break;
+    if (x[0] == -1) break;
     num = 0;
     leight = 1;
     while (cin >> x[leight]) {
-      if (x[leight] == 0)
-        break;
+      if (x[leight] == 0) break;
       leight++;
     }
     sort(x, x + leight + 1);
     for (i = 0; i < leight; i++)
       for (j = i + 1; j <= leight; j++)
-        if (2 * x[i] == x[j])
-          num++;
+        if (2 * x[i] == x[j]) num++;
     cout << num << endl;
   }
   return 0;
