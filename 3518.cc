7dac12561a61edbf4b3081eb141440e59729a661
sdecoder <sde.work.inbox@gmail.com>
2015-12-15 19:54:22 -0500

    2015-12-15 19:54:22

--- 3518.cc
+++ 3518.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-const int N = 1300000; //第10万个数为1299709
+const int N = 1300000;  //第10万个数为1299709
 int prime[N];
 bool notPrime[N];
 
 int main() {
-
   memset(prime, 0, sizeof(prime));
   memset(notPrime, 0, sizeof(notPrime));
   int count = 0;
-  for (int i = 2; i < N; i++) //求素数表
+  for (int i = 2; i < N; i++)  //求素数表
   {
     if (!notPrime[i]) {
       prime[count++] = i;
@@ -18,15 +17,14 @@ int main() {
 
     for (int j = 0; j < count && (i * prime[j] < N); j++) {
       notPrime[i * prime[j]] = true;
-      if (!(i % prime[j]))
-        break;
+      if (!(i % prime[j])) break;
     }
   }
 
   int test;
   while (cin >> test && test != 0) {
     for (int i = 0; i < 100000; i++) {
-      if (prime[i] == test) //输入的是素数
+      if (prime[i] == test)  //输入的是素数
       {
         cout << "0" << endl;
         break;
