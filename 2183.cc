6bfc44f4362e1dab66e5bb0cad8ef441d8848c99
sdecoder <sde.work.inbox@gmail.com>
2016-01-02 01:57:20 -0500

    2016-01-02 01:57:20

--- 2183.cc
+++ 2183.cc
@@ -9,24 +9,18 @@ using namespace std;
 int n;
 int vis[maxn];
 
-int main()
-{
-    scanf("%d", &n);
-    memset(vis, -1, sizeof(vis));
-    int i = 0;
-    while (1)
-    {
-        if (vis[n] != -1)
-            break;
-        vis[n] = i;
-        n = n / 10 % 10000;
-        n = n * n;
-        n %= 1000000;
-        i++;
-    }
-    printf("%d %d %d\n", n, i - vis[n], i);
-    return 0;
+int main() {
+  scanf("%d", &n);
+  memset(vis, -1, sizeof(vis));
+  int i = 0;
+  while (1) {
+    if (vis[n] != -1) break;
+    vis[n] = i;
+    n = n / 10 % 10000;
+    n = n * n;
+    n %= 1000000;
+    i++;
+  }
+  printf("%d %d %d\n", n, i - vis[n], i);
+  return 0;
 }
-
-
-
