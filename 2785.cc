102df92a7fc49e9f667136ece8f7d06358b10e71
sdecoder <sde.work.inbox@gmail.com>
2016-01-03 02:46:06 -0500

    2016-01-03 02:46:06

---
+++ 2785.cc
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 4100;
+long long a[maxn], b[maxn], c[maxn], d[maxn];
+long long cd[maxn * maxn];
+
+int main()
+{
+	int n;
+	while (scanf("%d", &n) != EOF) {
+		for (int i = 0; i < n; i++) {
+			scanf("%lld%lld%lld%lld", &a[i], &b[i], &c[i], &d[i]);
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cd[i*n+j] = c[i] + d[j];
+			}
+		}
+		sort(cd, cd + (n * n));
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				long long need = -(a[i] + b[j]);
+				ans += upper_bound(cd, cd + n * n, need) - lower_bound(cd, cd + n * n, need);
+			}
+		}
+
+		printf("%d\n", ans);
+	}
+	return 0;
+}
+
+
