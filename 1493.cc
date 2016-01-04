02dff8ef1535b9c151e9569acb8376329611777f
sdecoder <sde.work.inbox@gmail.com>
2016-01-03 23:20:23 -0500

    2016-01-03 23:20:23

--- 1493.cc
+++ 1493.cc
@@ -9,35 +9,26 @@ using namespace std;
 int f[maxn];
 int n, ans;
 
-void input()
-{
-    ans = 0;
+void input() {
+  ans = 0;
+  getchar();
+  for (int i = 0; i < n; i++) {
+    f[i] = 0;
+    for (int j = 0; j < 25; j++) {
+      char ch = getchar();
+      if (ch == 'X') f[i]++;
+    }
+    ans = max(ans, f[i]);
     getchar();
-    for (int i = 0; i < n; i++)
-    {
-        f[i] = 0;
-        for (int j = 0; j < 25; j++)
-        {
-            char ch = getchar();
-            if (ch == 'X')
-                f[i]++;
-        }
-        ans = max(ans, f[i]);
-        getchar();
-    }
+  }
 }
 
-int main()
-{
-    while (scanf("%d", &n), n)
-    {
-        input();
-        int d = 0;
-        for (int i = 0; i < n; i++)
-            d += ans - f[i];
-        printf("%d\n", d);
-    }
-    return 0;
+int main() {
+  while (scanf("%d", &n), n) {
+    input();
+    int d = 0;
+    for (int i = 0; i < n; i++) d += ans - f[i];
+    printf("%d\n", d);
+  }
+  return 0;
 }
-
-
