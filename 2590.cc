804b8777915f714db7fe4b969c7adf8fd414f9b6
sdecoder <sde.work.inbox@gmail.com>
2016-01-05 03:42:27 -0500

    2016-01-05 03:42:27

--- 2590.cc
+++ 2590.cc
@@ -3,34 +3,29 @@
 #include <math.h>
 
 int T;
-__int64 m,n;
+__int64 m, n;
 
-__int64 tostep(__int64 s)
-{
-	__int64 j,k;
-	k=(__int64 )ceil(pow(s*1.0 , 0.5));
-	j=k*(k-1);
-	if(s<=j)
-		return 2*k-2;
-	else
-		return 2*k-1;
+__int64 tostep(__int64 s) {
+  __int64 j, k;
+  k = (__int64)ceil(pow(s * 1.0, 0.5));
+  j = k * (k - 1);
+  if (s <= j)
+    return 2 * k - 2;
+  else
+    return 2 * k - 1;
 }
 
-int main()
-{
-	int i,j;
-	__int64 k;
-	scanf("%d",&T);
-	for(i=0;i<T;i++)
-	{
-		scanf("%d%d",&m,&n);
-		k=n-m;
-		if(k==0)
-			printf("0\n");
-		else
-			printf("%I64d\n",tostep(k));
-	}
-	return 0;
+int main() {
+  int i, j;
+  __int64 k;
+  scanf("%d", &T);
+  for (i = 0; i < T; i++) {
+    scanf("%d%d", &m, &n);
+    k = n - m;
+    if (k == 0)
+      printf("0\n");
+    else
+      printf("%I64d\n", tostep(k));
+  }
+  return 0;
 }
-
-
