e7cace5113a6bea29e26722d2d406976ab6f33db
sdecoder <sde.work.inbox@gmail.com>
2016-01-05 06:38:29 -0500

    2016-01-05 06:38:29

--- 3652.cc
+++ 3652.cc
@@ -2,45 +2,33 @@
 using namespace std;
 int A, B, C, S;
 
-int sovel()
-{
- int x = S;
- int state = 0;
- int n = 1;
- while ( (x = (A * x + B) % C ) && x != S && n <= C)
- {
-  n ++;
-  state |= (x ^ S);
- }
- return state;
-}
-int main()
-{
- int i;
- int state;
- while (scanf("%d", &A) && A)
- {
-  scanf("%d %d %d", &B, &C, &S);
-  state = sovel();
-  for (i = 1; i <= 16; i++)
-  {
-   if (state & (1 << (16 - i)))
-   {
-    printf("?");
-   }
-   else
-   {
-    if( S & (1 << (16 - i)) )
-     printf("1");
-    else
-     printf("0");
-   }
+int sovel() {
+  int x = S;
+  int state = 0;
+  int n = 1;
+  while ((x = (A * x + B) % C) && x != S && n <= C) {
+    n++;
+    state |= (x ^ S);
   }
-  printf("\n");
-
- }
- return 0;
+  return state;
 }
-
-
-
+int main() {
+  int i;
+  int state;
+  while (scanf("%d", &A) && A) {
+    scanf("%d %d %d", &B, &C, &S);
+    state = sovel();
+    for (i = 1; i <= 16; i++) {
+      if (state & (1 << (16 - i))) {
+        printf("?");
+      } else {
+        if (S & (1 << (16 - i)))
+          printf("1");
+        else
+          printf("0");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
