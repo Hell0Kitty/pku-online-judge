202de50d7593edd7552bb8c6829b027fe9794c2f
sdecoder <sde.work.inbox@gmail.com>
2016-01-08 18:23:41 -0500

    2016-01-08 18:23:41

--- 1299.cc
+++ 1299.cc
@@ -6,45 +6,36 @@ using namespace std;
 
 #define pi 3.14159
 
-bool reachable(int x, int y, int z, int &ans)
-{
-    if (360 - z < z)
-        z = 360 - z;
-    if (z == 0)
-    {
-        ans = y;
-        return true;
-    }
-    double dist = 2 * pi * x * z / 360;
-    double fuel_need = dist * 2 / 5;
-    if (y >= fuel_need)
-    {
-        ans = y - fuel_need;
-        return true;
-    }
-    ans = y * 5;
-    return false;
+bool reachable(int x, int y, int z, int &ans) {
+  if (360 - z < z) z = 360 - z;
+  if (z == 0) {
+    ans = y;
+    return true;
+  }
+  double dist = 2 * pi * x * z / 360;
+  double fuel_need = dist * 2 / 5;
+  if (y >= fuel_need) {
+    ans = y - fuel_need;
+    return true;
+  }
+  ans = y * 5;
+  return false;
 }
 
-int main()
-{
-    char st[20];
-    int x, y, z;
-    int ans;
-    while (1)
-    {
-        gets(st);
-        if (st[0] == 'E')
-            break;
-        scanf("%d%d%d", &x, &y, &z);
-        if (reachable(x, y, z, ans))
-            printf("YES %d\n", ans);
-        else
-            printf("NO %d\n", ans);
-        gets(st);
-        gets(st);
-    }
-    return 0;
+int main() {
+  char st[20];
+  int x, y, z;
+  int ans;
+  while (1) {
+    gets(st);
+    if (st[0] == 'E') break;
+    scanf("%d%d%d", &x, &y, &z);
+    if (reachable(x, y, z, ans))
+      printf("YES %d\n", ans);
+    else
+      printf("NO %d\n", ans);
+    gets(st);
+    gets(st);
+  }
+  return 0;
 }
-
-
