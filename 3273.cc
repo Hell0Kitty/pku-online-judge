70b865db4ab08fe72f9f12d57595996783797603
sdecoder <sde.work.inbox@gmail.com>
2016-01-11 05:28:26 -0500

    2016-01-11 05:28:26

--- 3273.cc
+++ 3273.cc
@@ -8,58 +8,45 @@ using namespace std;
 
 int n, m, f[maxn], l, r, mid;
 
-void init()
-{
-    scanf("%d%d", &n, &m);
-    l =0;
-    r =0;
-    for (int i =0; i < n; i++)
-    {
-        scanf("%d", &f[i]);
-        if (l < f[i])
-            l = f[i];
-        r += f[i];
-    }
+void init() {
+  scanf("%d%d", &n, &m);
+  l = 0;
+  r = 0;
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &f[i]);
+    if (l < f[i]) l = f[i];
+    r += f[i];
+  }
 }
 
-bool ok(int a)
-{
-    int sum =0, j =0;
+bool ok(int a) {
+  int sum = 0, j = 0;
 
-    for (int i =0; i < n; i++)
-    {
-        if (sum + f[i] > a)
-        {
-            sum = f[i];
-            j++;
-        }
-        else
-            sum += f[i];
-    }
-    j++;
-    if (j > m)
-        return false;
-    return true;
+  for (int i = 0; i < n; i++) {
+    if (sum + f[i] > a) {
+      sum = f[i];
+      j++;
+    } else
+      sum += f[i];
+  }
+  j++;
+  if (j > m) return false;
+  return true;
 }
 
-void binarysearch()
-{
-    while (l < r)
-    {
-        mid = (l + r) /2;
-        if (!ok(mid))
-            l = mid +1;
-        else
-            r = mid;
-    }
-    printf("%d\n", l);
+void binarysearch() {
+  while (l < r) {
+    mid = (l + r) / 2;
+    if (!ok(mid))
+      l = mid + 1;
+    else
+      r = mid;
+  }
+  printf("%d\n", l);
 }
 
-int main()
-{
-    init();
-    binarysearch();
-    return 0;
+int main() {
+  init();
+  binarysearch();
+  return 0;
 }
-
-
