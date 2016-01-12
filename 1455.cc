ca5efeb731d139b8c28bb662a719e45b9e3b7cd1
sdecoder <sde.work.inbox@gmail.com>
2016-01-12 13:55:19 -0500

    2016-01-12 13:55:19

--- 1455.cc
+++ 1455.cc
@@ -4,19 +4,14 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        int n;
-        scanf("%d", &n);
-        int ans = (1 + n / 2 - 1) * (n / 2 - 1) / 2 * 2;
-        if (n & 1)
-            ans += n / 2;
-        printf("%d\n", ans);
-    }
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int n;
+    scanf("%d", &n);
+    int ans = (1 + n / 2 - 1) * (n / 2 - 1) / 2 * 2;
+    if (n & 1) ans += n / 2;
+    printf("%d\n", ans);
+  }
 }
-
-
