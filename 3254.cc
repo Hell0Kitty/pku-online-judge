e539b350b69c13c4e056302257dc3bcbcfc8b0f0
sdecoder <sde.work.inbox@gmail.com>
2016-01-11 19:39:15 -0500

    2016-01-11 19:39:15

--- 3254.cc
+++ 3254.cc
@@ -11,61 +11,44 @@ int matrix[MAXN][MAXN];
 int vst[MAXSTATE];
 int vnum = 0, row = 0, col = 0;
 
-void getvalidstate()
-{
-    for(int i = 0; i < (1<<col); ++i)
-    {
-        if(0 == (i&(i<<1)))
-            vst[vnum++] = i;
-    }
+void getvalidstate() {
+  for (int i = 0; i < (1 << col); ++i) {
+    if (0 == (i & (i << 1))) vst[vnum++] = i;
+  }
 }
-bool valid(int state, int r)
-{
-    int rstate = 0;
-    for(int i = 1; i <= col; ++i)
-    {
-        if(matrix[r][i])
-            rstate += (1<<(col - i));
-    }
-    if(!(state&(~rstate)))
-        return true;
-    return false;
+bool valid(int state, int r) {
+  int rstate = 0;
+  for (int i = 1; i <= col; ++i) {
+    if (matrix[r][i]) rstate += (1 << (col - i));
+  }
+  if (!(state & (~rstate))) return true;
+  return false;
 }
-int main()
-{
-    int m, n, i, j;
-    while(EOF != scanf("%d%d", &m, &n)){
-    for(i = 1; i <= m; ++i)
-        for(j = 1; j <= n; ++j)
-            scanf("%d", &matrix[i][j]);
+int main() {
+  int m, n, i, j;
+  while (EOF != scanf("%d%d", &m, &n)) {
+    for (i = 1; i <= m; ++i)
+      for (j = 1; j <= n; ++j) scanf("%d", &matrix[i][j]);
     row = m;
     col = n;
     vnum = 0;
     getvalidstate();
-    memset(dp, 0, sizeof(int)*MAXN*MAXSTATE);
+    memset(dp, 0, sizeof(int) * MAXN * MAXSTATE);
     dp[0][0] = 1;
-    for(i = 1; i <= row; ++i)
-    {
-        for(int st = 0; st < vnum; ++st)
-        {
-            if(valid(vst[st], i))
-            {
-                for(int pst = 0; pst < vnum; ++pst)
-                {
-                    if(valid(vst[pst], i - 1) && !(vst[st]&vst[pst]))
-                        dp[i][vst[st]] += dp[i - 1][vst[pst]];
-                }
-            }
+    for (i = 1; i <= row; ++i) {
+      for (int st = 0; st < vnum; ++st) {
+        if (valid(vst[st], i)) {
+          for (int pst = 0; pst < vnum; ++pst) {
+            if (valid(vst[pst], i - 1) && !(vst[st] & vst[pst]))
+              dp[i][vst[st]] += dp[i - 1][vst[pst]];
+          }
         }
+      }
     }
     int ret = 0;
-    for(i = 0; i < vnum; ++i)
-    {
-        ret = (ret + dp[row][vst[i]])%MOD;
+    for (i = 0; i < vnum; ++i) {
+      ret = (ret + dp[row][vst[i]]) % MOD;
     }
     printf("%d\n", ret);
   }
 }
-
-
-
