e2053a5e53d3604f92e0ae21ecef5ff6121633b9
sdecoder <sde.work.inbox@gmail.com>
2016-01-13 09:16:39 -0500

    2016-01-13 09:16:39

--- 3173.cc
+++ 3173.cc
@@ -3,42 +3,37 @@ using namespace std;
 const int MAX = 25;
 int triangle[MAX][MAX];
 
-int main()
-{
-    int n, s, i, j, k;
-    bool flag = false;
-    cin >> n >> s;
-    for (i = 0; i <= n; i++){
-        for (j = 0; j <= i; j++){
-            triangle[j][i] = s;
-            s++;
-            if (s % 10 != 0){
-                s %= 10;
-            }
-            else (++s) %= 10;
-        }
+int main() {
+  int n, s, i, j, k;
+  bool flag = false;
+  cin >> n >> s;
+  for (i = 0; i <= n; i++) {
+    for (j = 0; j <= i; j++) {
+      triangle[j][i] = s;
+      s++;
+      if (s % 10 != 0) {
+        s %= 10;
+      } else
+        (++s) %= 10;
     }
+  }
 
-    for (i = 0; i < n; i++){
-        flag = false;
-        for (k = 0; k < i; k++){
-             cout << " ";
-        }
-        for (j = 0; j < n; j++){
-            if (triangle[i][j] != 0){
-                if (flag)  cout << " ";
-                cout << triangle[i][j];
-                flag = true;
-            }
-            else{
-                 cout << " ";
-            }
-        }
-        cout << endl;
+  for (i = 0; i < n; i++) {
+    flag = false;
+    for (k = 0; k < i; k++) {
+      cout << " ";
     }
+    for (j = 0; j < n; j++) {
+      if (triangle[i][j] != 0) {
+        if (flag) cout << " ";
+        cout << triangle[i][j];
+        flag = true;
+      } else {
+        cout << " ";
+      }
+    }
+    cout << endl;
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
