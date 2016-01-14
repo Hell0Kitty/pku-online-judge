dad14abfa02a149f29ffcf2512ef8bfa96993356
sdecoder <sde.work.inbox@gmail.com>
2016-01-14 14:01:15 -0500

    2016-01-14 14:01:15

--- 1187.cc
+++ 1187.cc
@@ -1,69 +1,59 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+const int maxn = 12;
+const int maxm = 32;
+const int mod = 11380;
+int dp[maxn][maxn][maxn][maxm];
+bool vis[maxn][maxn][maxn][maxm];
+int a, b, c, d;
 
-#include <cstdio>
-#include <cstring>
-#define MOD 11380
-//#define LOCAL
-
-int dp[15][15][15][35];
-bool vis[15][15][15][35];
-
-int dfs(int l1, int l2, int l3, int dd) {
-  if (!l1 && !l2 && !l3) {
-    vis[l1][l2][l3][dd] = true;
-    return dp[l1][l2][l3][dd] = 1;
+int dfs(int l1, int l2, int l3, int dep) {
+  if (l1 == 0 && l2 == 0 && l3 == 0) {
+    vis[l1][l2][l3][dep] = true;
+    return dp[l1][l2][l3][dep] = 1;
   }
-  if (!dd) {
-    vis[l1][l2][l3][dd] = true;
-    return dp[l1][l2][l3][dd] = 0;
+  if (dep == 0) {
+    vis[l1][l2][l3][dep] = true;
+    return dp[l1][l2][l3][dep] = 0;
   }
-  if (vis[l1][l2][l3][dd]) return dp[l1][l2][l3][dd];
-
-  int ans = 0;
+  if (vis[l1][l2][l3][dep]) {
+    return dp[l1][l2][l3][dep];
+  }
+  int res = 0;
   for (int i = 0; i <= l3; i++) {
     if (i) {
-      ans = (ans + dfs(0, 0, i - 1, dd - 1) * dfs(l1, l2, l3 - i, dd)) % MOD;
-      ans %= MOD;
+      res += (dfs(0, 0, i - 1, dep - 1) * dfs(l1, l2, l3 - i, dep)) % mod;
+      res %= mod;
     }
     for (int j = 0; j <= l2; j++) {
       if (j) {
-        ans = (ans + dfs(0, j - 1, i, dd - 1) * dfs(l1, l2 - j, l3 - i, dd)) %
-              MOD;
-        ans %= MOD;
+        res += (dfs(0, j - 1, i, dep - 1) * dfs(l1, l2 - j, l3 - i, dep)) % mod;
+        res %= mod;
       }
       for (int k = 1; k <= l1; k++) {
-        ans =
-            (ans + dfs(k - 1, j, i, dd - 1) * dfs(l1 - k, l2 - j, l3 - i, dd)) %
-            MOD;
-        ans %= MOD;
+        res += (dfs(k - 1, j, i, dep - 1) * dfs(l1 - k, l2 - j, l3 - i, dep)) %
+               mod;
+        res %= mod;
       }
     }
   }
-  vis[l1][l2][l3][dd] = true;
-  return dp[l1][l2][l3][dd] = ans;
+  vis[l1][l2][l3][dep] = true;
+  return dp[l1][l2][l3][dep] = res;
 }
 
 int main() {
-#ifdef LOCAL
-  freopen("input.txt", "r", stdin);
-  freopen("output.txt", "w", stdout);
-#endif
-
-  int l1, l2, l3, dd;
-
-  memset(vis, 0, sizeof(vis));
-  scanf("%d%d%d%d", &l1, &l2, &l3, &dd);
-
-  dp[l1][l2][l3][dd] = dfs(l1, l2, l3, dd);
-  if (dd) dp[l1][l2][l3][dd - 1] = dfs(l1, l2, l3, dd - 1);
-  // dp[l1][l2][l3][dd-1]=dfs(l1,l2,l3,dd-1);
-
-  if (!dd) {
-    printf("%d\n", dp[l1][l2][l3][dd]);
-    return 0;
+  memset(vis, false, sizeof(vis));
+  while (~scanf("%d %d %d %d", &a, &b, &c, &d)) {
+    dfs(a, b, c, d);
+    if (d) dfs(a, b, c, d - 1);
+    int ans;
+    if (d == 0)
+      ans = dp[a][b][c][d];
+    else
+      ans = ((dp[a][b][c][d] - dp[a][b][c][d - 1]) % mod + mod) % mod;
+    printf("%d\n", ans);
   }
-  int a1 = dp[l1][l2][l3][dd], a2 = dp[l1][l2][l3][dd - 1];
-
-  // printf("%d %d\n",a1,a2);
-  printf("%d\n", ((a1 - a2) % MOD + MOD) % MOD);
   return 0;
 }
