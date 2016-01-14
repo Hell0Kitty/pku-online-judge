158c1ca774486455d777a97c914778507c446617
sdecoder <sde.work.inbox@gmail.com>
2016-01-14 18:41:09 -0500

    2016-01-14 18:41:09

---
+++ 2715.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+#define EPS 0.00001
+
+int main()
+{
+    double B, W, C;
+    while(scanf("%lf%lf%lf", &B, &W, &C) && (B + W + C)) 
+    {
+        B *= 16, W *= 16;
+        double ans = B / W;
+        double raW = 0, raB = 1;
+        int sum = 0;
+        while(fabs(raW / (1 - raW) - ans) > EPS || fabs(raB / (1 - raB) - ans) > EPS) 
+        {
+            raW = 1 - (W * (1 - raW) + C * (1 - raB)) / (C + W);
+            raB = ((B - C) * raB + C * raW) / B;
+            sum ++;
+        }
+        printf("%d\n", sum);
+    }
+}
+
+
+
