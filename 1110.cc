503993b8bbcb89911b51796851baf8bff64bb75a
sdecoder <sde.work.inbox@gmail.com>
2016-01-15 02:44:18 -0500

    2016-01-15 02:44:17

--- 1110.cc
+++ 1110.cc
@@ -2,93 +2,87 @@
 #include <iostream>
 using namespace std;
 char a[10][81];
-int n, r, c;//n--num of symbols, r/c--num of rows/cols in each grid
+int n, r, c;  // n--num of symbols, r/c--num of rows/cols in each grid
 int upper_bound;
-bool search(int x){
-    int begin = x * (c + 1);
-    int end = begin + c - 1;//inclusive
-    for(int i = 0; i < r; i++){
-        for(int j = begin; j <= end; j++){
-            if(a[i][j] == 'o'){
-                int k = j;//forward,backward
-                while(k >= c + 1){
-                    k -= (c + 1);
-                }
-                for(; k <= upper_bound; k += c + 1){
-                    if(a[i][k] != '.' && k != j){
-                        break;
-                    }
-                }
-                if(k <= upper_bound){//find another
-                    continue;
-                }else{
-                    a[i][j] = '#';
-                    return true;
-                }
-            }
+bool search(int x) {
+  int begin = x * (c + 1);
+  int end = begin + c - 1;  // inclusive
+  for (int i = 0; i < r; i++) {
+    for (int j = begin; j <= end; j++) {
+      if (a[i][j] == 'o') {
+        int k = j;  // forward,backward
+        while (k >= c + 1) {
+          k -= (c + 1);
         }
-    }
-    //check 2#...
-    //
-    for(int i = 0; i < r; i++){
-        for(int j = begin; j <= end; j++){
-            if(a[i][j] != 'o'){
-                continue;
-            }
-            for(int s = i; s < r; s++){
-                for(int t = (s == i ? j + 1: begin); 
-                        t <= end; t++){
-                    if(a[s][t] != 'o'){
-                        continue;
-                    }
-                    int k1 = j, k2 = t;//forward,backward
-                    while(k1 >= c + 1){
-                        k1 -= (c + 1);
-                        k2 -= (c + 1);
-                    }
-                    for(; k1 <= upper_bound; 
-                            k1 += c + 1, k2 += c + 1){
-                        if(a[i][k1] != '.' && a[s][k2] != '.' 
-                                && k1 != j){
-                            break;
-                        }
-                    }
-                    if(k1 <= upper_bound){//find another
-                        continue;
-                    }else{
-                        a[i][j] = '#';
-                        a[s][t] = '#';
-                        return true;
-                    }
-                }
-            }
+        for (; k <= upper_bound; k += c + 1) {
+          if (a[i][k] != '.' && k != j) {
+            break;
+          }
         }
+        if (k <= upper_bound) {  // find another
+          continue;
+        } else {
+          a[i][j] = '#';
+          return true;
+        }
+      }
     }
-    return false;
+  }
+  // check 2#...
+  //
+  for (int i = 0; i < r; i++) {
+    for (int j = begin; j <= end; j++) {
+      if (a[i][j] != 'o') {
+        continue;
+      }
+      for (int s = i; s < r; s++) {
+        for (int t = (s == i ? j + 1 : begin); t <= end; t++) {
+          if (a[s][t] != 'o') {
+            continue;
+          }
+          int k1 = j, k2 = t;  // forward,backward
+          while (k1 >= c + 1) {
+            k1 -= (c + 1);
+            k2 -= (c + 1);
+          }
+          for (; k1 <= upper_bound; k1 += c + 1, k2 += c + 1) {
+            if (a[i][k1] != '.' && a[s][k2] != '.' && k1 != j) {
+              break;
+            }
+          }
+          if (k1 <= upper_bound) {  // find another
+            continue;
+          } else {
+            a[i][j] = '#';
+            a[s][t] = '#';
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
 }
-int main()
-{
-    int i;
-    int cnt = 1;
-    while(scanf("%d%d%d\n", &n, &r, &c) != EOF && n != 0){
-        printf("Test %d\n", cnt++);
-        upper_bound = (c + 1) * (n - 1) + c - 1;//last element
-        for(i = 0; i < r; i++){
-            fgets(a[i], 81, stdin);
-        }
-        for(i = 0; i < n; i++){
-            if(!search(i)){//check whether symbol i has unique #
-                printf("impossible\n");
-                break;
-            }
-        }
-        if(i == n){
-            for(i = 0; i < r; i++){
-                printf("%s", a[i]);
-            }
-        }
+int main() {
+  int i;
+  int cnt = 1;
+  while (scanf("%d%d%d\n", &n, &r, &c) != EOF && n != 0) {
+    printf("Test %d\n", cnt++);
+    upper_bound = (c + 1) * (n - 1) + c - 1;  // last element
+    for (i = 0; i < r; i++) {
+      fgets(a[i], 81, stdin);
     }
-    return 0;
+    for (i = 0; i < n; i++) {
+      if (!search(i)) {  // check whether symbol i has unique #
+        printf("impossible\n");
+        break;
+      }
+    }
+    if (i == n) {
+      for (i = 0; i < r; i++) {
+        printf("%s", a[i]);
+      }
+    }
+  }
+  return 0;
 }
-
-
