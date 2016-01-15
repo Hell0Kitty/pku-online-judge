297a92e3ca74bb86585f9cec01e5662dacef39be
sdecoder <sde.work.inbox@gmail.com>
2016-01-14 21:20:20 -0500

    2016-01-14 21:20:20

---
+++ 2773.cc
@@ -0,0 +1,59 @@
+#include <cstdio>
+typedef long long LL;
+#define SIZE 1000000
+int fac[ SIZE + 5 ], tot, m, k;
+void divide( int x )
+{
+	tot = 0;
+	for( int i = 2; i * i <= x; ++i )
+	{
+		if( x % i == 0 )
+		{
+			fac[ tot++ ] = i;
+			while( x % i == 0 ) x /= i;
+		}
+	}
+	if( x > 1 ) fac[ tot++ ] = x;
+}
+
+LL solve( LL x )
+{
+	LL ans = 0;
+	for( int i = 1; i < ( 1 << tot ); ++i )
+	{
+		LL bits = 0;
+		LL mult = 1;
+		for( int j = 0; j < tot; ++j )
+		{
+			if( i & ( 1 << j ) ) 
+			{
+				bits++;
+				mult *= fac[j];
+			}
+		}
+		if( bits & 1 ) ans += x / mult;
+		else	ans -= x / mult;
+	}
+	return x - ans;
+}
+
+int main()
+{
+	while( ~scanf( "%d %d", &m, &k ) )
+	{
+		divide( m );
+		LL l = 1, r = ( 1LL << 33 ), mid, tmp, ans;
+		while( l < r )
+		{
+			mid = ( l + r ) >> 1;
+			tmp = solve( mid );
+			if( tmp >= k )	r = mid;
+			else l = mid + 1;
+		}
+		printf( "%lld\n", l );
+	}
+}
+
+
+
+
