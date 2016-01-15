f36880666a2a99e58fdc3b86cdc7786e31987f5f
sdecoder <sde.work.inbox@gmail.com>
2016-01-15 02:26:38 -0500

    2016-01-15 02:26:38

--- 3672.cc
+++ 3672.cc
@@ -4,37 +4,24 @@ using namespace std;
 
 char unit[100005];
 
-int main()
-{
-    int m, t, u, f, d, i, ans = 0, sum = 0;
-    cin >> m >> t >> u >> f >> d;
-    for (i = 0; i < t; i++)
-        cin >> unit[i];
-    for (i = 0; i < t; i++)
-    {
-          if (unit[i] == 'u')
-          {
-              sum += u + d;
-              ans++;
-          }
-          else if (unit[i] == 'f')
-          {
-               sum += f + f;
-               ans++;
-          }
-          else if (unit[i] == 'd')
-          {
-               sum += d + u;
-               ans++;
-          }
-          if (sum >= m)
-          {
-               cout << ans-1 << endl;
-               break;
-          }
+int main() {
+  int m, t, u, f, d, i, ans = 0, sum = 0;
+  cin >> m >> t >> u >> f >> d;
+  for (i = 0; i < t; i++) cin >> unit[i];
+  for (i = 0; i < t; i++) {
+    if (unit[i] == 'u') {
+      sum += u + d;
+      ans++;
+    } else if (unit[i] == 'f') {
+      sum += f + f;
+      ans++;
+    } else if (unit[i] == 'd') {
+      sum += d + u;
+      ans++;
     }
-
+    if (sum >= m) {
+      cout << ans - 1 << endl;
+      break;
+    }
+  }
 }
-
-
-
