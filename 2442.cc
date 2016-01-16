1957b37d959c14e60c819541197eb27171f65bf1
sdecoder <sde.work.inbox@gmail.com>
2016-01-15 20:20:04 -0500

    2016-01-15 20:20:04

--- 2442.cc
+++ 2442.cc
@@ -1,73 +1,63 @@
-/*=============================================================================
-#     FileName: 2442.cpp
-#         Desc: poj 2442
-#       Author: zhuting
-#        Email: cnjs.zhuting@gmail.com
-#     HomePage: my.oschina.net/locusxt
-#      Version: 0.0.1
-#    CreatTime: 2013-12-22 17:51:34
-#   LastChange: 2013-12-22 17:51:34
-#      History:
-=============================================================================*/
-#include <cstdio>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <algorithm>
-#include <queue>
-#define maxm 2005
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<queue>
+#include<algorithm>
 using namespace std;
+const int N=2002;
+int main()
+{
+    int t,n,k,m,i,j;
+    int a[N],b[N];
+    priority_queue <int > q;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&n,&m);
+        for(i=0;i<n;i++)
+        {
+            if(i==0)
+            {
+                for(j=0;j<m;j++)
+                    scanf("%d",&a[j]);
+                sort(a,a+m);
+            }
+            else{
+                for(j=0;j<m;j++)
+                    scanf("%d",&b[j]);
+                sort(b,b+m);
 
-/*
- * 维护一个最大值堆,其中始终有n个元素
- * 读入第一行只data数组中,排序
- * a.读入下一行至data2数组,排序
- * b.data2[0]与data中各项相加 ,加入最大值堆
- * c.data2[i]加上data中的第0项,所得值如果比堆头元素大,则continue;否则,堆pop后加入所得值,继续加data中的第1,2,3,4...项.(i
- * >= 1)
- * d.将最大值堆导入到data数组
- * 重复abcd
- * 最后答案就在data中
- */
-
-int main() {
-  int t = 0;
-  int m = 0, n = 0;
-  scanf("%d", &t);
-  for (int i = 0; i < t; ++i) {
-    scanf("%d%d", &m, &n);
-    int data[maxm] = {0};
-    int data2[maxm] = {0};
-
-    for (int k = 0; k < n; ++k) {
-      scanf("%d", &data[k]);
-    }
-    sort(data, data + n);
-
-    for (int j = 1; j < m; ++j) {
-      for (int k = 0; k < n; ++k) scanf("%d", &data2[k]);
-      sort(data2, data2 + n);
-      priority_queue<int, vector<int>, less<int> > pq;
-
-      for (int k = 0; k < n; ++k) {
-        pq.push(data2[0] + data[k]);
-      }
-      for (int k = 1; k < n; ++k) {
-        for (int l = 0; l < n; ++l) {
-          int tmp = data2[k] + data[l];
-          if (tmp >= pq.top()) break;
-          pq.pop();
-          pq.push(tmp);
+                for(j=0;j<m;j++)
+                {
+                    for(k=0;k<m;k++)
+                    {
+                        if(j==0)
+                        {
+                            q.push(b[j]+a[k]);
+                        }else{
+                            if(b[j]+a[k]<q.top())
+                            {
+                                q.pop();
+                                q.push(b[j]+a[k]);
+                            }else break;
+                        }
+                    }
+                }
+                for(k=0;k<m;k++)
+                {
+                    a[k]=q.top();
+                    q.pop();
+                }
+                sort(a,a+m);
+            }
         }
-      }
-      for (int k = n - 1; k >= 0; --k) {
-        data[k] = pq.top();
-        pq.pop();
-      }
+        printf("%d",a[0]);
+        for(i=1;i<m;i++)
+            printf(" %d",a[i]);
+        printf("\n");
     }
-
-    for (int j = 0; j < n; ++j) printf("%d ", data[j]);
-    printf("\n");
-  }
-  return 0;
+    return 0;
 }
+
+
+
