b52f597efb0b00b2e3d2c951421c3de2b709bd51
sdecoder <sde.work.inbox@gmail.com>
2016-01-22 12:38:33 -0500

    2016-01-22 12:38:33

---
+++ 3427.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define maxn 30005
+
+int n, m;
+int f[maxn], sum[maxn];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &f[i]);
+    for (int i = 1; i <= n; i++)
+        f[i] = f[i] % m;
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+        if (f[i] != 0)
+            ans = max(ans, m - f[i]);
+    printf("%d\n", ans);
+    return 0;
+}
+
+
+
