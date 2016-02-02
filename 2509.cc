296ff49e029d65699f5d448d4eabd541004f2af7
sdecoder <sde.work.inbox@gmail.com>
2016-02-01 21:32:15 -0500

    2016-02-01 21:32:15

---
+++ 2509.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    long long n,m;
+    long long sum;
+    while(scanf("%lld%lld",&n,&m)!=EOF)
+    {
+        sum=n;
+        while(n>=m)
+        {
+            sum+=n/m;
+            n=n/m+n%m;
+        }
+        printf("%lld\n",sum);
+    }
+    return 0;
+}
+
+
+
