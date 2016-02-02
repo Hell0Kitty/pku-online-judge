7f5c5dc262b1bf389fe510d8364894c1c9e793bf
sdecoder <sde.work.inbox@gmail.com>
2016-02-01 20:29:27 -0500

    2016-02-01 20:29:27

--- 2853.cc
+++ 2853.cc
@@ -1,32 +1,24 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    while(n--)
-    {
-        int no;
-        long long num;
-        scanf("%d%I64d",&no,&num);
-        num*=2;
-        long long i;
-        long long ans=0;
-        for(i=2;i*i<=num;i++)
-        {
-            if(num%i==0)
-            {
-                long long b=i-1;
-                long long t=num/i;
-                if(t>=b&&(t-b)%2==0)
-                {
-                    ans++;
-                }
-            }
+#include <stdio.h>
+int main() {
+  int n;
+  scanf("%d", &n);
+  while (n--) {
+    int no;
+    long long num;
+    scanf("%d%I64d", &no, &num);
+    num *= 2;
+    long long i;
+    long long ans = 0;
+    for (i = 2; i * i <= num; i++) {
+      if (num % i == 0) {
+        long long b = i - 1;
+        long long t = num / i;
+        if (t >= b && (t - b) % 2 == 0) {
+          ans++;
         }
-        printf("%d %I64d\n",no,ans);
+      }
     }
-    return 0;
+    printf("%d %I64d\n", no, ans);
+  }
+  return 0;
 }
-
-
-
