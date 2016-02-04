566a14c0ff53291772e4a2f3553323b15df6b194
sdecoder <sde.work.inbox@gmail.com>
2016-02-03 19:09:16 -0500

    2016-02-03 19:09:16

--- 2291.cc
+++ 2291.cc
@@ -1,39 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-int comp(const void *a,const void *b)
-{
-    return *(int *)a-*(int *)b;
-}
-int main()
-{
-    int n,m,i,max;
+int comp(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+int main() {
+  int n, m, i, max;
 
-    while(scanf("%d",&n)!=EOF)
-    {
-        for(; n>0; n--)
-        {
-            scanf("%d",&m);
-            int shu[m];
-            for(i=0; i<m; i++)
-            {
-                scanf("%d",&shu[i]);
-            }
-            qsort(shu,m,sizeof(int),comp);
-            max=shu[0]*m;
-            for(i=1; i<m; i++)
-            {
-                if(shu[i]*(m-i)>max)
-                {
-                    max=shu[i]*(m-i);
-                }
-            }
-            printf("%d\n",max);
-
+  while (scanf("%d", &n) != EOF) {
+    for (; n > 0; n--) {
+      scanf("%d", &m);
+      int shu[m];
+      for (i = 0; i < m; i++) {
+        scanf("%d", &shu[i]);
+      }
+      qsort(shu, m, sizeof(int), comp);
+      max = shu[0] * m;
+      for (i = 1; i < m; i++) {
+        if (shu[i] * (m - i) > max) {
+          max = shu[i] * (m - i);
         }
-
+      }
+      printf("%d\n", max);
     }
-    return(0);
+  }
+  return (0);
 }
-
-
-
