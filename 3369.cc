5a768633edf392990ebebda60506c92375d186e0
sdecoder <sde.work.inbox@gmail.com>
2016-02-04 21:01:05 -0500

    2016-02-04 21:01:05

--- 3369.cc
+++ 3369.cc
@@ -1,22 +1,18 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int i,j,k,t;
-    for(i=1;i<160;i++)
-        for(j=i;j<1000;j++)
-            for(k=(2000-i-j)/2-1;k>=j;k--)
-            {
-                if(i*j*k<=1000000)break;
-                if(  ((i+j+k)*1000000)%(i*j*k-1000000)==0  )
-                {
-                    t=((i+j+k)*1000000)/(i*j*k-1000000);
-                    if(t+i+j+k>2000)continue;
-                    if(t<k)continue;
-                    printf("%.2lf %.2lf %.2lf %.2lf\n",i/100.0,j/100.0,k/100.0,t/100.0);
-                }
-            }
-    return 0;
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int i, j, k, t;
+  for (i = 1; i < 160; i++)
+    for (j = i; j < 1000; j++)
+      for (k = (2000 - i - j) / 2 - 1; k >= j; k--) {
+        if (i * j * k <= 1000000) break;
+        if (((i + j + k) * 1000000) % (i * j * k - 1000000) == 0) {
+          t = ((i + j + k) * 1000000) / (i * j * k - 1000000);
+          if (t + i + j + k > 2000) continue;
+          if (t < k) continue;
+          printf("%.2lf %.2lf %.2lf %.2lf\n", i / 100.0, j / 100.0, k / 100.0,
+                 t / 100.0);
+        }
+      }
+  return 0;
 }
-
-
