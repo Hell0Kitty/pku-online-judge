97d293d9c811a42c06bdd4195e9692d9d40f2adb
sdecoder <sde.work.inbox@gmail.com>
2016-02-09 19:42:20 -0500

    2016-11-08 12:45:56

---
+++ 3438.cc
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int t;
+	char digits[1001];
+	cin>>t;
+	while(t--)
+	{
+		cin>>digits;
+		int len = strlen(digits);
+		int count = 1;
+		for(int i = 0;i<len;i++)
+		{
+			if(digits[i] == digits[i+1])
+				count++;
+			else{
+				cout<<count<<digits[i];
+				count = 1;
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+
+
