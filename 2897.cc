986524dd0fd419719b91a5dfafe91cd467679f1f
sdecoder <sde.work.inbox@gmail.com>
2016-02-12 07:50:11 -0500

    2016-06-04 05:34:12

--- 2897.cc
+++ 2897.cc
@@ -15,45 +15,40 @@ using namespace std;
 #define E 1000000
 
 using namespace std;
-int T,n,k;
+int T, n, k;
 bool pig[100];
-int main()
-{
-    //freopen("pro.in","r",stdin);
-    scanf("%d",&T);
-    while(T--) {
-        scanf("%d%d",&n,&k);
-        if(n > k) {
-            cout<<0<<endl;
-            continue ;
-        }
-        string ans = "";
-        bool find = 1;
-        memset(pig,0,sizeof(pig));
-        int now = k;
-        while(1) {
-            int a = now / n;
-            int b = now % n;
-            ans += char(a + '0');
-            now = b * 10 + a;
-            if(pig[now] == 1) {
-                find = 0;
-                break;
-            }
-            pig[now] = 1; 
-            if(b == 0 && a == k) {
-                break;
-            }
-
-        }
-        if(find) {
-            cout<<ans<<endl;
-        } else {
-            cout<<0<<endl;
-        }
+int main() {
+  // freopen("pro.in","r",stdin);
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d%d", &n, &k);
+    if (n > k) {
+      cout << 0 << endl;
+      continue;
     }
-    return 0;
+    string ans = "";
+    bool find = 1;
+    memset(pig, 0, sizeof(pig));
+    int now = k;
+    while (1) {
+      int a = now / n;
+      int b = now % n;
+      ans += char(a + '0');
+      now = b * 10 + a;
+      if (pig[now] == 1) {
+        find = 0;
+        break;
+      }
+      pig[now] = 1;
+      if (b == 0 && a == k) {
+        break;
+      }
+    }
+    if (find) {
+      cout << ans << endl;
+    } else {
+      cout << 0 << endl;
+    }
+  }
+  return 0;
 }
-
-
-
