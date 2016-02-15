654d8bf4be402d6b9c96b0f95226c4729b78b528
sdecoder <sde.work.inbox@gmail.com>
2016-02-15 03:24:11 -0500

    2016-02-15 03:24:11

--- 3062.cc
+++ 3062.cc
@@ -1,22 +1,14 @@
-#include<stdio.h>
-#include<string.h>
-#include<iostream>
-#include<algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
-int cmpfunc (const void * a, const void * b)
-{
-   return ( *(int*)a - *(int*)b );
+int cmpfunc(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+int main() {
+  char str[100];
+  while (gets(str)) {
+    puts(str);
+  }
+  return 0;
 }
-int main()
-{
-    char str[100];
-     while(gets(str))
-     {
-         puts(str);
-     }
-    return 0;
-}
-
-
-
