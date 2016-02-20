150538e3f2317f62f754e6852671f3e6d2e8d668
sdecoder <sde.work.inbox@gmail.com>
2016-02-20 09:48:46 -0500

    2016-02-20 09:48:46

--- 2656.cc
+++ 2656.cc
@@ -11,23 +11,22 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
-
-    int n;
-    while(scanf("%d",&amp;n)!=EOF&amp;&amp;n){
-        int a,b,maxSum=0,pos=0;
-        for(int i=1;i&lt;=n;i++){
-            scanf("%d%d",&amp;a,&amp;b);
-            if(maxSum&lt;a+b){
-                maxSum=a+b;
-                pos=i;
-            }else if(maxSum==a+b&amp;&amp;i&lt;pos){
-                pos=i;
-            }
-        }
-        if(maxSum&lt;=8)    printf("0\n");
-        else    printf("%d\n",pos);
+int main() {
+  int n;
+  while (scanf("%d", &amp; n) != EOF & amp; &amp; n) {
+    int a, b, maxSum = 0, pos = 0;
+    for (int i = 1; i & lt; = n; i++) {
+      scanf("%d%d", &amp; a, &amp; b);
+      if (maxSum & lt; a + b) {
+        maxSum = a + b;
+        pos = i;
+      } else if (maxSum == a + b & amp; &amp; i & lt; pos) {
+        pos = i;
+      }
     }
-    return 0;
-
+    if (maxSum & lt; = 8)
+      printf("0\n");
+    else
+      printf("%d\n", pos);
+  }
+  return 0;
