ff5906ab27a97048f3b2444409e38d573c0312a4
sdecoder <sde.work.inbox@gmail.com>
2016-02-23 06:36:43 -0500

    2016-02-23 06:36:43

--- 2136.cc
+++ 2136.cc
@@ -6,50 +6,36 @@ using namespace std;
 
 int f[30];
 
-int find(int top, int pos)
-{
-    while (f[pos] < top && pos < 26)
-        pos++;
-    if (pos == 26)
-        return -1;
-    return pos;
+int find(int top, int pos) {
+  while (f[pos] < top && pos < 26) pos++;
+  if (pos == 26) return -1;
+  return pos;
 }
 
-int main()
-{
-    char ch;
-    while ((ch = getchar()) != EOF)
-        if (ch <= 'Z' && ch >= 'A')
-            f[ch - 'A']++;
-    int top = 0;
-    for (int i = 0; i < 26; i++)
-        top = max(f[i], top);
-    for (int i = top; i >= 1; i--)
-    {
-        int pos = 0;
-        bool first = true;
-        while (1)
-        {
-            int temp = find(i, pos);
-            if (temp == -1)
-                break;
-            if (!first)
-                printf(" ");
-            else
-                first = false;
-            for (int j = pos; j < temp; j++)
-                printf("  ");
-            printf("*");
-            pos = temp + 1;
-        }
-        printf("\n");
+int main() {
+  char ch;
+  while ((ch = getchar()) != EOF)
+    if (ch <= 'Z' && ch >= 'A') f[ch - 'A']++;
+  int top = 0;
+  for (int i = 0; i < 26; i++) top = max(f[i], top);
+  for (int i = top; i >= 1; i--) {
+    int pos = 0;
+    bool first = true;
+    while (1) {
+      int temp = find(i, pos);
+      if (temp == -1) break;
+      if (!first)
+        printf(" ");
+      else
+        first = false;
+      for (int j = pos; j < temp; j++) printf("  ");
+      printf("*");
+      pos = temp + 1;
     }
-    printf("A");
-    for (int i = 0; i < 25; i++)
-        printf(" %c", i + 'B');
     printf("\n");
-    return 0;
+  }
+  printf("A");
+  for (int i = 0; i < 25; i++) printf(" %c", i + 'B');
+  printf("\n");
+  return 0;
 }
-
-
-
