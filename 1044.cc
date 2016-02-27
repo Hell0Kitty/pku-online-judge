d4642dc4c5782a06b1cbeeec42592bd2baddbd59
sdecoder <sde.work.inbox@gmail.com>
2016-02-26 20:31:28 -0500

    2016-02-26 20:31:28

--- 1044.cc
+++ 1044.cc
@@ -1,40 +1,33 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
-typedef struct NODE
-{
-	int year,dic;
-}Node[20];
-int main()
-{
-	int n,i,j,m=1;
-    while(cin>>n&&n)
-	{
-		Node N;
-		int x,y;
-		for(i=0;i<n;i++)
-		{
-			cin>>N[i].year>>x>>y;
-			N[i].dic=y-x;
-		}
-		printf("Case #%d:\n",m++);
-		while(N[0].year<10000)
-		{
-			for(i=1;i<n;i++)
-				if((N[0].year-N[i].year)%N[i].dic!=0) break;
-				if(i==n)
-				{
-					for(j=1;j<n;j++)
-						if(N[j].year>N[0].year) break;
-					if(j==n)  {printf("The actual year is %d.\n",N[0].year);break;}
-				}
+typedef struct NODE { int year, dic; } Node[20];
+int main() {
+  int n, i, j, m = 1;
+  while (cin >> n && n) {
+    Node N;
+    int x, y;
+    for (i = 0; i < n; i++) {
+      cin >> N[i].year >> x >> y;
+      N[i].dic = y - x;
+    }
+    printf("Case #%d:\n", m++);
+    while (N[0].year < 10000) {
+      for (i = 1; i < n; i++)
+        if ((N[0].year - N[i].year) % N[i].dic != 0) break;
+      if (i == n) {
+        for (j = 1; j < n; j++)
+          if (N[j].year > N[0].year) break;
+        if (j == n) {
+          printf("The actual year is %d.\n", N[0].year);
+          break;
+        }
+      }
 
-			    N[0].year+=N[0].dic;
-		}
-        if(N[0].year>=10000) cout<<"Unknown bugs detected."<<endl;
-		cout<<endl;
-	}
-return 0;
+      N[0].year += N[0].dic;
+    }
+    if (N[0].year >= 10000) cout << "Unknown bugs detected." << endl;
+    cout << endl;
+  }
+  return 0;
 }
-
-
