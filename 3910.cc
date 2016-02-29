c68b4181f20ddbed90a5ac19dfd9fafa52a574c5
sdecoder <sde.work.inbox@gmail.com>
2016-02-28 20:12:06 -0500

    2016-02-28 20:12:06

--- 3910.cc
+++ 3910.cc
@@ -1,30 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int phi(int n){  
- 	int i,ans=n;
-	for(i=2;i*i<=n;i++){
-		if(n%i==0){
-			ans=ans/i*(i-1);
-			while(n%i==0) n/=i;
-		}
-	}
-	if(n>1) ans=ans/n*(n-1);
-	return ans;
+int phi(int n) {
+  int i, ans = n;
+  for (i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      ans = ans / i * (i - 1);
+      while (n % i == 0) n /= i;
+    }
+  }
+  if (n > 1) ans = ans / n * (n - 1);
+  return ans;
 }
 
-int main(){
-	int n,m;
-	while(cin>>n){
-		long long ans=1;
-		while(n--){
-			cin>>m;
-			ans*=phi(m);
-			if(ans>=1000000007) ans%=1000000007;
-		}
-		cout<<ans<<endl;
-	}
-	return 0;
+int main() {
+  int n, m;
+  while (cin >> n) {
+    long long ans = 1;
+    while (n--) {
+      cin >> m;
+      ans *= phi(m);
+      if (ans >= 1000000007) ans %= 1000000007;
+    }
+    cout << ans << endl;
+  }
+  return 0;
 }
-
-
