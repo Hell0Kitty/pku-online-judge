5629562013df88342a9461dab654bf2d2e3b5473
sdecoder <sde.work.inbox@gmail.com>
2016-03-03 01:31:57 -0500

    2016-03-03 01:31:57

--- 2331.cc
+++ 2331.cc
@@ -15,40 +15,35 @@ int xa, ya, xb, yb, k, c[4], l[4], ans;
 int len1, len2;
 
 void dfs2(int x, int cur, int len) {
-    if (len == 0) ans = min(cur, ans);
-    if (x == k) return ;
-    dfs2(x + 1, cur, len);
-    for (int i = 1; i <= c[x]; ++i) {
-        dfs2(x + 1, cur + i, len + i * l[x]);
-        dfs2(x + 1, cur + i, len - i * l[x]);
-    }
+  if (len == 0) ans = min(cur, ans);
+  if (x == k) return;
+  dfs2(x + 1, cur, len);
+  for (int i = 1; i <= c[x]; ++i) {
+    dfs2(x + 1, cur + i, len + i * l[x]);
+    dfs2(x + 1, cur + i, len - i * l[x]);
+  }
 }
 
 void dfs(int x, int cur, int len) {
-    if (len == 0) dfs2(0, cur, len2);
-    if (x == k) return ;
-    dfs(x + 1, cur, len);
-    for (int i = 1; i <= c[x]; ++i) {
-        c[x] -= i;
-        dfs(x + 1, cur + i, len + i * l[x]);
-        dfs(x + 1, cur + i, len - i * l[x]);
-        c[x] += i;
-    }
+  if (len == 0) dfs2(0, cur, len2);
+  if (x == k) return;
+  dfs(x + 1, cur, len);
+  for (int i = 1; i <= c[x]; ++i) {
+    c[x] -= i;
+    dfs(x + 1, cur + i, len + i * l[x]);
+    dfs(x + 1, cur + i, len - i * l[x]);
+    c[x] += i;
+  }
 }
 
 int main() {
-    scanf ("%d%d%d%d%d", &xa, &ya, &xb, &yb, &k);
-    for (int i = 0; i < k; ++i)
-        scanf ("%d", &l[i]);
-    for (int i = 0; i < k; ++i)
-        scanf ("%d", &c[i]);
-    len1 = abs(xa - xb), len2 = abs(ya - yb);
-    ans = inf;
-    dfs(0, 0, len1);
-    if (ans == inf) ans = -1;
-    printf ("%d\n", ans);
-    return 0;
+  scanf("%d%d%d%d%d", &xa, &ya, &xb, &yb, &k);
+  for (int i = 0; i < k; ++i) scanf("%d", &l[i]);
+  for (int i = 0; i < k; ++i) scanf("%d", &c[i]);
+  len1 = abs(xa - xb), len2 = abs(ya - yb);
+  ans = inf;
+  dfs(0, 0, len1);
+  if (ans == inf) ans = -1;
+  printf("%d\n", ans);
+  return 0;
 }
-
-
-
