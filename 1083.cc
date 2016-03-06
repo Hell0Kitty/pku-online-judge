ac06762b0a2dfb90012f74c071c2933c18bcb32b
sdecoder <sde.work.inbox@gmail.com>
2016-03-06 09:12:35 -0500

    2016-03-06 09:12:35

--- 1083.cc
+++ 1083.cc
@@ -3,35 +3,30 @@
 #include <algorithm>
 #include <iostream>
 #include <math.h>
-#define MAX(a,b) (a) > (b)? (a):(b)
-#define MIN(a,b) (a) < (b)? (a):(b)
-#define mem(a) memset(a,0,sizeof(a))
+#define MAX(a, b) (a) > (b) ? (a) : (b)
+#define MIN(a, b) (a) < (b) ? (a) : (b)
+#define mem(a) memset(a, 0, sizeof(a))
 #define INF 1000000007
 #define MAXN 105
 using namespace std;
 int a[402];
-int main()
-{
-    int cas;
-    while(~scanf("%d", &cas))while(cas--)
-    {
-        mem(a);
-        int n,i,j,x,y;
-        scanf("%d", &n);
-        for(i=0;i<n;i++)
-        {
-            scanf("%d%d", &x, &y);
-            if(x>y)swap(x,y);
-            if(!(x&1))x--;
-            if(y&1)y++;
-            for(j=x;j<=y;j++)
-                a[j]++;
-        }
-        int max=0;
-        for(i=1;i<=400;i++)max=MAX(max,a[i]);
-        printf("%d\n",max*10);
+int main() {
+  int cas;
+  while (~scanf("%d", &cas))
+    while (cas--) {
+      mem(a);
+      int n, i, j, x, y;
+      scanf("%d", &n);
+      for (i = 0; i < n; i++) {
+        scanf("%d%d", &x, &y);
+        if (x > y) swap(x, y);
+        if (!(x & 1)) x--;
+        if (y & 1) y++;
+        for (j = x; j <= y; j++) a[j]++;
+      }
+      int max = 0;
+      for (i = 1; i <= 400; i++) max = MAX(max, a[i]);
+      printf("%d\n", max * 10);
     }
-    return 0;
+  return 0;
 }
-
-
