0c373faadf10f75a9e801419ac55332ca3d7e74d
sdecoder <sde.work.inbox@gmail.com>
2016-03-06 02:32:51 -0500

    2016-03-06 02:32:51

--- 2390.cc
+++ 2390.cc
@@ -1,16 +1,11 @@
-#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int year;
-    double money,rate;
-    scanf("%lf%lf%d",&rate,&money,&year);
-    rate=pow((1+rate/100),year);
-    money=money*rate;
-    money=floor(money);
-    printf("%.0lf",money);
-
+#include <stdio.h>
+#include <math.h>
+int main() {
+  int year;
+  double money, rate;
+  scanf("%lf%lf%d", &rate, &money, &year);
+  rate = pow((1 + rate / 100), year);
+  money = money * rate;
+  money = floor(money);
+  printf("%.0lf", money);
 }
-
-
-
