5ec1695224db49ea07e6aec484e89e0046f6cb07
sdecoder <sde.work.inbox@gmail.com>
2016-03-12 19:00:18 +0800

    2016-3-12 11:00:21

---
+++ 1917.cc
@@ -0,0 +1,65 @@
+#include <iostream>  
+#include <cstdio>  
+#include <string>  
+using namespace std;  
+  
+int main()  
+{  
+    int tc, len1, len2, i, j, pos1, pos2, pos3, pos4;  
+    string input1, input2, s1, s2, s3, s4, s5, s6;  
+    bool flag1, flag2;  
+    cin >> tc;  
+    cin.get();//放在这里才对   
+    while (tc--){  
+          //cin.get();//之前放在这个位置，无限WA，因为每一次都取一个，所以就造成了输入的出错！   
+          getline(cin, input1);  
+          getline(cin, input2);  
+          s1.clear();  
+          s2.clear();  
+          s3.clear();  
+          s4.clear();  
+          s5.clear();  
+          s6.clear();  
+          flag1 = true;  
+          flag2 = true;  
+          len1 = input1.length();  
+          len2 = input2.length();  
+          for (i = 0; i < len1; i++){  
+              if (input1[i] == '<' && flag1){  
+                   flag1 = false;  
+                   pos1 = i;  
+              }  
+              else if (input1[i] == '<' && !flag1){  
+                   pos3 = i;  
+              }  
+              else if (input1[i] == '>' && flag2){  
+                   flag2 = false;  
+                   pos2 = i;  
+              }  
+              else if (input1[i] == '>' && !flag2){  
+                   pos4 = i;  
+              }  
+          }  
+          for (i = 0; i < pos1; i++){  
+              s5.push_back(input1[i]);  
+          }  
+          for (i = pos1+1; i < pos2; i++){  
+              s1.push_back(input1[i]);  
+          }  
+          for (i = pos2+1; i < pos3; i++){  
+              s2.push_back(input1[i]);  
+          }  
+          for (i = pos3+1; i < pos4; i++){  
+              s3.push_back(input1[i]);  
+          }  
+          for (i = pos4+1; i < len1; i++){  
+              s4.push_back(input1[i]);  
+          }  
+          for (i = 0; i < len2 && input2[i] != '.'; i++){  
+              s6.push_back(input2[i]);  
+          }  
+          cout << s5 << s1 << s2 << s3 << s4 << endl;  
+          cout << s6 << s3 << s2 << s1 << s4 << endl;  
+    }  
+      
+} 
---
+++ 1922.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+const int INF=0x3f3f3f3f;
+int n;
+double ans;
+int main()
+{
+    int v,t;
+    double time;
+    while(scanf("%d",&n),n){
+        ans=INF;
+        for(int i=1;i<=n;i++){
+            scanf("%d%d",&v,&t);
+            if(t<0)
+                continue;
+            time=t+4.5/v*3600;
+            ans=min(time,ans);
+            //if(ans>time)
+               // ans=time;
+        }
+        int res=ceil(ans);
+        printf("%d\n",res);
+    }
+    return 0;
+}
