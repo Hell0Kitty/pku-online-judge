bb29ff5425f468daf98fde0891d356e380ddde51
sdecoder <sde.work.inbox@gmail.com>
2016-03-12 19:02:52 -0500

    2016-03-12 19:02:52

--- 2636.cc
+++ 2636.cc
@@ -1,25 +1,18 @@
-#include<stdio.h>
-int main()
-{
-	int num;
-	int t,n,i,sum;
-	while(scanf("%d",&t)!=EOF)
-	{
-		while(t--)
-		{
-			sum=0;
-			scanf("%d",&n);	
-			for(i=0;i<n;i++)
-			{
-				scanf("%d",&num);
-				sum+=num;
-			}
-			printf("%d\n",sum-(n-1));
-		}
-	}
+#include <stdio.h>
+int main() {
+  int num;
+  int t, n, i, sum;
+  while (scanf("%d", &t) != EOF) {
+    while (t--) {
+      sum = 0;
+      scanf("%d", &n);
+      for (i = 0; i < n; i++) {
+        scanf("%d", &num);
+        sum += num;
+      }
+      printf("%d\n", sum - (n - 1));
+    }
+  }
 
-	return 0;
+  return 0;
 }
-
-
-
