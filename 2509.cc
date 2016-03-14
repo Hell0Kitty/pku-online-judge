89f76126764081d45a1dabb276f2611ed746b714
sdecoder <sde.work.inbox@gmail.com>
2016-03-14 05:39:11 -0400

    2016-03-14 05:39:11

--- 2509.cc
+++ 2509.cc
@@ -1,26 +1,20 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
 
 using namespace std;
 
-int main()
-{
-    long long n,m;
-    long long sum;
-    while(scanf("%lld%lld",&n,&m)!=EOF)
-    {
-        sum=n;
-        while(n>=m)
-        {
-            sum+=n/m;
-            n=n/m+n%m;
-        }
-        printf("%lld\n",sum);
+int main() {
+  long long n, m;
+  long long sum;
+  while (scanf("%lld%lld", &n, &m) != EOF) {
+    sum = n;
+    while (n >= m) {
+      sum += n / m;
+      n = n / m + n % m;
     }
-    return 0;
+    printf("%lld\n", sum);
+  }
+  return 0;
 }
-
-
-
