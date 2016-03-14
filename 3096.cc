c1c84d21083695a1dbce029a0b3a828f80e08aaf
sdecoder <sde.work.inbox@gmail.com>
2016-03-14 05:07:47 -0400

    2016-03-14 05:07:47

---
+++ 3096.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+#define maxn 100
+
+string st;
+char pairs[maxn][2];
+
+bool ok(char a, char b, int x)
+{
+    for (int i = 0; i < x; i++)
+        if (pairs[i][0] == a && pairs[i][1] == b)
+            return false;
+    pairs[x][0] = a;
+    pairs[x][1] = b;
+    return true;
+}
+
+int main()
+{
+       while (getline(cin, st) && st != "*")
+    {
+        int n = st.length();
+        bool s = true;
+        for (int i = 1; i < n - 1; i++)
+        {
+            for (int j = 0; j + i < n; j++)
+            {
+                if (!ok(st[j], st[i + j], j))
+                {
+                    s = false;
+                    break;
+                }
+            }
+            if (!s)
+                break;
+        }
+        if (s)
+            cout << st << " is surprising.\n";
+        else
+            cout << st << " is NOT surprising.\n";
+    }
+    return 0;
+}
+
+
