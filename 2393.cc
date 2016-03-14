b3eece6d5ebf2c1a4d4a3f28f66681bb2e5373fd
sdecoder <sde.work.inbox@gmail.com>
2016-03-14 12:38:59 -0400

    2016-03-14 12:38:59

--- 2393.cc
+++ 2393.cc
@@ -8,29 +8,24 @@ typedef long long LL;
 
 LL c[maxn], y[maxn], n, s;
 
-int main()
-{
-    while(~ scanf("%d%d", &n, &s)) {
-        for(int i = 1; i <= n; i++) {
-            scanf("%d%d", &c[i], &y[i]);
-        }
-
-        LL ans = 0, cost;
-        for(int i = 1; i <= n; i++) {
-            if(i == 1) {
-                cost = c[i];
-                ans += cost * y[i];
-            }
-            else {
-                cost = min(cost + s, c[i]);
-                ans += cost * y[i];
-            }
-        }
-
-        printf("%lld\n", ans);
+int main() {
+  while (~scanf("%d%d", &n, &s)) {
+    for (int i = 1; i <= n; i++) {
+      scanf("%d%d", &c[i], &y[i]);
     }
-    return 0;
+
+    LL ans = 0, cost;
+    for (int i = 1; i <= n; i++) {
+      if (i == 1) {
+        cost = c[i];
+        ans += cost * y[i];
+      } else {
+        cost = min(cost + s, c[i]);
+        ans += cost * y[i];
+      }
+    }
+
+    printf("%lld\n", ans);
+  }
+  return 0;
 }
-
-
-
