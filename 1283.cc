64e02823e5bfef2c23b5d933a660947d95add0d9
sdecoder <sde.work.inbox@gmail.com>
2016-03-16 15:17:46 -0400

    2016-03-16 15:17:46

--- 1283.cc
+++ 1283.cc
@@ -1,40 +1,27 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 long long dp[201][201];
 
-void read(){
+void read() {
+  int i, j, k, n, m;
 
-    int i,j,k,n,m;
+  for (i = 1; i <= 200; i++) dp[i][i] = 1;
 
-    for(i=1;i<=200;i++)
+  for (i = 1; i <= 200; i++)
 
-        dp[i][i]=1;
+    for (j = i + 1; j <= 200; j++) dp[i][j] = dp[i - 1][j - 1] + dp[i][j - i];
 
-    for(i=1;i<=200;i++)
+  while (cin >> n >> m) {
+    if (n == 0 && m == 0) return;
 
-        for(j=i+1;j<=200;j++)
-
-            dp[i][j]=dp[i-1][j-1]+dp[i][j-i];
-
-    while(cin>>n>>m){
-
-        if(n==0&&m==0) return;
-
-        cout<<dp[m][n]<<endl;
-
-    }
-
+    cout << dp[m][n] << endl;
+  }
 }
 
-int main(){
+int main() {
+  read();
 
-    read();
-
-    return 0;
-
+  return 0;
 }
-
-
-
