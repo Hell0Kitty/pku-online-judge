d3169095dc4a0f3a22b894d3fd6e7b23ebfb45aa
sdecoder <sde.work.inbox@gmail.com>
2016-03-16 20:43:41 -0400

    2016-03-16 20:43:41

--- 3044.cc
+++ 3044.cc
@@ -6,44 +6,35 @@
 #include <queue>
 #define r 500005
 using namespace std;
-struct data
-{
-	int y;
-	bool operator <(const data &A)const
-	{
-		return A.y>y;
-	}
+struct data {
+  int y;
+  bool operator<(const data &A) const { return A.y > y; }
 };
 priority_queue<data> q;
-int n,w,ok[r];
-int main()
-{
-	scanf("%d%d",&n,&w);
-	for (int i=1;i<=500000;i++)
-	   ok[i]=1;
-	int ans=n;
-	for (int i=1;i<=n;i++)
-	{
-	   int x,y; 
-	   scanf("%d%d",&x,&y);
-	   if (!q.empty())
-	   {
-		   data p=q.top();
-	       while (p.y>y)
-	       {
-			   ok[p.y]=1;
-	   	       q.pop();
-		       if (q.empty()) break;
-	   	       p=q.top();
-		   }
-	   }
-	   data e;
-	   e.y=y;
-	   if (ok[y]) q.push(e),ok[y]=0;
-	   else ans--;
+int n, w, ok[r];
+int main() {
+  scanf("%d%d", &n, &w);
+  for (int i = 1; i <= 500000; i++) ok[i] = 1;
+  int ans = n;
+  for (int i = 1; i <= n; i++) {
+    int x, y;
+    scanf("%d%d", &x, &y);
+    if (!q.empty()) {
+      data p = q.top();
+      while (p.y > y) {
+        ok[p.y] = 1;
+        q.pop();
+        if (q.empty()) break;
+        p = q.top();
+      }
     }
-    cout<<ans<<endl;
-	return 0;
+    data e;
+    e.y = y;
+    if (ok[y])
+      q.push(e), ok[y] = 0;
+    else
+      ans--;
+  }
+  cout << ans << endl;
+  return 0;
 }
-
-
