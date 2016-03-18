2e2c8c605ac10a658cb3e1d86dd8ed88e3922fe7
sdecoder <sde.work.inbox@gmail.com>
2016-03-18 15:06:48 -0400

    2016-03-18 15:06:48

--- 1185.cc
+++ 1185.cc
@@ -1,69 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+#define MAX(a, b) (a) > (b) ? (a) : (b)
+using namespace std;
+int dp[105][65][65];  // d[i][j][k]: “第i行状态是s[j]，第i-1行状态是s[k]”的
+int s[105];  //一行的状态选择s[0], s[1], ... , s[k-1]
+int n, m;  // n行×m列
+int k;  //一行的所有状态数
+int map[105];  //'H''P'地图map[0]~map[n-1]，地图每一行map[line]: 1001 表示HPPH
+int sum[105];
 
-/****************************************************************************************************
-Source Code
-Problem: 1185  User: new_star
-Memory: 316K  Time: 235MS
-Language: G++  Result: Accepted
-****************************************************************************************************/
-// Source Code
-#include <cstdio>
-char map[100][11];
-int vipmap[100], row, column, s[61], sNum, c[61], f[2][61][61], roll, connons;
-int main(void) {  //读图并压缩成相应的状态图
-  scanf("%d%d", &row, &column);
-  for (int i = 0; i < row; i++) scanf("%s", &map[i]);
-  for (int i = 0; i < row; i++)
-    for (int j = 0; j < column; j++)
-      if (map[i][j] == 'H') vipmap[i] += (1 << j);
+//状态s[x]是否造成行冲突
+bool ok(int x) {
+  if (x & (x << 1)) return false;
+  if (x & (x << 2)) return false;
+  return true;
+}
 
-  //合法状态及其总数,和相应状态的炮兵数
-  sNum = 0;
-  for (int k = 0; k < (1 << column); k++) {
-    int m = k;
-    if (((m << 1) & k) || ((m << 2) & k)) continue;
-    c[sNum] = m % 2;
-    while (m = (m >> 1)) c[sNum] += m % 2;
-    s[sNum++] = k;
+//状态s[x]下有多少个1
+int getsum(int x) {
+  int num = 0;
+  while (x > 0) {
+    if (x & 1) num++;
+    x >>= 1;
   }
+  return num;
+}
 
-  //用滚动数组依次求出每行的最优解
-  // roll为当前行,(roll+1)%2为前一行也即下一行
-  roll = 0;
-  for (int i = 0; i < row; i++) {
-    for (int j = 0; j < sNum; j++) {
-      if ((s[j] & vipmap[i])) continue;  //状态j是否与i行地图冲突
-      if (i == 0)
-        f[roll][j][0] = c[j];
-      else if (i == 1) {
-        for (int k = 0; k < sNum; k++) {
-          if ((s[k] & vipmap[i - 1])) continue;
-          if ((s[j] & s[k])) continue;
-          if (f[roll][j][k] < f[(roll + 1) % 2][k][0] + c[j])
-            f[roll][j][k] = f[(roll + 1) % 2][k][0] + c[j];
-        }
-      } else {
-        for (int k = 0; k < sNum; k++) {
-          if ((s[k] & vipmap[i - 1])) continue;  //状态k是否与i-1行地图冲突
-          if (s[j] & s[k]) continue;             //状态j、k是否彼此冲突
-          for (int l = 0; l < sNum; l++) {
-            if ((s[l] & vipmap[i - 2])) continue;  //状态l是否与i-2行地图冲突
-            if ((s[j] & s[l]) || (s[k] & s[l]))
-              continue;  //状态j、l、k是否彼此冲突
-            if (f[roll][j][k] < f[(roll + 1) % 2][k][l] + c[j])
-              f[roll][j][k] = f[(roll + 1) % 2][k][l] + c[j];
+void find() {
+  memset(s, 0, sizeof(s));
+  for (int i = 0; i < (1 << m); i++)  // i枚举所有m位的二进制数
+  {
+    if (ok(i)) {
+      s[k] = i;
+      sum[k++] = getsum(i);
+    }
+  }
+}
+
+int main() {
+  while (scanf("%d%d", &n, &m) != EOF) {
+    memset(dp, -1, sizeof(dp));
+
+    int i;
+    for (i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        char tmp;
+        cin >> tmp;
+        if (tmp == 'H')
+          map[i] = map[i] | (1 << j);  //把第i行原始状态取反后放入map[i]
+      }
+    }
+
+    k = 0;
+    find();
+
+    // 1. 初始化第0行状态（只考虑有效状态，无效状态为-1）
+    for (i = 0; i < k; i++)
+      if (!(s[i] & map[0]))  // s[i]为1的位如果对应平原(0)，则&运算后为0
+        dp[0][i][0] = sum[i];
+
+    // 2. 计算第1~n-1行状态（碰到无效状态,continue）
+    for (int r = 1; r < n; r++) {
+      for (int i = 0; i < k; i++)  //枚举第r行的状态 s[i]
+      {
+        if (map[r] & s[i]) continue;  //通过地形排除部分第r行的状态
+
+        for (int p = 0; p < k; p++)  //枚举第r-1行状态 s[p]
+        {
+          if (s[i] & s[p]) continue;  // r与r-1没有想接触的
+
+          for (int q = 0; q < k; q++)  //枚举第r-2行状态s[q]
+          {
+            if (s[p] & s[q]) continue;
+            if (s[i] & s[q]) continue;  // r与r-2行没有接触的
+
+            if (dp[r - 1][p][q] == -1)
+              continue;  //所有不可能的情形dp[i][j][k]都为-1（初始化的值）
+            dp[r][i][p] = MAX(dp[r][i][p], dp[r - 1][p][q] + sum[i]);
           }
         }
       }
     }
-    roll = (roll + 1) % 2;
-  }
-  roll = (roll + 1) % 2;
 
-  //搜索最优解
-  connons = 0;
-  for (int j = 0; j < sNum; j++)
-    for (int k = 0; k < sNum; k++)
-      if (connons < f[roll][j][k]) connons = f[roll][j][k];
-  printf("%d\n", connons);
+    int ans = 0;
+    for (i = 0; i < k; i++)
+      for (int j = 0; j < k; j++) ans = MAX(ans, dp[n - 1][i][j]);
+    printf("%d\n", ans);
+  }
   return 0;
 }
