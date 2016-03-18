8f1f06379456bd2074b915dbbd488d240d0b313b
sdecoder <sde.work.inbox@gmail.com>
2016-03-18 12:39:38 -0400

    2016-03-18 12:39:38

--- 3300.cc
+++ 3300.cc
@@ -6,9 +6,9 @@ using namespace std;
 
 float ratio[100];
 
-int f,r;
+int f, r;
 
-float front[10],rear[10];
+float front[10], rear[10];
 
 float max_ratio;
 
@@ -17,62 +17,41 @@ int index;
 int main()
 
 {
+  while (scanf("%d", &f) != EOF && f)
 
-    while(scanf("%d",&f)!=EOF && f)
+  {
+    scanf("%d", &r);
+
+    for (int i = 0; i < f; i++) scanf("%f", &front[i]);
+
+    for (int i = 0; i < r; i++) scanf("%f", &rear[i]);
+
+    index = 0;
+
+    for (int i = 0; i < r; i++)
 
     {
+      for (int j = 0; j < f; j++)
 
-        scanf("%d",&r);
-
-        for(int i=0; i<f; i++)
-
-            scanf("%f",&front[i]);
-
-        for(int i=0; i<r; i++)
-
-            scanf("%f",&rear[i]);
-
-        index=0;
-
-        for(int i=0; i<r; i++)
-
-        {
-
-            for(int j=0; j<f; j++)
-
-            {
-
-                ratio[index++]=rear[i]/front[j];
-
-            }
-
-        }
-
-        sort(ratio,ratio+index);
-
-        for(int i=0; i<index-1; i++)
-
-            ratio[i]=ratio[i+1]/ratio[i];
-
-        max_ratio=0.0;
-
-        for(int i=0; i<index-1; i++)
-
-        {
-
-            if(max_ratio<ratio[i])
-
-                max_ratio=ratio[i];
-
-        }
-
-        printf("%.2f\n",max_ratio);
-
+      {
+        ratio[index++] = rear[i] / front[j];
+      }
     }
 
-    return 0;
+    sort(ratio, ratio + index);
 
+    for (int i = 0; i < index - 1; i++) ratio[i] = ratio[i + 1] / ratio[i];
+
+    max_ratio = 0.0;
+
+    for (int i = 0; i < index - 1; i++)
+
+    {
+      if (max_ratio < ratio[i]) max_ratio = ratio[i];
+    }
+
+    printf("%.2f\n", max_ratio);
+  }
+
+  return 0;
 }
-
-
-
