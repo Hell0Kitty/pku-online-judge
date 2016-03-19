e00f2ca56cd6c57519364a975f1d719d01d824ea
sdecoder <sde.work.inbox@gmail.com>
2016-03-19 08:15:06 -0400

    2016-03-19 08:15:06

--- 2843.cc
+++ 2843.cc
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 #define ll long long
 #define MAX_N 1024
 using namespace std;
 
 struct queue_node {
-    int y, x;
+  int y, x;
 };
 
 int set[MAX_N][MAX_N], cut[MAX_N][MAX_N];
@@ -14,67 +14,57 @@ int left, top, right, bottom;
 struct queue_node queue[MAX_N * MAX_N];
 int head, tail;
 
-int find(int *arr, int idx){
-    static int stk[MAX_N * MAX_N], sp;
-    for (sp = 0; arr[idx]; idx = arr[idx])
-        stk[sp++] = idx;
-    for (sp--; sp >= 0; sp--)
-        arr[stk[sp]] = idx;
-    return idx;
+int find(int *arr, int idx) {
+  static int stk[MAX_N * MAX_N], sp;
+  for (sp = 0; arr[idx]; idx = arr[idx]) stk[sp++] = idx;
+  for (sp--; sp >= 0; sp--) arr[stk[sp]] = idx;
+  return idx;
 }
-void push(int y, int x){
-    if (x < left || x > right || y < top || y > bottom)
-        return ;
-    if (cut[y][x])
-        return ;
+void push(int y, int x) {
+  if (x < left || x > right || y < top || y > bottom) return;
+  if (cut[y][x]) return;
 
-    cut[y][x] = 1;
-    if (cut[y][x - 1] && cut[y][x + 1]) {
-        set[y][x] = x + 1;
-        set[y][x - 1] = x + 1;
-    } else if (cut[y][x - 1])
-        set[y][x - 1] = x;
-    else if (cut[y][x + 1])
-        set[y][x] = x + 1;
+  cut[y][x] = 1;
+  if (cut[y][x - 1] && cut[y][x + 1]) {
+    set[y][x] = x + 1;
+    set[y][x - 1] = x + 1;
+  } else if (cut[y][x - 1])
+    set[y][x - 1] = x;
+  else if (cut[y][x + 1])
+    set[y][x] = x + 1;
 
-    queue[tail].y = y;
-    queue[tail].x = x;
-    tail++;
+  queue[tail].y = y;
+  queue[tail].x = x;
+  tail++;
 }
-int bfs(int y, int x){
-    x = find(set[y], x);
-    if (cut[y][x])
-        x++;
-    if (x > right)
-        return 0;
+int bfs(int y, int x) {
+  x = find(set[y], x);
+  if (cut[y][x]) x++;
+  if (x > right) return 0;
 
-    head = tail = 0;
-    push(y, x);
-    while (head != tail) {
-        y = queue[head].y;
-        x = queue[head].x;
-        head++;
-        push(y - 1, x);
-        push(y + 1, x);
-        push(y, x - 1);
-        push(y, x + 1);
-    }
+  head = tail = 0;
+  push(y, x);
+  while (head != tail) {
+    y = queue[head].y;
+    x = queue[head].x;
+    head++;
+    push(y - 1, x);
+    push(y + 1, x);
+    push(y, x - 1);
+    push(y, x + 1);
+  }
 
-    return 1;
+  return 1;
 }
-int main(){
-    int i, j, ans;
+int main() {
+  int i, j, ans;
 
-    scanf("%d%d", &N, &M);
-    while (M--) {
-        scanf("%d%d%d%d", &left, &top, &right, &bottom);
-        ans = 0;
-        for (i = top; i <= bottom; i++)
-            while (bfs(i, left))
-                ans++;
-        printf("%d\n", ans);
-    }
+  scanf("%d%d", &N, &M);
+  while (M--) {
+    scanf("%d%d%d%d", &left, &top, &right, &bottom);
+    ans = 0;
+    for (i = top; i <= bottom; i++)
+      while (bfs(i, left)) ans++;
+    printf("%d\n", ans);
+  }
 }
-
-
-
