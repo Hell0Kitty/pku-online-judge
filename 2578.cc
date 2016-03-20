1c6d9460d2e295394ee53c988f18116ffbe7ceae
sdecoder <sde.work.inbox@gmail.com>
2016-03-20 10:49:52 -0400

    2016-03-20 10:49:52

--- 2578.cc
+++ 2578.cc
@@ -1,19 +1,14 @@
 #include <cstdio>
 
-int main()
-{
-    int f[5];
-    for (int i = 0; i < 3; i++)
-    {
-        scanf("%d", &f[i]);
-        if (f[i] <= 168)
-        {
-            printf("CRASH %d\n", f[i]);
-            return 0;
-        }
+int main() {
+  int f[5];
+  for (int i = 0; i < 3; i++) {
+    scanf("%d", &f[i]);
+    if (f[i] <= 168) {
+      printf("CRASH %d\n", f[i]);
+      return 0;
     }
-    printf("NO CRASH\n");
-    return 0;
+  }
+  printf("NO CRASH\n");
+  return 0;
 }
-
-
