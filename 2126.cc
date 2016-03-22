5a04c7984a17aaad785aee7e9822e205a4199c50
sdecoder <sde.work.inbox@gmail.com>
2016-03-22 02:44:32 -0400

    2016-03-22 02:44:32

--- 2126.cc
+++ 2126.cc
@@ -4,28 +4,22 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    if (n <= 1)
-    {
-        printf("YES\n");
-        return 0;
-    }
-    if (n > 2)
-    {
-        printf("NO\n");
-        return 0;
-    }
-    int a, b, c;
-    scanf("%d%d%d", &a, &b, &c);
-    if (b * b - 4 * a * c >= 0)
-        printf("NO\n");
-    else
-        printf("YES\n");
+int main() {
+  int n;
+  scanf("%d", &n);
+  if (n <= 1) {
+    printf("YES\n");
     return 0;
+  }
+  if (n > 2) {
+    printf("NO\n");
+    return 0;
+  }
+  int a, b, c;
+  scanf("%d%d%d", &a, &b, &c);
+  if (b * b - 4 * a * c >= 0)
+    printf("NO\n");
+  else
+    printf("YES\n");
+  return 0;
 }
-
-
-
