9083b960a13f9ffa6e99a7ba2517d08fe6d2b8b3
sdecoder <sde.work.inbox@gmail.com>
2016-03-24 02:46:19 -0400

    2016-03-24 02:46:19

---
+++ 3122.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+#define maxn 10005
+#define eps 1.0e-4
+#define PI acos(-1.0)
+
+int n, m;
+int f[maxn];
+double s[maxn], ms;
+
+void input()
+{
+    scanf("%d%d", &n, &m);
+    m++;
+    for (int i =0; i < n; i++)
+        scanf("%d", &f[i]);
+    ms =-1;
+    for (int i =0; i < n; i++)
+    {
+        s[i] = f[i] * f[i] * PI;
+        ms = max(s[i], ms);
+    }
+}
+
+bool ok(double a)
+{
+    int d =0;
+    for (int i =0; i < n; i++)
+        d += (int) (s[i] / a);
+    return d >= m;
+}
+
+double binarysearch()
+{
+    double l = eps;
+    double r = ms;
+    double mid;
+    while (l + eps < r)
+    {
+        mid = (l + r) /2;
+        if (ok(mid))
+            l = mid;
+        else
+            r = mid;
+    }
+    return l;
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        input();
+        double ans = binarysearch();
+        printf("%.4f\n", ans);
+    }
+    return 0;
+}
+
+
+
