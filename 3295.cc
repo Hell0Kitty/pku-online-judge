fd085478b59ccf9d5872fe29aefd90e00510c0f4
sdecoder <sde.work.inbox@gmail.com>
2016-03-27 15:46:33 -0400

    2016-03-27 15:46:33

--- 3295.cc
+++ 3295.cc
@@ -13,54 +13,61 @@ char s[maxn];
 bool hash[200], ans;
 
 void check(char pos) {
+  if (s[pos] == 'N') {
+    bool x = S.top();
+    S.pop();
+    S.push(!x);
+  } else if (s[pos] >= 'p' && s[pos] <= 't') {
+    S.push(hash[s[pos]]);
+  } else {
+    bool x, y;
+    x = S.top();
+    S.pop();
+    y = S.top();
+    S.pop();
 
-    if(s[pos] == 'N') {
-        bool x = S.top(); S.pop();
-        S.push(!x);
-    } else if(s[pos] >= 'p' && s[pos] <= 't') {
-        S.push(hash[s[pos]]);
-    } else {
-        bool x, y;
-        x = S.top(); S.pop();
-        y = S.top(); S.pop();
-
-        if(s[pos] == 'K') S.push(x&y);
-        else if(s[pos] == 'A') S.push(x|y);
-        else if(s[pos] == 'C') S.push(!x | y); //即蕴含
-        else if(s[pos] == 'E') S.push(!(x^y));
-    }
-
+    if (s[pos] == 'K')
+      S.push(x & y);
+    else if (s[pos] == 'A')
+      S.push(x | y);
+    else if (s[pos] == 'C')
+      S.push(!x | y);  //即蕴含
+    else if (s[pos] == 'E')
+      S.push(!(x ^ y));
+  }
 }
 
 int main() {
-    while(scanf("%s", s) == 1) {
-        if(s[0] == '0') break;
-        ans = true;
-        int i;
+  while (scanf("%s", s) == 1) {
+    if (s[0] == '0') break;
+    ans = true;
+    int i;
 
-        for(i=0; i<(1<<5); i++) {
-            hash['p'] = i & 1;
-            hash['q'] = i & (1<<1);
-            hash['r'] = i & (1<<2);
-            hash['s'] = i & (1<<3);
-            hash['t'] = i & (1<<4);
+    for (i = 0; i < (1 << 5); i++) {
+      hash['p'] = i & 1;
+      hash['q'] = i & (1 << 1);
+      hash['r'] = i & (1 << 2);
+      hash['s'] = i & (1 << 3);
+      hash['t'] = i & (1 << 4);
 
-            int pos = strlen(s)-1;
+      int pos = strlen(s) - 1;
 
-            while(pos >= 0) {
-                check(pos);
-                pos--;
-            }
+      while (pos >= 0) {
+        check(pos);
+        pos--;
+      }
 
-            ans = S.top(); S.pop();
+      ans = S.top();
+      S.pop();
 
-            if(ans == false) { printf("not\n"); break; }
-        }
-
-        if(i >= (1<<5)) printf("tautology\n");
+      if (ans == false) {
+        printf("not\n");
+        break;
+      }
     }
 
-    return 0;
+    if (i >= (1 << 5)) printf("tautology\n");
+  }
+
+  return 0;
 }
-
-
