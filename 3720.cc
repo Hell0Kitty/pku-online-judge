388c13bac7cdd3703aefb5d409699e243313d8fb
sdecoder <sde.work.inbox@gmail.com>
2016-04-02 03:01:40 -0400

    2016-04-02 03:01:40

--- 3720.cc
+++ 3720.cc
@@ -1,39 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 #define esp 1e-10
 int ans[102][11];
 int vis[1000];
-void getans(int n)
-{
-	int now=10;
-	memset(vis,0,sizeof(vis));
-	while(!vis[now]&&now!=0)
-	{
-		vis[now]=1;
-		int temp=now/n;
-		ans[n][temp]++;
-		now%=n;
-		now*=10;
-	}
+void getans(int n) {
+  int now = 10;
+  memset(vis, 0, sizeof(vis));
+  while (!vis[now] && now != 0) {
+    vis[now] = 1;
+    int temp = now / n;
+    ans[n][temp]++;
+    now %= n;
+    now *= 10;
+  }
 }
-int main()
-{
-	memset(ans,0,sizeof(ans));
-	for(int i=2;i<101;i++)
-	{
-		getans(i);
-	}
-	int n,k;
-	while(scanf("%d%d",&n,&k)!=EOF)
-	{
-		int sum=0;
-		for(int i=2;i<=n;i++)
-			sum+=ans[i][k];
-		cout<<sum<<endl;
-	}
-	return 0;
+int main() {
+  memset(ans, 0, sizeof(ans));
+  for (int i = 2; i < 101; i++) {
+    getans(i);
+  }
+  int n, k;
+  while (scanf("%d%d", &n, &k) != EOF) {
+    int sum = 0;
+    for (int i = 2; i <= n; i++) sum += ans[i][k];
+    cout << sum << endl;
+  }
+  return 0;
 }
-
-
-
-
