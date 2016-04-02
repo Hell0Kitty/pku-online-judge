170a56011359f3856425f18ae24c97d4c6a30e5b
sdecoder <sde.work.inbox@gmail.com>
2016-04-02 18:35:19 -0400

    2016-04-02 18:35:19

--- 3187.cc
+++ 3187.cc
@@ -6,55 +6,41 @@ const int MAX = 10;
 int data[MAX];
 int n, sum;
 
-void showResult()
-{
-	for(int i=0; i < n; i++)
-		cout << data[i] << ' ';
-	cout << endl;
+void showResult() {
+  for (int i = 0; i < n; i++) cout << data[i] << ' ';
+  cout << endl;
 }
 
-bool adjCase()
-{
-	int temp[MAX], i;
+bool adjCase() {
+  int temp[MAX], i;
 
-	for(i=0; i < n; i++)
-		temp[i] = data[i];
+  for (i = 0; i < n; i++) temp[i] = data[i];
 
-	for(int num=n; num > 0; num --)
-	{
-		for(i=0; i < num-1; i++)
-		{
-			temp[i]=temp[i]+temp[i+1];
-		}
-	}
-	if(temp[0]==sum) return true;
-	return false;
+  for (int num = n; num > 0; num--) {
+    for (i = 0; i < num - 1; i++) {
+      temp[i] = temp[i] + temp[i + 1];
+    }
+  }
+  if (temp[0] == sum) return true;
+  return false;
 }
 
-void solveCase()
-{
-	if(adjCase()){
-		showResult();
-		return ;
-	}
-	while(next_permutation(data, data+n))
-	{
-		if(adjCase())
-		{
-			showResult();
-			break;
-		}		
-	}
+void solveCase() {
+  if (adjCase()) {
+    showResult();
+    return;
+  }
+  while (next_permutation(data, data + n)) {
+    if (adjCase()) {
+      showResult();
+      break;
+    }
+  }
 }
 
-int main()
-{
-	cin >> n >> sum;
-	for(int i=1; i <= n; i++)
-		data[i-1]=i;
-	solveCase();
-	return 0;
+int main() {
+  cin >> n >> sum;
+  for (int i = 1; i <= n; i++) data[i - 1] = i;
+  solveCase();
+  return 0;
 }
-
-
-
