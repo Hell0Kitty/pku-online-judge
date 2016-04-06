da3d421d03f8d40f18e4a789b796ed254fedaad4
sdecoder <sde.work.inbox@gmail.com>
2016-04-06 07:33:50 -0400

    2016-04-06 07:33:50

--- 2812.cc
+++ 2812.cc
@@ -1,16 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-	double dVertice[20][2],dArea,dVolumn;int iVerticeNum,i;
-	while(scanf("%d",&iVerticeNum) && iVerticeNum!=0)
-	{
-		for(i=0;i<iVerticeNum;i++)scanf("%lf%lf",&dVertice[i][0],&dVertice[i][1]);
-		scanf("%lf",&dVolumn);
-		for(i=0,dArea=0.0;i<iVerticeNum;i++)
-			dArea+=dVertice[(i+1)%iVerticeNum][0]*dVertice[i%iVerticeNum][1]-dVertice[(i+1)%iVerticeNum][1]*dVertice[i%iVerticeNum][0];
-		printf("BAR LENGTH: %.2lf\n",2.0*dVolumn/dArea);
-	}
+int main() {
+  double dVertice[20][2], dArea, dVolumn;
+  int iVerticeNum, i;
+  while (scanf("%d", &iVerticeNum) && iVerticeNum != 0) {
+    for (i = 0; i < iVerticeNum; i++)
+      scanf("%lf%lf", &dVertice[i][0], &dVertice[i][1]);
+    scanf("%lf", &dVolumn);
+    for (i = 0, dArea = 0.0; i < iVerticeNum; i++)
+      dArea +=
+          dVertice[(i + 1) % iVerticeNum][0] * dVertice[i % iVerticeNum][1] -
+          dVertice[(i + 1) % iVerticeNum][1] * dVertice[i % iVerticeNum][0];
+    printf("BAR LENGTH: %.2lf\n", 2.0 * dVolumn / dArea);
+  }
 }
-
-
