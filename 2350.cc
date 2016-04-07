fb1f83426e92df79fbd2fe0bf3cc4ab7ae07dc19
sdecoder <sde.work.inbox@gmail.com>
2016-04-07 19:20:37 -0400

    2016-04-07 19:20:37

--- 2350.cc
+++ 2350.cc
@@ -10,38 +10,29 @@ int n;
 int f[maxn];
 double average;
 
-void input()
-{
-    int sum = 0;
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &f[i]);
-        sum += f[i];
-    }
-    average = sum * 1.0 / n;
+void input() {
+  int sum = 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &f[i]);
+    sum += f[i];
+  }
+  average = sum * 1.0 / n;
 }
 
-double work()
-{
-    int num = 0;
-    for (int i = 0; i < n; i++)
-        if (f[i] > average)
-            num++;
-    return num * 100.0 / n;
+double work() {
+  int num = 0;
+  for (int i = 0; i < n; i++)
+    if (f[i] > average) num++;
+  return num * 100.0 / n;
 }
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        input();
-        printf("%.3f%%\n", work());
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    input();
+    printf("%.3f%%\n", work());
+  }
+  return 0;
 }
-
-
-
