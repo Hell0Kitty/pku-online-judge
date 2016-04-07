1db68d2539a18305363ab117b1669180db3cfb92
sdecoder <sde.work.inbox@gmail.com>
2016-04-06 23:09:11 -0400

    2016-04-06 23:09:11

--- 2313.cc
+++ 2313.cc
@@ -1,43 +1,40 @@
-#include<iostream>
-#include<cstring>
-#include<cstdio>
-#include<algorithm>
-#include<string>
-#include<map>
-#include<cmath>
-#include<vector>
-#include<set>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <cmath>
+#include <vector>
+#include <set>
 using namespace std;
-int f_min(int x,int y){return x<y?x:y;}
-int f_max(int x,int y){return x>y?x:y;}
-int f_abs(int x){return x<0?-x:x;}
-int N,num[105],ans[105];
-void get_data(){
-	int i;
-	scanf("%d",&N);
-	for(i=0;i<N;i++)scanf("%d",&num[i]);
+int f_min(int x, int y) { return x < y ? x : y; }
+int f_max(int x, int y) { return x > y ? x : y; }
+int f_abs(int x) { return x < 0 ? -x : x; }
+int N, num[105], ans[105];
+void get_data() {
+  int i;
+  scanf("%d", &N);
+  for (i = 0; i < N; i++) scanf("%d", &num[i]);
 }
 
-void run(){
-	int i,res=0;
-	for(i=0;i<N;i++)ans[i]=num[i];
-	for(i=1;i<N-1;i++){
-		if(ans[i]<ans[i-1]&&num[i]<ans[i+1]){
-			ans[i]=f_min(ans[i-1],ans[i+1]);
-		}
-		if(ans[i]>ans[i-1]&&ans[i]>ans[i+1]){
-			ans[i]=f_max(ans[i-1],ans[i+1]);
-		}
-	}
-	for(i=0;i<N;i++)res+=f_abs(ans[i]-num[i]);
-	for(i=1;i<N;i++)res+=f_abs(ans[i-1]-ans[i]);
-	printf("%d\n",res);
+void run() {
+  int i, res = 0;
+  for (i = 0; i < N; i++) ans[i] = num[i];
+  for (i = 1; i < N - 1; i++) {
+    if (ans[i] < ans[i - 1] && num[i] < ans[i + 1]) {
+      ans[i] = f_min(ans[i - 1], ans[i + 1]);
+    }
+    if (ans[i] > ans[i - 1] && ans[i] > ans[i + 1]) {
+      ans[i] = f_max(ans[i - 1], ans[i + 1]);
+    }
+  }
+  for (i = 0; i < N; i++) res += f_abs(ans[i] - num[i]);
+  for (i = 1; i < N; i++) res += f_abs(ans[i - 1] - ans[i]);
+  printf("%d\n", res);
 }
-int main(){
-	get_data();
-	run();
-	return 0;
+int main() {
+  get_data();
+  run();
+  return 0;
 }
-
-
-
