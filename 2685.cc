834e0f2deccdd5799a76878df9c60c16f86d24b7
sdecoder <sde.work.inbox@gmail.com>
2016-04-07 03:16:08 -0400

    2016-04-07 03:16:08

--- 2685.cc
+++ 2685.cc
@@ -5,99 +5,85 @@
 
 #define CNT 2
 #define MAXSIZE 9
-int main()
-{
-	int cnt;
-	scanf("%d",&cnt);
-	while( cnt-- )
-	{
-		char input[CNT][MAXSIZE];
-		int  result[CNT] = {0,0};
+int main() {
+  int cnt;
+  scanf("%d", &cnt);
+  while (cnt--) {
+    char input[CNT][MAXSIZE];
+    int result[CNT] = {0, 0};
 
-		scanf("%s",input[0]);
-		scanf("%s",input[1]);
+    scanf("%s", input[0]);
+    scanf("%s", input[1]);
 
-		for(int j=0; j<2; j++)
-		{
-			for(int i=0; i<strlen(input[j]); i++)
-			{
-				if(isdigit(input[j][i]))
-				{
-					int temp = atoi(&input[j][i]);
-					switch(input[j][++i])
-					{
-						case 'm':
-							result[j] += 1000 * temp;
-							break;
-						case 'c':
-							result[j] += 100 * temp;
-							break;
-						case 'x':
-							result[j] += 10 * temp;
-							break;
-						case 'i':
-							result[j] += temp;
-							break;
-						default:
-							break;
-					}
-				}else{
-					switch(input[j][i])
-					{
-						case 'm':
-							result[j] += 1000 ;
-							break;
-						case 'c':
-							result[j] += 100 ;
-							break;
-						case 'x':
-							result[j] += 10 ;
-							break;
-						case 'i':
-							result[j] += 1;
-							break;
-						default:
-							break;
-					}
-				}
-			}
-		}
+    for (int j = 0; j < 2; j++) {
+      for (int i = 0; i < strlen(input[j]); i++) {
+        if (isdigit(input[j][i])) {
+          int temp = atoi(&input[j][i]);
+          switch (input[j][++i]) {
+            case 'm':
+              result[j] += 1000 * temp;
+              break;
+            case 'c':
+              result[j] += 100 * temp;
+              break;
+            case 'x':
+              result[j] += 10 * temp;
+              break;
+            case 'i':
+              result[j] += temp;
+              break;
+            default:
+              break;
+          }
+        } else {
+          switch (input[j][i]) {
+            case 'm':
+              result[j] += 1000;
+              break;
+            case 'c':
+              result[j] += 100;
+              break;
+            case 'x':
+              result[j] += 10;
+              break;
+            case 'i':
+              result[j] += 1;
+              break;
+            default:
+              break;
+          }
+        }
+      }
+    }
 
-		int sum = result[0] + result[1];
-		char print_sum[MAXSIZE] = {0};
-		int index = 0;
-		for(int div=1000; div!=0 ; div/=10)
-		{
-			if(sum / div)
-			{
-				if(sum / div != 1)
-				{
-					print_sum[index++] = sum/div + '0'; 
-				}
-				switch(div)
-				{
-					case 1000:
-						print_sum[index] = 'm';
-							break;
-					case 100:
-						print_sum[index] = 'c';
-							break;
-					case 10:
-						print_sum[index] = 'x';
-							break;
-					case 1:
-						print_sum[index] = 'i';
-							break;
-				}
+    int sum = result[0] + result[1];
+    char print_sum[MAXSIZE] = {0};
+    int index = 0;
+    for (int div = 1000; div != 0; div /= 10) {
+      if (sum / div) {
+        if (sum / div != 1) {
+          print_sum[index++] = sum / div + '0';
+        }
+        switch (div) {
+          case 1000:
+            print_sum[index] = 'm';
+            break;
+          case 100:
+            print_sum[index] = 'c';
+            break;
+          case 10:
+            print_sum[index] = 'x';
+            break;
+          case 1:
+            print_sum[index] = 'i';
+            break;
+        }
 
-				index ++;
-				sum %= div;
-			}
-		}
-		printf("%s\n",print_sum);
-	}
-	return 0;
+        index++;
+        sum %= div;
+      }
+    }
+    printf("%s\n", print_sum);
+  }
+  return 0;
 }
-
-
-
