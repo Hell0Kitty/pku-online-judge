6f044d26e5e74ac9efed9c2e6f23cd1f8015bce1
sdecoder <sde.work.inbox@gmail.com>
2016-04-06 21:18:18 -0400

    2016-06-09 21:21:41

--- 3105.cc
+++ 3105.cc
@@ -1,22 +1,18 @@
 #include <stdio.h>
-int main()
-{
+int main() {
+  int t, i, n, s;
+  double r, p;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d", &n);
+    r = 0;
+    for (i = 0; i < 32; i++) {
+      s = 1 << i;
+      p = ((n / s >> 1) * s + (((n & s) == 0) ? n % s : s)) / (double)n;
 
-    int t,i,n,s;
-    double r,p;
-    scanf("%d",&t);
-    while(t--){
-        scanf("%d",&n);
-        r=0;
-        for(i=0;i<32;i++){
-            s=1<<i;
-            p=((n/s>>1)*s+(((n&s)==0)?n%s:s))/(double)n;
-
-            r+= 2*p*(1-p)*s;
-        }
-        printf("%.2lf\n",r);
+      r += 2 * p * (1 - p) * s;
     }
-    return 1;
+    printf("%.2lf\n", r);
+  }
+  return 1;
 }
-
-
