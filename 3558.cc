fee4b8a6cf5aaa34764470141edd65ab0df7ce87
sdecoder <sde.work.inbox@gmail.com>
2016-04-09 04:21:55 -0400

    2016-04-09 04:21:55

--- 3558.cc
+++ 3558.cc
@@ -14,9 +14,9 @@
 #include <functional>
 
 #define sz(v) ((int)(v).size())
-#define rep(i, n) for(int i = 0; i < n; i++)
-#define repf(i, a, b) for(int i = a; i <= b; i++)
-#define repd(i, a, b) for(int i = a; i >= b; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define repf(i, a, b) for (int i = a; i <= b; i++)
+#define repd(i, a, b) for (int i = a; i >= b; i--)
 #define out(n) printf("%d\n", n)
 #define mset(a, b) memset(a, b, sizeof(a)
 
@@ -31,39 +31,32 @@ double f[2][22];
 int n;
 double p;
 
-void get_c()
-{
-    repf(i, 1, 21) {
-        c[i][0] = 1;
-        repf(j, 1, i - 1) {
-            c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
-        }
-        c[i][i] = 1;
+void get_c() {
+  repf(i, 1, 21) {
+    c[i][0] = 1;
+    repf(j, 1, i - 1) { c[i][j] = c[i - 1][j - 1] + c[i - 1][j]; }
+    c[i][i] = 1;
+  }
+}
+
+double mypow(double x, int y) {
+  double res = 1.0;
+  repf(i, 1, y) res *= x;
+  return res;
+}
+
+int main() {
+  get_c();
+
+  while (2 == scanf("%d%lf", &n, &p)) {
+    repf(i, 1, n) {
+      f[0][i] = 0.0;
+      repf(j, 1, i - 1) {
+        f[0][i] += c[i - 1][i - j] * f[1][j] * mypow(mypow((1 - p), j), i - j);
+      }
+      f[1][i] = 1 - f[0][i];
     }
+    printf("%.8lf", f[1][n]);
+  }
+  return 0;
 }
-
-double mypow(double x, int y)
-{
-    double res = 1.0;
-    repf(i, 1, y) res *= x;
-    return res;
-}
-
-int main()
-{
-    get_c();
-
-    while(2 == scanf("%d%lf", &n, &p)) {
-        repf(i, 1, n) {
-            f[0][i] = 0.0;
-            repf(j, 1, i - 1) {
-                f[0][i] += c[i - 1][i - j] * f[1][j] * mypow(mypow((1 - p), j), i - j);
-            }
-            f[1][i] = 1 - f[0][i];
-        }
-        printf("%.8lf", f[1][n]);
-    }
-    return 0;
-}
-
-
