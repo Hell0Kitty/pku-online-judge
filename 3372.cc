25fd0918e892a68cf18b7c8f42aa553791cb33cf
sdecoder <sde.work.inbox@gmail.com>
2016-04-11 21:07:03 -0400

    2016-04-11 21:07:03

---
+++ 3372.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+const int N = 100000;
+bool Num[N];
+int main(void)
+{
+    int n;
+
+    while(cin>>n)
+    {
+        printf((n & n - 1) ? "NO\n" : "YES\n");
+    }
+
+    return 0;
+}
+
+
