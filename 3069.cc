abc5539cc07f4ba1a93965402967f92e72ac74ed
sdecoder <sde.work.inbox@gmail.com>
2016-04-12 08:30:51 -0400

    2016-04-12 08:30:51

--- 3069.cc
+++ 3069.cc
@@ -1,39 +1,29 @@
-#include<cstdio>
-#include<iostream>
-#include<string>
-#include<algorithm>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-    int n, r;
-    int str[1010];
-    while(1)
-    {
-        int cnt = 0;
-        cin>>r>>n;
-        if( n == -1 && r == -1)
-            break;
-        for(int i = 0 ; i < n ; i++)
-        {
-            scanf("%d", &str[i]);
-        }
-        sort(str, str+n);
-        for(int i = 0 ; i < n; )
-        {
-            int s = str[i];
-            while(i < n && str[i] <= s + r)
-                i++;
-            int p = str[i-1];
-            while(i < n && str[i] <= p + r)
-                i++;
-            cnt++;
-        }
-        cout<<cnt<<endl;
-
+int main() {
+  int n, r;
+  int str[1010];
+  while (1) {
+    int cnt = 0;
+    cin >> r >> n;
+    if (n == -1 && r == -1) break;
+    for (int i = 0; i < n; i++) {
+      scanf("%d", &str[i]);
     }
-    return 0;
+    sort(str, str + n);
+    for (int i = 0; i < n;) {
+      int s = str[i];
+      while (i < n && str[i] <= s + r) i++;
+      int p = str[i - 1];
+      while (i < n && str[i] <= p + r) i++;
+      cnt++;
+    }
+    cout << cnt << endl;
+  }
+  return 0;
 }
-
-
