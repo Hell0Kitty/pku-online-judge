a9bd0d493ec65bbc07c6ae28389f7a5396968953
sdecoder <sde.work.inbox@gmail.com>
2016-04-16 18:08:02 -0400

    2016-04-16 18:08:02

--- 3313.cc
+++ 3313.cc
@@ -26,93 +26,93 @@ int tmp[18];
 int ca[22], cb[22];
 
 inline bool check() {
- int b, w;
- for (int i = 0; i < n; ++i) {
- b = w = 0;
- memset(ca, 0, sizeof ca);
- memset(cb, 0, sizeof cb);
- for (int j = 0; j < l; ++j) {
- if (rul[i][j] == tmp[j]) ++b;
- else {
- ++ca[rul[i][j]];
- ++cb[tmp[j]];
- }
- }
- for (int j = 0; j < c; ++j) {
- w += min(ca[j], cb[j]);
- }
- if (b != B[i] || w != W[i]) return false;
- }
- return true;
+  int b, w;
+  for (int i = 0; i < n; ++i) {
+    b = w = 0;
+    memset(ca, 0, sizeof ca);
+    memset(cb, 0, sizeof cb);
+    for (int j = 0; j < l; ++j) {
+      if (rul[i][j] == tmp[j])
+        ++b;
+      else {
+        ++ca[rul[i][j]];
+        ++cb[tmp[j]];
+      }
+    }
+    for (int j = 0; j < c; ++j) {
+      w += min(ca[j], cb[j]);
+    }
+    if (b != B[i] || w != W[i]) return false;
+  }
+  return true;
 }
 void dfs(int d) {
- if (d == l) {
- tmp[d] = 0;
- if (check()) {
- for (int i = 0; i < l; ++i) dat[cnt][i] = tmp[i];
- ++cnt;
- }
- for (int i = 0; i < l; ++i) _all[ac][i] = tmp[i];
- ++ac;
- return;
- }
- for (int i = 0; i < c; ++i) {
- tmp[d] = i;
- dfs(d + 1);
- }
+  if (d == l) {
+    tmp[d] = 0;
+    if (check()) {
+      for (int i = 0; i < l; ++i) dat[cnt][i] = tmp[i];
+      ++cnt;
+    }
+    for (int i = 0; i < l; ++i) _all[ac][i] = tmp[i];
+    ++ac;
+    return;
+  }
+  for (int i = 0; i < c; ++i) {
+    tmp[d] = i;
+    dfs(d + 1);
+  }
 }
 
 int mp[22][22];
 
 int main() {
- int T;
- for (scanf("%d", &T); T--; ) {
- scanf("%d%d%d", &l, &c, &n);
- for (int i = 0; i < n; ++i) {
- scanf(" %s %d %d", str, &B[i], &W[i]);
- for (int j = 0; j < l; ++j) {
- rul[i][j] = str[j] - 'A';
- }
- }
- cnt = ac = 0;
- dfs(0);
- int ret = inf, ch = -1;
- for (int i = 0; i < ac; ++i) {
- memset(mp, 0, sizeof mp);
- for (int j = 0; j < cnt; ++j) {
- memset(ca, 0, sizeof ca);
- memset(cb, 0, sizeof cb);
- int b = 0, w = 0;
- for (int k = 0; k < l; ++k) {
- if (_all[i][k] == dat[j][k]) ++b;
- else {
- ++ca[_all[i][k]];
- ++cb[dat[j][k]];
- }
- }
- for (int k = 0; k < c; ++k) {
- w += min(ca[k], cb[k]);
- }
- if (b == l) continue;
- ++mp[b][w];
- }
- int mx = 0;
- for (int j = 0; j <= l; ++j) {
- for (int k = 0; k <= l; ++k) {
- mx = max(mx, mp[j][k]);
- }
- }
- if (mx < ret) {
- ret = mx;
- ch = i;
- }
- }
- for (int i = 0; i < l; ++i) {
- putchar('A' + _all[ch][i]);
- }
- printf(" %d\n", ret);
- }
- return 0;
+  int T;
+  for (scanf("%d", &T); T--;) {
+    scanf("%d%d%d", &l, &c, &n);
+    for (int i = 0; i < n; ++i) {
+      scanf(" %s %d %d", str, &B[i], &W[i]);
+      for (int j = 0; j < l; ++j) {
+        rul[i][j] = str[j] - 'A';
+      }
+    }
+    cnt = ac = 0;
+    dfs(0);
+    int ret = inf, ch = -1;
+    for (int i = 0; i < ac; ++i) {
+      memset(mp, 0, sizeof mp);
+      for (int j = 0; j < cnt; ++j) {
+        memset(ca, 0, sizeof ca);
+        memset(cb, 0, sizeof cb);
+        int b = 0, w = 0;
+        for (int k = 0; k < l; ++k) {
+          if (_all[i][k] == dat[j][k])
+            ++b;
+          else {
+            ++ca[_all[i][k]];
+            ++cb[dat[j][k]];
+          }
+        }
+        for (int k = 0; k < c; ++k) {
+          w += min(ca[k], cb[k]);
+        }
+        if (b == l) continue;
+        ++mp[b][w];
+      }
+      int mx = 0;
+      for (int j = 0; j <= l; ++j) {
+        for (int k = 0; k <= l; ++k) {
+          mx = max(mx, mp[j][k]);
+        }
+      }
+      if (mx < ret) {
+        ret = mx;
+        ch = i;
+      }
+    }
+    for (int i = 0; i < l; ++i) {
+      putchar('A' + _all[ch][i]);
+    }
+    printf(" %d\n", ret);
+  }
+  return 0;
 }
-
-
