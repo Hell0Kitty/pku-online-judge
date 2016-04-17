ddf5fc73e04ae0b16f23a7e4d6f0b44ffadbbbe3
sdecoder <sde.work.inbox@gmail.com>
2016-04-17 15:16:01 -0400

    2016-04-17 15:16:01

--- 2876.cc
+++ 2876.cc
@@ -1,25 +1,20 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
 string s[13];
 
-int main()
-{
-    string tem;
-    int i;
-    s[0] = "-";
-    for(i = 1; i < 13; ++ i)
-    {
-        s[i] += s[i-1];
-        s[i] += s[i-1];
-        s[i] += s[i-1];
-        fill_n(s[i].begin()+s[i-1].length(),s[i-1].length(),' ');
-    }
-    while(cin>>i)
-    {
-        cout<<s[i]<<endl;
-    }
+int main() {
+  string tem;
+  int i;
+  s[0] = "-";
+  for (i = 1; i < 13; ++i) {
+    s[i] += s[i - 1];
+    s[i] += s[i - 1];
+    s[i] += s[i - 1];
+    fill_n(s[i].begin() + s[i - 1].length(), s[i - 1].length(), ' ');
+  }
+  while (cin >> i) {
+    cout << s[i] << endl;
+  }
 }
-
-
