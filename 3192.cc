b8b61fc55375a884eacfdabafa16e0e92f132601
sdecoder <sde.work.inbox@gmail.com>
2016-04-19 05:49:50 -0400

    2016-04-19 05:49:50

--- 3192.cc
+++ 3192.cc
@@ -23,67 +23,54 @@
 #include <ctime>
 using namespace std;
 typedef long long LL;
-string s[8],st;
+string s[8], st;
 int flag[8];
 int N;
-int Max=0x3f3f3f3f;
-string merge(string s1,string s2)
-{
-int i,j,l;
- for(i=0;i<s1.size();i++)
- {
- j=0;
- l=i;
- while(s1[l]==s2[j]&&l<s1.size()&&j<s2.size())
- {
- l++;
- j++;
- }
- if(l==s1.size())
- {
- s1.replace(i,s2.size(),s2);
- return s1;
- }
- }
- return s1+s2;
+int Max = 0x3f3f3f3f;
+string merge(string s1, string s2) {
+  int i, j, l;
+  for (i = 0; i < s1.size(); i++) {
+    j = 0;
+    l = i;
+    while (s1[l] == s2[j] && l < s1.size() && j < s2.size()) {
+      l++;
+      j++;
+    }
+    if (l == s1.size()) {
+      s1.replace(i, s2.size(), s2);
+      return s1;
+    }
+  }
+  return s1 + s2;
 }
-void dfs(int deep)
-{
- if(deep==N)
- {
- if(Max>st.size())
- Max=st.size();
- return;
- }
- int i;
- string temp=st;
- for(i=0;i<N;i++)
- {
- if(!flag[i])
- {
- if(!st.size())st+=s[i];
- else
- st=merge(st,s[i]);
- flag[i]=1;
- dfs(deep+1);
- flag[i]=0;
- st=temp;
- }
- }
+void dfs(int deep) {
+  if (deep == N) {
+    if (Max > st.size()) Max = st.size();
+    return;
+  }
+  int i;
+  string temp = st;
+  for (i = 0; i < N; i++) {
+    if (!flag[i]) {
+      if (!st.size())
+        st += s[i];
+      else
+        st = merge(st, s[i]);
+      flag[i] = 1;
+      dfs(deep + 1);
+      flag[i] = 0;
+      st = temp;
+    }
+  }
 }
-int main()
-{
- memset(flag,0,sizeof(flag));
- int i;
- cin>>N;
- for(i=0;i<N;i++)
- {
- cin>>s[i];
- }
- dfs(0);
- cout<<Max<<endl;
- return 0;
+int main() {
+  memset(flag, 0, sizeof(flag));
+  int i;
+  cin >> N;
+  for (i = 0; i < N; i++) {
+    cin >> s[i];
+  }
+  dfs(0);
+  cout << Max << endl;
+  return 0;
 }
-
-
-
