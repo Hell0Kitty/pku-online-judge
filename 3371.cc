71fe455e84d94b9604d001c7fbba709c8aefd3b7
sdecoder <sde.work.inbox@gmail.com>
2016-04-20 17:20:57 -0400

    2016-04-20 17:20:57

--- 3371.cc
+++ 3371.cc
@@ -5,8 +5,8 @@
 #include <iostream>
 using namespace std;
 
-#define print(x) cout<<x<<endl
-#define input(x) cin>>x
+#define print(x) cout << x << endl
+#define input(x) cin >> x
 
 #define EOS 1
 
@@ -17,85 +17,70 @@ using namespace std;
 
 char mark[ASC];
 
-void init()
-{
-    memset(mark,0,sizeof(mark));
-    mark['a']=mark['e']=mark['i']=mark['o']=mark['u']=mark['y']=VOVEL;
-    mark['.']=mark['!']=mark['?']=mark[':']=mark[';']=EOS;
+void init() {
+  memset(mark, 0, sizeof(mark));
+  mark['a'] = mark['e'] = mark['i'] = mark['o'] = mark['u'] = mark['y'] = VOVEL;
+  mark['.'] = mark['!'] = mark['?'] = mark[':'] = mark[';'] = EOS;
 }
 
-int sentence,word,vovel;
+int sentence, word, vovel;
 
-void check(char* buffer)
-{
-    int res=0;
-    int len=strlen(buffer);
-    if(mark[buffer[len-1]]==EOS)
-    {
-        sentence++;
-        buffer[len-1]='\0';
+void check(char* buffer) {
+  int res = 0;
+  int len = strlen(buffer);
+  if (mark[buffer[len - 1]] == EOS) {
+    sentence++;
+    buffer[len - 1] = '\0';
+    len--;
+  }
+  while (buffer[len - 1] < 'a' || buffer[len - 1] > 'z') {
+    buffer[len - 1] = '\0';
+    len--;
+  }
+  if (len <= 3)
+    res = 1;
+  else {
+    if (buffer[len - 2] == 'e') {
+      if (buffer[len - 1] == 's' || buffer[len - 1] == 'd') {
+        buffer[len - 2] = '\0';
+        len -= 2;
+      }
+    } else if (buffer[len - 1] == 'e') {
+      if (buffer[len - 2] != 'l') {
+        buffer[len - 1] = '\0';
         len--;
+      }
     }
-    while(buffer[len-1]<'a' || buffer[len-1]>'z')
-    {
-        buffer[len-1]='\0';
-        len--;
+    for (int i = 0; buffer[i]; i++) {
+      if (mark[buffer[i]] == VOVEL) {
+        if (i == 0)
+          res++;
+        else if (mark[buffer[i - 1]] != VOVEL)
+          res++;
+      }
     }
-    if(len<=3) res=1;
-    else
-    {
-        if(buffer[len-2]=='e')
-        {
-            if(buffer[len-1]=='s' || buffer[len-1]=='d')
-            {
-                buffer[len-2]='\0';
-                len-=2;
-            }
-        }
-        else if(buffer[len-1]=='e')
-        {
-            if(buffer[len-2]!='l')
-            {
-                buffer[len-1]='\0';
-                len--;
-            }
-        }
-        for(int i=0;buffer[i];i++)
-        {
-            if(mark[buffer[i]]==VOVEL)
-            {
-                if(i==0) res++;
-                else if(mark[buffer[i-1]]!=VOVEL) res++;
-            }
-        }
-    }
-    vovel+=res;
-    word++;
+  }
+  vovel += res;
+  word++;
 }
 
-double cal()
-{
-    return 206.835-1.015*((double)word/sentence)-84.6*((double)vovel/word);
+double cal() {
+  return 206.835 - 1.015 * ((double)word / sentence) -
+         84.6 * ((double)vovel / word);
 }
 
-int main()
-{
-    char buffer[BUFFER];
-    init();
-    sentence=word=vovel=0;
-    while(scanf("%s",buffer)!=EOF)
-    {
-        for(int i=0;buffer[i];i++)
-        {
-            if(buffer[i]>='A' && buffer[i]<='Z')
-            {
-                buffer[i]=buffer[i]-'A'+'a';
-            }
-        }
-        check(buffer);
+int main() {
+  char buffer[BUFFER];
+  init();
+  sentence = word = vovel = 0;
+  while (scanf("%s", buffer) != EOF) {
+    for (int i = 0; buffer[i]; i++) {
+      if (buffer[i] >= 'A' && buffer[i] <= 'Z') {
+        buffer[i] = buffer[i] - 'A' + 'a';
+      }
     }
-    printf("%.2f\n",cal());
-    return 0;
+    check(buffer);
+  }
+  printf("%.2f\n", cal());
+  return 0;
 }
-
-
