c9b6f6bfe03e57aa9477b7e18bf7cc3743138dfc
sdecoder <sde.work.inbox@gmail.com>
2016-04-20 21:39:01 -0400

    2016-04-20 21:39:01

--- 2419.cc
+++ 2419.cc
@@ -17,28 +17,23 @@ const int maxn = 105;
 int n, m;
 int vis[maxn][maxn];
 
-int main()
-{
-	int i, j, k, ans, u, v;
+int main() {
+  int i, j, k, ans, u, v;
 
-	scanf("%d%d", &n, &m);
-	while (scanf("%d%d", &u, &v) != EOF) vis[u][v] = true;
+  scanf("%d%d", &n, &m);
+  while (scanf("%d%d", &u, &v) != EOF) vis[u][v] = true;
 
-	ans = 0;
-	for (i = 1; i <= n; i++)
-	{
-		for (j = 1; j < i; j++)
-		{
-			for (k = 1; k <= m; k++)
-				if (vis[i][k] != vis[j][k]) break;
-			if (k > m) break;
-		}
-		if (j >= i) ans++;
-	}
+  ans = 0;
+  for (i = 1; i <= n; i++) {
+    for (j = 1; j < i; j++) {
+      for (k = 1; k <= m; k++)
+        if (vis[i][k] != vis[j][k]) break;
+      if (k > m) break;
+    }
+    if (j >= i) ans++;
+  }
 
-	printf("%d\n", ans);
+  printf("%d\n", ans);
 
-	return 0;
+  return 0;
 }
-
-
