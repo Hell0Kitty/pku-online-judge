ad495e66d8f14c2d7ad6fd987e51093e23dddb8e
sdecoder <sde.work.inbox@gmail.com>
2016-04-20 22:08:14 -0400

    2016-04-20 22:08:14

--- 3813.cc
+++ 3813.cc
@@ -1,355 +1,304 @@
-#include<stdio.h>
+#include <stdio.h>
 
-#include<math.h>
+#include <math.h>
 
 struct node
 
-{
-
-       double x,y;
-
+    {
+  double x, y;
 };
 
-node a,b,c,d,e,f,g;
+node a, b, c, d, e, f, g;
 
-double S1,lA,lB,lC,kAC,kAB,dHAB,dAB,sum,lA1,lB1,lC1,kAB1,h;
+double S1, lA, lB, lC, kAC, kAB, dHAB, dAB, sum, lA1, lB1, lC1, kAB1, h;
 
-double dxy(node xx,node yy)
+double dxy(node xx, node yy)
 
 {
-
-       return sqrt((xx.x-yy.x)*(xx.x-yy.x)+(xx.y-yy.y)*(xx.y-yy.y));
-
+  return sqrt((xx.x - yy.x) * (xx.x - yy.x) + (xx.y - yy.y) * (xx.y - yy.y));
 }
 
-void CS1()          //面积
+void CS1()  //面积
 
 {
+  double p, a1, b1, c1;
 
-       double p,a1,b1,c1;
+  a1 = dxy(d, e);
 
-       a1=dxy(d,e);
+  b1 = dxy(e, f);
 
-       b1=dxy(e,f);
+  c1 = dxy(d, f);
 
-       c1=dxy(d,f);
+  p = (a1 + b1 + c1) / 2;
 
-       p=(a1+b1+c1)/2;
-
-       S1=sqrt(p*(p-a1)*(p-b1)*(p-c1));
-
+  S1 = sqrt(p * (p - a1) * (p - b1) * (p - c1));
 }
 
-void CL()         //直线
+void CL()  //直线
 
 {
+  if (b.x - a.x == 0)
 
-       if(b.x-a.x==0)
+  {
+    lA = (b.y - a.y);
 
-       {
+    lC = b.x * (b.y - a.y);
 
-              lA=(b.y-a.y);
+  }
 
-              lC=b.x*(b.y-a.y);
+  else if (b.y - a.y == 0)
 
-       }
+  {
+    lB = (a.x - b.x);
 
-       else if(b.y-a.y==0)
+    lC = b.y * (b.x - a.x);
 
-       {
+  }
 
-              lB=(a.x-b.x);
+  else
 
-              lC=b.y*(b.x-a.x);
+  {
+    lA = (b.y - a.y);
 
-       }
+    lB = (a.x - b.x);
 
-       else
-
-       {
-
-              lA=(b.y-a.y);
-
-              lB=(a.x-b.x);
-
-              lC=b.y*(b.x-a.x)-b.x*(b.y-a.y);
-
-       }
-
+    lC = b.y * (b.x - a.x) - b.x * (b.y - a.y);
+  }
 }
 
-void CL2()         //直线
+void CL2()  //直线
 
 {
+  if (c.x - a.x == 0)
 
-       if(c.x-a.x==0)
+  {
+    lA1 = (c.y - a.y);
 
-       {
+    lC1 = c.x * (c.y - a.y);
 
-              lA1=(c.y-a.y);
+  }
 
-              lC1=c.x*(c.y-a.y);
+  else if (c.y - a.y == 0)
 
-       }
+  {
+    lB1 = (a.x - c.x);
 
-       else if(c.y-a.y==0)
+    lC1 = c.y * (c.x - a.x);
 
-       {
+  }
 
-              lB1=(a.x-c.x);
+  else
 
-              lC1=c.y*(c.x-a.x);
+  {
+    lA1 = (c.y - a.y);
 
-       }
+    lB1 = (a.x - c.x);
 
-       else
-
-       {
-
-              lA1=(c.y-a.y);
-
-              lB1=(a.x-c.x);
-
-              lC1=c.y*(c.x-a.x)-c.x*(c.y-a.y);
-
-       }
-
+    lC1 = c.y * (c.x - a.x) - c.x * (c.y - a.y);
+  }
 }
 
 int main()
 
 {
+  node H;
 
-       node H;
+  while (scanf("%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf", &a.x, &a.y, &b.x, &b.y,
+               &c.x, &c.y, &d.x, &d.y, &e.x, &e.y, &f.x, &f.y) != EOF)
 
-       while(scanf("%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf%lf",&a.x,&a.y,&b.x,&b.y,&c.x,&c.y,&d.x,&d.y,&e.x,&e.y,&f.x,&f.y)!=EOF)
+  {
+    if (!a.x && !a.y && !b.x && !b.y && !c.x && !c.y && !d.x && !d.y && !e.x &&
+        !e.y && !f.x && !f.y)
 
-       {
+      break;
 
-              if(!a.x&&!a.y&&!b.x&&!b.y&&!c.x&&!c.y&&!d.x&&!d.y&&!e.x&&!e.y&&!f.x&&!f.y)
+    CS1();
 
-                     break;
+    CL();
 
-              CS1();
+    CL2();
 
-              CL();
+    h = S1 / dxy(a, b);
 
-              CL2();
+    if (a.x == b.x)
 
-              h=S1/dxy(a,b);
+    {
+      if (a.y == c.y)
 
-              if(a.x==b.x)
+      {
+        if (a.x < c.x)
 
-              {
+        {
+          H.x = a.x + h;
 
-                     if(a.y==c.y)
+          H.y = a.y;
 
-                     {
+        }
 
-                            if(a.x<c.x)
+        else
 
-                            {
+        {
+          H.x = a.x - h;
 
-                                   H.x=a.x+h;
+          H.y = a.y;
+        }
 
-                                   H.y=a.y;
+      }
 
-                            }
+      else
 
-                            else
+      {
+        if (a.x < c.x)
 
-                            {
+        {
+          H.x = a.x + h;
 
-                                   H.x=a.x-h;
+          H.y = (-1) * (lC1 + lA1 * H.x) / lB1;
 
-                                   H.y=a.y;
+        }
 
-                            }
+        else
 
-                     }
+        {
+          H.x = a.x - h;
 
-                     else
+          H.y = (-1) * (lC1 + lA1 * H.x) / lB1;
+        }
+      }
 
-                     {
+    }
 
-                            if(a.x<c.x)
+    else if (a.x == c.x)
 
-                            {
+    {
+      if (a.y == b.y)
 
-                                   H.x=a.x+h;
+      {
+        if (a.y < c.y)
 
-                                   H.y=(-1)*(lC1+lA1*H.x)/lB1;
+        {
+          H.x = a.x;
 
-                            }
+          H.y = a.y + h;
 
-                            else
+        }
 
-                            {
+        else
 
-                                   H.x=a.x-h;
+        {
+          H.x = a.x;
 
-                                   H.y=(-1)*(lC1+lA1*H.x)/lB1;
+          H.y = a.y - h;
+        }
 
-                            }
+      }
 
-                     }
+      else
 
-              }
+      {
+        if (a.y < c.y)
 
-              else if(a.x==c.x)
+        {
+          H.x = a.x;
 
-              {
+          H.y = (h * sqrt(lA * lA + lB * lB) - lA * H.x - lC) / lB;
 
-                     if(a.y==b.y)
+          if (H.y < a.y)
 
-                     {
+            H.y = ((-1) * h * sqrt(lA * lA + lB * lB) - lA * H.x - lC) / lB;
+        } else {
+          H.x = a.x;
+          H.y = (h * sqrt(lA * lA + lB * lB) - lA * H.x - lC) / lB;
+          if (H.y > a.y)
+            H.y = ((-1) * h * sqrt(lA * lA + lB * lB) - lA * H.x - lC) / lB;
+        }
+      }
+    } else if (a.y == c.y) {
+      if (a.x < c.x)
 
-                            if(a.y<c.y)
+      {
+        H.y = a.y;
 
-                            {
+        H.x = (h * sqrt(lA * lA + lB * lB) - lB * H.y - lC) / lA;
 
-                                   H.x=a.x;
+        if (H.x < a.x)
 
-                                   H.y=a.y+h;
+          H.x = ((-1) * h * sqrt(lA * lA + lB * lB) - lB * H.y - lC) / lA;
 
-                            }
+      }
 
-                            else
+      else
 
-                            {
+      {
+        H.y = a.y;
 
-                                   H.x=a.x;
+        H.x = (h * sqrt(lA * lA + lB * lB) - lB * H.y - lC) / lA;
 
-                                   H.y=a.y-h;
+        if (H.x > a.x)
 
-                            }
+          H.x = ((-1) * h * sqrt(lA * lA + lB * lB) - lB * H.y - lC) / lA;
+      }
 
-                     }
+    }
 
-                     else
+    else if (a.y == b.y)
 
-                     {
+    {
+      if (a.y < c.y)
 
-                            if(a.y<c.y)
+      {
+        H.y = a.y + h;
 
-                            {
+        H.x = (-1) * (lC1 + lB1 * H.y) / lA1;
 
-                                   H.x=a.x;
+      }
 
-                                   H.y=(h*sqrt(lA*lA+lB*lB)-lA*H.x-lC)/lB;
+      else
 
-                                   if(H.y<a.y)
+      {
+        H.y = a.y - h;
 
-                                          H.y=((-1)*h*sqrt(lA*lA+lB*lB)-lA*H.x-lC)/lB;
-                            }
-                            else
-                            {
-                                   H.x=a.x;
-                                   H.y=(h*sqrt(lA*lA+lB*lB)-lA*H.x-lC)/lB;
-                                   if(H.y>a.y)
-                                          H.y=((-1)*h*sqrt(lA*lA+lB*lB)-lA*H.x-lC)/lB;
-                            }
-                     }
-              }
-              else if(a.y==c.y)
-              {
-                     if(a.x<c.x)
+        H.x = (-1) * (lC1 + lB1 * H.y) / lA1;
+      }
 
-                     {
+    }
 
-                            H.y=a.y;
+    else
 
-                            H.x=(h*sqrt(lA*lA+lB*lB)-lB*H.y-lC)/lA;
+    {
+      if (a.x < c.x)
 
-                            if(H.x<a.x)
+      {
+        H.x = (h * sqrt(lA * lA + lB * lB) - lC + (lB * lC1 / lB1)) /
+              ((lA * lB1 - lB * lA1) / lB1);
 
-                                   H.x=((-1)*h*sqrt(lA*lA+lB*lB)-lB*H.y-lC)/lA;
+        if (H.x < a.x)
 
-                     }
+          H.x = ((-1) * h * sqrt(lA * lA + lB * lB) - lC + (lB * lC1 / lB1)) /
+                ((lA * lB1 - lB * lA1) / lB1);
 
-                     else
+        H.y = (-1) * (lC1 + lA1 * H.x) / lB1;
 
-                     {
+      }
 
-                            H.y=a.y;
+      else
 
-                            H.x=(h*sqrt(lA*lA+lB*lB)-lB*H.y-lC)/lA;
+      {
+        H.x = (h * sqrt(lA * lA + lB * lB) - lC + (lB * lC1 / lB1)) /
+              ((lA * lB1 - lB * lA1) / lB1);
 
-                            if(H.x>a.x)
+        if (H.x > a.x)
 
-                                   H.x=((-1)*h*sqrt(lA*lA+lB*lB)-lB*H.y-lC)/lA;
+          H.x = ((-1) * h * sqrt(lA * lA + lB * lB) - lC + (lB * lC1 / lB1)) /
+                ((lA * lB1 - lB * lA1) / lB1);
 
-                     }
+        H.y = (-1) * (lC1 + lA1 * H.x) / lB1;
+      }
+    }
 
-              }
+    printf("%.3f %.3f %.3f %.3f\n", (b.x - a.x) + H.x, (b.y - a.y) + H.y, H.x,
+           H.y);
+  }
 
-              else if(a.y==b.y)
-
-              {
-
-                     if(a.y<c.y)
-
-                     {
-
-                            H.y=a.y+h;
-
-                            H.x=(-1)*(lC1+lB1*H.y)/lA1;
-
-                     }
-
-                     else
-
-                     {
-
-                            H.y=a.y-h;
-
-                            H.x=(-1)*(lC1+lB1*H.y)/lA1;
-
-                     }
-
-              }
-
-              else
-
-              {
-
-                     if(a.x<c.x)
-
-                     {
-
-                            H.x=(h*sqrt(lA*lA+lB*lB)-lC+(lB*lC1/lB1))/((lA*lB1-lB*lA1)/lB1);
-
-                            if(H.x<a.x)
-
-                                   H.x=((-1)*h*sqrt(lA*lA+lB*lB)-lC+(lB*lC1/lB1))/((lA*lB1-lB*lA1)/lB1);
-
-                            H.y=(-1)*(lC1+lA1*H.x)/lB1;
-
-                     }
-
-                     else
-
-                     {
-
-                            H.x=(h*sqrt(lA*lA+lB*lB)-lC+(lB*lC1/lB1))/((lA*lB1-lB*lA1)/lB1);
-
-                            if(H.x>a.x)
-
-                                   H.x=((-1)*h*sqrt(lA*lA+lB*lB)-lC+(lB*lC1/lB1))/((lA*lB1-lB*lA1)/lB1);
-
-                            H.y=(-1)*(lC1+lA1*H.x)/lB1;
-
-                     }
-
-              }
-
-              printf("%.3f %.3f %.3f %.3f\n",(b.x-a.x)+H.x,(b.y-a.y)+H.y,H.x,H.y);
-
-       }
-
-       return 0;
-
+  return 0;
 }
-
-
-
