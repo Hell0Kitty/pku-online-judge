f437bbe2c0403f74586cd9a1a64417bc4505c93a
sdecoder <sde.work.inbox@gmail.com>
2016-04-23 16:01:20 -0400

    2016-04-23 16:01:20

--- 1304.cc
+++ 1304.cc
@@ -1,88 +1,68 @@
-#include<iostream>
-#include<string.h>
-#include<stdio.h>
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
 using namespace std;
 
-char start_end[15];  
-int routine[25][15];  
-char c[200];            
-int a,n;
+char start_end[15];
+int routine[25][15];
+char c[200];
+int a, n;
 const char ss[] = "ENDOFINPUT";
-int arrivalTime;       
-int sum[25];            
-int size[25];        
+int arrivalTime;
+int sum[25];
+int size[25];
 
-void input()
-{
- int i,j;
- a = 0;
- cin>>n;
- gets(c);
- for(i = 0;i < n;i++)
- {
-  j = 0;
+void input() {
+  int i, j;
+  a = 0;
+  cin >> n;
   gets(c);
-  int k = 0;
-  while(c[j] != '\0')
-  {
-   a = 0;
+  for (i = 0; i < n; i++) {
+    j = 0;
+    gets(c);
+    int k = 0;
+    while (c[j] != '\0') {
+      a = 0;
 
-   while(c[j] != ' ' && c[j] != '\0')
-   {
-    a = a*10 + (c[j] - '0');
-    j++;
-   }
-   routine[i][k] = a;
-   k++;
-   a = 0;
-   if(c[j] == ' ')
-      j++;
+      while (c[j] != ' ' && c[j] != '\0') {
+        a = a * 10 + (c[j] - '0');
+        j++;
+      }
+      routine[i][k] = a;
+      k++;
+      a = 0;
+      if (c[j] == ' ') j++;
+    }
+    size[i] = k;
   }
-  size[i] = k;
- }
- cin>>arrivalTime;
-
+  cin >> arrivalTime;
 }
-void solve()
-{
- int i,j;
- memset(sum,0,sizeof(sum));
- int mmin = 10000000;
- for(i = 0;i < n;i++)
- {
-  j = 0;
-  while(1)
-  {
-   if(sum[i] < arrivalTime)
-   {
-    sum[i] += (routine[i][j%size[i]]);
-    j++;
-   }
-   else
-   {
-    sum[i] = sum[i] - arrivalTime;
-    if(mmin > sum[i])
-     mmin = sum[i];
-    break;
-
-   }
+void solve() {
+  int i, j;
+  memset(sum, 0, sizeof(sum));
+  int mmin = 10000000;
+  for (i = 0; i < n; i++) {
+    j = 0;
+    while (1) {
+      if (sum[i] < arrivalTime) {
+        sum[i] += (routine[i][j % size[i]]);
+        j++;
+      } else {
+        sum[i] = sum[i] - arrivalTime;
+        if (mmin > sum[i]) mmin = sum[i];
+        break;
+      }
+    }
   }
- }
- cout<<mmin<<endl;
+  cout << mmin << endl;
 }
-int main()
-{
- while(1)
- {
-  cin>>start_end;
-  if(strcmp(start_end,ss) == 0)
-   break;
-  input();
-  cin>>start_end;
-  solve();
- }
- return 0;
+int main() {
+  while (1) {
+    cin >> start_end;
+    if (strcmp(start_end, ss) == 0) break;
+    input();
+    cin >> start_end;
+    solve();
+  }
+  return 0;
 }
-
-
-
