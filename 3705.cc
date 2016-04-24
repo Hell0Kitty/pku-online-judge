55353e376db332355eebf66428b1acaf8a050abd
sdecoder <sde.work.inbox@gmail.com>
2016-04-24 12:49:16 -0400

    2016-04-24 12:49:16

--- 3705.cc
+++ 3705.cc
@@ -1,39 +1,35 @@
 #include <iostream>
 using namespace std;
 void solve(int n) {
-    int x = (n+1)/2 - 1;
-    int y = n;
-    for (int i = 0; i < x; ++i) {
-        cout << n/2 << " " << 2 << " " << y-2-i << endl;
-        n -= 2;
-    }
-    cout <<"2 " << x << " " << x + 1 << endl;
+  int x = (n + 1) / 2 - 1;
+  int y = n;
+  for (int i = 0; i < x; ++i) {
+    cout << n / 2 << " " << 2 << " " << y - 2 - i << endl;
+    n -= 2;
+  }
+  cout << "2 " << x << " " << x + 1 << endl;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    if (n == 1) {
-        cout << 0 <<endl;
-        return 0;
-    }
-    if (n == 2) {
-        cout << "1" <<endl;
-        cout << "1 1 1" <<endl;
-        return 0;
-    }
-    if (n % 2 != 0) {
-        cout << (n+1)/2 <<endl;
-         solve(n);
-    }
-    else {
-        cout << n/2 + 1 << endl;
-        solve(n-1);
-        cout << 1 << " "<< n-1 <<" 1" <<endl;
-    }
-
+  int n;
+  cin >> n;
+  if (n == 1) {
+    cout << 0 << endl;
     return 0;
+  }
+  if (n == 2) {
+    cout << "1" << endl;
+    cout << "1 1 1" << endl;
+    return 0;
+  }
+  if (n % 2 != 0) {
+    cout << (n + 1) / 2 << endl;
+    solve(n);
+  } else {
+    cout << n / 2 + 1 << endl;
+    solve(n - 1);
+    cout << 1 << " " << n - 1 << " 1" << endl;
+  }
+
+  return 0;
 }
-
-
-
