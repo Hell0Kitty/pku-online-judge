cdfa517093317d7a6a5cf091c2065fe504d9e2f1
sdecoder <sde.work.inbox@gmail.com>
2016-04-24 03:12:19 -0400

    2016-04-24 03:12:19

---
+++ 2232.cc
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num;
+   char *a;
+   int s,f,c,flag;
+   while(cin>>num)
+   {
+       flag=0;
+       s=f=c=0;
+       a= new char[num];
+       for(int i=0;i<num;i++)
+       {
+           cin>>a[num];
+           if(a[num]=='S')s++;
+           else if(a[num]=='F')f++;
+           else if(a[num]=='C')c++;
+       }
+       if(s==num||f==num||c==num)
+           flag=num;
+       else if(s!=0&&f!=0&&c!=0)
+           flag=num;
+       else
+       {
+           if(s==0)flag=f;
+           else if(f==0)flag=c;
+           else if(c==0)flag=s;
+       }
+       cout<<flag<<endl;
+   }
+}
+
+
+
