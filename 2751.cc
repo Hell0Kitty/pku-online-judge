4b71d7a4e0a1f0264d85d3ab0ca1a99c04717e6d
sdecoder <sde.work.inbox@gmail.com>
2016-04-25 10:11:04 -0400

    2016-04-25 10:11:04

--- 2751.cc
+++ 2751.cc
@@ -1,35 +1,30 @@
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 const int N = 10009;
 struct Node {
-	int x, y;
+  int x, y;
 } a[N];
-bool cmp(Node a, Node b) {
-	return min(a.x, b.y) < min(b.x, a.y);
-}
+bool cmp(Node a, Node b) { return min(a.x, b.y) < min(b.x, a.y); }
 int main() {
-	int n, i;
+  int n, i;
 
-	while(~scanf("%d", &n) && n) {
-		for(i = 0; i < n; ++i) {
-			scanf("%d%d", &a[i].x, &a[i].y);
-		}
+  while (~scanf("%d", &n) && n) {
+    for (i = 0; i < n; ++i) {
+      scanf("%d%d", &a[i].x, &a[i].y);
+    }
 
-		sort(a, a + n, cmp);
-		int t1 = 0, t2 = 0;
+    sort(a, a + n, cmp);
+    int t1 = 0, t2 = 0;
 
-		for(i = 0; i < n; ++i) {
-			t1 += a[i].x;
-			t2 = max(t1, t2) + a[i].y;
-		}
+    for (i = 0; i < n; ++i) {
+      t1 += a[i].x;
+      t2 = max(t1, t2) + a[i].y;
+    }
 
-		printf("%d\n", t2);
-	}
+    printf("%d\n", t2);
+  }
 
-	return 0;
+  return 0;
 }
-
-
-
