fa77fa9fba4eafdf03fa67584fa1d47bd421442f
sdecoder <sde.work.inbox@gmail.com>
2016-05-01 02:02:55 -0400

    2016-05-01 02:02:55

--- 2100.cc
+++ 2100.cc
@@ -6,41 +6,36 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<ll,ll> pii;
+typedef pair<ll, ll> pii;
 
 #define maxn 1000000
 
 ll n;
 pii ans[maxn];
 
-int main()
-{
-    while(~scanf("%I64d",&n)) {
-            ll s = 1,pos = 1,sum = 0;
-            int len = 0;
-            for(;s * s <= n; ++s) {
-                    while(sum < n) {
-                            sum += pos * pos;
-                            ++pos;
-                    }
-                    if(sum == n) {
-                            ans[len].first = pos - s;
-                            ans[len++].second = s;
-                    }
-                    sum -= s * s;
-            }
-
-            printf("%d\n",len);
-            for(int i = 0; i < len; ++i) {
-                    printf("%I64d",ans[i].first);
-                    for(int j = 0; j < ans[i].first; ++j)
-                            printf(" %I64d",ans[i].second + j);
-                    printf("\n");
-            }
-
+int main() {
+  while (~scanf("%I64d", &n)) {
+    ll s = 1, pos = 1, sum = 0;
+    int len = 0;
+    for (; s * s <= n; ++s) {
+      while (sum < n) {
+        sum += pos * pos;
+        ++pos;
+      }
+      if (sum == n) {
+        ans[len].first = pos - s;
+        ans[len++].second = s;
+      }
+      sum -= s * s;
     }
-    return 0;
+
+    printf("%d\n", len);
+    for (int i = 0; i < len; ++i) {
+      printf("%I64d", ans[i].first);
+      for (int j = 0; j < ans[i].first; ++j)
+        printf(" %I64d", ans[i].second + j);
+      printf("\n");
+    }
+  }
+  return 0;
 }
-
-
-
