53c0aee12718dd4c8e80adef0d0b3d1d422333f1
sdecoder <sde.work.inbox@gmail.com>
2016-05-02 13:16:29 -0400

    2016-05-02 13:16:29

--- 1509.cc
+++ 1509.cc
@@ -1,29 +1,22 @@
-#include<cstdio>
-#include<string>
-#include<algorithm>
+#include <cstdio>
+#include <string>
+#include <algorithm>
 using namespace std;
-int main()
-{
-    int T;
-    for(scanf("%d",&T),getchar();T;T--)
-    {
-        char ss[10005];
-        gets(ss);
-        string st=ss,ans=ss,tp;
-        int len=st.size(),id=1;
-        for(int i=1;i<len;i++)
-        {
-            tp=st.substr(i,len-i)+st.substr(0,i);
-            if(tp<ans)
-            {
-                ans=tp;
-                id=i+1;
-            }
-        }
-        printf("%d\n",id);
+int main() {
+  int T;
+  for (scanf("%d", &T), getchar(); T; T--) {
+    char ss[10005];
+    gets(ss);
+    string st = ss, ans = ss, tp;
+    int len = st.size(), id = 1;
+    for (int i = 1; i < len; i++) {
+      tp = st.substr(i, len - i) + st.substr(0, i);
+      if (tp < ans) {
+        ans = tp;
+        id = i + 1;
+      }
     }
-    return 0;
+    printf("%d\n", id);
+  }
+  return 0;
 }
-
-
-
