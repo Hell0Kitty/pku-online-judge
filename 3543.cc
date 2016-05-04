a5d669836c8613fb3354804c523bc021fc93406f
sdecoder <sde.work.inbox@gmail.com>
2016-05-04 18:30:43 -0400

    2016-05-04 18:30:43

--- 3543.cc
+++ 3543.cc
@@ -3,33 +3,22 @@
 
 using namespace std;
 
-int main()
-{
-	int b, w;
-	int size;
+int main() {
+  int b, w;
+  int size;
 
-	while(2 == scanf("%d %d", &b, &w))
-	{
-		if(b == 0 && w == 0 )
-		{
-			printf("Impossible\n");
-		}
-		else
-		{
-			if(w == b)
-			{
-				size = (int)sqrt(double(w+b)); //这边也要注意！
-			}
-			else 
-			{
-				size = (int)sqrt(double(2 * (b > w ? w : b) + 1)); 
-			}
-			printf("%d\n", size);
-		}
-	}
+  while (2 == scanf("%d %d", &b, &w)) {
+    if (b == 0 && w == 0) {
+      printf("Impossible\n");
+    } else {
+      if (w == b) {
+        size = (int)sqrt(double(w + b));  //这边也要注意！
+      } else {
+        size = (int)sqrt(double(2 * (b > w ? w : b) + 1));
+      }
+      printf("%d\n", size);
+    }
+  }
 
-	return 0;
+  return 0;
 }
-
-
-
