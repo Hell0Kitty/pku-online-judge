a1b04eef4b4d64da30397afde50d201641568473
sdecoder <sde.work.inbox@gmail.com>
2016-05-05 16:21:20 -0400

    2016-05-05 16:21:20

--- 2795.cc
+++ 2795.cc
@@ -1,8 +1,8 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,31 +10,24 @@ using namespace std;
 #define MOD 1000000000
 
 char str[MAXN];
-long long  dp[MAXN][MAXN];
+long long dp[MAXN][MAXN];
 
-long long dfs(int i,int j)
-{
-    if(i==j) return 1;
-    if(str[i]!=str[j]) return 0;
-    if(dp[i][j]>=0) return dp[i][j];
-    dp[i][j]=0;
-    for(int k=i+2;k<=j;k++)
-        if(str[i]==str[k])
-            dp[i][j]=(dp[i][j]+dfs(i+1,k-1)*dfs(k,j))%MOD;
-    return dp[i][j];
+long long dfs(int i, int j) {
+  if (i == j) return 1;
+  if (str[i] != str[j]) return 0;
+  if (dp[i][j] >= 0) return dp[i][j];
+  dp[i][j] = 0;
+  for (int k = i + 2; k <= j; k++)
+    if (str[i] == str[k])
+      dp[i][j] = (dp[i][j] + dfs(i + 1, k - 1) * dfs(k, j)) % MOD;
+  return dp[i][j];
 }
 
-int main()
-{
-    while(~scanf("%s",str))
-    {
-       for(int i=0;i<310;i++)
-        for(int j=0;j<310;j++)
-            dp[i][j]=-1;
-        printf("%lld\n",dfs(0,strlen(str)-1));
-    }
-    return 0;
+int main() {
+  while (~scanf("%s", str)) {
+    for (int i = 0; i < 310; i++)
+      for (int j = 0; j < 310; j++) dp[i][j] = -1;
+    printf("%lld\n", dfs(0, strlen(str) - 1));
+  }
+  return 0;
 }
-
-
-
