55d9cacd0ce1017ea7f35dbfffeeda117a353728
sdecoder <sde.work.inbox@gmail.com>
2016-05-06 13:04:46 -0400

    2016-05-06 13:04:46

--- 1406.cc
+++ 1406.cc
@@ -2,32 +2,28 @@
 #include <cmath>
 #include <stdio.h>
 using namespace std;
-int x,cn,tn,n,m,xbs,xmin,flag,bmax;
-int main(){
-	while (cin>>x,x) {
-		cn=(int)pow(x*1.0,1.0/3.0)+1;
-		tn=(int)pow(x*6.0,1.0/3.0)+1;
-		flag=0;
-		xmin=x;
-		for (m=0;m<=tn&&!flag;++m) {
-			for(n=cn;n>=0;--n){
-				xbs = x - (n*n*n+m*(m+1)*(m+2)/6);
-				if(xbs==0){
-					flag=1;
-					bmax=x;
-					break;
-				}
-				if(xbs>0&&xbs<xmin){
-					xmin=xbs;
-				}
-			}
-		}
-		if(!flag)
-			bmax=x-xmin;
-		printf("%d\n",bmax);
-	}
-	return 0;
+int x, cn, tn, n, m, xbs, xmin, flag, bmax;
+int main() {
+  while (cin >> x, x) {
+    cn = (int)pow(x * 1.0, 1.0 / 3.0) + 1;
+    tn = (int)pow(x * 6.0, 1.0 / 3.0) + 1;
+    flag = 0;
+    xmin = x;
+    for (m = 0; m <= tn && !flag; ++m) {
+      for (n = cn; n >= 0; --n) {
+        xbs = x - (n * n * n + m * (m + 1) * (m + 2) / 6);
+        if (xbs == 0) {
+          flag = 1;
+          bmax = x;
+          break;
+        }
+        if (xbs > 0 && xbs < xmin) {
+          xmin = xbs;
+        }
+      }
+    }
+    if (!flag) bmax = x - xmin;
+    printf("%d\n", bmax);
+  }
+  return 0;
 }
-
-
-
