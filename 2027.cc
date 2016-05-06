8c58092e1efc4244f47c9c36ed8030cd29e8cea5
sdecoder <sde.work.inbox@gmail.com>
2016-05-05 23:04:32 -0400

    2016-05-05 23:04:32

---
+++ 2027.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+                int n, a, b;
+
+                cin >> n;
+
+                while(n-- > 0){
+
+                                cin >> a >> b;
+
+                                cout << (a<b?"NO BRAINS\n":"MMM BRAINS\n");
+
+                }
+
+                return 0;
+
+}
+
+
+
