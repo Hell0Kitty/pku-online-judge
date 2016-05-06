73bfcd0a75bf5d63e825eac9749df2cc0f92f9d5
sdecoder <sde.work.inbox@gmail.com>
2016-05-06 10:29:37 -0400

    2016-05-06 10:29:37

--- 3589.cc
+++ 3589.cc
@@ -1,36 +1,27 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 
-int main()
-{
-	int T;
-	string a,b;
-	int count1,count2;
-	cin>>T;
-	while(T--)
-	{
-		count1 = 0;
-		count2 = 0;
-		cin>>a>>b;
-		for(int i = 0;i<4;i++)
-		{
-			if(a[i] == b[i])
-				count1++;
-			else
-			{
-				for(int j = i+1;j<4;j++)
-				{
-					if(a[i] == b[j])
-						count2++;
-					if(b[i] == a[j])
-						count2++;
-				}
-			}
-		}
-		cout<<count1<<'A'<<count2<<'B'<<endl;
-	}
-	return 0;
+int main() {
+  int T;
+  string a, b;
+  int count1, count2;
+  cin >> T;
+  while (T--) {
+    count1 = 0;
+    count2 = 0;
+    cin >> a >> b;
+    for (int i = 0; i < 4; i++) {
+      if (a[i] == b[i])
+        count1++;
+      else {
+        for (int j = i + 1; j < 4; j++) {
+          if (a[i] == b[j]) count2++;
+          if (b[i] == a[j]) count2++;
+        }
+      }
+    }
+    cout << count1 << 'A' << count2 << 'B' << endl;
+  }
+  return 0;
 }
-
-
