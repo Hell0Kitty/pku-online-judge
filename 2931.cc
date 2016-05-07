757deb5765ade69cb51f82ec90e28c18287b14c1
sdecoder <sde.work.inbox@gmail.com>
2016-05-07 05:56:05 -0400

    2016-05-07 05:56:05

--- 2931.cc
+++ 2931.cc
@@ -1,18 +1,18 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
-#include<algorithm>
-#include<vector>
-#include<set>
-#include<queue>
-#include<map>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <queue>
+#include <map>
 
 using namespace std;
 
-#define FOR(i,a,b) for(int (i)=(a);(i)<=(b);(i)++)
-#define DOR(i,a,b) for(int (i)=(a);(i)>=(b);(i)--)
-#define CLR(a) memset((a),0,sizeof((a)))
+#define FOR(i, a, b) for (int(i) = (a); (i) <= (b); (i)++)
+#define DOR(i, a, b) for (int(i) = (a); (i) >= (b); (i)--)
+#define CLR(a) memset((a), 0, sizeof((a)))
 #define pb push_back
 #define mp make_pair
 #define ins insert
@@ -26,73 +26,73 @@ using namespace std;
 
 #define LL long long
 #define Vec vector<int>
-#define Pai pair<int,int>
+#define Pai pair<int, int>
 
 char s[nMax];
-int b[nMax],l[nMax];
+int b[nMax], l[nMax];
 int g[3];
-class Sur_Number{
-public:
-    LL sn;
-    void make(int*,int );
-    friend Sur_Number operator + (const Sur_Number& ,const Sur_Number& );
-    friend bool operator < (const Sur_Number& ,const Sur_Number& );
+class Sur_Number {
+ public:
+  LL sn;
+  void make(int*, int);
+  friend Sur_Number operator+(const Sur_Number&, const Sur_Number&);
+  friend bool operator<(const Sur_Number&, const Sur_Number&);
 };
-void Sur_Number::make(int* b,int l){
-    sn=0LL;
-    LL k=1LL<<52;
-    //printf("k = %lld \n",k);
-    int i=1;
-    b[0]=b[1];
-    while(i<=l) {
-        if(b[i]==b[i-1]){
-            if(b[i]==0) sn += k;
-            else        sn -= k;
-        }else break;
-        i++;
-    }
-    k>>=1;
-    while(i<=l){
-        if(b[i]==0) sn += k;
-        else sn -= k;
-        i++;
-        k>>=1;
-    }
+void Sur_Number::make(int* b, int l) {
+  sn = 0LL;
+  LL k = 1LL << 52;
+  // printf("k = %lld \n",k);
+  int i = 1;
+  b[0] = b[1];
+  while (i <= l) {
+    if (b[i] == b[i - 1]) {
+      if (b[i] == 0)
+        sn += k;
+      else
+        sn -= k;
+    } else
+      break;
+    i++;
+  }
+  k >>= 1;
+  while (i <= l) {
+    if (b[i] == 0)
+      sn += k;
+    else
+      sn -= k;
+    i++;
+    k >>= 1;
+  }
 }
 
-Sur_Number operator + (const Sur_Number& a,const Sur_Number& b){
-    Sur_Number c;
-    c.sn = a.sn + b.sn;
-    return c;
+Sur_Number operator+(const Sur_Number& a, const Sur_Number& b) {
+  Sur_Number c;
+  c.sn = a.sn + b.sn;
+  return c;
 }
 
-bool operator < (const Sur_Number& a,const Sur_Number& b){
-    return a.sn < b.sn;
-}
+bool operator<(const Sur_Number& a, const Sur_Number& b) { return a.sn < b.sn; }
 
-int main(){
-    int t,cas;
-    scanf("%d",&t);
-    Sur_Number p[2],r;
-    while(t--){
-        scanf("%s %d\n",s,&cas);
-        for(int k=0;k<2;k++){
-            p[k].sn=0;
-            for(int i=0;i<3;i++) scanf("%d",&l[i]);
-            for(int j=0;j<3;j++){
-                for(int i=1;i<=l[j];i++) {
-                    scanf("%s",s);
-                    b[i] = s[0]=='W'?0:1;
-                }
-                r.make(b,l[j]);
-                //printf("---%lld\n",r.sn);
-                p[k] = p[k]+r;
-            }
+int main() {
+  int t, cas;
+  scanf("%d", &t);
+  Sur_Number p[2], r;
+  while (t--) {
+    scanf("%s %d\n", s, &cas);
+    for (int k = 0; k < 2; k++) {
+      p[k].sn = 0;
+      for (int i = 0; i < 3; i++) scanf("%d", &l[i]);
+      for (int j = 0; j < 3; j++) {
+        for (int i = 1; i <= l[j]; i++) {
+          scanf("%s", s);
+          b[i] = s[0] == 'W' ? 0 : 1;
         }
-        printf("Test %d: %s\n",cas,p[0]<p[1]?"No":"Yes");
+        r.make(b, l[j]);
+        // printf("---%lld\n",r.sn);
+        p[k] = p[k] + r;
+      }
     }
-    return 0;
+    printf("Test %d: %s\n", cas, p[0] < p[1] ? "No" : "Yes");
+  }
+  return 0;
 }
-
-
-
