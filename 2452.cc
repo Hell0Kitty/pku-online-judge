51d89f643d733b32af9f24a67c848ef0fa4fd313
sdecoder <sde.work.inbox@gmail.com>
2016-05-09 19:34:25 -0400

    2016-05-09 19:34:25

--- 2452.cc
+++ 2452.cc
@@ -1,51 +1,55 @@
-#include <stdio.h>
-#define ma(x, y, z) x > (y > z ? y : z) ? x : (y > z ? y : z);
-int i, j, n, a[50000], x, y, z, max, min, tmp;
-int find(int start, int end) {
-  if (start >= end) return -1;
-  int tmp, min, max, x, y, z;
-  tmp = min = max = start;
-  while (start <= end) {
-    if (a[start] < a[min]) min = start;
-    if (a[start] > a[max]) max = start;
-    start++;
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#define MX 55555
+#define lson l, m, n << 1
+#define rson m + 1, r, n << 1 | 1
+#define lc n << 1
+#define rc n << 1 | 1
+using namespace std;
+int num[MX], ma[MX << 2], mi[MX << 2];
+int L, R, N, ans;
+inline void up(int n) {
+  ma[n] = num[ma[lc]] > num[ma[rc]] ? ma[lc] : ma[rc];
+  mi[n] = num[mi[lc]] < num[mi[rc]] ? mi[lc] : mi[rc];
+}
+void B(int l = 1, int r = N, int n = 1) {
+  if (l == r) {
+    scanf("%d", &num[l]);
+    ma[n] = mi[n] = l;
+    return;
   }
-  if (max > min) {
-    x = max - min;
-    y = find(tmp, min - 1);
-    z = find(max + 1, end);
-  } else {
-    x = find(tmp, max);
-    y = find(max + 1, min - 1);
-    z = find(min, end);
-  }
-  return ma(x, y, z);
+  int m = l + r >> 1;
+  B(lson), B(rson), up(n);
+}
+int QA(int l = 1, int r = N, int n = 1) {
+  if (L <= l && r <= R) return ma[n];
+  int ans = 0, m = l + r >> 1, t;
+  if (L <= m) t = QA(lson), ans = (num[ans] > num[t]) ? ans : t;
+  if (m < R) t = QA(rson), ans = (num[ans] > num[t]) ? ans : t;
+  return ans;
+}
+int QB(int l = 1, int r = N, int n = 1) {
+  if (L <= l && r <= R) return mi[n];
+  int ans = N + 1, m = l + r >> 1, t;
+  if (L <= m) t = QB(lson), ans = (num[ans] < num[t]) ? ans : t;
+  if (m < R) t = QB(rson), ans = (num[ans] < num[t]) ? ans : t;
+  return ans;
+}
+int S(int l, int r) {
+  if (l >= r) return -1;
+  L = l, R = r;
+  int ans = -1, a = QA(), b = QB();
+  if (a > b)
+    ans = max(a - b, max(S(l, b), S(a, r)));
+  else
+    ans = max(S(a + 1, b - 1), max(S(l, a), S(b, r)));
+  return ans;
 }
 int main() {
-  while (scanf("%d", &n) != EOF) {
-    max = min = 0;
-    for (i = 0; i < n; i++) {
-      scanf("%d", &a[i]);
-      if (a[i] < a[min]) min = i;
-      if (a[i] > a[max]) max = i;
-    }
-    i = min - 2;
-    while (i > -1 && a[i] > a[i + 1])  // 把递减到最小值的删掉
-      i--;
-    i += 2;
-    tmp = i;
-    for (j = min; j < n; j++, tmp++) a[tmp] = a[j];
-    n = n - (min - i);
-    if (max > min) max = max - (min - i);
-    min = i;
-    i = max + 1;
-    while (i < n && a[i] > a[i + 1])  // 把从最大值开始递减的删掉
-      i++;
-    tmp = max + 1;
-    for (j = i; j < n; j++, tmp++) a[tmp] = a[j];
-    n = n - (i - (max + 1));
-    if (min > max) min = min - (i - (max + 1));
-    printf("%d\n", find(0, n - 1));
+  while (~scanf("%d", &N)) {
+    B(), num[N + 1] = 111111;
+    printf("%d\n", S(1, N));
   }
   return 0;
 }
