3537bce40490f76dbdf149a8d3a689de9b842eb2
sdecoder <sde.work.inbox@gmail.com>
2016-05-10 05:18:25 -0400

    2016-05-10 05:18:25

--- 3321.cc
+++ 3321.cc
@@ -7,94 +7,83 @@ using namespace std;
 
 int TreeArray[MAXN], Left[MAXN], Right[MAXN], Fork[MAXN];
 typedef vector<int> Ve;
-vector<Ve>Edge(MAXN);
-int N,M;
+vector<Ve> Edge(MAXN);
+int N, M;
 int key;
 
-void init()//初始化数组和
+void init()  //初始化数组和
 {
-    mem(Left);  mem(Right);
-    mem(Fork);  mem(TreeArray);
-    for(int i=0;i<MAXN;i++)Edge[i].clear();
+  mem(Left);
+  mem(Right);
+  mem(Fork);
+  mem(TreeArray);
+  for (int i = 0; i < MAXN; i++) Edge[i].clear();
 }
 
-void DFS(int node)
-{
-    Left[node] = key;
-    for(int i=0;i<Edge[node].size();i++)
-    {
-        key+=1;
-        DFS(Edge[node][i]);
-    }
-    Right[node] = key;
+void DFS(int node) {
+  Left[node] = key;
+  for (int i = 0; i < Edge[node].size(); i++) {
+    key += 1;
+    DFS(Edge[node][i]);
+  }
+  Right[node] = key;
 }
 
-int LowBit(int x)//返回的是2^k
+int LowBit(int x)  //返回的是2^k
 {
-    return x & (x ^ (x-1));
+  return x & (x ^ (x - 1));
 }
 
-void Edit(int k, int num)
-{
-    while(k <= N)
-    {
-        TreeArray[k] += num;
-        k += LowBit(k);
-    }
+void Edit(int k, int num) {
+  while (k <= N) {
+    TreeArray[k] += num;
+    k += LowBit(k);
+  }
 }
 
-int GetSum(int k)//得到1...k的和
+int GetSum(int k)  //得到1...k的和
 {
-    int sum = 0;
-    while(k>=1)
-    {
-        sum += TreeArray[k];
-        k -= LowBit(k);
-    }
-    return sum;
+  int sum = 0;
+  while (k >= 1) {
+    sum += TreeArray[k];
+    k -= LowBit(k);
+  }
+  return sum;
 }
 
-void ReadDataAndDo()
-{
-    int a,b;
-    char ch;
-    for(int i=1;i<N;i++)
+void ReadDataAndDo() {
+  int a, b;
+  char ch;
+  for (int i = 1; i < N; i++) {
+    scanf("%d%d", &a, &b);
+    Edge[a].push_back(b);
+  }
+  key = 1;
+  DFS(1);
+  for (int i = 1; i <= N; i++) {
+    Fork[i] = 1;
+    Edit(i, 1);
+  }
+  scanf("%d%*c", &M);
+  for (int i = 0; i < M; i++) {
+    scanf("%c %d%*c", &ch, &b);
+    if (ch == 'Q')  // b的子树就是[Left[b], right[b]]
     {
-        scanf("%d%d", &a, &b);
-        Edge[a].push_back(b);
+      printf("%d\n", GetSum(Right[b]) - GetSum(Left[b] - 1));
+    } else {
+      if (Fork[b])
+        Edit(Left[b], -1);
+      else
+        Edit(Left[b], 1);
+      Fork[b] = !Fork[b];
     }
-    key = 1;    DFS(1);
-    for(int i=1;i<=N;i++)
-    {
-        Fork[i] = 1;
-        Edit(i,1);
-    }
-    scanf("%d%*c", &M);
-    for(int i=0;i<M;i++)
-    {
-        scanf("%c %d%*c", &ch, &b);
-        if(ch == 'Q')//b的子树就是[Left[b], right[b]]
-        {
-            printf("%d\n", GetSum(Right[b]) - GetSum(Left[b]-1));
-        }
-        else
-        {
-            if(Fork[b]) Edit(Left[b],-1);
-            else Edit(Left[b],1);
-            Fork[b] = !Fork[b];
-        }
-    }
+  }
 }
 
-int main()
-{
-    while(~scanf("%d", &N))
-    {
-        init();
-        ReadDataAndDo();
-    }
-    return 0;
+int main() {
+  while (~scanf("%d", &N)) {
+    init();
+    ReadDataAndDo();
+  }
+  return 0;
 }
-
-
-
