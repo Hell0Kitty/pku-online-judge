826eac5941ff7fc57da5f5da703f984baeb59198
sdecoder <sde.work.inbox@gmail.com>
2016-05-09 20:03:18 -0400

    2016-05-09 20:03:18

--- 3423.cc
+++ 3423.cc
@@ -22,52 +22,48 @@ typedef long long LL;
 LL positions[MAXN];
 int mark[MAXN];
 
-int main()
-{
-	LL N, S, F, L;
-	cin >> N;
-	for(int i = 0; i < N; i++ )
-		cin >> positions[i];
-	cin >> S >> F >> L;
+int main() {
+  LL N, S, F, L;
+  cin >> N;
+  for (int i = 0; i < N; i++) cin >> positions[i];
+  cin >> S >> F >> L;
 
-	for(int i = 0; i < N; i++ ){
-		LL ii = 1;
-		LL jj = positions[i];
-		LL startjj_S = positions[i] - (S - 1);
-		LL endjj_S = positions[i] + (S - 1); 
-		LL rowStart = S - 1;
-		LL colStart = 0;
+  for (int i = 0; i < N; i++) {
+    LL ii = 1;
+    LL jj = positions[i];
+    LL startjj_S = positions[i] - (S - 1);
+    LL endjj_S = positions[i] + (S - 1);
+    LL rowStart = S - 1;
+    LL colStart = 0;
 
-		if(endjj_S < F) continue; 
-		if(startjj_S > F + L - 1) continue;
+    if (endjj_S < F) continue;
+    if (startjj_S > F + L - 1) continue;
 
-		LL startjj_inbound = max(F, startjj_S);
-		LL endjj_inbound = min(F + L - 1, endjj_S);
-		if((startjj_inbound - startjj_S) % 2) startjj_inbound++;
-		if((endjj_S - endjj_inbound) % 2) endjj_inbound--;
+    LL startjj_inbound = max(F, startjj_S);
+    LL endjj_inbound = min(F + L - 1, endjj_S);
+    if ((startjj_inbound - startjj_S) % 2) startjj_inbound++;
+    if ((endjj_S - endjj_inbound) % 2) endjj_inbound--;
 
-		if(startjj_inbound > endjj_inbound) continue;
+    if (startjj_inbound > endjj_inbound) continue;
 
-		LL diff = startjj_inbound - startjj_S;
-		LL rowStart_inbound = rowStart - diff / 2;
-		LL colStart_inbound = colStart + diff / 2;
-		for(LL j = startjj_inbound; j <= endjj_inbound; j += 2){
-			LL value = rowStart_inbound & colStart_inbound;
-			if(value == 0){
-				mark[j - F] ^= 1;
-			}
-			rowStart_inbound--;
-			colStart_inbound++;
-		}
-	}
+    LL diff = startjj_inbound - startjj_S;
+    LL rowStart_inbound = rowStart - diff / 2;
+    LL colStart_inbound = colStart + diff / 2;
+    for (LL j = startjj_inbound; j <= endjj_inbound; j += 2) {
+      LL value = rowStart_inbound & colStart_inbound;
+      if (value == 0) {
+        mark[j - F] ^= 1;
+      }
+      rowStart_inbound--;
+      colStart_inbound++;
+    }
+  }
 
-	for(int i = 0; i < L; i++ ){
-		if(i) cout << " ";
-		cout << mark[i];
-	}
-	cout << endl;
+  for (int i = 0; i < L; i++) {
+    if (i) cout << " ";
+    cout << mark[i];
+  }
+  cout << endl;
 
-	return 0;
+  return 0;
 }
-
-
