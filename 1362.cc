8f1a3ceb6222944ba6b45493f97a1857d7443ff7
sdecoder <sde.work.inbox@gmail.com>
2016-05-12 01:26:25 -0400

    2016-05-12 01:26:25

--- 1362.cc
+++ 1362.cc
@@ -1,30 +1,24 @@
-#include<stdio.h>
-int main()
-{
- int T,i,j,n;
- int base[27]={1},result[27];
- for(i=1;i<27;++i)
-  base[i]=(base[i-1]<<1)+1;
- scanf("%d",&T);
- while(T--)
- {
-  scanf("%d",&n);
-  printf("%d [",n);
-  if(!n){
-   printf("]\n");
-   continue;
+#include <stdio.h>
+int main() {
+  int T, i, j, n;
+  int base[27] = {1}, result[27];
+  for (i = 1; i < 27; ++i) base[i] = (base[i - 1] << 1) + 1;
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d", &n);
+    printf("%d [", n);
+    if (!n) {
+      printf("]\n");
+      continue;
+    }
+    for (j = 0, i = 26; n; --i) {
+      while (n >= base[i]) {
+        result[j++] = i;
+        n -= base[i];
+      }
+    }
+    while (j - 1) printf("%d,", result[--j]);
+    printf("%d]\n", result[--j]);
   }
-  for(j=0,i=26;n;--i){
-   while(n>=base[i]){
-    result[j++]=i;
-    n-=base[i];
-   }
-  }
-  while(j-1)
-   printf("%d,",result[--j]);
-  printf("%d]\n",result[--j]);
- }
- return 0;
+  return 0;
 }
-
-
