7b8f47a6366690d95da4619d252558939f561c49
sdecoder <sde.work.inbox@gmail.com>
2016-05-12 13:24:19 -0400

    2016-05-12 13:24:19

--- 3632.cc
+++ 3632.cc
@@ -1,20 +1,17 @@
 #include <cstdio>
 using namespace std;
-int main(){
-	int nCase;
-	scanf("%d",&nCase);
-	while(nCase--){
-		int max=0,min=200,n,a;
-		scanf("%d",&n);
-		while(n--){
-			scanf("%d",&a);
-			if(a>max)max=a;
-			if(a<min)min=a;	
-		}
-		printf("%d\n",2*(max-min));
-	}
-	return 0;
+int main() {
+  int nCase;
+  scanf("%d", &nCase);
+  while (nCase--) {
+    int max = 0, min = 200, n, a;
+    scanf("%d", &n);
+    while (n--) {
+      scanf("%d", &a);
+      if (a > max) max = a;
+      if (a < min) min = a;
+    }
+    printf("%d\n", 2 * (max - min));
+  }
+  return 0;
 }
-
-
-
