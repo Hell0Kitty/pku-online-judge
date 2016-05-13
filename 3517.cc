85a83ca580f437f1b0c0247f3f60d349d4bcd231
sdecoder <sde.work.inbox@gmail.com>
2016-05-13 06:27:15 -0400

    2016-05-13 06:27:15

--- 3517.cc
+++ 3517.cc
@@ -1,33 +1,27 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
-#include<cmath>
-#include<queue>
-#include<stack>
-#include<string>
-#include<vector>
-#include<cstdlib>
-#include<map>
-#include<set>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <map>
+#include <set>
 using namespace std;
-#define CL(x,v) memset(x,v,sizeof(x));
+#define CL(x, v) memset(x, v, sizeof(x));
 
-int n,k,m;
+int n, k, m;
 
-int main()
-{
-    while(~scanf("%d%d%d",&n,&k,&m))
-    {
-        if(n==0) break;
-        int x=0,i=2;
-        for(;i<n;i++)
-            x=(x+k)%i;
-        x=(x+m)%i+1;
-        printf("%d\n",x);
-    }
-    return 0;
+int main() {
+  while (~scanf("%d%d%d", &n, &k, &m)) {
+    if (n == 0) break;
+    int x = 0, i = 2;
+    for (; i < n; i++) x = (x + k) % i;
+    x = (x + m) % i + 1;
+    printf("%d\n", x);
+  }
+  return 0;
 }
-
-
-
