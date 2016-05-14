d5d649d61b8e305c43585dd31454a011ad0f7699
sdecoder <sde.work.inbox@gmail.com>
2016-05-14 06:35:38 -0400

    2016-05-14 06:35:38

--- 2378.cc
+++ 2378.cc
@@ -9,59 +9,47 @@ using namespace std;
 
 int p[maxn];
 int dp[maxn];
-int num,res;
+int num, res;
 int con[maxn];
-struct node
-{
-    int en,next;
-}bian[maxn*3];
+struct node {
+  int en, next;
+} bian[maxn * 3];
 
-void add(int st,int en)
-{
-    bian[num].en=en;
-    bian[num].next=p[st];
-    p[st]=num++;
+void add(int st, int en) {
+  bian[num].en = en;
+  bian[num].next = p[st];
+  p[st] = num++;
 }
-void dfs(int u,int fa,int n)
-{
-    con[u]=1,dp[u]=0;
-    int i,v;
-    for(i=p[u];i+1;i=bian[i].next)
-    {
-        v=bian[i].en;
-        if(v==fa)
-            continue;
-        dfs(v,u,n);
-        con[u]+=con[v];
-        dp[u]=max(dp[u],con[v]);
+void dfs(int u, int fa, int n) {
+  con[u] = 1, dp[u] = 0;
+  int i, v;
+  for (i = p[u]; i + 1; i = bian[i].next) {
+    v = bian[i].en;
+    if (v == fa) continue;
+    dfs(v, u, n);
+    con[u] += con[v];
+    dp[u] = max(dp[u], con[v]);
+  }
+
+  dp[u] = max(dp[u], n - con[u]);
+
+  res = min(dp[u], res);
+}
+int main() {
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    num = 0;
+    memset(p, -1, sizeof(p));
+    int i, x, y;
+    for (i = 1; i < n; i++) {
+      scanf("%d %d", &x, &y);
+      add(x, y);
+      add(y, x);
     }
-
-    dp[u]=max(dp[u],n-con[u]);
-
-    res=min(dp[u],res);
+    res = inff;
+    dfs(1, -1, n);
+    for (i = 1; i <= n; i++)
+      if (dp[i] == res) printf("%d\n", i);
+  }
+  return 0;
 }
-int main()
-{
-    int n;
-    while(scanf("%d",&n)!=EOF)
-    {
-        num=0;
-        memset(p,-1,sizeof(p));
-        int i,x,y;
-        for(i=1;i<n;i++)
-        {
-            scanf("%d %d",&x,&y);
-            add(x,y);
-            add(y,x);
-        }
-        res=inff;
-        dfs(1,-1,n);
-        for(i=1;i<=n;i++)
-            if(dp[i]==res)
-                printf("%d\n",i);
-    }
-    return 0;
-}
-
-
-
