960ee70d2e1f2ee75bfe5f7a69de832c4eee463a
sdecoder <sde.work.inbox@gmail.com>
2016-05-17 08:13:14 -0400

    2016-05-17 08:13:14

--- 3627.cc
+++ 3627.cc
@@ -4,34 +4,25 @@
 #include <vector>
 using namespace std;
 
-bool cmp(int a,int b)
-{
-	return a>b;
+bool cmp(int a, int b) { return a > b; }
+
+int main() {
+  int N, B, i;
+  int height[20005];
+  cin >> N >> B;
+
+  for (i = 0; i < N; i++) {
+    cin >> height[i];
+  }
+  sort(height, height + N, cmp);
+
+  int ans = 0;
+  for (i = 0; i < N; i++) {
+    ans += height[i];
+    if (ans >= B) {
+      cout << i + 1 << endl;
+      return 0;
+    }
+  }
+  return 0;
 }
-
-int main()
-{
-	int N,B,i;
-	int height[20005];
-	cin>>N>>B;
-
-	for(i=0;i<N;i++)
-	{
-		cin>>height[i];
-	}
-	sort(height,height+N,cmp);
-
-	int ans=0;
-	for(i=0;i<N;i++)
-	{
-		ans += height[i];
-		if(ans>=B)
-		{
-			cout<<i+1<<endl;
-			return 0;
-		}
-	}
-	return 0;
-}
-
-
