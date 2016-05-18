118f0da8736b12e50c81345553625bcd8a0f56b3
sdecoder <sde.work.inbox@gmail.com>
2016-05-18 06:08:16 -0400

    2016-05-18 06:08:16

--- 2498.cc
+++ 2498.cc
@@ -7,58 +7,45 @@ using namespace std;
 #define maxn 20
 
 char id[maxn];
-int fac[] =
-{ 9, 3, 7 };
+int fac[] = {9, 3, 7};
 
-int getpos(char ch)
-{
-    int len = strlen(id);
+int getpos(char ch) {
+  int len = strlen(id);
 
-    for (int i = 0; i < len; i++)
-        if (id[i] == ch)
-            return i;
-    return -1;
+  for (int i = 0; i < len; i++)
+    if (id[i] == ch) return i;
+  return -1;
 }
 
-bool check()
-{
-    int sum = 0;
-    int len = strlen(id);
+bool check() {
+  int sum = 0;
+  int len = strlen(id);
 
-    for (int i = len - 1; i >= 0; i--)
-        sum += (id[i] - '0') * fac[(len - 1 - i) % 3];
-    if (0 == sum % 10)
-        return true;
-    return false;
+  for (int i = len - 1; i >= 0; i--)
+    sum += (id[i] - '0') * fac[(len - 1 - i) % 3];
+  if (0 == sum % 10) return true;
+  return false;
 }
 
-void work()
-{
-    gets(id);
-    int pos = getpos('?');
-    int len = strlen(id);
-    for (int i = 0; i <= 9; i++)
-    {
-        id[pos] = i + '0';
-        if (check())
-            return;
-    }
+void work() {
+  gets(id);
+  int pos = getpos('?');
+  int len = strlen(id);
+  for (int i = 0; i <= 9; i++) {
+    id[pos] = i + '0';
+    if (check()) return;
+  }
 }
 
-int main()
-{
-    int t, s = 0;
-    scanf("%d", &t);
-    getchar();
-    while (t--)
-    {
-        s++;
-        printf("Scenario #%d:\n", s);
-        work();
-        printf("%s\n\n", id);
-    }
-    return 0;
+int main() {
+  int t, s = 0;
+  scanf("%d", &t);
+  getchar();
+  while (t--) {
+    s++;
+    printf("Scenario #%d:\n", s);
+    work();
+    printf("%s\n\n", id);
+  }
+  return 0;
 }
-
-
-
