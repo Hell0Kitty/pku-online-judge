8263d5a7851e7b071f0691ea9f9396cdff6a2078
sdecoder <sde.work.inbox@gmail.com>
2016-05-18 07:40:03 -0400

    2016-05-18 07:40:03

--- 2646.cc
+++ 2646.cc
@@ -1,28 +1,24 @@
 #include <iostream>
 using namespace std;
 double a[1000];
-int main()
-{
-int n;
-double avg, big, small;
-while(cin >> n && n){
-   big = small = avg = 0;
-   for(int i=0; i<n; i++){
-    cin >> a[i];
-    avg += a[i];
-   }
-   avg /= (double)n;
-   avg =(int)(avg*100+0.5)/100.0;
-   for(int i=0; i<n; i++){
-    if(a[i] <= avg)
-     big += (avg - a[i]);
-    else
-     small -= (avg - a[i]);
-   }
-   cout.precision(2);
-   cout << '$' << fixed << min(big, small) << endl;
+int main() {
+  int n;
+  double avg, big, small;
+  while (cin >> n && n) {
+    big = small = avg = 0;
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      avg += a[i];
+    }
+    avg /= (double)n;
+    avg = (int)(avg * 100 + 0.5) / 100.0;
+    for (int i = 0; i < n; i++) {
+      if (a[i] <= avg)
+        big += (avg - a[i]);
+      else
+        small -= (avg - a[i]);
+    }
+    cout.precision(2);
+    cout << '$' << fixed << min(big, small) << endl;
   }
 }
-
-
-
