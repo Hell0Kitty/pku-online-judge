aa34fb52e945d717de1825445cebb849c2edaee4
sdecoder <sde.work.inbox@gmail.com>
2016-05-18 19:08:11 -0400

    2016-05-18 19:08:11

---
+++ 3365.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+double w,h;
+const double pi=acos(-1.0);
+int main()
+{;
+    while(scanf("%lf%lf",&h,&w),w+h)
+    {
+        double ans=0,r;
+        r=min(h/2,w/(2*(pi+1)));
+        ans=max(ans,pi*r*r*h);
+        r=h/2/pi;
+        ans=max(ans,pi*r*r*(w-2*r));
+        printf("%.3f\n",ans);
+
+    }
+    return 0;
+}
+
+
