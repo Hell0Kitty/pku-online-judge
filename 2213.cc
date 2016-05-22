0fb32980b2b3b5c53a5531b09bd5b8770f2ac20c
sdecoder <sde.work.inbox@gmail.com>
2016-05-21 20:55:55 -0400

    2016-05-21 20:55:55

--- 2213.cc
+++ 2213.cc
@@ -1,45 +1,73 @@
-#include<iostream>
-#include<string>
-#include<cstdio>
-#include<cstring>
-#include<cmath>
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
 using namespace std;
 #define DBG 0
 #define sz(c) ((int)(c).size())
-#define    forl(i, a, b) for(int i = (a); i <  (b); ++i)
-#define   forle(i, a, b) for(int i = (a); i <= (b); ++i)
-#define    forg(i, a, b) for(int i = (a); i >  (b); --i)
-#define   forge(i, a, b) for(int i = (a); i >= (b); --i)
-#define   forlc(i, a, b) for(int i##_b = (b), i = (a); i <  i##_b; ++i)
-#define  forlec(i, a, b) for(int i##_b = (b), i = (a); i <= i##_b; ++i)
-#define   forgc(i, a, b) for(int i##_b = (b), i = (a); i >  i##_b; --i)
-#define  forgec(i, a, b) for(int i##_b = (b), i = (a); i >= i##_b; --i)
-#define  forall(i, v   )  forl(i, 0, sz(v))
-#define forallc(i, v   ) forlc(i, 0, sz(v))
-#define  forlla(i, v   ) forge(i, sz(v)-1, 0)
-#define   forls(i, n, a, b) for(int i = a; i != b; i = n[i])
-#define rep(n)  for(int               repp = 0; repp <    (n); ++repp)
-#define repc(n) for(int repp_b = (n), repp = 0; repp < repp_b; ++repp)
+#define forl(i, a, b) for (int i = (a); i < (b); ++i)
+#define forle(i, a, b) for (int i = (a); i <= (b); ++i)
+#define forg(i, a, b) for (int i = (a); i > (b); --i)
+#define forge(i, a, b) for (int i = (a); i >= (b); --i)
+#define forlc(i, a, b) for (int i##_b = (b), i = (a); i < i##_b; ++i)
+#define forlec(i, a, b) for (int i##_b = (b), i = (a); i <= i##_b; ++i)
+#define forgc(i, a, b) for (int i##_b = (b), i = (a); i > i##_b; --i)
+#define forgec(i, a, b) for (int i##_b = (b), i = (a); i >= i##_b; --i)
+#define forall(i, v) forl(i, 0, sz(v))
+#define forallc(i, v) forlc(i, 0, sz(v))
+#define forlla(i, v) forge(i, sz(v) - 1, 0)
+#define forls(i, n, a, b) for (int i = a; i != b; i = n[i])
+#define rep(n) for (int repp = 0; repp < (n); ++repp)
+#define repc(n) for (int repp_b = (n), repp = 0; repp < repp_b; ++repp)
 #define rst(a, v) memset(a, v, sizeof a)
 #define cpy(a, b) memcpy(a, b, sizeof a)
-#define rstn(a, v, n) memset(a, v, (n)*sizeof((a)[0]))
-#define cpyn(a, b, n) memcpy(a, b, (n)*sizeof((a)[0]))
-#define ast(b) if(DBG && !(b)) { printf("%d!!|\n", __LINE__); while(1) getchar(); }
-#define dout DBG && cout << __LINE__ << ">>| "
-#define pr(x) #x"=" << (x) << " | "
-#define mk(x) DBG && cout << __LINE__ << "**| "#x << endl
-#define pra(arr, a, b) if(DBG) { \
-    dout<<#arr"[] | "; \
-    forlec(i, a, b) cout<<"["<<i<<"]="<<arr[i]<<" |"<<((i-(a)+1)%13?" ":"\n"); \
-    if(((b)-(a)+1)%13) puts(""); \
+#define rstn(a, v, n) memset(a, v, (n) * sizeof((a)[0]))
+#define cpyn(a, b, n) memcpy(a, b, (n) * sizeof((a)[0]))
+#define ast(b)                   \
+  if (DBG && !(b)) {             \
+    printf("%d!!|\n", __LINE__); \
+    while (1) getchar();         \
   }
-#define rd(type, x) type x; cin >> x
-inline int     rdi() { int d; scanf("%d", &d); return d; }
-inline char    rdc() { scanf(" "); return getchar(); }
-inline string  rds() { rd(string, s); return s; }
-inline double rddb() { double d; scanf("%lf", &d); return d; }
-template<class T> inline bool updateMin(T& a, T b) { return a>b? a=b, true: false; }
-template<class T> inline bool updateMax(T& a, T b) { return a<b? a=b, true: false; }
+#define dout DBG&& cout << __LINE__ << ">>| "
+#define pr(x) #x "=" << (x) << " | "
+#define mk(x) DBG&& cout << __LINE__ << "**| " #x << endl
+#define pra(arr, a, b)                                         \
+  if (DBG) {                                                   \
+    dout << #arr "[] | ";                                      \
+    forlec(i, a, b) cout << "[" << i << "]=" << arr[i] << " |" \
+                         << ((i - (a) + 1) % 13 ? " " : "\n"); \
+    if (((b) - (a) + 1) % 13) puts("");                        \
+  }
+#define rd(type, x) \
+  type x;           \
+  cin >> x
+inline int rdi() {
+  int d;
+  scanf("%d", &d);
+  return d;
+}
+inline char rdc() {
+  scanf(" ");
+  return getchar();
+}
+inline string rds() {
+  rd(string, s);
+  return s;
+}
+inline double rddb() {
+  double d;
+  scanf("%lf", &d);
+  return d;
+}
+template <class T>
+inline bool updateMin(T& a, T b) {
+  return a > b ? a = b, true : false;
+}
+template <class T>
+inline bool updateMax(T& a, T b) {
+  return a < b ? a = b, true : false;
+}
 
 const int N = 32;
 
@@ -51,14 +79,11 @@ int main() {
     scanf("%d %s", &n, s);
     int xorsum = 0, ans = 0;
     forl(i, 0, n) {
-      xorsum ^= s[i]=='1';
-      ans = ans<<1 | xorsum;
+      xorsum ^= s[i] == '1';
+      ans = ans << 1 | xorsum;
     }
     static int cas;
     printf("Poslanec %d se posadi na sedadlo cislo %d.\n", ++cas, ++ans);
   }
   return 0;
 }
-
-
-
