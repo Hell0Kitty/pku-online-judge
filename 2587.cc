5f23e5c055ab0ec80d6667f4928b6df8d1b45fd6
sdecoder <sde.work.inbox@gmail.com>
2016-05-21 22:54:31 -0400

    2016-05-21 22:54:31

--- 2587.cc
+++ 2587.cc
@@ -5,47 +5,45 @@
 #include <cstdio>
 using namespace std;
 
-struct Node{
-    double latitude, longitude, x, y, z;
-}node[1111];
+struct Node {
+  double latitude, longitude, x, y, z;
+} node[1111];
 
-double Distance(const Node &a, const Node &b){
-    return (a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y) + (a.z-b.z)*(a.z-b.z);
+double Distance(const Node &a, const Node &b) {
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y) +
+         (a.z - b.z) * (a.z - b.z);
 }
 
-int main(){
+int main() {
 #ifndef ONLINE_JUDGE
-    freopen("in", "r", stdin);
+  freopen("in", "r", stdin);
 #endif
-    int n;
-    while(cin >>n){
-       for(int i=0; i<n; i++){
-           scanf("%lf %lf", &node[i].latitude, &node[i].longitude);
-           double lat = node[i].latitude/180;
-           double lon = node[i].longitude/180;
-           node[i].x = cos(lat)*cos(lon);
-           node[i].y = cos(lat)*sin(lon);
-           node[i].z = sin(lat);
-       }
-
-       int tag ;
-       double Min = 1000000.0;
-       for(int i=0; i<n; i++){
-           double Max = 0.0;
-           for(int j=0; j<n; j++){
-              double tmp = Distance(node[i], node[j]);
-              Max = max(tmp, Max);
-           }
-           if(Max < Min){         
-              Min = Max;
-              tag = i;
-           }
-       }
-
-       printf("%.2lf %.2lf\n", node[tag].latitude, node[tag].longitude);
+  int n;
+  while (cin >> n) {
+    for (int i = 0; i < n; i++) {
+      scanf("%lf %lf", &node[i].latitude, &node[i].longitude);
+      double lat = node[i].latitude / 180;
+      double lon = node[i].longitude / 180;
+      node[i].x = cos(lat) * cos(lon);
+      node[i].y = cos(lat) * sin(lon);
+      node[i].z = sin(lat);
     }
-    return 0;
+
+    int tag;
+    double Min = 1000000.0;
+    for (int i = 0; i < n; i++) {
+      double Max = 0.0;
+      for (int j = 0; j < n; j++) {
+        double tmp = Distance(node[i], node[j]);
+        Max = max(tmp, Max);
+      }
+      if (Max < Min) {
+        Min = Max;
+        tag = i;
+      }
+    }
+
+    printf("%.2lf %.2lf\n", node[tag].latitude, node[tag].longitude);
+  }
+  return 0;
 }
-
-
-
