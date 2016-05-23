d2d8a11f180133b4db1e9ed1a7c31dbd81f756fd
sdecoder <sde.work.inbox@gmail.com>
2016-05-23 04:40:38 -0400

    2016-05-23 04:40:38

--- 2101.cc
+++ 2101.cc
@@ -7,27 +7,21 @@ using namespace std;
 
 int n, m;
 
-int main()
-{
-    int x, y;
-    scanf("%d%d", &n, &m);
-    x = 0;
-    y = 0;
-    for (int i = 0; i < n - 1; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        x += a;
-    }
-    for (int i = 0; i < m - 1; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        y += a;
-    }
-    printf("%d\n", (int)ceil(sqrt(x * x + y * y)));
-    return 0;
+int main() {
+  int x, y;
+  scanf("%d%d", &n, &m);
+  x = 0;
+  y = 0;
+  for (int i = 0; i < n - 1; i++) {
+    int a;
+    scanf("%d", &a);
+    x += a;
+  }
+  for (int i = 0; i < m - 1; i++) {
+    int a;
+    scanf("%d", &a);
+    y += a;
+  }
+  printf("%d\n", (int)ceil(sqrt(x * x + y * y)));
+  return 0;
 }
-
-
-
