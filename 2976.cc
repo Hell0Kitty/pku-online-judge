bfd22c81a19d7e8f871c0019cc6971d23257d31a
sdecoder <sde.work.inbox@gmail.com>
2016-06-01 06:59:13 -0400

    2016-06-01 06:59:13

--- 2976.cc
+++ 2976.cc
@@ -2,36 +2,28 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-const int maxn=1000+200;
-int N,K;
-int a[maxn],b[maxn];
+const int maxn = 1000 + 200;
+int N, K;
+int a[maxn], b[maxn];
 double d[maxn];
-int main()
-{
-	while(scanf("%d%d",&N,&K)==2)
-	{
-		if(N==0 && K==0) return 0;
-		for(int i=0;i!=N;++i)
-			scanf("%d",a+i);
-		for(int i=0;i!=N;++i)
-			scanf("%d",b+i);
-		double L=0,R=1e10;
-		while(R-L>1e-6)
-		{
-			double  ans=0;
-			double mid=(L+R)/2.0;
-			for(int i=0;i!=N;++i)
-			d[i]=a[i]-(mid*b[i]);
-			sort(d,d+N,greater<double>());
-			for(int i=0;i!=N-K;++i)
-				ans+=d[i];
-			if(ans>=0) L=mid;
-			else R=mid;
-		}
-		printf("%d\n",int(L*100+0.5));
-	}
-	return 0;
+int main() {
+  while (scanf("%d%d", &N, &K) == 2) {
+    if (N == 0 && K == 0) return 0;
+    for (int i = 0; i != N; ++i) scanf("%d", a + i);
+    for (int i = 0; i != N; ++i) scanf("%d", b + i);
+    double L = 0, R = 1e10;
+    while (R - L > 1e-6) {
+      double ans = 0;
+      double mid = (L + R) / 2.0;
+      for (int i = 0; i != N; ++i) d[i] = a[i] - (mid * b[i]);
+      sort(d, d + N, greater<double>());
+      for (int i = 0; i != N - K; ++i) ans += d[i];
+      if (ans >= 0)
+        L = mid;
+      else
+        R = mid;
+    }
+    printf("%d\n", int(L * 100 + 0.5));
+  }
+  return 0;
 }
-
-
-
