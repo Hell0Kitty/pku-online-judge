3957fd8fdc070155b862921a2d6a5904fad7e29e
sdecoder <sde.work.inbox@gmail.com>
2016-06-03 09:54:15 -0400

    2016-06-03 09:54:15

--- 3112.cc
+++ 3112.cc
@@ -10,40 +10,29 @@ int n, m, p;
 int ans;
 int f[maxn];
 
-void work()
-{
-    ans = 0;
-    memset(f, 0, sizeof(f));
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            int a;
-            scanf("%d", &a);
-            if (a)
-                f[j]++;
-            else if (f[j] != 0)
-            {
-                if (f[j] >= p)
-                    ans++;
-                f[j] = 0;
-            }
-        }
+void work() {
+  ans = 0;
+  memset(f, 0, sizeof(f));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      int a;
+      scanf("%d", &a);
+      if (a)
+        f[j]++;
+      else if (f[j] != 0) {
+        if (f[j] >= p) ans++;
+        f[j] = 0;
+      }
     }
-    for (int i = 0; i < m; i++)
-        if (f[i] >= p && f[i] != 0)
-            ans++;
-    printf("%d\n", ans);
+  }
+  for (int i = 0; i < m; i++)
+    if (f[i] >= p && f[i] != 0) ans++;
+  printf("%d\n", ans);
 }
 
-int main()
-{
-
-    while (scanf("%d%d%d", &m, &n, &p), n | m | p)
-    {
-        work();
-    }
-    return 0;
+int main() {
+  while (scanf("%d%d%d", &m, &n, &p), n | m | p) {
+    work();
+  }
+  return 0;
 }
-
-
