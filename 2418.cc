b61bfdc492f38130e7bd89be00c34c0e9da03f4e
sdecoder <sde.work.inbox@gmail.com>
2016-06-03 17:43:25 -0400

    2016-06-03 17:43:25

--- 2418.cc
+++ 2418.cc
@@ -1,22 +1,21 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<map>
-#include<string>
-#include<iomanip>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <map>
+#include <string>
+#include <iomanip>
 using namespace std;
 map<string, int> mm;
-int main(){
-	string name; int cnt = 0;
-	while (getline(cin, name)){
-		mm[name]++;
-		cnt++;
-	}
-	map<string, int>::iterator iter;
-	for (iter = mm.begin(); iter != mm.end(); iter++)
-		cout << iter->first << ' ' << fixed << setprecision(4) << (100.0*mm[iter->first])/cnt<< endl;
-	return 0;
+int main() {
+  string name;
+  int cnt = 0;
+  while (getline(cin, name)) {
+    mm[name]++;
+    cnt++;
+  }
+  map<string, int>::iterator iter;
+  for (iter = mm.begin(); iter != mm.end(); iter++)
+    cout << iter->first << ' ' << fixed << setprecision(4)
+         << (100.0 * mm[iter->first]) / cnt << endl;
+  return 0;
 }
-
-
-
