991f8ad9d59988eb75ef5d166466b514e038c774
sdecoder <sde.work.inbox@gmail.com>
2016-06-07 22:08:17 -0400

    2016-06-07 22:08:17

--- 2363.cc
+++ 2363.cc
@@ -1,27 +1,26 @@
 
 
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main() {
-    int T , V;
-    scanf("%d",&T);        
-    while (T --) {
-        scanf("%d",&V);
-        int l = ceil(pow(V , 1.0/3));
-        bool flag = 0;
-        int i , j , k;
-        int minn = 2*(1 + V + V);
-        for (i = l ; i > 0 ; i --) 
-          for (j = l ; j <= V ; j ++) {
-            if (V%(i*j) == 0) {
-                k = V/(i*j);
-                minn = 2*(i*j+i*k+j*k)<minn?2*(i*j+i*k+j*k):minn;    
-            }
-          }
-          printf("%d\n",minn);    
-    }
+  int T, V;
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d", &V);
+    int l = ceil(pow(V, 1.0 / 3));
+    bool flag = 0;
+    int i, j, k;
+    int minn = 2 * (1 + V + V);
+    for (i = l; i > 0; i--)
+      for (j = l; j <= V; j++) {
+        if (V % (i * j) == 0) {
+          k = V / (i * j);
+          minn = 2 * (i * j + i * k + j * k) < minn
+                     ? 2 * (i * j + i * k + j * k)
+                     : minn;
+        }
+      }
+    printf("%d\n", minn);
+  }
 }
-
-
-
