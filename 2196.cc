5f64b2f698fbaedfff6fec1d9509aaf587aac91d
sdecoder <sde.work.inbox@gmail.com>
2016-06-09 19:37:43 -0400

    2016-06-09 19:37:43

--- 2196.cc
+++ 2196.cc
@@ -1,23 +1,16 @@
 #include <cstdio>
 
-int sum(int a, int b)
-{
-    int ret = 0;
-    while (a)
-    {
-        ret += a % b;
-        a /= b;
-    }
-    return ret;
+int sum(int a, int b) {
+  int ret = 0;
+  while (a) {
+    ret += a % b;
+    a /= b;
+  }
+  return ret;
 }
 
-int main()
-{
-    for (int i = 2992; i <= 9999; i++)
-        if (sum(i, 10) == sum(i, 12) && sum(i, 10) == sum(i, 16))
-            printf("%d\n", i);
-    return 0;
+int main() {
+  for (int i = 2992; i <= 9999; i++)
+    if (sum(i, 10) == sum(i, 12) && sum(i, 10) == sum(i, 16)) printf("%d\n", i);
+  return 0;
 }
-
-
-
