c850dac2a13eb37d67d288acc54f78bf9cfb22fb
sdecoder <sde.work.inbox@gmail.com>
2016-06-10 23:39:28 -0400

    2016-06-10 23:39:28

---
+++ 1975.cc
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+
+using namespace std;
+int h[101][101];
+int d[101][101];
+
+int main()
+
+{
+ int t;
+ scanf("%d",&t);
+ for(int l=1;l<=t;l++)
+ {
+ memset(h,0,sizeof(h));
+ memset(d,0,sizeof(d));
+ int n,m;
+ scanf("%d %d",&n,&m);
+ int a,b;
+ for(int i=1;i<=m;i++)
+ {
+ scanf("%d %d",&a,&b);
+ h[a][b]=1;
+ d[b][a]=1;
+ }
+
+ for(int k=1;k<=n;k++)
+ for(int i=1;i<=n;i++)
+ for(int j=1;j<=n;j++)
+ if(d[i][j]==1)
+ d[i][j]=1;
+ else if(d[i][k]&&d[k][j])
+ d[i][j]=1;
+
+ for(int k=1;k<=n;k++)
+ for(int i=1;i<=n;i++)
+ for(int j=1;j<=n;j++)
+ if(h[i][j]==1)
+ h[i][j]=1;
+ else if(h[i][k]&&h[k][j])
+ h[i][j]=1;
+
+ int sum=0;
+
+ for(int i=1;i<=n;i++)
+ {
+ int ans=0;
+ for(int j=1;j<=n;j++)
+ {
+ if(d[i][j]==1)
+ ans++;
+ }
+ if(ans>=(n+1)/2)
+ sum++;
+ }
+
+ for(int i=1;i<=n;i++)
+ {
+ int ans=0;
+ for(int j=1;j<=n;j++)
+ {
+ if(h[i][j]==1)
+ ans++;
+ }
+
+ if(ans>=(n+1)/2)
+ sum++;
+ }
+
+ printf("%d\n",sum);
+ }
+ return 0;
+}
+
+
+
