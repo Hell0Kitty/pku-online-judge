8a61fadf22accdbbcabbd52e6ff9835d1c58259f
sdecoder <sde.work.inbox@gmail.com>
2016-06-11 17:23:02 -0400

    2016-06-11 17:23:02

--- 3844.cc
+++ 3844.cc
@@ -1,26 +1,23 @@
-#include<stdio.h>  
-#include<string.h>  
-int f[1000005];  
-__int64 ans;  
-int main()  
-{  
-    int i,j,k,t,n,d;  
-    scanf("%d",&t);  
-    while(t--){  
-        scanf("%d%d",&d,&n);  
-        memset(f,0,sizeof(f));  
-        int num=0; ans=0;  
-        while(n--){  
-            scanf("%d",&k);  
-            num=(num+k)%d;  
-            if(num==0) f[num]++;  
-            ans+=f[num];  
-            if(num!=0) f[num]++;  
-        }  
-        printf("%I64d\n",ans);  
-    }  
-    return 0;  
+#include <stdio.h>
+#include <string.h>
+int f[1000005];
+__int64 ans;
+int main() {
+  int i, j, k, t, n, d;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d%d", &d, &n);
+    memset(f, 0, sizeof(f));
+    int num = 0;
+    ans = 0;
+    while (n--) {
+      scanf("%d", &k);
+      num = (num + k) % d;
+      if (num == 0) f[num]++;
+      ans += f[num];
+      if (num != 0) f[num]++;
+    }
+    printf("%I64d\n", ans);
+  }
+  return 0;
 }
-
-
-
