74c901561d7fdc11abcc1acaabcddc41d5e252aa
sdecoder <sde.work.inbox@gmail.com>
2016-06-12 10:03:05 -0400

    2016-06-12 10:03:05

--- 2623.cc
+++ 2623.cc
@@ -33,50 +33,49 @@ const int MAXI = 0x7fffffff;
 long long a[1111111];
 
 int partition(int l, int r) {
-    int i = rand() % (r - l + 1) + l;
-    swap(a[l], a[i]);
-    int s = l, e = r;
-    LL pivot = a[l];
-    while (s < e) {
-        while (s < e && a[e] > pivot) e--;
-        if(s < e) a[s++] = a[e];
-        while (s < e && a[s] < pivot) s++;
-        if(s < e) a[e--] = a[s];
-    }
-    a[s] = pivot;
-    return s;
+  int i = rand() % (r - l + 1) + l;
+  swap(a[l], a[i]);
+  int s = l, e = r;
+  LL pivot = a[l];
+  while (s < e) {
+    while (s < e && a[e] > pivot) e--;
+    if (s < e) a[s++] = a[e];
+    while (s < e && a[s] < pivot) s++;
+    if (s < e) a[e--] = a[s];
+  }
+  a[s] = pivot;
+  return s;
 }
 
 LL solve(int l, int r, int k) {
-    int p = partition(l, r);
+  int p = partition(l, r);
 
-    if (p == k) {
-        return a[p];
-    }
+  if (p == k) {
+    return a[p];
+  }
 
-    if (k > r || k < 0) {
-        return -1;
-    }
+  if (k > r || k < 0) {
+    return -1;
+  }
 
-    if (k < p) {
-        return solve(l, p-1, k);
-    }
+  if (k < p) {
+    return solve(l, p - 1, k);
+  }
 
-    if (k > p) {
-        return solve(p+1, r, k);
-    }
-    return 0;
+  if (k > p) {
+    return solve(p + 1, r, k);
+  }
+  return 0;
 }
 
 int main() {
-    int n, k;
-    while (cin >> n) {
-        for (int i = 1; i <= n; i++) scanf("%lld", a+i);
-        sort(a+1, a+n+1);
-        if(n&1) printf("%.1f\n", a[(n+1)/2]*1.0);
-        else printf("%.1f\n", (a[n/2]*1.0+a[n/2+1]*1.0)/2);
-    }
+  int n, k;
+  while (cin >> n) {
+    for (int i = 1; i <= n; i++) scanf("%lld", a + i);
+    sort(a + 1, a + n + 1);
+    if (n & 1)
+      printf("%.1f\n", a[(n + 1) / 2] * 1.0);
+    else
+      printf("%.1f\n", (a[n / 2] * 1.0 + a[n / 2 + 1] * 1.0) / 2);
+  }
 }
-
-
-
