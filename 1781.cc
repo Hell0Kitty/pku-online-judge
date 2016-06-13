5fb73faea33347ad5d204b3c37ae4a2f66a8c741
sdecoder <sde.work.inbox@gmail.com>
2016-06-13 15:57:07 -0400

    2016-06-13 15:57:07

---
+++ 1781.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+typedef long long int ll;
+
+ll get_num(ll x)
+{
+    if (x == 1)return 1;
+    if (x&1)return (2*get_num(x/2)+1);
+    else return (2*get_num(x/2)-1);
+}
+
+int main()
+{
+    ll n,m;
+    while (scanf("%I64de%I64d",&n,&m) && !(n == 0 && m == 0)) {
+        for (int i(0); i<m; ++i) {
+            n *= 10;
+        }
+        printf("%I64d\n",get_num(n));
+    }
+    return 0;
+}
+
+
+
