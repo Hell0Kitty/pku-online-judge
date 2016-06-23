90a86b987de357344d3a3b55d7e628628a6d56e7
sdecoder <sde.work.inbox@gmail.com>
2016-06-23 12:06:18 -0400

    2016-06-23 12:06:18

--- 3645.cc
+++ 3645.cc
@@ -1,47 +1,42 @@
-#include<iostream>
-#include<cstdio>
-#include<string>
-#include<stack>
-#include<cstring>
-#include<cstdlib>
-#include<cctype>
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <stack>
+#include <cstring>
+#include <cstdlib>
+#include <cctype>
 using namespace std;
-int main()
-{
-    string str;
-    while(getline(cin,str))
-    {
-        if(str=="()") break;
-        stack<double> q;
-        for(int i=0;i<str.size();)
-        {
-            if(str[i]==' '||str[i]=='(') i++;
-            else if(isdigit(str[i])||str[i]=='-') //考虑负数
-            {
-                string temp;
-                for(;i<str.size()&&str[i]!=' '&&str[i]!=')';i++)
-                {
-                    temp+=str[i];
-                }
-                double x=atof(temp.c_str());
-                q.push(x);
-            }
-            else 
-            {
-                double p,x,y;
-                y=q.top();q.pop();
-                x=q.top();q.pop();
-                p=q.top();q.pop();
-                double cnt=p*(x+y)+(1-p)*(x-y);
-                q.push(cnt);
-                i++;
-            }
+int main() {
+  string str;
+  while (getline(cin, str)) {
+    if (str == "()") break;
+    stack<double> q;
+    for (int i = 0; i < str.size();) {
+      if (str[i] == ' ' || str[i] == '(')
+        i++;
+      else if (isdigit(str[i]) || str[i] == '-')  //考虑负数
+      {
+        string temp;
+        for (; i < str.size() && str[i] != ' ' && str[i] != ')'; i++) {
+          temp += str[i];
         }
-        double ans=q.top();
-        printf("%.2lf\n",ans);
+        double x = atof(temp.c_str());
+        q.push(x);
+      } else {
+        double p, x, y;
+        y = q.top();
+        q.pop();
+        x = q.top();
+        q.pop();
+        p = q.top();
+        q.pop();
+        double cnt = p * (x + y) + (1 - p) * (x - y);
+        q.push(cnt);
+        i++;
+      }
     }
-    return 0;
+    double ans = q.top();
+    printf("%.2lf\n", ans);
+  }
+  return 0;
 }
-
-
-
