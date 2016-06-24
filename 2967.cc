b6abb0506156b77b697ae13b29719c79f0401bd3
sdecoder <sde.work.inbox@gmail.com>
2016-06-24 06:34:22 -0400

    2016-06-24 06:34:22

--- 2967.cc
+++ 2967.cc
@@ -10,52 +10,37 @@ using namespace std;
 int n;
 int f[maxn];
 
-int in()
-{
-    char ch = '0';
-    int ret = 0;
-    while (ch <= '9' && ch >= '0')
-    {
-        ret = ret * 10 + ch - '0';
-        ch = getchar();
-    }
-    return ret;
+int in() {
+  char ch = '0';
+  int ret = 0;
+  while (ch <= '9' && ch >= '0') {
+    ret = ret * 10 + ch - '0';
+    ch = getchar();
+  }
+  return ret;
 }
 
-void input()
-{
-    n = in();
-    for (int i = 0; i < n; i++)
-        f[i] = in();
+void input() {
+  n = in();
+  for (int i = 0; i < n; i++) f[i] = in();
 }
 
-bool ok(int a, int b, int c)
-{
-    return a > c - b;
+bool ok(int a, int b, int c) { return a > c - b; }
+
+bool work() {
+  if (n <= 3) return false;
+  if (ok(f[0], f[1], f[n - 1])) return false;
+  for (int i = 0; i < n - 2; i++)
+    if (ok(f[i], f[i + 1], f[i + 2])) return true;
+  return false;
 }
 
-bool work()
-{
-    if (n <= 3)
-        return false;
-    if (ok(f[0], f[1], f[n - 1]))
-        return false;
-    for (int i = 0; i < n - 2; i++)
-        if (ok(f[i], f[i + 1], f[i + 2]))
-            return true;
-    return false;
+int main() {
+  input();
+  sort(f, f + n);
+  if (work())
+    printf("The set is accepted.\n");
+  else
+    printf("The set is rejected.\n");
+  return 0;
 }
-
-int main()
-{
-    input();
-    sort(f, f + n);
-    if (work())
-        printf("The set is accepted.\n");
-    else
-        printf("The set is rejected.\n");
-    return 0;
-}
-
-
-
