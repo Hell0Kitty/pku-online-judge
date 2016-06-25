32709da7ba286963e0e60d12c2ece3bde22ef6ff
sdecoder <sde.work.inbox@gmail.com>
2016-06-25 04:32:19 -0400

    2016-06-25 04:32:19

--- 2508.cc
+++ 2508.cc
@@ -1,44 +1,34 @@
-#include<cstdio>
+#include <cstdio>
 
-#include<cstring>
+#include <cstring>
 
-#include<algorithm>
+#include <algorithm>
 
-#include<cmath>
+#include <cmath>
 
 using namespace std;
 
-const double eps=1e-8,PI=acos(-1.0);
+const double eps = 1e-8, PI = acos(-1.0);
 
 int main()
 
 {
+  double r, h, l, p1, p2, th1, th2, dt;
 
-    double r,h,l,p1,p2,th1,th2,dt;
+  while (scanf("%lf%lf%lf%lf%lf%lf", &r, &h, &p1, &th1, &p2, &th2) != EOF)
 
-    while(scanf("%lf%lf%lf%lf%lf%lf",&r,&h,&p1,&th1,&p2,&th2)!=EOF)
+  {
+    dt = fabs((th1 - th2) / 180.0) * PI;
 
-    {
+    while (dt > 2 * PI + eps) dt -= 2 * PI;
 
-        dt=fabs((th1-th2)/180.0)*PI;
+    if (dt > PI + eps) dt = 2 * PI - dt;
 
-        while(dt>2*PI+eps)
+    l = sqrt(h * h + r * r);
 
-            dt-=2*PI;
+    printf("%.2lf\n",
+           sqrt(p1 * p1 + p2 * p2 - 2.0 * p1 * p2 * cos(dt * r / l)));
+  }
 
-        if(dt>PI+eps)
-
-            dt=2*PI-dt;
-
-        l=sqrt(h*h+r*r);
-
-        printf("%.2lf\n",sqrt(p1*p1+p2*p2-2.0*p1*p2*cos(dt*r/l)));
-
-    }
-
-    return 0;
-
+  return 0;
 }
-
-
-
