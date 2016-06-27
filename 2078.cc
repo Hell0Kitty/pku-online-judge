4c80be22af9b0d1b270227a8df185ce2d65e04e5
sdecoder <sde.work.inbox@gmail.com>
2016-06-27 11:40:22 -0400

    2016-06-27 11:40:22

--- 2078.cc
+++ 2078.cc
@@ -10,56 +10,41 @@ int n;
 int f[maxn][maxn];
 int shift[maxn];
 
-void input()
-{
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            scanf("%d", &f[i][j]);
+void input() {
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++) scanf("%d", &f[i][j]);
 }
 
-bool make()
-{
-    int i = 0;
-    while (i < n - 1)
-    {
-        shift[i] = (shift[i] + 1) % n;
-        if (shift[i] != 0)
-            return true;
-        i++;
+bool make() {
+  int i = 0;
+  while (i < n - 1) {
+    shift[i] = (shift[i] + 1) % n;
+    if (shift[i] != 0) return true;
+    i++;
+  }
+  return false;
+}
+
+void work() {
+  memset(shift, 0, sizeof(shift));
+  int ans = 0x3f3f3f3f;
+  while (make()) {
+    int temp = 0;
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for (int j = 0; j < n; j++) sum += f[j][(i + shift[j]) % n];
+      temp = max(temp, sum);
+      if (temp > ans) break;
     }
-    return false;
+    ans = min(ans, temp);
+  }
+  printf("%d\n", ans);
 }
 
-void work()
-{
-    memset(shift, 0, sizeof(shift));
-    int ans = 0x3f3f3f3f;
-    while (make())
-    {
-        int temp = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int sum = 0;
-            for (int j = 0; j < n; j++)
-                sum += f[j][(i + shift[j]) % n];
-            temp = max(temp, sum);
-            if (temp > ans)
-                break;
-        }
-        ans = min(ans, temp);
-    }
-    printf("%d\n", ans);
+int main() {
+  while (scanf("%d", &n), ~n) {
+    input();
+    work();
+  }
+  return 0;
 }
-
-int main()
-{
-    while (scanf("%d", &n), ~n)
-    {
-        input();
-        work();
-    }
-    return 0;
-}
-
-
-
