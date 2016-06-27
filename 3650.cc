ebb0edf0024cf4358522a94a635c741e963b9c9a
sdecoder <sde.work.inbox@gmail.com>
2016-06-27 04:16:58 -0400

    2016-06-27 04:16:58

--- 3650.cc
+++ 3650.cc
@@ -2,36 +2,31 @@
 #include <string>
 using namespace std;
 
-int main()
-{
-    string input, ans;
-    int i, len;
-    while (getline(cin, input))
-    {
-          if (input == "#")  break;
-          ans.clear();
-          len = input.length();
-          for (i = 0; i < len; i++)
-          {
-              if (input[i] == ' ')
-                 ans += "%20";
-              else if (input[i] == '!')
-                 ans += "%21";
-              else if (input[i] == '$')
-                 ans += "%24";
-              else if (input[i] == '%')
-                 ans += "%25";
-              else if (input[i] == '(')
-                 ans += "%28";
-              else if (input[i] == ')')
-                 ans += "%29";
-              else if (input[i] == '*')
-                 ans += "%2a";
-              else 
-                 ans.push_back(input[i]);
-          } 
-          cout << ans << endl;
+int main() {
+  string input, ans;
+  int i, len;
+  while (getline(cin, input)) {
+    if (input == "#") break;
+    ans.clear();
+    len = input.length();
+    for (i = 0; i < len; i++) {
+      if (input[i] == ' ')
+        ans += "%20";
+      else if (input[i] == '!')
+        ans += "%21";
+      else if (input[i] == '$')
+        ans += "%24";
+      else if (input[i] == '%')
+        ans += "%25";
+      else if (input[i] == '(')
+        ans += "%28";
+      else if (input[i] == ')')
+        ans += "%29";
+      else if (input[i] == '*')
+        ans += "%2a";
+      else
+        ans.push_back(input[i]);
     }
-
+    cout << ans << endl;
+  }
 }
-
