4829264f5e19a60afdaed37f26fa563fb6ba5622
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 07:49:30 -0400

    2016-07-04 07:49:30

---
+++ 2291.cc
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+int comp(const void *a,const void *b)
+{
+    return *(int *)a-*(int *)b;
+}
+int main()
+{
+    int n,m,i,max;
+
+    while(scanf("%d",&n)!=EOF)
+    {
+        for(; n>0; n--)
+        {
+            scanf("%d",&m);
+            int shu[m];
+            for(i=0; i<m; i++)
+            {
+                scanf("%d",&shu[i]);
+            }
+            qsort(shu,m,sizeof(int),comp);
+            max=shu[0]*m;
+            for(i=1; i<m; i++)
+            {
+                if(shu[i]*(m-i)>max)
+                {
+                    max=shu[i]*(m-i);
+                }
+            }
+            printf("%d\n",max);
+
+        }
+
+    }
+    return(0);
+}
+
+
+
