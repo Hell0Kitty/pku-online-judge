e6e464c1737d0492501c58b407b0888cfff3205d
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 03:24:31 -0400

    2016-07-04 03:24:31

--- 2605.cc
+++ 2605.cc
@@ -1,19 +1,15 @@
-#include<stdio.h>
-#include<iostream>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
-main()
-{
-    int M,N;
-    while(scanf("%d%d",&M,&N)!=EOF)
-    {
-        if (M==1||N==1) cout<<(M+N)/2<<endl;
+main() {
+  int M, N;
+  while (scanf("%d%d", &M, &N) != EOF) {
+    if (M == 1 || N == 1)
+      cout << (M + N) / 2 << endl;
 
-          else cout<<((M*N)%3?1:2)<<endl;
-    }
+    else
+      cout << ((M * N) % 3 ? 1 : 2) << endl;
+  }
 
-          return 0;
-
+  return 0;
 }
-
-
-
