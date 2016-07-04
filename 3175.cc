45c8c34c77e200b56dea8bb8c5f36aeb211f30c9
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 12:51:05 -0400

    2016-07-04 12:51:05

--- 3175.cc
+++ 3175.cc
@@ -1,26 +1,25 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 #define eps 1e-8
 
-double a[11]={1,1e-1,1e-2,1e-3,1e-4,1e-5,1e-6,1e-7,1e-8,1e-9,1e-10};
+double a[11] = {1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10};
 
-int main(){
-	int n;
-	int m;
-	double p,q,x,y;
-	while(scanf("%d",&n)!=EOF){
-		scanf("%d",&m);
-		p=m*a[n];  q=(m+1)*a[n];
-		x=y=0;
-		while(x==y){
-			p+=1; q+=1;
-			x=floor(p*p);  y=floor(q*q-eps);
-		}
-		printf("%.f\n",floor(y));
-	}
-	return 0;
+int main() {
+  int n;
+  int m;
+  double p, q, x, y;
+  while (scanf("%d", &n) != EOF) {
+    scanf("%d", &m);
+    p = m * a[n];
+    q = (m + 1) * a[n];
+    x = y = 0;
+    while (x == y) {
+      p += 1;
+      q += 1;
+      x = floor(p * p);
+      y = floor(q * q - eps);
+    }
+    printf("%.f\n", floor(y));
+  }
+  return 0;
 }
-
-
-
-
