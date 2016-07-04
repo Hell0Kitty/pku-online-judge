d941c76e0e727f73d90d1af95182c3b14f5f0072
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 10:18:29 -0400

    2016-07-04 10:18:29

---
+++ 3364.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+    int n,m,i;
+    while(scanf("%d%d%d",&n,&m,&i))
+    {
+        if(n == 0 && m == 0 && i == 0)
+            break;
+        n = n-7;
+        m = m-7;
+        if((n*m)%2 == 0)
+        {
+            cout << (n*m)/2 <<endl;
+        }
+        else
+        {
+            if(i == 1)
+            {
+                cout << (n*m)/2 +1 <<endl;
+            }
+            if(i == 0)
+            {
+                cout << (n*m)/2 <<endl;
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
