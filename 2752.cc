72ab92a47f8604474210d3b4e86e9458aa56029e
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 21:38:02 -0400

    2016-07-04 21:38:02

--- 2752.cc
+++ 2752.cc
@@ -1,50 +1,49 @@
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<map>
-#include<set>
-#include<queue>
-#include<string>
-#include<cstring>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <queue>
+#include <string>
+#include <cstring>
 using namespace std;
 
-const int Maxn=400010;
+const int Maxn = 400010;
 
 string pattern;
 int plen;
 
 int nxt[Maxn];
 
-inline void getNxt(){
-	memset(nxt,0,sizeof(nxt));
-	int i=0,j=-1;
-	nxt[0]=-1;
-	while(i<plen){
-		if (j==-1 || pattern[i]==pattern[j]){
-			i++;j++;
-			nxt[i]=j;
-		}else{
-			j=nxt[j];
-		}
-	}
+inline void getNxt() {
+  memset(nxt, 0, sizeof(nxt));
+  int i = 0, j = -1;
+  nxt[0] = -1;
+  while (i < plen) {
+    if (j == -1 || pattern[i] == pattern[j]) {
+      i++;
+      j++;
+      nxt[i] = j;
+    } else {
+      j = nxt[j];
+    }
+  }
 }
 
 vector<int> Ans;
-int main(){
-	while(cin>>pattern){
-		plen=pattern.length();
-		getNxt();
-		Ans.clear();
-		int k=plen;
-		while(k>0){
-			Ans.push_back(k);
-			k=nxt[k];
-		}
-		sort(Ans.begin(),Ans.end());
-		for (int i=0;i<Ans.size();i++) printf("%d ",Ans[i]);
-		printf("\n");
-	}
-	return 0;
+int main() {
+  while (cin >> pattern) {
+    plen = pattern.length();
+    getNxt();
+    Ans.clear();
+    int k = plen;
+    while (k > 0) {
+      Ans.push_back(k);
+      k = nxt[k];
+    }
+    sort(Ans.begin(), Ans.end());
+    for (int i = 0; i < Ans.size(); i++) printf("%d ", Ans[i]);
+    printf("\n");
+  }
+  return 0;
 }
-
-
