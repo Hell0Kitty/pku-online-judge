67b646bb7d364e9b1e4a90a8e7039d24effe882c
sdecoder <sde.work.inbox@gmail.com>
2016-07-04 21:03:29 -0400

    2016-07-04 21:03:29

--- 3183.cc
+++ 3183.cc
@@ -3,27 +3,20 @@ using namespace std;
 
 int h[50050];
 
-int main()
-{
-    int n, i;
-    cin >> n;
-    for (i = 1; i <= n; i++){
-        cin >> h[i];
+int main() {
+  int n, i;
+  cin >> n;
+  for (i = 1; i <= n; i++) {
+    cin >> h[i];
+  }
+  if (h[1] >= h[2])  //边界的处理
+    cout << 1 << endl;
+  for (i = 2; i <= n; i++) {
+    if (i == n) {
+      if (h[i] >= h[i - 1]) cout << i << endl;
+    } else {
+      if (h[i] >= h[i - 1] && h[i] >= h[i + 1]) cout << i << endl;
     }
-    if (h[1] >= h[2])//边界的处理 
-       cout << 1 << endl;
-    for (i = 2; i <= n; i++){
-        if (i == n){
-              if (h[i] >= h[i-1])
-                   cout << i << endl;
-        }
-        else{ 
-              if (h[i] >= h[i-1] && h[i] >= h[i+1])
-                  cout << i << endl;
-           }
-    }
-    return 0;
+  }
+  return 0;
 }
-
-
-
