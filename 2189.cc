06d18cebd3946cb6ebd3544f64d7489e1c0f1a7a
sdecoder <sde.work.inbox@gmail.com>
2016-07-08 17:10:06 -0400

    2016-07-08 17:10:06

--- 2189.cc
+++ 2189.cc
@@ -2,31 +2,23 @@
 #include <cstdio>
 #include <cstring>
 using namespace std;
-const int maxn=1e3+9;
+const int maxn = 1e3 + 9;
 int a[maxn];
 int ans;
-int main()
-{ 
-
-int n,p,c;
-    scanf("%d %d %d",&n,&p,&c);
-    for(int i=1,tmp;i<=n;i++)
-    {
-        scanf("%d",&tmp);
-        a[tmp]++;
+int main() {
+  int n, p, c;
+  scanf("%d %d %d", &n, &p, &c);
+  for (int i = 1, tmp; i <= n; i++) {
+    scanf("%d", &tmp);
+    a[tmp]++;
+  }
+  for (int i = 1; i < p; i++) {
+    int tmp = 0, j = i;
+    while (j < p && tmp + a[j] <= c) {
+      tmp += a[j++];
     }
-    for(int i=1;i<p;i++)
-    {
-        int tmp=0,j=i;
-        while(j<p&&tmp+a[j]<=c)
-        {
-            tmp+=a[j++];
-        }
-        ans=max(ans,j-i);
-    }
-    printf("%d\n",ans);
-    return 0;
+    ans = max(ans, j - i);
+  }
+  printf("%d\n", ans);
+  return 0;
 }
-
-
-
