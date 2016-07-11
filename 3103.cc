b5f06e8eff167ab6f3a9b789f3b34ab07ffe2b49
sdecoder <sde.work.inbox@gmail.com>
2016-07-10 21:41:41 -0400

    2016-07-10 21:41:41

--- 3103.cc
+++ 3103.cc
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int x,y,z,n;
-	scanf("%d%d%d%d",&x,&y,&z,&n);
-	for(int i=0;i<n;++i)
-		printf("0 0 %.8lf %d %d %.8lf\n",(z*1.0/n)*i,x,y,(z*1.0/n)*(i+1));
-	return 0;	
+int main() {
+  int x, y, z, n;
+  scanf("%d%d%d%d", &x, &y, &z, &n);
+  for (int i = 0; i < n; ++i)
+    printf("0 0 %.8lf %d %d %.8lf\n", (z * 1.0 / n) * i, x, y,
+           (z * 1.0 / n) * (i + 1));
+  return 0;
 }
-
-
