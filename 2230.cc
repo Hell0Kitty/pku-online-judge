73d6d46e557ff3aa594e0180dc798e205f5bb2a7
sdecoder <sde.work.inbox@gmail.com>
2016-07-12 10:20:10 -0400

    2016-07-12 10:20:10

---
+++ 2230.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+#define maxn 10005
+#define maxm 100005
+
+struct Edge
+{
+    int v, next;
+}edge[maxm];
+
+int n, m;
+int head[maxn];
+int ecount;
+bool vis[maxm];
+
+void addedge(int a, int b)
+{
+    edge[ecount].v = b;
+    edge[ecount].next = head[a];
+    head[a] = ecount++;
+}
+
+void dfs(int a)
+{
+    for (int i = head[a]; i != -1; i = edge[i].next)
+    {
+        if (vis[i])
+            continue;
+        int v= edge[i].v;
+        vis[i] = true;
+        dfs(v);
+    }
+    printf("%d\n", a + 1);
+}
+
+int main()
+{
+    memset(head, -1, sizeof(head));
+    ecount = 0;
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        a--;
+        b--;
+        addedge(a, b);
+        addedge(b, a);
+    }
+    memset(vis, 0, sizeof(vis));
+    dfs(0);
+    return 0;
+}
+
+
+
