79fbce0b31fb4dd7d996acbba7d75ffcf7b9b440
sdecoder <sde.work.inbox@gmail.com>
2016-07-16 04:24:43 -0400

    2016-07-16 04:24:43

--- 3040.cc
+++ 3040.cc
@@ -4,56 +4,48 @@
 #include <algorithm>
 using namespace std;
 
-struct Coin{
-    int value, amount;
-    bool operator < (const Coin &t)const{
-        return value < t.value;
-    }
-}coin[22];
+struct Coin {
+  int value, amount;
+  bool operator<(const Coin &t) const { return value < t.value; }
+} coin[22];
 
 int need[22];
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    int n, c;
-    cin >> n >> c;
-    for (int i = 0; i < n; ++i)
-        cin >> coin[i].value >> coin[i].amount;
-    sort(coin, coin + n);
-    int ans = 0;
-    while (1){
-        int rest = c;
-        memset(need, 0, sizeof need);
-        for (int i = n - 1; i >= 0; --i){
-            if (coin[i].amount > 0){
-                int require = min(coin[i].amount, rest / coin[i].value);
-                rest -= require * coin[i].value;
-                need[i] = require;
-            }
-            if (rest == 0) break;
-        }
-        if (rest > 0){
-            for (int i = 0; i < n; ++i){
-                if (coin[i].amount > 0 && coin[i].value >= rest){
-                    rest = 0;
-                    ++need[i];
-                    break;
-                }
-            }
-        }
-        if (rest > 0) break;
-        int week = 0x3f3f3f3f;
-        for (int i = 0; i < n; ++i)
-            if (need[i])
-                week = min(week, coin[i].amount / need[i]);
-        for (int i = 0; i < n; ++i)
-            if (need[i])
-                coin[i].amount -= week * need[i];
-        ans += week;
+  ios_base::sync_with_stdio(0);
+  int n, c;
+  cin >> n >> c;
+  for (int i = 0; i < n; ++i) cin >> coin[i].value >> coin[i].amount;
+  sort(coin, coin + n);
+  int ans = 0;
+  while (1) {
+    int rest = c;
+    memset(need, 0, sizeof need);
+    for (int i = n - 1; i >= 0; --i) {
+      if (coin[i].amount > 0) {
+        int require = min(coin[i].amount, rest / coin[i].value);
+        rest -= require * coin[i].value;
+        need[i] = require;
+      }
+      if (rest == 0) break;
     }
-    cout << ans << '\n';
-    return 0;
+    if (rest > 0) {
+      for (int i = 0; i < n; ++i) {
+        if (coin[i].amount > 0 && coin[i].value >= rest) {
+          rest = 0;
+          ++need[i];
+          break;
+        }
+      }
+    }
+    if (rest > 0) break;
+    int week = 0x3f3f3f3f;
+    for (int i = 0; i < n; ++i)
+      if (need[i]) week = min(week, coin[i].amount / need[i]);
+    for (int i = 0; i < n; ++i)
+      if (need[i]) coin[i].amount -= week * need[i];
+    ans += week;
+  }
+  cout << ans << '\n';
+  return 0;
 }
-
-
-
