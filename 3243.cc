cd144ff8563963928082c8519a990f3255c700d7
sdecoder <sde.work.inbox@gmail.com>
2016-07-16 01:46:12 -0400

    2016-07-16 01:46:12

--- 3243.cc
+++ 3243.cc
@@ -7,169 +7,136 @@ typedef long long INT;
 INT nData[MAX];
 INT nKey[MAX];
 
-INT HashPos(INT key)
-{
-    return ((unsigned)(key ^ 0xA5A5A5A5)) % MAX;
+INT HashPos(INT key) { return ((unsigned)(key ^ 0xA5A5A5A5)) % MAX; }
+
+void HashAdd(INT key, INT data) {
+  INT nPos = HashPos(key);
+  while (nData[nPos] != -1) {
+    nPos = (nPos + 1) % MAX;
+  }
+  nData[nPos] = data;
+  nKey[nPos] = key;
 }
 
-void HashAdd(INT key, INT data)
-{
-    INT nPos = HashPos(key);
-    while (nData[nPos] != -1)
-    {
-        nPos = (nPos + 1) % MAX;
+INT HashQuery(INT key) {
+  INT nPos = HashPos(key);
+  while (nData[nPos] != -1) {
+    if (nKey[nPos] == key) {
+      return nData[nPos];
     }
-    nData[nPos] = data;
-    nKey[nPos] = key;
+    nPos = (nPos + 1) % MAX;
+  }
+
+  return -1;
 }
 
-INT HashQuery(INT key)
-{
-    INT nPos = HashPos(key);
-    while (nData[nPos] != -1)
-    {
-        if (nKey[nPos] == key)
-        {
-            return nData[nPos];
-        }
-        nPos = (nPos + 1) % MAX;
+INT MultMod(INT nA, INT nB, INT nC) {
+  INT nAns = 0;
+  while (nB) {
+    if (nB & 1) {
+      nAns = (nAns + nA) % nC;
     }
-
-    return -1;
+    nA = (2 * nA) % nC;
+    nB >>= 1;
+  }
+  return nAns;
 }
 
-INT MultMod(INT nA, INT nB, INT nC)
-{
-    INT nAns = 0;
-    while (nB)
-    {
-        if (nB & 1)
-        {
-            nAns = (nAns + nA) % nC;
-        }
-        nA = (2 * nA) % nC;
-        nB >>= 1;
+INT PowerMod(INT nA, INT nX, INT nC) {
+  INT nAns = 1;
+  nA %= nC;
+  while (nX) {
+    if (nX & 1) {
+      nAns = MultMod(nAns, nA, nC);
     }
-    return nAns;
+    nA = MultMod(nA, nA, nC);
+    nX >>= 1;
+  }
+  return nAns;
 }
 
-INT PowerMod(INT nA, INT nX, INT nC)
-{
-    INT nAns = 1;
-    nA %= nC;
-    while (nX)
-    {
-        if (nX & 1)
-        {
-            nAns = MultMod(nAns, nA, nC); 
-        }
-        nA = MultMod(nA, nA, nC);
-        nX >>= 1;
-    }
-    return nAns;
+INT gcd(INT nA, INT nB) {
+  if (nA < nB) swap(nA, nB);
+  while (nB) {
+    INT nT = nA;
+    nA = nB;
+    nB = nT % nB;
+  }
+  return nA;
 }
 
-INT gcd(INT nA, INT nB)
-{
-    if (nA < nB)swap(nA, nB);
-    while (nB)
-    {
-        INT nT = nA;
-        nA = nB;
-        nB = nT % nB;
-    }
+INT egcd(INT nA, INT nB, INT& nX, INT& nY) {
+  if (nA < nB) swap(nA, nB);
+  if (nB == 0) {
+    nX = 1;
+    nY = 0;
     return nA;
+  }
+  INT nRet = egcd(nB, nA % nB, nX, nY);
+  INT nT = nX;
+  nX = nY;
+  nY = nT - (nA / nB) * nY;
+  return nRet;
 }
 
-INT egcd(INT nA, INT nB, INT& nX, INT& nY)
-{
-    if (nA < nB)swap(nA, nB);
-    if (nB == 0)
-    {
-        nX = 1;
-        nY = 0;
-        return nA;
+INT GetAns(INT nA, INT nB, INT nC) {
+  if (nC == 0) return -1;
+
+  INT nTemp = 1;
+  nB %= nC;
+  for (INT i = 0; i <= 50; ++i) {
+    if (nTemp == nB) {
+      return i;
     }
-    INT nRet = egcd(nB, nA % nB, nX, nY);
-    INT nT = nX;
-    nX = nY;
-    nY = nT - (nA / nB) * nY;
-    return nRet;
+    nTemp = MultMod(nTemp, nA, nC);
+  }
+
+  INT d;
+  INT nD = 1;  // nD最后是A^k次,k是nC中因子d的次数
+  INT k = 0;
+  while ((d = gcd(nC, nA)) != 1) {
+    k++;
+    nC /= d;
+    if (nB % d) return -1;
+    nB /= d;
+    nD = MultMod(nD, nA / d, nC);
+  }
+
+  memset(nKey, -1, sizeof(nKey));
+  memset(nData, -1, sizeof(nData));
+  INT nM = ceil(sqrt(1.0 * nC));
+  nTemp = 1;
+  for (INT j = 0; j <= nM; ++j) {
+    HashAdd(nTemp, j);
+    nTemp = MultMod(nTemp, nA, nC);
+  }
+  INT nK = PowerMod(nA, nM, nC);
+  for (int i = 0; i <= nM; ++i) {
+    INT x, y;
+    egcd(nC, nD, x, y);
+    y = (y + nC) % nC;
+    INT nR = MultMod(y, nB, nC);
+    int j = HashQuery(nR);
+    if (j != -1) {
+      return nM * i + j + k;
+    }
+
+    nD = MultMod(nD, nK, nC);
+  }
+  return -1;
 }
 
-INT GetAns(INT nA, INT nB, INT nC)
-{
-    if (nC == 0)return -1;
+int main() {
+  INT nA, nB, nC;
 
-    INT nTemp = 1;
-    nB %= nC;
-    for (INT i = 0; i <= 50; ++i)
-    {
-        if (nTemp == nB)
-        {
-            return i;
-        }
-        nTemp = MultMod(nTemp, nA, nC);
+  while (scanf("%I64d%I64d%I64d", &nA, &nC, &nB), nA + nB + nC) {
+    INT nAns = GetAns(nA, nB, nC);
+    if (nAns == -1) {
+      printf("No Solution\n");
+    } else {
+      printf("%I64d\n", nAns);
     }
+  }
 
-    INT d;
-    INT nD = 1;//nD最后是A^k次,k是nC中因子d的次数
-    INT k = 0;
-    while ((d = gcd(nC, nA)) != 1)
-    {
-        k++;
-        nC /= d;
-        if (nB % d)return -1;
-        nB /= d;
-        nD = MultMod(nD, nA / d, nC);
-    }
-
-    memset(nKey, -1, sizeof(nKey));
-    memset(nData, -1, sizeof(nData));
-    INT nM = ceil(sqrt(1.0 * nC));
-    nTemp = 1;
-    for (INT j = 0; j <= nM; ++j)
-    {
-        HashAdd(nTemp, j);
-        nTemp = MultMod(nTemp, nA, nC);
-    }
-    INT nK = PowerMod(nA, nM, nC);
-    for (int i = 0; i <= nM; ++i)
-    {
-        INT x, y;
-        egcd(nC, nD, x, y);
-        y = (y + nC) % nC;
-        INT nR = MultMod(y, nB, nC);
-        int j = HashQuery(nR);
-        if (j != -1)
-        {
-            return nM * i + j + k;
-        }
-
-        nD = MultMod(nD, nK, nC);
-    }
-    return -1;
+  return 0;
 }
-
-int main()
-{
-    INT nA, nB, nC;
-
-    while (scanf("%I64d%I64d%I64d", &nA, &nC, &nB), nA + nB + nC)
-    {
-        INT nAns = GetAns(nA, nB, nC);
-        if (nAns == -1)
-        {
-            printf("No Solution\n");
-        }
-        else
-        {
-            printf("%I64d\n", nAns);
-        }
-    }
-
-    return 0;
-}
-
-
-
