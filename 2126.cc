e138a7b1574ee50a6b7ecccb1a366ce72a599fdc
sdecoder <sde.work.inbox@gmail.com>
2016-07-19 11:16:11 -0400

    2016-07-19 11:16:11

---
+++ 2126.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    if (n <= 1)
+    {
+        printf("YES\n");
+        return 0;
+    }
+    if (n > 2)
+    {
+        printf("NO\n");
+        return 0;
+    }
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    if (b * b - 4 * a * c >= 0)
+        printf("NO\n");
+    else
+        printf("YES\n");
+    return 0;
+}
+
+
+
