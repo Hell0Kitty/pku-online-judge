66b4569408c4c95efff828f77a01383bd606a465
sdecoder <sde.work.inbox@gmail.com>
2016-07-18 20:04:40 -0400

    2016-07-18 20:04:40

--- 2205.cc
+++ 2205.cc
@@ -7,72 +7,54 @@
 #include <algorithm>
 using namespace std;
 
-struct Node
-{
-    char self[2005];
+struct Node {
+  char self[2005];
 };
 Node p[1000];
 int a[2005];
 int m;
-int b,n;
+int b, n;
 
-char Change(int x)
-{
-    if (x<=9)
-        return '0'+x;
-    else
-        return 'A'+x-10;
+char Change(int x) {
+  if (x <= 9)
+    return '0' + x;
+  else
+    return 'A' + x - 10;
 }
 //从低位往高位搜索
-void dfs(int dep,int sum)
-{
-    if(dep>n)
-    {
-        if(a[n]>0 || n == 1)
-        {
-            for(int i=n; i>=1; i--)
-            {
-                p[m].self[n-i] = Change(a[i]);
-            }
-            p[m].self[n] = '\0';
-            m++;
-        }
-        return;
+void dfs(int dep, int sum) {
+  if (dep > n) {
+    if (a[n] > 0 || n == 1) {
+      for (int i = n; i >= 1; i--) {
+        p[m].self[n - i] = Change(a[i]);
+      }
+      p[m].self[n] = '\0';
+      m++;
     }
-    int tol = 0;
-    for(a[dep]=0; a[dep]<b; a[dep]++)
-    {
-        tol = 0;
-        for(int i=1; i<=dep; i++)
-        {
-            int j = dep-i+1;
-            tol += a[i]*a[j];
-        }
-        if((tol+sum)%b == a[dep])
-        {
-            dfs(dep+1,(tol+sum)/b);
-        }
+    return;
+  }
+  int tol = 0;
+  for (a[dep] = 0; a[dep] < b; a[dep]++) {
+    tol = 0;
+    for (int i = 1; i <= dep; i++) {
+      int j = dep - i + 1;
+      tol += a[i] * a[j];
     }
-}
-bool cmp(Node a,Node b)
-{
-    return strcmp(a.self,b.self)<0;
-}
-int main()
-{
-    while(scanf(" %d %d",&b,&n)!=EOF)
-    {
-        m = 0;
-        dfs(1,0);
-        sort(p,p+m,cmp);
-        printf("%d\n",m);
-        for(int i=0; i<m; i++)
-        {
-            printf("%s\n",p[i].self);
-        }
+    if ((tol + sum) % b == a[dep]) {
+      dfs(dep + 1, (tol + sum) / b);
     }
-    return 0;
+  }
 }
-
-
-
+bool cmp(Node a, Node b) { return strcmp(a.self, b.self) < 0; }
+int main() {
+  while (scanf(" %d %d", &b, &n) != EOF) {
+    m = 0;
+    dfs(1, 0);
+    sort(p, p + m, cmp);
+    printf("%d\n", m);
+    for (int i = 0; i < m; i++) {
+      printf("%s\n", p[i].self);
+    }
+  }
+  return 0;
+}
