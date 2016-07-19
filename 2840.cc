20eeb5de5ecc21f9c9477fda3bc15c09d6563d54
sdecoder <sde.work.inbox@gmail.com>
2016-07-18 23:45:16 -0400

    2016-07-18 23:45:16

--- 2840.cc
+++ 2840.cc
@@ -4,13 +4,12 @@ using namespace std;
 int T, h, m;
 
 int main() {
-	for (scanf("%d", &T); T; --T) {
-		scanf("%d:%d", &h, &m);
-		if (!m) printf("%d\n", (h + 11) % 24 + 1);
-		else putchar('0'), putchar('\n');
-	}
-	return 0;
+  for (scanf("%d", &T); T; --T) {
+    scanf("%d:%d", &h, &m);
+    if (!m)
+      printf("%d\n", (h + 11) % 24 + 1);
+    else
+      putchar('0'), putchar('\n');
+  }
+  return 0;
 }
-
-
-
