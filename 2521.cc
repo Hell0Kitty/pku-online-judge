6bd6b9dab2305e5f2bbe9c5a0c94c16e5a73579e
sdecoder <sde.work.inbox@gmail.com>
2016-07-20 16:49:39 -0400

    2016-07-20 16:49:39

---
+++ 2521.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+      int m, p, n, c;
+    while (scanf("%d%d%d%d", &n, &m, &p, &c) != EOF && !(n == 0 && m == 0 && p == 0 && c == 0))
+        printf("%d\n", -(m - n - p));
+    return 0;
+}
+
+
+
