652c1673e73f94128fe7f66036705e6760ab25cf
sdecoder <sde.work.inbox@gmail.com>
2016-07-21 14:07:29 -0400

    2016-07-21 14:07:29

--- 2864.cc
+++ 2864.cc
@@ -4,48 +4,34 @@ using namespace std;
 
 int _m[MAXN][MAXN];
 
-int main()
-{
-    int c;
-    int r;
-    int i;
-    int j;
-    int sum;
+int main() {
+  int c;
+  int r;
+  int i;
+  int j;
+  int sum;
 
-    while(cin>>c>>r,c||r)
-    {
+  while (cin >> c >> r, c || r) {
+    for (i = 0; i < r; ++i) {
+      for (j = 0; j < c; ++j) {
+        cin >> _m[i][j];
+      }
+    }
 
-    for(i = 0; i < r; ++ i)
-    {
-        for(j = 0; j < c; ++ j)
-        {
-            cin>>_m[i][j];
+    for (j = 0; j < c; ++j) {
+      sum = 0;
+      for (i = 0; i < r; ++i) {
+        if (_m[i][j] == 0) {
+          break;
         }
+      }
+      if (i == r) {
+        cout << "yes" << endl;
+        break;
+      }
     }
-
-    for(j = 0; j < c; ++ j)
-    {
-        sum = 0;
-        for(i = 0; i < r; ++ i)
-        {
-            if(_m[i][j] == 0)
-            {
-                break;
-            }
-        }
-        if(i == r)
-        {
-            cout<<"yes"<<endl;
-            break;
-        }
+    if (j == c) {
+      cout << "no" << endl;
     }
-    if(j == c)
-    {
-        cout<<"no"<<endl;
-    }
-    }
-
+  }
 }
-
-
-
