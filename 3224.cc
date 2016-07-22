20e181e36a60c94b9795ff16baf22e2434cab46e
sdecoder <sde.work.inbox@gmail.com>
2016-07-22 10:01:56 -0400

    2016-07-22 10:01:56

--- 3224.cc
+++ 3224.cc
@@ -5,27 +5,22 @@ using namespace std;
 int matrix[110][110];
 int ans[110];
 
-int main()
-{
-    int n, i, j, max = 0, cur;
-    cin >> n;
-    memset(ans, 0, sizeof(ans));
-    for (i = 1; i <= n; i++)
-       for (j = 1; j <= n; j++){
-        cin >> matrix[i][j];
-        if (matrix[i][j] == 3)
-            ans[i]++;
+int main() {
+  int n, i, j, max = 0, cur;
+  cin >> n;
+  memset(ans, 0, sizeof(ans));
+  for (i = 1; i <= n; i++)
+    for (j = 1; j <= n; j++) {
+      cin >> matrix[i][j];
+      if (matrix[i][j] == 3) ans[i]++;
     }
-    for (i = 1; i <= n; i++){
-        if (ans[i] > max){
-            max = ans[i];
-            cur = i;
-        }
-    } 
-    cout << cur << endl;
+  for (i = 1; i <= n; i++) {
+    if (ans[i] > max) {
+      max = ans[i];
+      cur = i;
+    }
+  }
+  cout << cur << endl;
 
-   return 0;
+  return 0;
 }
-
-
-
