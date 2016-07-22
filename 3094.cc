f2598784170c8eea666a3efd95c1a2cc988a4925
sdecoder <sde.work.inbox@gmail.com>
2016-07-22 10:37:15 -0400

    2016-07-22 10:37:15

---
+++ 3094.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio> 
+using namespace std;  
+int main(){  
+    char str[256];  
+    long sum=0;  
+    while (gets(str)){
+        if(str[0] == '#') break;  
+        for (int i=0;i<strlen(str);i++){
+            if (str[i] == ' ')
+                continue;
+            sum += (i+1)*(str[i] - 'A' + 1);  
+        }
+        cout << sum << endl;
+        sum = 0;  
+    }
+    return 0;  
+}
+
+
+
