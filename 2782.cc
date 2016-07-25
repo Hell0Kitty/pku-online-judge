0cd2ca43da68e34a6b3db53cb08346c6b0321032
sdecoder <sde.work.inbox@gmail.com>
2016-07-25 08:26:45 -0400

    2016-07-25 08:26:45

--- 2782.cc
+++ 2782.cc
@@ -5,37 +5,26 @@
 using namespace std;
 
 int w[100010];
-bool cmp(int a, int b)
-{
-	return a > b;
+bool cmp(int a, int b) { return a > b; }
+
+int main() {
+  int n, m;
+  while (cin >> n >> m) {
+    int ans = 0;
+    // scanf("%d%d", &n,&m);
+    for (int i = 0; i < n; i++) scanf("%d", &w[i]);
+    sort(w, w + n, cmp);
+    int i = 0, j = n - 1;
+    while (i <= j) {
+      if (i == j) {
+        ans++;
+        break;
+      }
+      if (w[i] + w[j] <= m) j--;
+      i++;
+      ans++;
+    }
+    cout << ans << endl;
+  }
+  return 0;
 }
-
-int main()
-{
-	int n, m;
-	while (cin>>n>>m)
-	{
-		int ans=0;
-		//scanf("%d%d", &n,&m);
-		for (int i = 0; i < n; i++)
-			scanf("%d", &w[i]);
-		sort(w, w + n,cmp);
-		int i = 0, j = n - 1;
-		while (i <= j)
-		{
-			if (i == j)
-			{
-				ans++;
-				break;
-			}
-			if (w[i] + w[j] <= m)
-				j--;
-			i++;
-			ans++;
-		}
-		cout << ans << endl;
-	}
-	return 0;
-}
-
-
