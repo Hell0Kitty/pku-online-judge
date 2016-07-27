b3e9bfc2567f82d5aaf019d965b236b878beb8ae
sdecoder <sde.work.inbox@gmail.com>
2016-07-27 10:19:22 -0400

    2016-07-27 10:19:22

---
+++ 1799.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        double r;
+        int n;
+        scanf("%lf%d", &r, &n);
+        printf("Scenario #%d:\n%.3f\n\n", i + 1, (1 - 1 / (1 + sin(3.1415926 / n))) * r);
+    }
+    return 0;
+}
+
+
