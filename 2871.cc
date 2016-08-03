4bd507c237caa695248016ef3dd7ca80fe4af61b
sdecoder <sde.work.inbox@gmail.com>
2016-08-02 21:41:11 -0400

    2016-08-02 21:41:11

---
+++ 2871.cc
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+#include<iostream>
+using namespace std;
+int main() {
+	double a,b;
+	scanf("%lf",&a);
+	if(a!=999)
+	{
+		while(scanf("%lf",&b),b!=999) {
+			printf("%.2f\n",b-a);
+			a=b;
+		}
+	}
+	printf("End of Output\n");
+	return 0;
+}
+
+
