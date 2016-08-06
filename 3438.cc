dc86996e5f960bcad344a4ec988347ad3436e876
sdecoder <sde.work.inbox@gmail.com>
2016-08-05 20:25:47 -0400

    2016-08-05 20:25:47

--- 3438.cc
+++ 3438.cc
@@ -1,29 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
-{
-	int t;
-	char digits[1001];
-	cin>>t;
-	while(t--)
-	{
-		cin>>digits;
-		int len = strlen(digits);
-		int count = 1;
-		for(int i = 0;i<len;i++)
-		{
-			if(digits[i] == digits[i+1])
-				count++;
-			else{
-				cout<<count<<digits[i];
-				count = 1;
-			}
-		}
-		cout<<endl;
-	}
-	return 0;
+int main() {
+  int t;
+  char digits[1001];
+  cin >> t;
+  while (t--) {
+    cin >> digits;
+    int len = strlen(digits);
+    int count = 1;
+    for (int i = 0; i < len; i++) {
+      if (digits[i] == digits[i + 1])
+        count++;
+      else {
+        cout << count << digits[i];
+        count = 1;
+      }
+    }
+    cout << endl;
+  }
+  return 0;
 }
-
-
-
