33511c71192bdc84cbb40ddec3c71583268db8d6
sdecoder <sde.work.inbox@gmail.com>
2016-08-10 14:08:59 -0400

    2016-08-10 14:08:59

--- 3425.cc
+++ 3425.cc
@@ -1,32 +1,28 @@
 #include <iostream>
 using namespace std;
-const int maxN=1000024;
+const int maxN = 1000024;
 int vis[maxN];
 
-int main()
-{
-	int n;
-	memset(vis,0,sizeof(vis));
-	int q,a,x,sum=0,correct_num=0;
-	scanf("%d",&n);
-	while(n--){
-		scanf("%d%d%d",&q,&a,&x);
-		if(a==0) sum+=10;
-		else{
-			sum+=20;
-			if(vis[q]==1)
-				sum+=10*correct_num;
-			else{
-				vis[q]=1;
-				++correct_num;
-			}	
-			if(x==1)
-				sum+=20;
-		} 
-	}
-	printf("%d",sum);
-	return 0;	
+int main() {
+  int n;
+  memset(vis, 0, sizeof(vis));
+  int q, a, x, sum = 0, correct_num = 0;
+  scanf("%d", &n);
+  while (n--) {
+    scanf("%d%d%d", &q, &a, &x);
+    if (a == 0)
+      sum += 10;
+    else {
+      sum += 20;
+      if (vis[q] == 1)
+        sum += 10 * correct_num;
+      else {
+        vis[q] = 1;
+        ++correct_num;
+      }
+      if (x == 1) sum += 20;
+    }
+  }
+  printf("%d", sum);
+  return 0;
 }
-
-
-
