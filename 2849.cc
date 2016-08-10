e0353539c4b201f502c359880f5f17c4ace75e93
sdecoder <sde.work.inbox@gmail.com>
2016-08-10 14:24:13 -0400

    2016-08-10 14:24:13

--- 2849.cc
+++ 2849.cc
@@ -26,7 +26,7 @@
 
 using namespace std;
 
-#define rep(i,n) for(int i=0;i<(n);i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
 #define MAX 32767
 
@@ -38,104 +38,80 @@ int po[32767];
 
 stack<int> cycle;
 
-void compile()
-{
-    while (!cycle.empty()) cycle.pop();
-    memset(po, 0, sizeof(po));
-    memset(jump, 0, sizeof(jump));
-    int n = c;
-    int p = 0;
-    rep(c, n)
-    {
-        if (bf[c] == '[')
-            cycle.push(c);
-        if (bf[c] == ']')
-        {
-            if (cycle.empty() == true)
-            {
-                printf("COMPILE ERROR");
-                return ;
-            }
-            int t = cycle.top();
-            cycle.pop();
-            jump[c] = t;
-            jump[t] = c;
-        }
-
-    }
-    if (cycle.empty() == false)
-    {
+void compile() {
+  while (!cycle.empty()) cycle.pop();
+  memset(po, 0, sizeof(po));
+  memset(jump, 0, sizeof(jump));
+  int n = c;
+  int p = 0;
+  rep(c, n) {
+    if (bf[c] == '[') cycle.push(c);
+    if (bf[c] == ']') {
+      if (cycle.empty() == true) {
         printf("COMPILE ERROR");
-        return ;
+        return;
+      }
+      int t = cycle.top();
+      cycle.pop();
+      jump[c] = t;
+      jump[t] = c;
     }
-    rep(c, n)
-    {
-        if (bf[c] == '+')
-        {
-            po[p]++;
-            po[p] &= 255;
-        }
-        if (bf[c] == '-')
-        {
-            po[p] += 255;
-            po[p] &= 255;
-        }
-        if (bf[c] == '>')
-        {
-            p++;
-            p &= MAX;
-        }
-        if (bf[c] == '<')
-        {
-            p += MAX;
-            p &= MAX;
-        }
-        if (bf[c] == '.')
-        {
-            putchar((unsigned char)(po[p]));
-        }
-        if (bf[c] == '[')
-        {
-            if (po[p] == 0)
-            {
-                c = jump[c];
-            }
-        }
-        if (bf[c] == ']')
-        {
-            if (po[p] != 0)
-                c = jump[c];
-        }
+  }
+  if (cycle.empty() == false) {
+    printf("COMPILE ERROR");
+    return;
+  }
+  rep(c, n) {
+    if (bf[c] == '+') {
+      po[p]++;
+      po[p] &= 255;
     }
-    return ;
+    if (bf[c] == '-') {
+      po[p] += 255;
+      po[p] &= 255;
+    }
+    if (bf[c] == '>') {
+      p++;
+      p &= MAX;
+    }
+    if (bf[c] == '<') {
+      p += MAX;
+      p &= MAX;
+    }
+    if (bf[c] == '.') {
+      putchar((unsigned char)(po[p]));
+    }
+    if (bf[c] == '[') {
+      if (po[p] == 0) {
+        c = jump[c];
+      }
+    }
+    if (bf[c] == ']') {
+      if (po[p] != 0) c = jump[c];
+    }
+  }
+  return;
 }
-int main()
-{
-    int T;
-    scanf("%d", &T);
-    rep(cases, T)
-    {
-        c = 0;
-        while (1)
-        {
-            gets(t);
-            if (strcmp(t, "end") == false)
-            {
-                printf("PROGRAM #%d:\n", cases + 1);
-                compile();
-                printf("\n");
-                break;
-            }
-            int len = strlen(t);
-            rep(i, len)
-            {
-                if (t[i] == '%') break;
-                if (t[i] == '+' || t[i] == '-' || t[i] == '>' || t[i] == '<' || t[i] == '.' || t[i] == '[' || t[i] == ']')
-                    bf[c++] = t[i];
-            }
-        }
+int main() {
+  int T;
+  scanf("%d", &T);
+  rep(cases, T) {
+    c = 0;
+    while (1) {
+      gets(t);
+      if (strcmp(t, "end") == false) {
+        printf("PROGRAM #%d:\n", cases + 1);
+        compile();
+        printf("\n");
+        break;
+      }
+      int len = strlen(t);
+      rep(i, len) {
+        if (t[i] == '%') break;
+        if (t[i] == '+' || t[i] == '-' || t[i] == '>' || t[i] == '<' ||
+            t[i] == '.' || t[i] == '[' || t[i] == ']')
+          bf[c++] = t[i];
+      }
     }
+  }
 }
-
-
-
