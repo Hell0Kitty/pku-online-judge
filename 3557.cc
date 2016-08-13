803fdb5fa764bcb610138c911face62c21dce794
sdecoder <sde.work.inbox@gmail.com>
2016-08-13 17:45:10 -0400

    2016-08-13 17:45:10

--- 3557.cc
+++ 3557.cc
@@ -14,40 +14,32 @@
 using namespace std;
 
 #define eps 1e-8
-#define zero(_) (abs(_)<=eps)
-const double pi=acos(-1.0);
+#define zero(_) (abs(_) <= eps)
+const double pi = acos(-1.0);
 typedef long long LL;
-const int Max=22;
-const LL INF=0x3FFFFFFF;
+const int Max = 22;
+const LL INF = 0x3FFFFFFF;
 double ans[23];
 int n;
 double p;
 LL C[30][30];
-void init()
-{
-    for(int i=0; i<Max; i++)
-        for(int j=0; j<=i; j++)
-            C[i][j]= j==0? 1 : C[i-1][j-1]+C[i-1][j];
-
+void init() {
+  for (int i = 0; i < Max; i++)
+    for (int j = 0; j <= i; j++)
+      C[i][j] = j == 0 ? 1 : C[i - 1][j - 1] + C[i - 1][j];
 }
-int main()
-{
-    init();
-    while(scanf("%d%lf",&n,&p)==2)
-    {
-       memset(ans,0,sizeof ans);
-       ans[1]=1.0;
-       for(int i=2;i<=n;i++)
-       {
-           double help=0;
-           for(int j=1;j<i;j++)
-           help+=C[i-1][j-1]*ans[j]*pow(1.0-p,double(j*(i-j)));
-           ans[i]=1.0-help;
-       }
-       printf("%.3lf\n",ans[n]);
+int main() {
+  init();
+  while (scanf("%d%lf", &n, &p) == 2) {
+    memset(ans, 0, sizeof ans);
+    ans[1] = 1.0;
+    for (int i = 2; i <= n; i++) {
+      double help = 0;
+      for (int j = 1; j < i; j++)
+        help += C[i - 1][j - 1] * ans[j] * pow(1.0 - p, double(j * (i - j)));
+      ans[i] = 1.0 - help;
     }
-    return 0;
+    printf("%.3lf\n", ans[n]);
+  }
+  return 0;
 }
-
-
-
