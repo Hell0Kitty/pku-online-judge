2d08b913d68b0f6414ab338fd98e1f2697081ebe
sdecoder <sde.work.inbox@gmail.com>
2016-08-12 23:58:35 -0400

    2016-08-12 23:58:35

---
+++ 2665.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    int l, m;
+    while (scanf("%d%d", &l, &m) != EOF && !(l == 0 && m == 0))
+    {
+        int ans = l + 1;
+        for (int i = 0; i < m; i++)
+        {
+            int a, b;
+            scanf("%d%d", &a, &b);
+            ans -= b - a + 1;
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
+
+
+
