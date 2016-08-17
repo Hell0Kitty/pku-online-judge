26e16f7a3aa9d5651d4265d626db7cc937de1f45
sdecoder <sde.work.inbox@gmail.com>
2016-08-17 17:46:38 -0400

    2016-08-17 17:46:38

--- 3781.cc
+++ 3781.cc
@@ -1,21 +1,18 @@
-#include<iostream>
-#include<algorithm>
-#include<cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
 using namespace std;
 
-int main(){
-	int i,j,k;
-	scanf("%d",&j);
-	for(i = 0;i<j; i++){
-		int a[10]={0};
-		for(k = 0;k<=10;k++){
-			scanf("%d",&a[k]);
-		}
-		sort(a+1,a+11);
-		printf("%d %d\n",i+1,a[8]);
-	}
-	return 0;
+int main() {
+  int i, j, k;
+  scanf("%d", &j);
+  for (i = 0; i < j; i++) {
+    int a[10] = {0};
+    for (k = 0; k <= 10; k++) {
+      scanf("%d", &a[k]);
+    }
+    sort(a + 1, a + 11);
+    printf("%d %d\n", i + 1, a[8]);
+  }
+  return 0;
 }
-
-
-
