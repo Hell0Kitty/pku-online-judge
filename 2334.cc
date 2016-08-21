d7f9946e79c78e290de3f4420594cee62d265868
sdecoder <sde.work.inbox@gmail.com>
2016-08-21 19:49:42 -0400

    2016-08-21 19:49:42

--- 2334.cc
+++ 2334.cc
@@ -4,37 +4,30 @@
 
 using namespace std;
 
-int main()
-{
-    int N;
-    int total = 0;
-    string a1,a2;
-    scanf("%d",&N);
-    while(N--)
-    {
-        char tem[300];
-        scanf("%s",tem);
-        if(total)
-        {
-            a2 = tem;
-            int k = 0;
-            for(unsigned i = 0;i < a1.length() && i < a2.length();++i)
-            {
-                if(a1[i] == a2[i])k++;
-                else break;
-            }
-            total = total + a2.length() - k + 1;
-            a1 = a2;
-        }
+int main() {
+  int N;
+  int total = 0;
+  string a1, a2;
+  scanf("%d", &N);
+  while (N--) {
+    char tem[300];
+    scanf("%s", tem);
+    if (total) {
+      a2 = tem;
+      int k = 0;
+      for (unsigned i = 0; i < a1.length() && i < a2.length(); ++i) {
+        if (a1[i] == a2[i])
+          k++;
         else
-        {
-            a1 = tem;
-            total = a1.length();
-        }
+          break;
+      }
+      total = total + a2.length() - k + 1;
+      a1 = a2;
+    } else {
+      a1 = tem;
+      total = a1.length();
     }
-    printf("%d\n",total);
-    return 0;
+  }
+  printf("%d\n", total);
+  return 0;
 }
-
-
-
