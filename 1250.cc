194c1e1c603834933cb7b9e1c69937c888318493
sdecoder <sde.work.inbox@gmail.com>
2016-08-21 14:03:42 -0400

    2016-08-21 14:03:42

--- 1250.cc
+++ 1250.cc
@@ -1,34 +1,35 @@
-#include<iostream>
-#include<stdio.h>
-#include<string.h>
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 const int MAX = 60;
 
-int main(){
-    int n, i, id, ans;
-    char str[MAX];
-    bool vis[MAX], tan[MAX];
-    while(cin >> n && n){
-        memset(vis, 0, sizeof(vis));
-        memset(tan, 0, sizeof(tan));
-        cin >> str;
-        ans = 0;
-        for(i = 0; i < strlen(str); i ++){
-            id = str[i] - 'A';
-            if(!vis[id]){
-                vis[id] = true;
-                if(n > 0){
-                    n --;
-                    tan[id] = true;
-                }else ans ++;
-            }else{
-                if(tan[id]) n ++;
-            }
-        }
-        if(ans == 0)printf("All customers tanned successfully.\n");
-        else printf("%d customer(s) walked away.\n", ans);
+int main() {
+  int n, i, id, ans;
+  char str[MAX];
+  bool vis[MAX], tan[MAX];
+  while (cin >> n && n) {
+    memset(vis, 0, sizeof(vis));
+    memset(tan, 0, sizeof(tan));
+    cin >> str;
+    ans = 0;
+    for (i = 0; i < strlen(str); i++) {
+      id = str[i] - 'A';
+      if (!vis[id]) {
+        vis[id] = true;
+        if (n > 0) {
+          n--;
+          tan[id] = true;
+        } else
+          ans++;
+      } else {
+        if (tan[id]) n++;
+      }
     }
-    return 0;
+    if (ans == 0)
+      printf("All customers tanned successfully.\n");
+    else
+      printf("%d customer(s) walked away.\n", ans);
+  }
+  return 0;
 }
-
-
