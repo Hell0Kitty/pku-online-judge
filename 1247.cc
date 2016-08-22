b91b3c6b59cbea0c98c4d8c87389532ff83f6829
sdecoder <sde.work.inbox@gmail.com>
2016-08-22 13:37:42 -0400

    2016-08-22 13:37:42

--- 1247.cc
+++ 1247.cc
@@ -4,32 +4,25 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n,a[50],i,j,sum,t;
-    while(~scanf("%d",&n),n)
-    {
-        sum = 0;
-        for(i = 1;i<=n;i++)
-        {
-            scanf("%d",&a[i]);
-            sum+=a[i];
-        }
-        t = 0;
-        for(i = 1;i<=n;i++)
-        {
-            t+=a[i];
-            if(2*t==sum)
-            break;
-        }
-        if(i<=n)
-        printf("Sam stops at position %d and Ella stops at position %d.\n",i,i+1);
-        else
-        printf("No equal partitioning.\n");
+int main() {
+  int n, a[50], i, j, sum, t;
+  while (~scanf("%d", &n), n) {
+    sum = 0;
+    for (i = 1; i <= n; i++) {
+      scanf("%d", &a[i]);
+      sum += a[i];
     }
+    t = 0;
+    for (i = 1; i <= n; i++) {
+      t += a[i];
+      if (2 * t == sum) break;
+    }
+    if (i <= n)
+      printf("Sam stops at position %d and Ella stops at position %d.\n", i,
+             i + 1);
+    else
+      printf("No equal partitioning.\n");
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
