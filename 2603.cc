eadd315f9f88abf2bda5ace133b98025602ef6bd
sdecoder <sde.work.inbox@gmail.com>
2016-08-23 05:10:35 -0400

    2016-08-23 05:10:35

--- 2603.cc
+++ 2603.cc
@@ -1,29 +1,20 @@
-#include<stdio.h>
-int main()
-{
-    int m,n=1,i,j,k,a[10001]={0};
-    for(i=0; i<10; i++)
-    {
-        scanf("%d",&m);
-        for(j=2; m!=1; j++)
-        {
-            while(m%j==0)
-            {
-                a[j]++;
-                m=m/j;
-            }
-        }
+#include <stdio.h>
+int main() {
+  int m, n = 1, i, j, k, a[10001] = {0};
+  for (i = 0; i < 10; i++) {
+    scanf("%d", &m);
+    for (j = 2; m != 1; j++) {
+      while (m % j == 0) {
+        a[j]++;
+        m = m / j;
+      }
     }
-    for(i=0; i<10001; i++)
-    {
-        if(a[i]!=0)
-        {
-            n*=(a[i]+1);
-        }
+  }
+  for (i = 0; i < 10001; i++) {
+    if (a[i] != 0) {
+      n *= (a[i] + 1);
     }
-    printf("%d\n",n%10);
-    return 0;
+  }
+  printf("%d\n", n % 10);
+  return 0;
 }
-
-
-
