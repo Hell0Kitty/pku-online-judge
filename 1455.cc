d79dfec8bedb7021085dc76458c1388b3feab830
sdecoder <sde.work.inbox@gmail.com>
2016-09-01 03:58:23 -0400

    2016-09-01 03:58:23

---
+++ 1455.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        int ans = (1 + n / 2 - 1) * (n / 2 - 1) / 2 * 2;
+        if (n & 1)
+            ans += n / 2;
+        printf("%d\n", ans);
+    }
+}
+
+
