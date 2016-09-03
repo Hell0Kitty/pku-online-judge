c72c064951c3c0d0d2c7af72383657943fb2ad80
sdecoder <sde.work.inbox@gmail.com>
2016-09-03 15:58:05 -0400

    2016-09-03 15:58:05

--- 2858.cc
+++ 2858.cc
@@ -16,62 +16,51 @@
 using namespace std;
 
 typedef long long LL;
-typedef pair<int, int>PII;
-typedef pair<PII, int>PII2;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PII2;
 
 string s;
-set<string>S;
+set<string> S;
 int n;
 
 char c[20];
 int v[20];
 
-int main()
-{
-    scanf("%d", &n);
-    for(int i = 0; i < n; i++ )
-    {
-        cin >> s;
-        sort(s.begin(), s.end());
-        S.insert(s);
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    cin >> s;
+    sort(s.begin(), s.end());
+    S.insert(s);
+  }
+
+  int m;
+  scanf("%d", &m);
+  for (int mm = 0; mm < m; mm++) {
+    int num;
+    scanf("%d", &num);
+    for (int i = 0; i < num; i++) {
+      scanf(" %c %d", &c[i], &v[i]);
     }
 
-    int m;
-    scanf("%d", &m);
-    for(int mm = 0; mm < m; mm ++ )
-    {
-        int num;
-        scanf("%d", &num);
-        for(int i = 0; i < num; i++ )
-        {
-            scanf(" %c %d", &c[i], &v[i]);
+    int maxv = 0;
+    for (int i = 0; i < (1 << num); i++) {
+      string s = "";
+      int cval = 0;
+      for (int j = 0; j < num; j++) {
+        if (i & (1 << j)) {
+          cval += v[j];
+          s += c[j];
         }
-
-        int maxv = 0;
-        for(int i = 0; i < (1<<num); i++ )
-        {
-            string s = "";
-            int cval = 0;
-            for(int j = 0; j < num; j++ )
-            {
-                if(i & (1<<j))
-                {
-                    cval += v[j];
-                    s += c[j];
-                }
-            }
-            sort(s.begin(), s.end());
-            if(S.count(s) && cval > maxv)
-            {
-                maxv = cval;
-            }
-        }
-
-        printf("%d\n", maxv);
+      }
+      sort(s.begin(), s.end());
+      if (S.count(s) && cval > maxv) {
+        maxv = cval;
+      }
     }
 
-return 0;
+    printf("%d\n", maxv);
+  }
+
+  return 0;
 }
-
-
-
