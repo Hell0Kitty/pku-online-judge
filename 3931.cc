83be48aa4c19b58aabd04ca85a62a741398cb964
sdecoder <sde.work.inbox@gmail.com>
2016-09-03 11:08:22 -0400

    2016-03-24 12:31:05

--- 3931.cc
+++ 3931.cc
@@ -2,88 +2,84 @@
 
 using namespace std;
 
-__int64 min(__int64 m,__int64 k)  
+__int64 min(__int64 m, __int64 k)
 
 {
+  if (m > k)
+    return k;
 
-  if(m>k) return k;
-
-  else return m;
-
+  else
+    return m;
 }
 
-__int64 max(__int64 m,__int64 k) 
+__int64 max(__int64 m, __int64 k)
 
 {
+  if (m < k)
+    return k;
 
-  if(m<k) return k;
-
-  else return m;
-
+  else
+    return m;
 }
 
-__int64 count(__int64 m,__int64 k)  
+__int64 count(__int64 m, __int64 k)
 
 {
+  __int64 n, n1, n2;
 
-  __int64 n,n1,n2;
+  if (m == 1)
+    return 2 * k - 1;
 
-  if(m==1)    return 2*k-1;  
-
-  else if(m%2==0) 
+  else if (m % 2 == 0)
 
   {
+    n = m / 2 - 1 + k;
 
-    n=m/2-1+k;
+    if (n >= m)
+      return n;
 
-    if(n>=m) return n;
-
-    else return 0;
+    else
+      return 0;
 
   }
 
   else
 
   {
+    n1 = count((m + 1) / 2, k) * 2;
 
-    n1=count((m+1)/2,k)*2;    
+    n2 = count((m - 1) / 2, k) * 2 + 1;
 
-    n2=count((m-1)/2,k)*2+1;     
+    if (n2 == 1) n2 = 0;
 
-if(n2==1) n2=0;
+    if (n1 > 0 && n2 > 0)
+      return min(n1, n2);
 
-    if(n1>0&&n2>0) return min(n1,n2);
-
-    else return max(n1,n2);
-
+    else
+      return max(n1, n2);
   }
-
 }
 
 int main()
 
 {
+  __int64 m, k, n;
 
-  __int64 m,k,n;
-
-  while(1)
+  while (1)
 
   {
+    scanf("%I64d%I64d", &m, &k);
 
-  scanf("%I64d%I64d",&m,&k);
+    if (m == 0 && k == 0) break;
 
-  if(m==0&&k==0) break;
+    n = count(m, k);
 
-  n=count(m,k);
+    if (n)
+      printf("%I64d\n", n);
 
-  if(n) printf("%I64d\n",n);
-
-  else printf("Impossible\n");
-
+    else
+      printf("Impossible\n");
   }
 
   return 0;
-
 }
-
-
