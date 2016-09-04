13fb910bb16a542ba154f0c594c3be6c4d446cf3
sdecoder <sde.work.inbox@gmail.com>
2016-09-04 06:39:35 -0400

    2016-09-04 06:39:35

---
+++ 1989.cc
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+#define clr(x)memset(x,0,sizeof(x))
+int v[10005];
+int main()
+{
+    int n,k,p,i,res,num;
+    while(scanf("%d%d",&n,&k)!=EOF)
+    {
+        res=num=0;
+        clr(v);
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&p);
+            if(!v[p])
+            {
+                v[p]=1;
+                num++;
+                if(num==k)
+                {
+                    res++;
+                    clr(v);
+                    num=0;
+                }
+            }
+        }
+        printf("%d\n",res+1);
+    }
+    return 0;
+}
+
+
+
