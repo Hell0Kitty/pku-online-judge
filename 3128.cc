76c4a4370acec88d44e75c1101fdb8d2e34cc1d9
sdecoder <sde.work.inbox@gmail.com>
2016-09-05 12:21:47 -0400

    2016-09-05 12:21:47

--- 3128.cc
+++ 3128.cc
@@ -14,77 +14,59 @@ bool vis[30];
 
 int cnt[30];
 
-bool confirm(){
+bool confirm() {
+  bool flag = true;
 
-    bool flag=true;
+  for (int i = 1; i <= 26; i++)
 
-    for(int i=1;i<=26;i++)
+    if (i % 2 == 0) {
+      if (cnt[i] % 2) {
+        flag = false;
 
-    if(i%2==0){
-
-        if(cnt[i]%2){
-
-            flag=false;
-
-            break;
-
-        }
-
+        break;
+      }
     }
 
-    return flag;
-
+  return flag;
 }
 
-int main(){
+int main() {
+  int T, cnted;
 
-    int T,cnted;
+  scanf("%d", &T);
 
-    scanf("%d",&T);
+  while (T--) {
+    scanf("%s", str);
 
-    while(T--){
+    memset(vis, false, sizeof(vis));
 
-        scanf("%s",str);
+    memset(cnt, 0, sizeof(cnt));
 
-        memset(vis,false,sizeof(vis));
+    for (int i = 0; i < 26; i++) {
+      if (!vis[i]) {
+        int k = i;
 
-        memset(cnt,0,sizeof(cnt));
+        cnted = 0;
 
-        for(int i=0;i<26;i++){
+        while (!vis[k]) {
+          vis[k] = true;
 
-            if(!vis[i]){
+          k = str[k] - 'A';
 
-                int k=i;
-
-                cnted=0;
-
-                while(!vis[k]){
-
-                    vis[k]=true;
-
-                    k=str[k]-'A';
-
-                    cnted++;
-
-                }
-
-                cnt[cnted]++;
-
-            }
-
+          cnted++;
         }
 
-        if(confirm())
-
-        printf("Yes\n");
-
-        else printf("No\n");
-
+        cnt[cnted]++;
+      }
     }
 
-    return 0;
+    if (confirm())
 
+      printf("Yes\n");
+
+    else
+      printf("No\n");
+  }
+
+  return 0;
 }
-
-
-
