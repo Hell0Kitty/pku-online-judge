50386a59735b49e16c2d098e44fbe1f50c3db90b
sdecoder <sde.work.inbox@gmail.com>
2016-09-09 11:52:27 -0400

    2016-09-09 11:52:27

---
+++ 2663.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#include<vector>
+using namespace std;
+long long f[50];
+int main()
+{
+	int n;
+	while(scanf("%d",&n)&&n!=-1)
+	{
+		f[0]=1,f[2]=3;
+		for(int i=4;i<=n;i++)
+		f[i]=f[i-2]*4-f[i-4];
+		printf("%I64d\n",f[n]);
+	}
+}
+
+
+
