d1130ddbee0e3df2dbc6270c51fffd3aa7e79322
sdecoder <sde.work.inbox@gmail.com>
2016-09-09 09:06:59 -0400

    2016-09-09 09:06:59

--- 2059.cc
+++ 2059.cc
@@ -1,47 +1,36 @@
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 int po[60][2];
-int s,n;
-bool solve(int x,int y)
-{
-    int len=min(min(x,y),min(s-x,s-y));
-    len=len*len;
-    for(int i=0;i<n;i++)
-    {
-        int xx=po[i][0],yy=po[i][1];
-        if((xx==x&&yy==y)||((xx-x)*(xx-x)+(yy-y)*(yy-y))>len)
-            return false;
-    }
-    return true;
+int s, n;
+bool solve(int x, int y) {
+  int len = min(min(x, y), min(s - x, s - y));
+  len = len * len;
+  for (int i = 0; i < n; i++) {
+    int xx = po[i][0], yy = po[i][1];
+    if ((xx == x && yy == y) ||
+        ((xx - x) * (xx - x) + (yy - y) * (yy - y)) > len)
+      return false;
+  }
+  return true;
 }
-int main()
-{
-    int T;
-    for(scanf("%d",&T);T;T--)
-    {
-        scanf("%d%d",&s,&n);
-        for(int i=0;i<n;i++)
-            scanf("%d%d",&po[i][0],&po[i][1]);
-        bool flag=false;
-        for(int x=1;x<s;x++)
-        {
-            for(int y=1;y<s;y++)
-            {
-                if(solve(x,y))
-                {
-                    flag=true;
-                    printf("%d %d\n",x,y);
-                    x=y=s;
-                }
-            }
+int main() {
+  int T;
+  for (scanf("%d", &T); T; T--) {
+    scanf("%d%d", &s, &n);
+    for (int i = 0; i < n; i++) scanf("%d%d", &po[i][0], &po[i][1]);
+    bool flag = false;
+    for (int x = 1; x < s; x++) {
+      for (int y = 1; y < s; y++) {
+        if (solve(x, y)) {
+          flag = true;
+          printf("%d %d\n", x, y);
+          x = y = s;
         }
-        if(!flag)
-            printf("poodle\n");
+      }
     }
-    return 0;
+    if (!flag) printf("poodle\n");
+  }
+  return 0;
 }
-
-
-
