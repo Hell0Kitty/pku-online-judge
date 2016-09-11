3105590b7d20ee5bcc764b6f033976e9d254365b
sdecoder <sde.work.inbox@gmail.com>
2016-09-11 08:21:49 -0400

    2016-09-11 08:21:49

--- 1277.cc
+++ 1277.cc
@@ -1,21 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cin>>n;
-    while(n--)
-    {
-        int m;
-        cin>>m;
-        if(m<=3) cout<<0<<endl;
-        else
-        {
-            if(m%2==0) cout<<((m-3)*2+(m-4)*(m-2))/2 <<endl;
-            else cout<<m*(m-3)/2<<endl;
-        }
-
+int main() {
+  int n;
+  cin >> n;
+  while (n--) {
+    int m;
+    cin >> m;
+    if (m <= 3)
+      cout << 0 << endl;
+    else {
+      if (m % 2 == 0)
+        cout << ((m - 3) * 2 + (m - 4) * (m - 2)) / 2 << endl;
+      else
+        cout << m*(m - 3) / 2 << endl;
     }
+  }
 }
-
-
