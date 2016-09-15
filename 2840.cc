901c79013086d7408eec8aacf5096faba49af3d3
sdecoder <sde.work.inbox@gmail.com>
2016-09-15 12:59:16 -0400

    2016-09-15 12:59:16

---
+++ 2840.cc
@@ -0,0 +1,16 @@
+#include <cstdio>
+using namespace std;
+
+int T, h, m;
+
+int main() {
+	for (scanf("%d", &T); T; --T) {
+		scanf("%d:%d", &h, &m);
+		if (!m) printf("%d\n", (h + 11) % 24 + 1);
+		else putchar('0'), putchar('\n');
+	}
+	return 0;
+}
+
+
+
