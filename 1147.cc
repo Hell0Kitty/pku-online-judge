b18fc2f9327a2aedd039e87f72aa8c499dc9f415
sdecoder <sde.work.inbox@gmail.com>
2016-09-15 14:02:57 -0400

    2016-09-15 14:02:57

--- 1147.cc
+++ 1147.cc
@@ -12,36 +12,27 @@ int g[maxn];
 int next[maxn];
 int cnt, cnt1;
 
-int main()
-{
-    scanf("%d", &n);
-    cnt = cnt1 =0;
-    for (int i =0; i < n; i++)
-    {
-        scanf("%d", &f[i]);
-        if (f[i] ==0)
-            cnt++;
-    }
-    for (int i =0; i < n; i++)
-    {
-        if (f[i])
-            next[i] = cnt + cnt1++;
-        else
-            next[i] = i - cnt1;
-    }
-    memset(f, 0, sizeof(f));
-    for (int i = cnt; i < n; i++)
-        f[i] =1;
-    printf("%d", f[0]);
-    for (int i =0; i < n -1; i++)
-    {
-        for (int j =0; j < n; j++)
-            g[next[j]] = f[j];
-        memcpy(f, g, n *sizeof(int));
-        printf(" %d", f[0]);
-    }
-    putchar('\n');
-    return 0;
+int main() {
+  scanf("%d", &n);
+  cnt = cnt1 = 0;
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &f[i]);
+    if (f[i] == 0) cnt++;
+  }
+  for (int i = 0; i < n; i++) {
+    if (f[i])
+      next[i] = cnt + cnt1++;
+    else
+      next[i] = i - cnt1;
+  }
+  memset(f, 0, sizeof(f));
+  for (int i = cnt; i < n; i++) f[i] = 1;
+  printf("%d", f[0]);
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n; j++) g[next[j]] = f[j];
+    memcpy(f, g, n * sizeof(int));
+    printf(" %d", f[0]);
+  }
+  putchar('\n');
+  return 0;
 }
-
-
