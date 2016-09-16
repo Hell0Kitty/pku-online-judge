12125e0e851c22b5e6160fe72d34c2f6a45bb5af
sdecoder <sde.work.inbox@gmail.com>
2016-09-15 21:01:48 -0400

    2016-09-15 21:01:48

--- 3991.cc
+++ 3991.cc
@@ -1,38 +1,35 @@
-#include<cstdio>
-#include<cmath>
-#include<stack>
-#include<cstring>
-#include<string>
-#include<iostream>
-#include<algorithm>
+#include <cstdio>
+#include <cmath>
+#include <stack>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int main(){
-	int n,l,i,j,k,cs=1;
-	char a1[3000];
-	int a[3000];
-	while(gets(a1)){
-		if(a1[0]=='-')
-			break;
-		l=strlen(a1);
-		for(i=0;i<l;i++){
-			if(a1[i]=='{')
-				a[i]=1;
-			else if(a1[i]=='}')
-				a[i]=-1;
-		}
-		int sum=0;k=0;
-		for(i=0;i<l;i++){
-			sum+=a[i];
-			if(sum<0){
-				sum=1;
-				k++;
-			}
-		}
-		if(sum>0)
-			k+=sum/2;
-		printf("%d. %d\n",cs++,k);
-	}
-	return 0;
+int main() {
+  int n, l, i, j, k, cs = 1;
+  char a1[3000];
+  int a[3000];
+  while (gets(a1)) {
+    if (a1[0] == '-') break;
+    l = strlen(a1);
+    for (i = 0; i < l; i++) {
+      if (a1[i] == '{')
+        a[i] = 1;
+      else if (a1[i] == '}')
+        a[i] = -1;
+    }
+    int sum = 0;
+    k = 0;
+    for (i = 0; i < l; i++) {
+      sum += a[i];
+      if (sum < 0) {
+        sum = 1;
+        k++;
+      }
+    }
+    if (sum > 0) k += sum / 2;
+    printf("%d. %d\n", cs++, k);
+  }
+  return 0;
 }
-
-
