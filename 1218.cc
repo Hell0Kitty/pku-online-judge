61c5b584645192c3eac22866ae2d522d4e264188
sdecoder <sde.work.inbox@gmail.com>
2016-09-15 21:57:46 -0400

    2016-09-15 21:57:46

--- 1218.cc
+++ 1218.cc
@@ -1,17 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-int main(int argc, char *argv[])
-{
-	int T,n,m;
-	scanf("%d",&T);
-	while(T--)
-	{
-		scanf("%d",&n);
-		m=(int)sqrt(n);
-		printf("%d\n",m);
-
-	}
-	return 0;
+int main(int argc, char *argv[]) {
+  int T, n, m;
+  scanf("%d", &T);
+  while (T--) {
+    scanf("%d", &n);
+    m = (int)sqrt(n);
+    printf("%d\n", m);
+  }
+  return 0;
 }
-
-
