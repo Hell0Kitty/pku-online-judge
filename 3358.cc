43957977edb1ab1e0f3183a81b064b180966a5c3
sdecoder <sde.work.inbox@gmail.com>
2016-09-16 21:36:46 -0400

    2016-09-16 21:36:46

--- 3358.cc
+++ 3358.cc
@@ -16,162 +16,121 @@ int temp, num;
 
 int fac[1000000];
 
-int gcd(int x, int y){
+int gcd(int x, int y) {
+  int r;
 
-    int r;
+  if (x < y) {
+    swap(x, y);
+  }
 
-    if(x < y){
+  while (x % y) {
+    r = x % y;
 
-        swap(x, y);
+    x = y;
 
-    }
+    y = r;
+  }
 
-    while(x%y){
-
-        r = x%y;
-
-        x = y;
-
-        y = r;
-
-    }
-
-    return y;
-
+  return y;
 }
 
-int euler(int x){
+int euler(int x) {
+  int y = (int)sqrt(x + 0.5);
 
-    int y = (int)sqrt(x+0.5);
+  int curans = x;
 
-    int curans = x;
+  for (int i = 2; i <= y; i++) {
+    if (x % i == 0) {
+      curans = curans / i * (i - 1);
 
-    for(int i = 2;i <= y;i++){
-
-        if(x%i == 0){
-
-            curans = curans/i*(i-1);
-
-            while(x%i == 0){
-
-                x /= i;
-
-            }
-
-        }
-
+      while (x % i == 0) {
+        x /= i;
+      }
     }
+  }
 
-    if(x > 1){
+  if (x > 1) {
+    curans = curans / x * (x - 1);
+  }
 
-        curans = curans/x*(x-1);
-
-    }
-
-    return curans;
-
+  return curans;
 }
 
-int quickpow(int m, int n, int k){
+int quickpow(int m, int n, int k) {
+  int b = 1;
 
-    int b = 1;
+  while (n > 0) {
+    if (n & 1) {
+      b = (long long)b * m % k;
+    }
 
-    while(n > 0){
+    n >>= 1;
 
-        if(n&1){
+    m = (long long)m * m % k;
+  }
 
-            b = (long long)b*m%k;
+  return b;
+}
 
-        }
+int main() {
+  int T = 0;
 
-        n >>= 1;
+  while (scanf("%d/%d", &n, &m) != EOF) {
+    T++;
 
-        m = (long long)m*m%k;
+    GCD = gcd(n, m);
+
+    n /= GCD;
+
+    m /= GCD;
+
+    t = 0;
+
+    while (m % 2 == 0) {
+      t++;
+
+      m /= 2;
+    }
+
+    ans1 = t + 1;
+
+    phi = euler(m);
+
+    if (phi == 1) {
+      ans2 == 1;
 
     }
 
-    return b;
+    else {
+      num = 0;
 
-}
+      for (int i = 1; i * i <= phi; i++) {
+        if (phi % i == 0)
 
-int main(){
+        {
+          fac[num++] = i;
 
-    int T = 0;
-
-    while(scanf("%d/%d", &n, &m)!=EOF){
-
-        T++;
-
-        GCD = gcd(n, m);
-
-        n /= GCD;
-
-        m /= GCD;
-
-        t = 0;
-
-        while(m%2 == 0){
-
-            t++;
-
-            m /= 2;
-
+          fac[num++] = phi / i;
         }
+      }
 
-        ans1 = t+1;
+      sort(fac, fac + num);
 
-        phi = euler(m);
+      for (int i = 0; i < num; i++) {
+        temp = quickpow(2, fac[i], m);
 
-        if(phi == 1){
+        if (temp == 1) {
+          ans2 = fac[i];
 
-            ans2 == 1;
-
+          break;
         }
-
-        else{
-
-            num = 0;
-
-            for(int i = 1;i*i <= phi;i++){
-
-                if(phi%i == 0)
-
-                {
-
-                    fac[num++] = i;
-
-                    fac[num++] = phi/i;
-
-                }
-
-            }
-
-            sort(fac, fac+num);
-
-            for(int i = 0;i < num;i++){
-
-                temp = quickpow(2, fac[i], m);
-
-                if(temp == 1){
-
-                    ans2 = fac[i];
-
-                    break;
-
-                }
-
-            }
-
-        }
-
-        printf("Case #%d: ", T);
-
-        printf("%d,%d\n", ans1, ans2);
-
+      }
     }
 
-    return 0;
+    printf("Case #%d: ", T);
 
+    printf("%d,%d\n", ans1, ans2);
+  }
+
+  return 0;
 }
-
-
