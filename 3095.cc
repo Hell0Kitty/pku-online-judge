2f741a5e196617b025182dd2e87bbb37f7b1b078
sdecoder <sde.work.inbox@gmail.com>
2016-09-18 08:32:12 -0400

    2016-09-18 08:32:12

--- 3095.cc
+++ 3095.cc
@@ -2,22 +2,34 @@
 #include <cstring>
 #include <cstdio>
 using namespace std;
-int main(){
-    char str[100];
-    while(scanf("%s", str)&&str[0]!='#'){
-        int flag=1, ans=0, len=strlen(str), t=0, i;
-        for(i=0; i<len; i++){
-            switch(str[i]){
-                case '/':if(flag)ans+=100;flag=0;break;
-                case '\\':t=100;break;
-                case '.':ans+=t+100;t=0;flag=1;break;
-                case '_':break;
-                case '|':if(flag)ans+=50;flag=0;t=50;break;
-            }
-        }
-        printf("%d\n", (ans+t)/len);
+int main() {
+  char str[100];
+  while (scanf("%s", str) && str[0] != '#') {
+    int flag = 1, ans = 0, len = strlen(str), t = 0, i;
+    for (i = 0; i < len; i++) {
+      switch (str[i]) {
+        case '/':
+          if (flag) ans += 100;
+          flag = 0;
+          break;
+        case '\\':
+          t = 100;
+          break;
+        case '.':
+          ans += t + 100;
+          t = 0;
+          flag = 1;
+          break;
+        case '_':
+          break;
+        case '|':
+          if (flag) ans += 50;
+          flag = 0;
+          t = 50;
+          break;
+      }
     }
-    return 0;
+    printf("%d\n", (ans + t) / len);
+  }
+  return 0;
 }
-
-
