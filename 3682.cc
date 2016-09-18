426d4753483fb2289cd999a4b22ffbe067605128
sdecoder <sde.work.inbox@gmail.com>
2016-09-18 14:16:48 -0400

    2016-09-18 14:16:48

--- 3682.cc
+++ 3682.cc
@@ -3,26 +3,20 @@
 double E[1005];
 double F[1005];
 
-int main()
-{
-	int i,j,n;
-	double q,p;
-	while(1)
-	{
-		scanf("%d",&n);
-		if (n==0) break;
-		scanf("%lf",&p);
-		E[0]=0;
-		F[0]=0;
-		for (i=1;i<=n;i++)
-		{
-			E[i]=1/p+E[i-1];
-			F[i]=F[i-1]+2*E[i-1]-2*E[i]+(1+2*E[i])/p;
-		}
-		printf("%.3lf %.3lf\n",E[n],F[n]);
-	}
-	return 0;
+int main() {
+  int i, j, n;
+  double q, p;
+  while (1) {
+    scanf("%d", &n);
+    if (n == 0) break;
+    scanf("%lf", &p);
+    E[0] = 0;
+    F[0] = 0;
+    for (i = 1; i <= n; i++) {
+      E[i] = 1 / p + E[i - 1];
+      F[i] = F[i - 1] + 2 * E[i - 1] - 2 * E[i] + (1 + 2 * E[i]) / p;
+    }
+    printf("%.3lf %.3lf\n", E[n], F[n]);
+  }
+  return 0;
 }
-
-
-
