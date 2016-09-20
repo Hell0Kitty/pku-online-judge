4dbd28641b4562b69f4a4b6e7759a79416e6eaad
sdecoder <sde.work.inbox@gmail.com>
2016-09-20 02:38:38 -0400

    2016-09-20 02:38:38

--- 3916.cc
+++ 3916.cc
@@ -1,28 +1,21 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
-int main (int argc, char **argv)
-{
-	int n;
-	int i;
-	int before;
-	int now;
-	while(cin>>n)
-	{
-		if (n ==0)
-			break;
-		cin>>before;
-		cout<<before<<" ";
-		for(i =0; i < n-1; i++)
-		{
-			cin>>now;
-			if(now!=before)
-				cout<<now<<" ";
-			before=now;
-		}
-		cout<<"$"<<endl;
-	}
-	return 0;
+int main(int argc, char **argv) {
+  int n;
+  int i;
+  int before;
+  int now;
+  while (cin >> n) {
+    if (n == 0) break;
+    cin >> before;
+    cout << before << " ";
+    for (i = 0; i < n - 1; i++) {
+      cin >> now;
+      if (now != before) cout << now << " ";
+      before = now;
+    }
+    cout << "$" << endl;
+  }
+  return 0;
 }
-
-
