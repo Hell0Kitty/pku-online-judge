93afa037a6d1d5b5e4d4a7fd5d261cde35c3c6db
sdecoder <sde.work.inbox@gmail.com>
2016-09-21 16:55:26 -0400

    2016-09-21 16:55:26

--- 3646.cc
+++ 3646.cc
@@ -1,36 +1,28 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 #define MAX 20001
 using namespace std;
-int n,m,a[MAX],b[MAX];
-int main()
-{
-    while(scanf("%d%d",&n,&m)&&(n||m))
-    {
-	for(int i=0;i<n;i++)
-	    scanf("%d",&a[i]);
-	for(int i=0;i<m;i++)
-	    scanf("%d",&b[i]);
-	sort(a,a+n);
-	sort(b,b+m);
-	int ans=0,cou=0,i;
-	for(i=0;i<n;i++)
-	{
-	    while(cou<m&&b[cou]<a[i])
-		cou++;
-	    if(cou<m)
-		ans+=b[cou++];
-	    else
-		break;
-	}
-	if(i==n)
-	    printf("%d\n",ans);
-	else
-	    printf("Loowater is doomed!\n");
+int n, m, a[MAX], b[MAX];
+int main() {
+  while (scanf("%d%d", &n, &m) && (n || m)) {
+    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+    for (int i = 0; i < m; i++) scanf("%d", &b[i]);
+    sort(a, a + n);
+    sort(b, b + m);
+    int ans = 0, cou = 0, i;
+    for (i = 0; i < n; i++) {
+      while (cou < m && b[cou] < a[i]) cou++;
+      if (cou < m)
+        ans += b[cou++];
+      else
+        break;
     }
-    return 0;
+    if (i == n)
+      printf("%d\n", ans);
+    else
+      printf("Loowater is doomed!\n");
+  }
+  return 0;
 }
-
-
