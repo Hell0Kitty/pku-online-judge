f0907e79f4ca3f7f4ce1584cea7040c3ebb580d6
sdecoder <sde.work.inbox@gmail.com>
2016-09-25 07:33:42 -0400

    2016-09-25 07:33:42

--- 1363.cc
+++ 1363.cc
@@ -1,55 +1,43 @@
-#include<cstdio>
-#include<stack>
-#include<cstring>
-#include<cstdlib>
+#include <cstdio>
+#include <stack>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
-int main()
-{
-    int i,j,k,T;
-    int num,flag;
-    while(scanf("%d",&T),T)
-    {
-        flag=0;//必须再次初始化为0 
-        while(1)
-        {
-            stack <int> s;
-            int temp=1;
-            s.push(temp);
-            for(i=1;i<=T;i++)
-            {   
-                scanf("%d",&num);
-                if(0==num)
-                {
-                    flag=1;
-                    break;
-                }
-                if(s.empty())
-                {
-                    temp++;
-                    s.push(temp);
-                }
-                int top=s.top();
-                while(top!=num)
-                {
-                    if(temp==T)
-                       break;
-                    temp++;
-                    s.push(temp);
-                    top=s.top();
-                }
-                if(top==num)
-                    s.pop();
-            }
-            if(flag)
-                break;
-            if(s.empty())
-                puts("Yes");
-            else
-                puts("No");
+int main() {
+  int i, j, k, T;
+  int num, flag;
+  while (scanf("%d", &T), T) {
+    flag = 0;  //必须再次初始化为0
+    while (1) {
+      stack<int> s;
+      int temp = 1;
+      s.push(temp);
+      for (i = 1; i <= T; i++) {
+        scanf("%d", &num);
+        if (0 == num) {
+          flag = 1;
+          break;
         }
-        printf("\n");
+        if (s.empty()) {
+          temp++;
+          s.push(temp);
+        }
+        int top = s.top();
+        while (top != num) {
+          if (temp == T) break;
+          temp++;
+          s.push(temp);
+          top = s.top();
+        }
+        if (top == num) s.pop();
+      }
+      if (flag) break;
+      if (s.empty())
+        puts("Yes");
+      else
+        puts("No");
     }
-    return 0;
+    printf("\n");
+  }
+  return 0;
 }
-
-
