bef21b1802f563fbdf0ff62d1fe3106b73b92dea
sdecoder <sde.work.inbox@gmail.com>
2016-09-27 13:53:58 -0400

    2016-09-27 13:53:58

--- 2975.cc
+++ 2975.cc
@@ -1,36 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int a[1002];
-int main()
-{
-    int n, x;
-    while(cin >> n && n)
-    {
-        int res = 0, cnt = 0;
-        for(int i = 0; i < n; i++)
-        {
-            cin>>a[i];
-            res ^= a[i];
-        }
-        if(res)
-        {
-            for(int i = 0; i < n; i++)
-            {
-                if((res ^ a[i]) <= a[i])  
-                {
-                    cnt++;
-                }
-            }
-            cout << cnt << endl;
-        }
-        else
-        {
-            cout << res << endl;
-        }
-
+int main() {
+  int n, x;
+  while (cin >> n && n) {
+    int res = 0, cnt = 0;
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
+      res ^= a[i];
     }
-    return 0;
+    if (res) {
+      for (int i = 0; i < n; i++) {
+        if ((res ^ a[i]) <= a[i]) {
+          cnt++;
+        }
+      }
+      cout << cnt << endl;
+    } else {
+      cout << res << endl;
+    }
+  }
+  return 0;
 }
-
-
-
