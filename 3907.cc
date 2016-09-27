1621f173dd5ed58cf9e2c6ebebc02e9ee6becea7
sdecoder <sde.work.inbox@gmail.com>
2016-09-27 13:42:04 -0400

    2016-09-27 13:42:04

--- 3907.cc
+++ 3907.cc
@@ -1,45 +1,35 @@
 #include <stdio.h>
 #include <math.h>
 #define MAXN 1000
-typedef struct nDate
-{
-	double x, y;
-}nNode;
+typedef struct nDate { double x, y; } nNode;
 nNode nP[MAXN];
 int nNum;
 double ans;
 double funcSolve();
 double procGetArea(int i, int j, int k);
-int main()
-{
-	int i;
-	while (1 == scanf("%d", &nNum), nNum)
-	{
-		for (i = 0; i != nNum; ++i)
-		{
-			scanf("%lf %lf", &nP[i].x, &nP[i].y);
-		}
-		printf("%.lf\n", funcSolve() / 2);
-	}
-	return 0;
+int main() {
+  int i;
+  while (1 == scanf("%d", &nNum), nNum) {
+    for (i = 0; i != nNum; ++i) {
+      scanf("%lf %lf", &nP[i].x, &nP[i].y);
+    }
+    printf("%.lf\n", funcSolve() / 2);
+  }
+  return 0;
 }
-double funcSolve()
-{
-	int i;
-	double nArea = 0;
-	if (nNum < 3) return 0;
-	nNum -= 2;
-	for (i = 0; i != nNum; ++i)
-	{
-		nArea += procGetArea(0, i + 1, i + 2);
-	}
-	if (nArea < 0) nArea = - nArea;
-	return nArea;
+double funcSolve() {
+  int i;
+  double nArea = 0;
+  if (nNum < 3) return 0;
+  nNum -= 2;
+  for (i = 0; i != nNum; ++i) {
+    nArea += procGetArea(0, i + 1, i + 2);
+  }
+  if (nArea < 0) nArea = -nArea;
+  return nArea;
 }
-double procGetArea(int i, int j, int k)
-{
-	double s = (nP[k].y - nP[i].y) * (nP[j].x - nP[i].x) - (nP[j].y - nP[i].y) * (nP[k].x - nP[i].x);   
-    return s;  
+double procGetArea(int i, int j, int k) {
+  double s = (nP[k].y - nP[i].y) * (nP[j].x - nP[i].x) -
+             (nP[j].y - nP[i].y) * (nP[k].x - nP[i].x);
+  return s;
 }
-
-
