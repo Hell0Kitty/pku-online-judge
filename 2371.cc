4b4f97e3611b4c3511a6deb148747f6287818007
sdecoder <sde.work.inbox@gmail.com>
2016-10-01 04:07:41 -0400

    2016-10-01 04:07:41

--- 2371.cc
+++ 2371.cc
@@ -9,23 +9,17 @@ using namespace std;
 
 int n, m, f[maxn];
 
-int main()
-{
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &f[i]);
-    sort(f, f + n);
-    char st[30];
-    scanf("%s", st);
-    scanf("%d", &m);
-    for (int i = 0; i < m; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        printf("%d\n", f[a - 1]);
-    }
-    return 0;
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d", &f[i]);
+  sort(f, f + n);
+  char st[30];
+  scanf("%s", st);
+  scanf("%d", &m);
+  for (int i = 0; i < m; i++) {
+    int a;
+    scanf("%d", &a);
+    printf("%d\n", f[a - 1]);
+  }
+  return 0;
 }
-
-
-
