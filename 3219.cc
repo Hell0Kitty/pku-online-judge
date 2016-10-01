36cd9f5412ccfc61d61a21eed4c72342da9dafc4
sdecoder <sde.work.inbox@gmail.com>
2016-10-01 17:26:49 -0400

    2016-10-01 17:26:49

--- 3219.cc
+++ 3219.cc
@@ -1,26 +1,24 @@
-#include<stdio.h>
-int main()
-{
-	int a, b;
-	while (scanf("%d%d", &a, &b) == 2) {
-		printf("%d\n", (a - b) & b ? 0 : 1);
-		continue;
-		int c = a - b;
-		int x = 0, y = 0, z = 0;
-		while (a = a >> 1) {
-			x += a;
-		}
-		while (b = b >> 1) {
-			y += b;
-		}
-		while (c = c >> 1) {
-			z += c;
-		}
-		if (x - y > z) printf("0\n");
-		else printf("1\n");
-	}
-    return 0;
+#include <stdio.h>
+int main() {
+  int a, b;
+  while (scanf("%d%d", &a, &b) == 2) {
+    printf("%d\n", (a - b) & b ? 0 : 1);
+    continue;
+    int c = a - b;
+    int x = 0, y = 0, z = 0;
+    while (a = a >> 1) {
+      x += a;
+    }
+    while (b = b >> 1) {
+      y += b;
+    }
+    while (c = c >> 1) {
+      z += c;
+    }
+    if (x - y > z)
+      printf("0\n");
+    else
+      printf("1\n");
+  }
+  return 0;
 }
-
-
-
