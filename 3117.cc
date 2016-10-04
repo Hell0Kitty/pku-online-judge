18c18d2716ad2ef02fa5943c69701f4531ca7022
sdecoder <sde.work.inbox@gmail.com>
2016-10-04 03:32:44 -0400

    2016-10-04 03:32:44

--- 3117.cc
+++ 3117.cc
@@ -4,25 +4,20 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
-    int n, t;
-    while (scanf("%d%d", &t, &n) != EOF && !(t == 0 && n == 0))
-    {
-        int sum = 0;
-        getchar();
-        for (int i = 0; i < t; i++)
-        {
-            string st;
-            int a;
-            cin >> st;
-            scanf("%d", &a);
-            sum += a;
-            getchar();
-        }
-        printf("%d\n", 3 * n - sum);
+int main() {
+  int n, t;
+  while (scanf("%d%d", &t, &n) != EOF && !(t == 0 && n == 0)) {
+    int sum = 0;
+    getchar();
+    for (int i = 0; i < t; i++) {
+      string st;
+      int a;
+      cin >> st;
+      scanf("%d", &a);
+      sum += a;
+      getchar();
     }
-    return 0;
+    printf("%d\n", 3 * n - sum);
+  }
+  return 0;
 }
-
-
