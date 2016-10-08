ab454ae476f1dc50a184ffaef7079f27ec144f6f
sdecoder <sde.work.inbox@gmail.com>
2016-10-07 23:41:28 -0400

    2016-10-07 23:41:28

--- 2185.cc
+++ 2185.cc
@@ -1,72 +1,71 @@
-#include<cstdio>
-#include<cstring>
-#include<iostream>
-#include<vector>
-#include<cmath>
-#include<algorithm>
-#include<cmath>
-#include<cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
 using namespace std;
-const int N = 10000+10;
+const int N = 10000 + 10;
 int f[N];
-void getFail(char *s,int f[]) {
-    int m = strlen(s);
-    f[0] = -1;
-    for (int i = 1; i < m; i++) {
-        int j = f[i-1];
-        while (j != -1 && s[j+1] != s[i]) j = f[j];
-        if (s[j+1] == s[i]) j++;
-        f[i] = j;
-    }
+void getFail(char *s, int f[]) {
+  int m = strlen(s);
+  f[0] = -1;
+  for (int i = 1; i < m; i++) {
+    int j = f[i - 1];
+    while (j != -1 && s[j + 1] != s[i]) j = f[j];
+    if (s[j + 1] == s[i]) j++;
+    f[i] = j;
+  }
 }
-int n,m;
+int n, m;
 char mz[N][80];
 int cnt[N];
-int check(char *s,int x) {
-    int j = 0;
-    for (int i = 0; i < m; i++) {
-        if (s[i] != s[j]) return 0;
-        j++;
-        if (j > x) j = 0;
-    }
-    return 1;
+int check(char *s, int x) {
+  int j = 0;
+  for (int i = 0; i < m; i++) {
+    if (s[i] != s[j]) return 0;
+    j++;
+    if (j > x) j = 0;
+  }
+  return 1;
 }
-void solve(int r){
-    f[0] = -1;
-    for (int i = 1; i < n; i++) {
-        int j = f[i-1];
-        while (j != -1 && strcmp(mz[j+1],mz[i])) j = f[j];
-        if (strcmp(mz[j+1],mz[i]) == 0) j++;
-        f[i] = j;
-    }
-    printf("%d\n",(n - 1 - f[n-1]) * (r));
+void solve(int r) {
+  f[0] = -1;
+  for (int i = 1; i < n; i++) {
+    int j = f[i - 1];
+    while (j != -1 && strcmp(mz[j + 1], mz[i])) j = f[j];
+    if (strcmp(mz[j + 1], mz[i]) == 0) j++;
+    f[i] = j;
+  }
+  printf("%d\n", (n - 1 - f[n - 1]) * (r));
 }
-int main(){
-    while (~scanf("%d%d",&n,&m)) {
-        memset(cnt,0,sizeof(cnt));
+int main() {
+  while (~scanf("%d%d", &n, &m)) {
+    memset(cnt, 0, sizeof(cnt));
 
-        for (int i = 0; i < n; i++) {
-            scanf("%s",mz[i]);
-            getFail(mz[i],f);
-            int j = m-1;
-            while (j != -1) {
-              //  cout<<m - 1 - f[j]<<endl;
-                cnt[m - 1 - f[j]]++;
-                j = f[j];
-            }
-        }
-        int r = 0;
-        for (int i = 1; i <= m; i++) if (cnt[i] == n) {
-            r = i; break;
-        }
-        for (int i = 0; i < n; i++) {
-            mz[i][r] = 0;
-        }
-
-        solve(r);
+    for (int i = 0; i < n; i++) {
+      scanf("%s", mz[i]);
+      getFail(mz[i], f);
+      int j = m - 1;
+      while (j != -1) {
+        //  cout<<m - 1 - f[j]<<endl;
+        cnt[m - 1 - f[j]]++;
+        j = f[j];
+      }
     }
-    return 0;
+    int r = 0;
+    for (int i = 1; i <= m; i++)
+      if (cnt[i] == n) {
+        r = i;
+        break;
+      }
+    for (int i = 0; i < n; i++) {
+      mz[i][r] = 0;
+    }
+
+    solve(r);
+  }
+  return 0;
 }
-
-
-
