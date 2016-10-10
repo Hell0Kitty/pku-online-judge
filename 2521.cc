f5318f498df7e7f86fa927f3b93b622da76696e5
sdecoder <sde.work.inbox@gmail.com>
2016-10-10 19:21:55 -0400

    2016-10-10 19:21:55

--- 2521.cc
+++ 2521.cc
@@ -4,13 +4,10 @@
 #include <cstdio>
 using namespace std;
 
-int main()
-{
-      int m, p, n, c;
-    while (scanf("%d%d%d%d", &n, &m, &p, &c) != EOF && !(n == 0 && m == 0 && p == 0 && c == 0))
-        printf("%d\n", -(m - n - p));
-    return 0;
+int main() {
+  int m, p, n, c;
+  while (scanf("%d%d%d%d", &n, &m, &p, &c) != EOF &&
+         !(n == 0 && m == 0 && p == 0 && c == 0))
+    printf("%d\n", -(m - n - p));
+  return 0;
 }
-
-
-
