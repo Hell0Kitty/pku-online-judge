5ee291e72e54fbd6f5df13dbea97b7adb6128fad
sdecoder <sde.work.inbox@gmail.com>
2016-10-12 18:41:37 -0400

    2016-10-12 18:41:37

--- 2959.cc
+++ 2959.cc
@@ -9,23 +9,17 @@ using namespace std;
 
 double d, D, r, s;
 
-int main()
-{
-
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        scanf("%lf%lf%lf", &D, &d, &s);
-        d /= 2;
-        D /= 2;
-        r = D - d;
-        d = 2 * d + s;
-        int ans = (int)(2 * pi / acos((r * r * 2 - d * d) / (2 * r * r)));
-        printf("%d\n", ans);
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%lf%lf%lf", &D, &d, &s);
+    d /= 2;
+    D /= 2;
+    r = D - d;
+    d = 2 * d + s;
+    int ans = (int)(2 * pi / acos((r * r * 2 - d * d) / (2 * r * r)));
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
-
