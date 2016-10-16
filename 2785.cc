83d609ea4477c189b405a5a84ce6fdde546ff0a8
sdecoder <sde.work.inbox@gmail.com>
2016-10-16 12:19:31 -0400

    2016-10-16 12:19:31

--- 2785.cc
+++ 2785.cc
@@ -8,30 +8,28 @@ const int maxn = 4100;
 long long a[maxn], b[maxn], c[maxn], d[maxn];
 long long cd[maxn * maxn];
 
-int main()
-{
-	int n;
-	while (scanf("%d", &n) != EOF) {
-		for (int i = 0; i < n; i++) {
-			scanf("%lld%lld%lld%lld", &a[i], &b[i], &c[i], &d[i]);
-		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				cd[i*n+j] = c[i] + d[j];
-			}
-		}
-		sort(cd, cd + (n * n));
-		int ans = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				long long need = -(a[i] + b[j]);
-				ans += upper_bound(cd, cd + n * n, need) - lower_bound(cd, cd + n * n, need);
-			}
-		}
+int main() {
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    for (int i = 0; i < n; i++) {
+      scanf("%lld%lld%lld%lld", &a[i], &b[i], &c[i], &d[i]);
+    }
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        cd[i * n + j] = c[i] + d[j];
+      }
+    }
+    sort(cd, cd + (n * n));
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        long long need = -(a[i] + b[j]);
+        ans += upper_bound(cd, cd + n * n, need) -
+               lower_bound(cd, cd + n * n, need);
+      }
+    }
 
-		printf("%d\n", ans);
-	}
-	return 0;
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
