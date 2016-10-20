d9b91cd871703ed790a227278d7b6643fc5a3b2d
sdecoder <sde.work.inbox@gmail.com>
2016-10-20 09:46:37 -0400

    2016-10-20 09:46:37

--- 1236.cc
+++ 1236.cc
@@ -1,168 +1,146 @@
-#include<iostream>
+#include <iostream>
 
-#include<string.h>
+#include <string.h>
 
-#include<vector>
+#include <vector>
 
-#include<stack>
+#include <stack>
 
 using namespace std;
 
-vector <int> map[101];
+vector<int> map[101];
 
-stack <int> S;
+stack<int> S;
 
-int low[101],pre[101],post[101],indegree[101],outdegree[101],ss[101],cnt,time,in,out;
+int low[101], pre[101], post[101], indegree[101], outdegree[101], ss[101], cnt,
+    time, in, out;
 
 int tarjan(int s)
 
 {
+  int i, k, e;
 
-    int i,k,e;
+  pre[s] = cnt++;
+  low[s] = pre[s];
 
-    pre[s]=cnt++;low[s]=pre[s];
+  post[s] = 1;
+  S.push(s);
 
-    post[s]=1;S.push(s);
+  for (i = 0; i < map[s].size(); i++)
 
-    for(i=0;i<map[s].size();i++)
+  {
+    k = map[s][i];
+
+    if (!pre[k])
 
     {
+      tarjan(k);
 
-        k=map[s][i];
-
-        if(!pre[k])
-
-        {
-
-            tarjan(k);
-
-            low[s]=(low[s]<low[k]) ? low[s]:low[k];
-
-        }
-
-        else if(post[k] && low[s]>pre[k])
-
-        {
-
-            low[s]=pre[k];
-
-        }
+      low[s] = (low[s] < low[k]) ? low[s] : low[k];
 
     }
 
-    if(pre[s]==low[s])
+    else if (post[k] && low[s] > pre[k])
 
     {
+      low[s] = pre[k];
+    }
+  }
 
-        time++;
+  if (pre[s] == low[s])
 
-        for(e=S.top(),S.pop();e!=s;e=S.top(),S.pop())
+  {
+    time++;
 
-        {
+    for (e = S.top(), S.pop(); e != s; e = S.top(), S.pop())
 
-            ss[e]=time;  post[e]=0; 
-
-        }
-
-        ss[e]=time; post[e]=0;
-
+    {
+      ss[e] = time;
+      post[e] = 0;
     }
 
-    return 0;
+    ss[e] = time;
+    post[e] = 0;
+  }
 
+  return 0;
 }
 
 int solve(int n)
 
 {
+  int i, j, k;
 
-    int i,j,k;
+  if (time == 1)
 
-    if(time==1)
+  {
+    in = 1;
+    return 0;
+  }
 
-    {
+  memset(indegree, 0, sizeof(indegree));
 
-        in=1; return 0;
+  memset(outdegree, 0, sizeof(outdegree));
 
-    }
+  for (i = 1; i <= n; i++)
 
-    memset(indegree,0,sizeof(indegree));
-
-    memset(outdegree,0,sizeof(outdegree));
-
-    for(i=1;i<=n;i++)
-
-        for(j=0;j<map[i].size();j++)
-
-        {
-
-            k=map[i][j];
-
-            if(ss[i]!=ss[k])
-
-            {
-
-                indegree[ss[k]]++;  outdegree[ss[i]]++;
-
-            }
-
-        }
-
-    for(out=in=0,i=1;i<=time;i++)
+    for (j = 0; j < map[i].size(); j++)
 
     {
+      k = map[i][j];
 
-        if(indegree[i]==0) in++;
+      if (ss[i] != ss[k])
 
-        if(outdegree[i]==0) out++;
-
+      {
+        indegree[ss[k]]++;
+        outdegree[ss[i]]++;
+      }
     }
 
-    return in>out? in:out;
+  for (out = in = 0, i = 1; i <= time; i++)
 
+  {
+    if (indegree[i] == 0) in++;
+
+    if (outdegree[i] == 0) out++;
+  }
+
+  return in > out ? in : out;
 }
 
 int main()
 
 {
+  int i, m, n, k;
 
-    int i,m,n,k;
+  while (cin >> n)
 
-    while(cin>>n)
+  {
+    for (i = 1; i <= n; i++)
 
-    {
+      while (cin >> m && m)
 
-        for(i=1;i<=n;i++)
+        if (m != i) map[i].push_back(m);
 
-            while(cin>>m && m)
+    memset(low, 0, sizeof(low));
 
-                if(m!=i) map[i].push_back(m);
+    memset(pre, 0, sizeof(pre));
 
-        memset(low,0,sizeof(low));
+    memset(post, 0, sizeof(post));
 
-        memset(pre,0,sizeof(pre));
+    memset(ss, 0, sizeof(ss));
 
-        memset(post,0,sizeof(post));
+    for (cnt = 1, time = 0, i = 1; i <= n; i++)
 
-        memset(ss,0,sizeof(ss));
+      if (!pre[i]) tarjan(i);
 
-        for(cnt=1,time=0,i=1;i<=n;i++)
+    k = solve(n);
 
-            if(!pre[i]) tarjan(i);
+    cout << in << endl
+         << k << endl;
 
-        k=solve(n);
+    for (i = 0; i <= n; i++) map[i].clear();
+  }
 
-        cout<<in<<endl<<k<<endl;
-
-        for(i=0;i<=n;i++)
-
-            map[i].clear();
-
-    }
-
-    return 0;
-
+  return 0;
 }
-
-
-
