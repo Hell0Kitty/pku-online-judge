2d6f481dea3b6e34cebadb18f4c05835a046e1a8
sdecoder <sde.work.inbox@gmail.com>
2016-10-21 03:51:37 -0400

    2016-10-21 03:51:37

--- 3790.cc
+++ 3790.cc
@@ -1,24 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int f[1005];
-int main()
-{
-    int n,m,i,j;
-    f[0]=1;
-    f[1]=1;
-    for(j=2; j<=1000; j++)
-    {
-        f[j]=0;
-        for(i=0; i<=j/2; i++) f[j]+=f[i];
-    }
-    scanf("%d",&n);
-    for(i=1; i<=n; i++)
-    {
-        scanf("%d",&m);
-        printf("%d %d\n",i,f[m]);
-    }
-    return 0;
+int main() {
+  int n, m, i, j;
+  f[0] = 1;
+  f[1] = 1;
+  for (j = 2; j <= 1000; j++) {
+    f[j] = 0;
+    for (i = 0; i <= j / 2; i++) f[j] += f[i];
+  }
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++) {
+    scanf("%d", &m);
+    printf("%d %d\n", i, f[m]);
+  }
+  return 0;
 }
-
-
-
-
