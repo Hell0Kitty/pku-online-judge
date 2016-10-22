4d1260b264de8b5b6f2cbb9770f0b826cd4a84fd
sdecoder <sde.work.inbox@gmail.com>
2016-10-22 02:14:46 -0400

    2016-10-22 02:14:46

--- 2389.cc
+++ 2389.cc
@@ -7,39 +7,29 @@
 #include <queue>
 using namespace std;
 
-int main()
-{
-    char a[50] , b[50] ;
-    int i , j , c[100] , la , lb , lc  ;
-    scanf(" %s %s",a,b);
-    la = strlen(a);
-    lb = strlen(b);
-    memset(c,0,sizeof(c));
-    for ( i = la-1 ; i >= 0 ; i--)
-        for ( j = lb-1 ; j >= 0 ; j--)
-        {
-            c[la-1-i+lb-1-j] += (a[i] - '0') * (b[j] - '0' );
-        }
-    lc = 0 ;
-    while ( lc < 100 )
-    {
-        if ( c[lc] > 9)
-        {
-            c[lc+1] += c[lc] /10;
-            c[lc] %= 10;
-        }
-        lc++;
+int main() {
+  char a[50], b[50];
+  int i, j, c[100], la, lb, lc;
+  scanf(" %s %s", a, b);
+  la = strlen(a);
+  lb = strlen(b);
+  memset(c, 0, sizeof(c));
+  for (i = la - 1; i >= 0; i--)
+    for (j = lb - 1; j >= 0; j--) {
+      c[la - 1 - i + lb - 1 - j] += (a[i] - '0') * (b[j] - '0');
     }
-    for ( lc = 99 ; lc > 0 ; lc--)
-        if ( c[lc] )
-            break;
-    for ( i = lc ; i >= 0 ; i--)
-    {
-
-        printf("%d",c[i]);
+  lc = 0;
+  while (lc < 100) {
+    if (c[lc] > 9) {
+      c[lc + 1] += c[lc] / 10;
+      c[lc] %= 10;
     }
-    return 0;
+    lc++;
+  }
+  for (lc = 99; lc > 0; lc--)
+    if (c[lc]) break;
+  for (i = lc; i >= 0; i--) {
+    printf("%d", c[i]);
+  }
+  return 0;
 }
-
-
-
