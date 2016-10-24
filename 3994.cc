40ff1d7142c8ade6f7b2c944056054612ed0c4ff
sdecoder <sde.work.inbox@gmail.com>
2016-10-23 20:20:16 -0400

    2016-10-23 20:20:16

--- 3994.cc
+++ 3994.cc
@@ -1,13 +1,9 @@
-#include<stdio.h>
-int main()
-{
-	int i=0,n;
-	while(scanf("%d",&n),n)
-	{
-		i++;
-		printf("%d. %s %d\n",i,n%2?"odd":"even",n/2);
-	}
-        return 0;
+#include <stdio.h>
+int main() {
+  int i = 0, n;
+  while (scanf("%d", &n), n) {
+    i++;
+    printf("%d. %s %d\n", i, n % 2 ? "odd" : "even", n / 2);
+  }
+  return 0;
 }
-
-
