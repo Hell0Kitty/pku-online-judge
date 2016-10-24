eebd45101857fad2f2bae2d5640a989182406d88
sdecoder <sde.work.inbox@gmail.com>
2016-10-24 19:37:26 -0400

    2016-10-24 19:37:26

---
+++ 2323.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long dp[19][200]= {0};
+    dp[1][0]=1;
+    for(int i=1; i<18; i++)
+        for(int j=0; dp[i][j]; j++)
+            for(int k=0; k<=i; k++)
+                dp[i+1][j+k]+=dp[i][j];
+    int x,y;
+    while(cin>>x>>y,x)
+    cout<<dp[x][y]<<endl;
+}
+
+
+
