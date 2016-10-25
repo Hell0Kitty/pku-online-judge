d3183672e9a323871f95cfa4aeb4994debeaead1
sdecoder <sde.work.inbox@gmail.com>
2016-10-24 23:20:17 -0400

    2016-10-24 23:20:17

--- 2403.cc
+++ 2403.cc
@@ -1,33 +1,29 @@
-#include<cstdio>
-#include<iostream>
-#include<string>
-#include<map>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
-map<string,int> ma;
+map<string, int> ma;
 
-int main(){
-    int m,n;
-    cin>>m>>n;
-    while(m--){
-        string a;
-        int b;
-        cin>>a;
-        scanf("%d",&b);
-        ma[a]=b;
+int main() {
+  int m, n;
+  cin >> m >> n;
+  while (m--) {
+    string a;
+    int b;
+    cin >> a;
+    scanf("%d", &b);
+    ma[a] = b;
+  }
+  while (n--) {
+    string a;
+    int sum = 0;
+    while (cin >> a) {
+      if (a == ".") break;
+      // cout<<"de:"<<ma[a]<<endl;
+      sum += ma[a];
     }
-    while(n--){
-        string a;
-        int sum=0;
-        while(cin>>a){
-            if(a==".")  break;
-            //cout<<"de:"<<ma[a]<<endl;
-            sum+=ma[a];
-        }
-        printf("%d\n",sum);
-
-    }
+    printf("%d\n", sum);
+  }
 }
-
-
-
