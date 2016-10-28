0926010f007660e99015e26f264de5f7aa3246f8
sdecoder <sde.work.inbox@gmail.com>
2016-10-28 16:34:55 -0400

    2016-10-28 16:34:55

--- 2531.cc
+++ 2531.cc
@@ -19,51 +19,40 @@ int a[21][21];
 int nx, ny;
 int x[21], y[21];
 
-int cal()
-{
-    int sum = 0;
-    for (int i = 0; i < nx; ++i)
-    {
-        for (int j = 0; j < ny; ++j)
-        {
-            if (a[x[i]][y[j]] != -1)
-                sum += a[x[i]][y[j]];
-        }
+int cal() {
+  int sum = 0;
+  for (int i = 0; i < nx; ++i) {
+    for (int j = 0; j < ny; ++j) {
+      if (a[x[i]][y[j]] != -1) sum += a[x[i]][y[j]];
     }
-    return sum;
+  }
+  return sum;
 }
 
-int dfs(int i)
-{
-    int ii, jj;
-    if (i == n)
-    {
-        return cal();
-    }
-    x[nx] = i;
-    nx++;
-    ii = dfs(i+1);
-    nx--;
-    y[ny] = i;
-    ny++;
-    jj = dfs(i+1);
-    ny--;
-    return max(ii, jj);
+int dfs(int i) {
+  int ii, jj;
+  if (i == n) {
+    return cal();
+  }
+  x[nx] = i;
+  nx++;
+  ii = dfs(i + 1);
+  nx--;
+  y[ny] = i;
+  ny++;
+  jj = dfs(i + 1);
+  ny--;
+  return max(ii, jj);
 }
 
-int main()
-{
-    while (scanf("%d", &n) != EOF)
-    {
-        memset(a, -1, sizeof(a));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < n; ++j)
-                scanf("%d", &a[i][j]);
-        nx = 0; ny = 0;
-        printf("%d\n", dfs(0));
-    }
-    return 0;
+int main() {
+  while (scanf("%d", &n) != EOF) {
+    memset(a, -1, sizeof(a));
+    for (int i = 0; i < n; ++i)
+      for (int j = 0; j < n; ++j) scanf("%d", &a[i][j]);
+    nx = 0;
+    ny = 0;
+    printf("%d\n", dfs(0));
+  }
+  return 0;
 }
-
-
-
