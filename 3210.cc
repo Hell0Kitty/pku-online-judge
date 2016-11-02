6950ef7f9d23f75da7c9769b968edd4114c92b3b
sdecoder <sde.work.inbox@gmail.com>
2016-11-02 13:39:55 -0400

    2016-11-02 13:39:55

--- 3210.cc
+++ 3210.cc
@@ -2,18 +2,14 @@
 
 using namespace std;
 
-int main()
-{
-    int n;
-    while(cin >> n , n != 0) {
-        if(n % 2 == 0)
-            cout << "No Solution!" << endl;
-        else
-            cout << n - 1 << endl;
-    }
+int main() {
+  int n;
+  while (cin >> n, n != 0) {
+    if (n % 2 == 0)
+      cout << "No Solution!" << endl;
+    else
+      cout << n - 1 << endl;
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
