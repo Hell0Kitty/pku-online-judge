75614c31e749d394e4c88283927f775d6fc61da8
sdecoder <sde.work.inbox@gmail.com>
2016-11-06 16:21:46 -0500

    2016-11-06 16:21:46

--- 3427.cc
+++ 3427.cc
@@ -10,20 +10,13 @@ using namespace std;
 int n, m;
 int f[maxn], sum[maxn];
 
-int main()
-{
-    scanf("%d%d", &n, &m);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &f[i]);
-    for (int i = 1; i <= n; i++)
-        f[i] = f[i] % m;
-    int ans = 0;
-    for (int i = 1; i <= n; i++)
-        if (f[i] != 0)
-            ans = max(ans, m - f[i]);
-    printf("%d\n", ans);
-    return 0;
+int main() {
+  scanf("%d%d", &n, &m);
+  for (int i = 1; i <= n; i++) scanf("%d", &f[i]);
+  for (int i = 1; i <= n; i++) f[i] = f[i] % m;
+  int ans = 0;
+  for (int i = 1; i <= n; i++)
+    if (f[i] != 0) ans = max(ans, m - f[i]);
+  printf("%d\n", ans);
+  return 0;
 }
-
-
-
