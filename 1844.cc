c605ecf6f9e6006c6d7310c56535cc3792b07423
sdecoder <sde.work.inbox@gmail.com>
2016-11-08 06:32:54 -0500

    2016-11-08 06:32:54

---
+++ 1844.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+int n;
+    scanf("%d", &n);
+    int sum =0;
+    int i =0;
+    while (sum < n || (sum - n) &1)
+    {
+        i++;
+        sum += i;
+    }
+    printf("%d\n", i);
+    return 0;
+}
+
+
+
+
