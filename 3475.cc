0d4f435b259ef2a1359ffae1c2bf83d7fff8e706
sdecoder <sde.work.inbox@gmail.com>
2016-11-10 17:41:14 -0500

    2016-11-10 17:41:14

---
+++ 3475.cc
@@ -0,0 +1,31 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    double a,b,c,d;
+    while(scanf("%lf%lf%lf%lf",&a,&b,&c,&d)!=EOF)
+    {
+        int ans=0;
+        if(a>b)
+            swap(a,b);
+        if(c>d)
+            swap(c,d);
+        while(c>a+1e-8)
+        {
+            ans++;
+            c/=2.0;
+        }
+        while(d>b+1e-8)
+        {
+            ans++;
+            d/=2.0;
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}
+
+
+
