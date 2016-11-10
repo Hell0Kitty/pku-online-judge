313116b67e813f6806b37fe799df1429efee0d05
sdecoder <sde.work.inbox@gmail.com>
2016-11-10 17:11:10 -0500

    2016-11-10 17:11:10

--- 2664.cc
+++ 2664.cc
@@ -1,44 +1,35 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
-	int i,j,r,c;
-	int k,m,a,b,flag;
-	int map[10000];
+int main() {
+  int i, j, r, c;
+  int k, m, a, b, flag;
+  int map[10000];
 
-	while(scanf("%d",&k)!=EOF,k)
-	{
-		memset(map,0,sizeof map);
-		scanf("%d",&m);
-		for(i=0;i<k;i++)
-		{
-			scanf("%d",&a);
-			map[a]++;
-		}
-		flag=1;
-		int count;
-		for(i=0;i<m;i++)
-		{
-			scanf("%d %d",&r,&c);
-			count=0;
-			for(j=0;j<r;j++)
-			{
-				scanf("%d",&b);
-				if(map[b]>0)//娌￠�
-					count++;
-			}
-			if(count<c)
-				flag=0;
-		}
-		if(flag==0)
-			puts("no");
-		else
-			puts("yes");
-	}
+  while (scanf("%d", &k) != EOF, k) {
+    memset(map, 0, sizeof map);
+    scanf("%d", &m);
+    for (i = 0; i < k; i++) {
+      scanf("%d", &a);
+      map[a]++;
+    }
+    flag = 1;
+    int count;
+    for (i = 0; i < m; i++) {
+      scanf("%d %d", &r, &c);
+      count = 0;
+      for (j = 0; j < r; j++) {
+        scanf("%d", &b);
+        if (map[b] > 0)  //娌￠�
+          count++;
+      }
+      if (count < c) flag = 0;
+    }
+    if (flag == 0)
+      puts("no");
+    else
+      puts("yes");
+  }
 
-return 0;
+  return 0;
 }
-
-
-
