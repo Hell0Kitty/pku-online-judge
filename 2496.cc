9fe54d49f518364605be67d3eca80a636e1bc8c7
sdecoder <sde.work.inbox@gmail.com>
2016-11-10 04:16:58 -0500

    2016-11-10 04:16:58

--- 2496.cc
+++ 2496.cc
@@ -11,73 +11,59 @@
 
 using namespace std;
 
-int data[100+4][100+4];
+int data[100 + 4][100 + 4];
 
-int dis[100*100+4];
+int dis[100 * 100 + 4];
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+  int t;
+  int n, m;
 
-        int t;
-        int n,m;
+  int i, j, k;
 
-        int i,j,k;
+  scanf("%d", &t);
 
-        scanf("%d",&t);
+  int p;
 
-        int p;
+  int z;
+  for (z = 1; z <= t; z++) {
+    scanf("%d", &p);
+    scanf("%d", &n);
 
-        int z;
-        for(z=1;z<=t;z++)
-        {
-                         scanf("%d",&p);
-                         scanf("%d",&n);
+    for (i = 1; i <= n; i++) {
+      for (j = 1; j <= n; j++) {
+        scanf("%d", &data[i][j]);
+        if (data[i][j] <= 0) data[i][j] = MAXINT;
+      }
+    }
 
-                         for(i=1;i<=n;i++)
-                         {
-                                          for(j=1;j<=n;j++)
-                                          {
-                                                           scanf("%d",&data[i][j]);
-                                                           if(data[i][j]<=0)
-                                                           data[i][j]=MAXINT;
-                                          }
-                         }
-
-                         for(k=1;k<=n;k++)
-                         for(i=1;i<=n;i++)
-                         for(j=1;j<=n;j++)
-                         {if(data[i][j]>data[i][k]+data[k][j])
-                          data[i][j]=data[i][k]+data[k][j];
-                         }
-
-                         k=0;
-                         for(i=1;i<=n;i++)
-                         for(j=1;j<=n;j++)
-                         {
-
-                                          if(i==j)
-                                          continue;
-                                          if(data[i][j]>=MAXINT)
-                                          continue;
-                                          dis[k]=data[i][j];
-                         k++;
-                         }
-
-                             sort(dis,dis+k);
-
-                             k=n*(n-1)*p;
-
-                             if(k%100==0)
-                             k=k/100;
-                             else
-                             k=k/100+1;
-
-                             printf("Scenario #%d:\n%d\n\n",z,dis[k-1]);
-
+    for (k = 1; k <= n; k++)
+      for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++) {
+          if (data[i][j] > data[i][k] + data[k][j])
+            data[i][j] = data[i][k] + data[k][j];
         }
 
-    return EXIT_SUCCESS;
+    k = 0;
+    for (i = 1; i <= n; i++)
+      for (j = 1; j <= n; j++) {
+        if (i == j) continue;
+        if (data[i][j] >= MAXINT) continue;
+        dis[k] = data[i][j];
+        k++;
+      }
+
+    sort(dis, dis + k);
+
+    k = n * (n - 1) * p;
+
+    if (k % 100 == 0)
+      k = k / 100;
+    else
+      k = k / 100 + 1;
+
+    printf("Scenario #%d:\n%d\n\n", z, dis[k - 1]);
+  }
+
+  return EXIT_SUCCESS;
 }
-
-
-
