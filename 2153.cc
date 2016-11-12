81bbeb053f096d6794c85a5010f2a0bbe13b7c4b
sdecoder <sde.work.inbox@gmail.com>
2016-11-12 02:46:08 -0500

    2016-11-12 02:46:08

--- 2153.cc
+++ 2153.cc
@@ -10,45 +10,36 @@ using namespace std;
 char li[40];
 int n, m;
 string name[maxn];
-map <string, int> score;
+map<string, int> score;
 
-int main()
-{
-    scanf("%d", &n);
-    getchar();
+int main() {
+  scanf("%d", &n);
+  getchar();
+  for (int i = 0; i < n; i++) {
+    gets(li);
+    name[i] = li;
+    string st(li);
+    score[st] = 0;
+  }
+  scanf("%d", &m);
+  getchar();
+  char na[40];
+  strcpy(li, "Li Ming");
+  int mark;
+  while (m--) {
+    for (int i = 0; i < n; i++) {
+      int a;
+      scanf("%d", &a);
+      getchar();
+      gets(na);
+      string st(na);
+      score[st] += a;
+      if (strcmp(na, li) == 0) mark = score[st];
+    }
+    int rank = 1;
     for (int i = 0; i < n; i++)
-    {
-        gets(li);
-        name[i] = li;
-        string st(li);
-        score[st] = 0;
-    }
-    scanf("%d", &m);
-    getchar();
-    char na[40];
-    strcpy(li, "Li Ming");
-    int mark;
-    while (m--)
-    {
-        for (int i = 0; i < n; i++)
-        {
-            int a;
-            scanf("%d", &a);
-            getchar();
-            gets(na);
-            string st(na);
-            score[st] += a;
-            if (strcmp(na, li) == 0)
-                mark = score[st];
-        }
-        int rank = 1;
-        for (int i = 0; i < n; i++)
-            if (score[name[i]] > mark)
-                rank++;
-        printf("%d\n", rank);
-    }
-    return 0;
+      if (score[name[i]] > mark) rank++;
+    printf("%d\n", rank);
+  }
+  return 0;
 }
-
-
-
