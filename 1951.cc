83c1c0a7b8a2edbb1ea30a943950be01900cdbb4
sdecoder <sde.work.inbox@gmail.com>
2016-11-13 03:57:49 -0500

    2016-11-13 03:57:49

---
+++ 1951.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+
+char st[100];
+bool vis[300];
+
+int main()
+{
+    bool first = true;
+    memset(vis, 0, sizeof(vis));
+    vis['A'] = true;
+    vis['E'] = true;
+    vis['I'] = true;
+    vis['O'] = true;
+    vis['U'] = true;
+    while (scanf("%s", st) != EOF)
+    {
+        int len = strlen(st);
+        bool did = false;
+        for (int i = 0; i < len; i++)
+            if (!vis[st[i]])
+            {
+                if (st[i] <= 'Z' && st[i] >= 'A')
+                    vis[st[i]] = true;
+                if (!did)
+                {
+                    did = true;
+                    if (st[i] <= 'Z' && st[i] >= 'A')
+                    {
+                        if (first)
+                            first = false;
+                        else
+                            putchar(' ');
+                    }
+                }
+                putchar(st[i]);
+            }
+    }
+    putchar('\n');
+    return 0;
+}
+
+
+
