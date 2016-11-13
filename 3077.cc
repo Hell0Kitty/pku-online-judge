84efad63ddd823abbdf504a10d8dd41383a25772
sdecoder <sde.work.inbox@gmail.com>
2016-11-13 01:30:30 -0500

    2016-11-13 01:30:30

---
+++ 3077.cc
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int t,n,k1,k2,k3,r,ans,gun;
+   scanf("%d",&t);
+   while(t--)
+   {
+     scanf("%d",&n);
+     k3=k2=k1=n;
+     r=1;
+     ans=n;
+     while(k1>=10)
+     {
+         gun=ans/r;
+         gun=gun%10;
+         r=r*10;
+         ans=ans/r;
+         if(gun>=5)
+         {
+             ans=ans+1;
+             ans=ans*r;
+         }
+         else
+         {
+             ans=ans*r;
+         }
+         k1=k1/10;
+     }
+     printf("%d\n",ans);
+   }
+    return 0;
+}
+
+
+
