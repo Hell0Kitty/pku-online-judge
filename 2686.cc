46f0e9e68b4e966be59e1cc89b8e51fe5cb8cb4b
sdecoder <sde.work.inbox@gmail.com>
2016-11-13 21:03:22 -0500

    2016-11-13 21:03:22

--- 2686.cc
+++ 2686.cc
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 #define INF 1000000000
 #define MAXN 40
 using namespace std;
@@ -10,52 +10,38 @@ int d[MAXN][MAXN];
 int t[MAXN];
 int n, m, p, a, b;
 
-void solve()
-{
-	for (int i = 0; i<(1 << n); i++)
-		fill(dp[i], dp[i] + m, INF);
-	dp[(1 << n) - 1][a-1] = 0;
-	double res = INF;
-	for (int s = (1 << n) - 1; s >= 0; s--)
-	{
-		res = min(res, dp[s][b - 1]);
-		for (int v = 0; v < m; v++)
-			for (int i = 0; i < n; i++)
-			{
-			if (s >> i & 1)
-			{
-				for (int u = 0; u < m; u++)
-					if (d[v][u] >= 0)
-						dp[s&~(1 << i)][u] = min(dp[s&~(1 << i)][u], dp[s][v] + (double)d[v][u] / t[i]);
-
-			}
-			}
-	}
-	if (res == INF)
-		cout << "Impossible" << endl;
-	else
-		printf("%.3f\n", res);
-
+void solve() {
+  for (int i = 0; i < (1 << n); i++) fill(dp[i], dp[i] + m, INF);
+  dp[(1 << n) - 1][a - 1] = 0;
+  double res = INF;
+  for (int s = (1 << n) - 1; s >= 0; s--) {
+    res = min(res, dp[s][b - 1]);
+    for (int v = 0; v < m; v++)
+      for (int i = 0; i < n; i++) {
+        if (s >> i & 1) {
+          for (int u = 0; u < m; u++)
+            if (d[v][u] >= 0)
+              dp[s & ~(1 << i)][u] =
+                  min(dp[s & ~(1 << i)][u], dp[s][v] + (double)d[v][u] / t[i]);
+        }
+      }
+  }
+  if (res == INF)
+    cout << "Impossible" << endl;
+  else
+    printf("%.3f\n", res);
 }
 
-int main()
-{
-	int x, y, z;
-	while (cin >> n >> m >> p >> a >> b&&n || m || p || a || b)
-	{
-		for (int i = 0; i < n; i++)
-			cin >> t[i];
-		memset(d, -1, sizeof(d));
-		for (int i = 1; i <= p; i++)
-		{
-			cin >> x >> y >> z;
-			d[x-1][y-1] = d[y-1][x-1] = z;
-		}
-		solve();
-	}
-	return 0;
+int main() {
+  int x, y, z;
+  while (cin >> n >> m >> p >> a >> b && n || m || p || a || b) {
+    for (int i = 0; i < n; i++) cin >> t[i];
+    memset(d, -1, sizeof(d));
+    for (int i = 1; i <= p; i++) {
+      cin >> x >> y >> z;
+      d[x - 1][y - 1] = d[y - 1][x - 1] = z;
+    }
+    solve();
+  }
+  return 0;
 }
-
-
-
-
