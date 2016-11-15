5b369896ce02560e8b5136c5a75001d5167bf1e5
sdecoder <sde.work.inbox@gmail.com>
2016-11-15 02:32:06 -0500

    2016-11-15 02:32:06

---
+++ 2390.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int year;
+    double money,rate;
+    scanf("%lf%lf%d",&rate,&money,&year);
+    rate=pow((1+rate/100),year);
+    money=money*rate;
+    money=floor(money);
+    printf("%.0lf",money);
+
+}
+
+
+
