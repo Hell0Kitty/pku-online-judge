93d8a47648991dd986e68d5b9767c7313408e230
sdecoder <sde.work.inbox@gmail.com>
2016-11-15 19:22:46 -0500

    2016-11-15 19:22:46

--- 3484.cc
+++ 3484.cc
@@ -8,50 +8,44 @@
 #define N 500005
 typedef long long LL;
 using namespace std;
-int n ;
-LL X[N] , Y[N] , Z[N];
+int n;
+LL X[N], Y[N], Z[N];
 char str[55];
 
-LL cal(LL k)
-{
-  LL sum = 0 , x;
-  for (int i = 1 ; i <= n; ++ i)
-  {
+LL cal(LL k) {
+  LL sum = 0, x;
+  for (int i = 1; i <= n; ++i) {
     if (k < X[i]) continue;
-    x = min(k , Y[i]);
+    x = min(k, Y[i]);
     sum += (x - X[i]) / Z[i] + 1;
   }
   return sum;
 }
 
-void work()
-{
+void work() {
   n = 1;
   X[n] = 0;
-  sscanf(str , "%I64d %I64d %I64d" , &X[n] , &Y[n] , &Z[n]);
+  sscanf(str, "%I64d %I64d %I64d", &X[n], &Y[n], &Z[n]);
   if (!X[n]) return;
-  memset(str , 0 , sizeof(str));
-  while (gets(str) , *str)
-    ++ n , sscanf(str , "%I64d %I64d %I64d" , &X[n] , &Y[n] , &Z[n]) , memset(str , 0 , sizeof(str));
-  LL l = 1 , r = 1LL << 33 , m;
-  while (l < r)
-  {
+  memset(str, 0, sizeof(str));
+  while (gets(str), *str)
+    ++n, sscanf(str, "%I64d %I64d %I64d", &X[n], &Y[n], &Z[n]),
+        memset(str, 0, sizeof(str));
+  LL l = 1, r = 1LL << 33, m;
+  while (l < r) {
     m = (l + r) >> 1;
     if (cal(m) & 1)
       r = m;
-    else l = m + 1;
+    else
+      l = m + 1;
   }
   if (l == 1LL << 33)
     puts("no corruption");
-  else printf("%I64d %I64d\n" , l , (cal(l) - cal(l - 1)));
+  else
+    printf("%I64d %I64d\n", l, (cal(l) - cal(l - 1)));
 }
 
-int main()
-{
-  while(gets(str))
-    work();
+int main() {
+  while (gets(str)) work();
   return 0;
 }
-
-
-
