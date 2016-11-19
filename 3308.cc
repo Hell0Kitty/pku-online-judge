5e3e63f6e16ee940c1a96ff45ba37edc0bf7cbbc
sdecoder <sde.work.inbox@gmail.com>
2016-11-19 10:57:23 -0500

    2016-11-19 10:57:23

--- 3308.cc
+++ 3308.cc
@@ -14,125 +14,97 @@ using namespace std;
 int n, m, num;
 int s, t;
 
-struct edge
-{
-    int x, y, nxt;
-    double c;
-}bf[E];
+struct edge {
+  int x, y, nxt;
+  double c;
+} bf[E];
 
 int ne, head[N], cur[N], ps[N], dep[N];
 
-void addedge1(int x, int y, double c)
-{
-    bf[ne].x = x;
-    bf[ne].y = y;
-    bf[ne].c = c;
-    bf[ne].nxt = head[x];
-    head[x] = ne++;
+void addedge1(int x, int y, double c) {
+  bf[ne].x = x;
+  bf[ne].y = y;
+  bf[ne].c = c;
+  bf[ne].nxt = head[x];
+  head[x] = ne++;
 }
 
-void addedge(int x, int y, double c)
-{
-    addedge1(x, y, c);
-    addedge1(y, x, 0);
+void addedge(int x, int y, double c) {
+  addedge1(x, y, c);
+  addedge1(y, x, 0);
 }
 
-double flow(int n, int s, int t)
-{
-    double tr, res = 0;
-    int i, j, k, f, r, top;
-    while (1)
-    {
-        memset(dep, -1, n * sizeof(int));
-        for (f = dep[ps[0] = s] = 0, r = 1; f !=r;)
-            for (i = ps[f++], j = head[i]; j; j = bf[j].nxt)
-            {
-                if (bf[j].c && -1 == dep[k = bf[j].y])
-                {
-                    dep[k] = dep[i] + 1;
-                    ps[r++] = k;
-                    if (k == t)
-                    {
-                        f = r;
-                        break;
-                    }
-                }
-            }
-        if (-1 == dep[t])
+double flow(int n, int s, int t) {
+  double tr, res = 0;
+  int i, j, k, f, r, top;
+  while (1) {
+    memset(dep, -1, n * sizeof(int));
+    for (f = dep[ps[0] = s] = 0, r = 1; f != r;)
+      for (i = ps[f++], j = head[i]; j; j = bf[j].nxt) {
+        if (bf[j].c && -1 == dep[k = bf[j].y]) {
+          dep[k] = dep[i] + 1;
+          ps[r++] = k;
+          if (k == t) {
+            f = r;
             break;
-
-        memcpy(cur, head, n * sizeof(int));
-        for (i = s, top = 0;;)
-        {
-            if (i == t)
-            {
-                for (k = 0, tr = inf; k  < top; ++k)
-                    if (bf[ps[k]].c < tr)
-                        tr = bf[ps[f = k]].c;
-                for (k = 0; k < top; ++k)
-                    bf[ps[k]].c -=tr, bf[ps[k]^1].c += tr;
-                res += tr;
-                i = bf[ps[top = f]].x;
-            }
-            for (j = cur[i]; cur[i]; j = cur[i] = bf[cur[i]].nxt)
-                if (bf[j].c && dep[i] + 1 == dep[bf[j].y])
-                    break;
-            if (cur[i])
-            {
-                ps[top++] = cur[i];
-                i = bf[cur[i]].y;
-            }
-            else
-            {
-                if (0 == top)
-                    break;
-                dep[i] = -1;
-                i = bf[ps[--top]].x;
-            }
+          }
         }
+      }
+    if (-1 == dep[t]) break;
+
+    memcpy(cur, head, n * sizeof(int));
+    for (i = s, top = 0;;) {
+      if (i == t) {
+        for (k = 0, tr = inf; k < top; ++k)
+          if (bf[ps[k]].c < tr) tr = bf[ps[f = k]].c;
+        for (k = 0; k < top; ++k) bf[ps[k]].c -= tr, bf[ps[k] ^ 1].c += tr;
+        res += tr;
+        i = bf[ps[top = f]].x;
+      }
+      for (j = cur[i]; cur[i]; j = cur[i] = bf[cur[i]].nxt)
+        if (bf[j].c && dep[i] + 1 == dep[bf[j].y]) break;
+      if (cur[i]) {
+        ps[top++] = cur[i];
+        i = bf[cur[i]].y;
+      } else {
+        if (0 == top) break;
+        dep[i] = -1;
+        i = bf[ps[--top]].x;
+      }
     }
-    return res;
+  }
+  return res;
 }
 
-void input()
-{
-    scanf("%d%d%d", &n, &m, &num);
-    s = 0;
-    t = n + m + 1;
-    for (int i = 1; i <= n; i++)
-    {
-        double a;
-        scanf("%lf", &a);
-        addedge(s, i, log(a));
-    }
-    for (int i = n + 1; i <= n + m; i++)
-    {
-        double a;
-        scanf("%lf", &a);
-        addedge(i, t, log(a));
-    }
-    for (int i = 0; i < num; i++)
-    {
-        int a, b;
-        scanf("%d%d", &a, &b);
-        addedge(a, n + b, inf);
-    }
+void input() {
+  scanf("%d%d%d", &n, &m, &num);
+  s = 0;
+  t = n + m + 1;
+  for (int i = 1; i <= n; i++) {
+    double a;
+    scanf("%lf", &a);
+    addedge(s, i, log(a));
+  }
+  for (int i = n + 1; i <= n + m; i++) {
+    double a;
+    scanf("%lf", &a);
+    addedge(i, t, log(a));
+  }
+  for (int i = 0; i < num; i++) {
+    int a, b;
+    scanf("%d%d", &a, &b);
+    addedge(a, n + b, inf);
+  }
 }
 
-int main()
-{
-    int case_num;
-    scanf("%d", &case_num);
-    while (case_num--)
-    {
-        ne = 2;
-        memset(head, 0, sizeof(head));
-        input();
-        printf("%.4f\n", exp(flow(n + m + 2, s, t)));
-    }
-    return 0;
+int main() {
+  int case_num;
+  scanf("%d", &case_num);
+  while (case_num--) {
+    ne = 2;
+    memset(head, 0, sizeof(head));
+    input();
+    printf("%.4f\n", exp(flow(n + m + 2, s, t)));
+  }
+  return 0;
 }
-
-
-
-
