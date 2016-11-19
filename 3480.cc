4273f2d770fe1babf902ebba9df807d9fbf1dfac
sdecoder <sde.work.inbox@gmail.com>
2016-11-19 14:48:26 -0500

    2016-11-19 14:48:26

--- 3480.cc
+++ 3480.cc
@@ -4,30 +4,23 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        int sg =0, n;
-        bool flag =false;
-        scanf("%d", &n);
-        for (int i =0; i < n; i++)
-        {
-            int a;
-            scanf("%d", &a);
-            sg ^= a;
-            if (a !=1)
-                flag =true;
-        }
-        if ((flag && sg) || (!flag &&!sg))
-            printf("John\n");
-        else
-            printf("Brother\n");
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    int sg = 0, n;
+    bool flag = false;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+      int a;
+      scanf("%d", &a);
+      sg ^= a;
+      if (a != 1) flag = true;
     }
-    return 0;
+    if ((flag && sg) || (!flag && !sg))
+      printf("John\n");
+    else
+      printf("Brother\n");
+  }
+  return 0;
 }
-
-
-
