25dbad14eeadcbafddb6589c159def6b9ca05c91
sdecoder <sde.work.inbox@gmail.com>
2016-11-20 14:53:14 -0500

    2016-11-20 14:53:14

--- 2894.cc
+++ 2894.cc
@@ -1,31 +1,24 @@
-# include <iostream>
-# include <cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
-int main()
-{
-    int test;
-    cin>>test;
-    while(test--)
-    {
-       int n;
-       cin>>n;
-       char l;
-       int s,e,c[1001],max=-1,min=0xfffffff;
-       memset(c,0,sizeof(c));
-       while(n--)
-       {
-          cin>>l>>s>>e;
-          for(int i=s;i<e;i++)
-            c[i]++;
-          if(e>max) max=e;
-          if(s<min) min=s;
-       }
-       for(int i=min;i<=max;i++)
-          if(c[i])
-            cout<<(char)(c[i]+64);
-       cout<<endl;
+int main() {
+  int test;
+  cin >> test;
+  while (test--) {
+    int n;
+    cin >> n;
+    char l;
+    int s, e, c[1001], max = -1, min = 0xfffffff;
+    memset(c, 0, sizeof(c));
+    while (n--) {
+      cin >> l >> s >> e;
+      for (int i = s; i < e; i++) c[i]++;
+      if (e > max) max = e;
+      if (s < min) min = s;
     }
-    return 0;
+    for (int i = min; i <= max; i++)
+      if (c[i]) cout << (char)(c[i] + 64);
+    cout << endl;
+  }
+  return 0;
 }
-
-
