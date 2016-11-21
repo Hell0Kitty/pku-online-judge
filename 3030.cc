34e4f8b47a6813ec244c5e2fe518bbcf901dc590
sdecoder <sde.work.inbox@gmail.com>
2016-11-20 20:47:23 -0500

    2016-11-20 20:47:23

--- 3030.cc
+++ 3030.cc
@@ -1,32 +1,27 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<string>
-#include<math.h>
-#include<algorithm>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <math.h>
+#include <algorithm>
 
 using namespace std;
 
-int main()
-{
-    long long int test, a, b, c;
+int main() {
+  long long int test, a, b, c;
 
-    cin>>test;
+  cin >> test;
 
-    for(int i = 0; i < test; i++)
-    {
-        cin>>a>>b>>c;
+  for (int i = 0; i < test; i++) {
+    cin >> a >> b >> c;
 
-        if(b-c>a)
-            cout<<"advertise"<<endl;
-        else if(b-c==a)
-            cout<<"does not matter"<<endl;
+    if (b - c > a)
+      cout << "advertise" << endl;
+    else if (b - c == a)
+      cout << "does not matter" << endl;
 
-        else
-            cout<<"do not advertise"<<endl;
-    }
-    return 0;
+    else
+      cout << "do not advertise" << endl;
+  }
+  return 0;
 }
-
-
-
