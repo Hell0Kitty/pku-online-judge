481d8642d5ec13da904f3670025a0e7e76c67fc3
sdecoder <sde.work.inbox@gmail.com>
2016-11-22 07:48:42 -0500

    2016-11-22 07:48:42

--- 2140.cc
+++ 2140.cc
@@ -1,27 +1,21 @@
-#include<stdio.h>
-int main()
-{
-        int n;
-        while(scanf("%d",&n)!=EOF)
-        {
-                int i,j;
-                int count=0;
-                int sum;
-                for(i=1;i<=n;i++)
-                {
-                        sum=0;
-                        for(j=i;j<=n;j++)
-                        {
-                                sum+=j;
-                                if(sum==n)
-                                        count++;
-                                else if(sum>n)
-                                        break;
-                        }
-                }
-                printf("%d\n",count);
-        }
-        return 0;
+#include <stdio.h>
+int main() {
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    int i, j;
+    int count = 0;
+    int sum;
+    for (i = 1; i <= n; i++) {
+      sum = 0;
+      for (j = i; j <= n; j++) {
+        sum += j;
+        if (sum == n)
+          count++;
+        else if (sum > n)
+          break;
+      }
+    }
+    printf("%d\n", count);
+  }
+  return 0;
 }
-
-
