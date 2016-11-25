e1d6cbe6df022d35bb3669f55fc6e44cadc4fc86
sdecoder <sde.work.inbox@gmail.com>
2016-11-25 09:12:22 -0500

    2016-11-25 09:12:22

--- 2231.cc
+++ 2231.cc
@@ -1,35 +1,29 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<queue>
-#include<cmath>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <algorithm>
 #define llong long long
-#define Min(a,b) (a<b?a:b)
-#define Max(a,b) (a>b?a:b)
-#define Abs(a) ((a)>0?(a):-(a))
-#define Mod(a,b) (((a)-1+(b))%(b)+1)
+#define Min(a, b) (a < b ? a : b)
+#define Max(a, b) (a > b ? a : b)
+#define Abs(a) ((a) > 0 ? (a) : -(a))
+#define Mod(a, b) (((a)-1 + (b)) % (b) + 1)
 using namespace std;
-int n,m,t;
-const int N=10005;
-const int M=105;
-const int inf=(1<<30);
+int n, m, t;
+const int N = 10005;
+const int M = 105;
+const int inf = (1 << 30);
 int a[N];
 llong dp[N];
-int main()
-{
-	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
-		scanf("%d",a+i);
-	sort(a+1,a+1+n);
-	for(int i=2;i<=n;i++)
-		dp[i]=dp[i-1]+(i-1)*llong(a[i]-a[i-1]);
-	llong ans=0;
-	for(int i=1;i<=n;i++)
-		ans+=dp[i];
-	printf("%lld\n",ans*2);
-	return 0;
+int main() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) scanf("%d", a + i);
+  sort(a + 1, a + 1 + n);
+  for (int i = 2; i <= n; i++)
+    dp[i] = dp[i - 1] + (i - 1) * llong(a[i] - a[i - 1]);
+  llong ans = 0;
+  for (int i = 1; i <= n; i++) ans += dp[i];
+  printf("%lld\n", ans * 2);
+  return 0;
 }
-
-
-
