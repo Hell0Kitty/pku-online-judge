21221adacfe9a814585e5b8c4ceb26de485a4186
sdecoder <sde.work.inbox@gmail.com>
2016-11-28 01:47:18 -0500

    2016-11-28 01:47:18

--- 3751.cc
+++ 3751.cc
@@ -3,37 +3,32 @@
 #include <cstring>
 using namespace std;
 
-int main(void)
-{
-	int t;
-	int year,mon,day,hour,mini,sec;
-	scanf("%d",&t);
-	while(t--)
-	{
-		scanf("%d",&year);
-		getchar();
-		scanf("%d",&mon);
-		getchar();
-		scanf("%d",&day);
-		getchar();
-		scanf("%d",&hour);
-		getchar();
-		scanf("%d",&mini);
-		getchar();
-		scanf("%d",&sec);
-		char sign[] = "am";
-		if(hour == 0)
-			hour = 12;
-		else if(hour >= 12)
-		{
-			if(hour > 12)
-				hour -= 12;
-			strcpy(sign,"pm");
-		}
-		printf("%d%d/%d%d/%d-%d%d:%d%d:%d%d%s\n",mon/10,mon%10,day/10,day%10,year,hour/10,hour%10,mini/10,mini%10,sec/10,sec%10,sign);
-	}
-	return 0;
+int main(void) {
+  int t;
+  int year, mon, day, hour, mini, sec;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d", &year);
+    getchar();
+    scanf("%d", &mon);
+    getchar();
+    scanf("%d", &day);
+    getchar();
+    scanf("%d", &hour);
+    getchar();
+    scanf("%d", &mini);
+    getchar();
+    scanf("%d", &sec);
+    char sign[] = "am";
+    if (hour == 0)
+      hour = 12;
+    else if (hour >= 12) {
+      if (hour > 12) hour -= 12;
+      strcpy(sign, "pm");
+    }
+    printf("%d%d/%d%d/%d-%d%d:%d%d:%d%d%s\n", mon / 10, mon % 10, day / 10,
+           day % 10, year, hour / 10, hour % 10, mini / 10, mini % 10, sec / 10,
+           sec % 10, sign);
+  }
+  return 0;
 }
-
-
-
