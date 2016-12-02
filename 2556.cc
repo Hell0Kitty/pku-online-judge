9faeb3c74190805759f9289f61112163c81a76de
sdecoder <sde.work.inbox@gmail.com>
2016-12-01 19:11:41 -0500

    2016-12-01 19:11:41

--- 2556.cc
+++ 2556.cc
@@ -1,29 +1,22 @@
-#include<cstdio>
-#include<cstring>
+#include <cstdio>
+#include <cstring>
 using namespace std;
-int dr[][2]={10,0,0,-10,-10,0,0,10};
-int main()
-{
-    char s[250];
-    while(gets(s))
-    {
-        int x=310,y=420,d=0;
-        printf("300 420 moveto\n310 420 lineto\n");
-        for(int i=0;s[i]!='\0';i++)
-        {
-            if(s[i]=='A')
-                d=(d+1)%4;
-            else
-                d=(d+3)%4;
-            x+=dr[d][0];
-            y+=dr[d][1];
-            printf("%d %d lineto\n",x,y);
-        }
-        printf("stroke\nshowpage\n");
+int dr[][2] = {10, 0, 0, -10, -10, 0, 0, 10};
+int main() {
+  char s[250];
+  while (gets(s)) {
+    int x = 310, y = 420, d = 0;
+    printf("300 420 moveto\n310 420 lineto\n");
+    for (int i = 0; s[i] != '\0'; i++) {
+      if (s[i] == 'A')
+        d = (d + 1) % 4;
+      else
+        d = (d + 3) % 4;
+      x += dr[d][0];
+      y += dr[d][1];
+      printf("%d %d lineto\n", x, y);
     }
-    return 0;
+    printf("stroke\nshowpage\n");
+  }
+  return 0;
 }
-
-
-
-
