37bdb447f4e6668539d7d26833306a46b3533f7e
sdecoder <sde.work.inbox@gmail.com>
2016-12-02 20:22:23 -0500

    2016-12-02 20:22:23

--- 3927.cc
+++ 3927.cc
@@ -2,87 +2,69 @@
 #include <iostream>
 #include <cstdio>
 
-#define	MAXCPN		100000
+#define MAXCPN 100000
 
 using namespace std;
 
-struct	Wed {//wedding，婚礼
+struct Wed {  // wedding，婚礼
 
-	int		s, t;
-	int		as, at;
+  int s, t;
+  int as, at;
 
-	void
-	init(void) {
+  void init(void) {
+    scanf("%d%d", &s, &t);
 
-		scanf("%d%d", &s, &t);
+    if ((t - s) % 2) {
+      as = s + ((t - s - 1) >> 1);
+      at = as + 1;
+    } else {
+      as = s + ((t - s) >> 1) - 1;
+      at = as + 2;
+    }
+  }
 
-		if ( ( t - s ) % 2 ) {
+  int mintv(void) { return (t - s + 2) >> 1; }
 
-			as = s + ( ( t - s - 1 ) >> 1 );
-			at = as + 1;
-		}
-		else {
-
-			as = s + ( ( t - s ) >> 1 ) - 1;
-			at = as + 2;
-		}
-	}
-
-	int
-	mintv(void) {
-
-		return ( t - s + 2 ) >> 1;
-	}
-
-	bool
-	operator<(const Wed &oth)
-	const {
-
-		return as < oth.as || ( as == oth.as && at < oth.at );
-	}
+  bool operator<(const Wed &oth) const {
+    return as < oth.as || (as == oth.as && at < oth.at);
+  }
 };
 
-Wed		w[MAXCPN];
+Wed w[MAXCPN];
 
-bool
-can_hold_all(int n) {
+bool can_hold_all(int n) {
+  int cur_s, cur_t;
+  int prv_t;
 
-	int		cur_s, cur_t;
-	int		prv_t;
+  int i;
 
-	int		i;
+  prv_t = (*w).mintv();
+  for (i = 1; i < n; i++) {
+    cur_s = w[i].s;
+    if (cur_s < prv_t) cur_s = prv_t;
 
-	prv_t = (*w).mintv();
-	for ( i = 1; i < n; i++ ) {
+    cur_t = cur_s + w[i].mintv();
+    if (cur_t > w[i].t) return false;
 
-		cur_s = w[i].s;
-		if ( cur_s < prv_t ) cur_s = prv_t;
+    prv_t = cur_t;
+  }
 
-		cur_t = cur_s + w[i].mintv();
-		if ( cur_t > w[i].t ) return false;
-
-		prv_t = cur_t;
-	}
-
-	return true;
+  return true;
 }
 
-int
-main() {
+int main() {
+  int n;
+  int i;
 
-	int		n;
-	int		i;
+  while (scanf("%d", &n), n) {
+    for (i = 0; i < n; i++) w[i].init();
+    sort(w, w + n);
 
-	while ( scanf("%d", &n), n ) {
+    if (can_hold_all(n))
+      puts("YES");
+    else
+      puts("NO");
+  }
 
-		for ( i = 0; i < n; i++ ) w[i].init();
-		sort(w, w + n);
-
-		if ( can_hold_all(n) ) puts("YES");
-		else puts("NO");
-	}
-
-	return 0;
+  return 0;
 }
-
-
