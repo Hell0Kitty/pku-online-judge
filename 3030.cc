9c8893ae837aff5b87aff37fb9218670d3c34de3
sdecoder <sde.work.inbox@gmail.com>
2016-12-03 14:15:54 -0500

    2016-12-03 14:15:54

---
+++ 3030.cc
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<string>
+#include<math.h>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long int test, a, b, c;
+
+    cin>>test;
+
+    for(int i = 0; i < test; i++)
+    {
+        cin>>a>>b>>c;
+
+        if(b-c>a)
+            cout<<"advertise"<<endl;
+        else if(b-c==a)
+            cout<<"does not matter"<<endl;
+
+        else
+            cout<<"do not advertise"<<endl;
+    }
+    return 0;
+}
+
+
+
