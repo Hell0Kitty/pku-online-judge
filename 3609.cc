137c2b33bf07f87b5a419c4eac1fc60449e9f75d
sdecoder <sde.work.inbox@gmail.com>
2016-12-05 18:26:36 -0500

    2016-12-05 18:26:36

--- 3609.cc
+++ 3609.cc
@@ -1,109 +1,87 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int tran[ 10 ][ 20 ], N, M;
+int tran[10][20], N, M;
 
-struct Command
-{
- int command;
- int pre;
+struct Command {
+  int command;
+  int pre;
 };
 const int MAX = 200000;
-Command cmdList[ MAX ]; int cCount;
-int queue[ MAX ][ 8 ], front, rear;
+Command cmdList[MAX];
+int cCount;
+int queue[MAX][8], front, rear;
 
-int cmds[ MAX ], cmdSize;
+int cmds[MAX], cmdSize;
 
-char xx[ 17 ] = "0123456789abcdef";
+char xx[17] = "0123456789abcdef";
 
-void read()
-{
- scanf( "%d %d", &N, &M );
- int i, j;
- for ( i = 0; i < N; i ++ )
- {
-  for ( j = 0; j < M; j ++ )
-   scanf( "%d", &tran[ i ][ j ] );
-  queue[ 0 ][ i ] = i;
- }
+void read() {
+  scanf("%d %d", &N, &M);
+  int i, j;
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) scanf("%d", &tran[i][j]);
+    queue[0][i] = i;
+  }
 }
 
-bool checkCurState( int front, int size )
-{
- for ( int i = 0; i < size; i ++ )
-  if ( queue[ front ][ i ] != 0 )
-   return false;
- return true;
+bool checkCurState(int front, int size) {
+  for (int i = 0; i < size; i++)
+    if (queue[front][i] != 0) return false;
+  return true;
 }
 
-bool checkHaveUsed( int cur, int curState[], int size )
-{
- for ( int x = cur; x != 0; x = cmdList[ x ].pre )
- {
-  bool flag = false;
-  for ( int i = 0; i < size; i ++ )
-   if ( curState[ i ] != queue[ x ][ i ] )
-    flag = true;
-  if ( !flag )
-   return true;
- }
- return false;
+bool checkHaveUsed(int cur, int curState[], int size) {
+  for (int x = cur; x != 0; x = cmdList[x].pre) {
+    bool flag = false;
+    for (int i = 0; i < size; i++)
+      if (curState[i] != queue[x][i]) flag = true;
+    if (!flag) return true;
+  }
+  return false;
 }
 
-void getPath( int front )
-{
- cmdSize = 0;
- for ( int x = front; x != 0; x = cmdList[ x ].pre )
-  cmds[ cmdSize ++ ] = cmdList[ x ].command;
+void getPath(int front) {
+  cmdSize = 0;
+  for (int x = front; x != 0; x = cmdList[x].pre)
+    cmds[cmdSize++] = cmdList[x].command;
 }
 
-bool bfs()
-{
- front = 0; rear = 1;
+bool bfs() {
+  front = 0;
+  rear = 1;
 
- while( front < rear )
- {
-   if ( checkCurState( front, N ) ) 
-   {
-    getPath( front );
-    return true;
-   }
+  while (front < rear) {
+    if (checkCurState(front, N)) {
+      getPath(front);
+      return true;
+    }
 
-   for ( int i = 0; i < M; i ++ )
-   {
-    for ( int j = 0; j < N; j ++ )
-     queue[ rear ][ j ] = tran[ queue[ front ][ j ] ][ i ];
-    if ( !checkHaveUsed( front, queue[ rear ], N ) )
-    {
-     cmdList[ rear ].command = i;
-     cmdList[ rear ].pre = front;
-     rear ++;
-    }  
-   }
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) queue[rear][j] = tran[queue[front][j]][i];
+      if (!checkHaveUsed(front, queue[rear], N)) {
+        cmdList[rear].command = i;
+        cmdList[rear].pre = front;
+        rear++;
+      }
+    }
 
-   front ++;
- }
+    front++;
+  }
 
- return false;
+  return false;
 }
 
-void print( int size )
-{
- for ( int i = size - 1; i >= 0; i -- )
-  printf( "%c", xx[ cmds[ i ] ]);
- putchar( '\n' );
+void print(int size) {
+  for (int i = size - 1; i >= 0; i--) printf("%c", xx[cmds[i]]);
+  putchar('\n');
 }
 
-int main()
-{
- read();
- if ( bfs() )
-  print( cmdSize );
- else
-  printf( "*\n" );
- return 0;
+int main() {
+  read();
+  if (bfs())
+    print(cmdSize);
+  else
+    printf("*\n");
+  return 0;
 }
-
-
-
-
