6d41c842eef6a68e70b9edf895d15b45f838e9fc
sdecoder <sde.work.inbox@gmail.com>
2016-12-05 15:59:36 -0500

    2016-12-05 15:59:36

--- 1326.cc
+++ 1326.cc
@@ -8,40 +8,29 @@ using namespace std;
 
 int ans;
 
-int cal(char ch, int d)
-{
-    if (ch == 'Y')
-        return max(500, d);
-    if (ch == 'B')
-        return int(1.5 * d + 0.5);
-    return 2 * d;
+int cal(char ch, int d) {
+  if (ch == 'Y') return max(500, d);
+  if (ch == 'B') return int(1.5 * d + 0.5);
+  return 2 * d;
 }
 
-void input()
-{
-    char name1[maxl], name2[maxl], type[maxl];
-    int dist;
-    while (true)
-    {
-        scanf("%s", name1);
-        if (strcmp(name1, "0") == 0)
-            return;
-        if (strcmp(name1, "#") == 0)
-            exit(0);
-        scanf("%s%d%s", name2, &dist, type);
-        ans += cal(type[0], dist);
-    }
+void input() {
+  char name1[maxl], name2[maxl], type[maxl];
+  int dist;
+  while (true) {
+    scanf("%s", name1);
+    if (strcmp(name1, "0") == 0) return;
+    if (strcmp(name1, "#") == 0) exit(0);
+    scanf("%s%d%s", name2, &dist, type);
+    ans += cal(type[0], dist);
+  }
 }
 
-int main()
-{
-    while (1)
-    {
-        ans = 0;
-        input();
-        printf("%d\n", ans);
-    }
-    return 0;
+int main() {
+  while (1) {
+    ans = 0;
+    input();
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
