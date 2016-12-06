b8bf5db0d7978e4f58170324d5b8592272c002ab
sdecoder <sde.work.inbox@gmail.com>
2016-12-05 23:54:03 -0500

    2016-12-05 23:54:03

--- 3895.cc
+++ 3895.cc
@@ -12,54 +12,43 @@
 #include <vector>
 using namespace std;
 
-const int kMAX=10010;
-const double kEPS=10E-6;
+const int kMAX = 10010;
+const double kEPS = 10E-6;
 vector<int> vec[kMAX];
 bool visit[kMAX];
-int ans,step[kMAX];
+int ans, step[kMAX];
 
-void dfs(const int &x,const int &t)
-{
-	visit[x]=true;
-	step[x]=t;
-	for(size_t i=0;i<vec[x].size();++i)
-	{
-		if(!visit[vec[x][i]])
-			dfs(vec[x][i],t+1);
-		else
-			ans=max(ans,step[vec[x][i]]-step[x]+1);
-	}
+void dfs(const int &x, const int &t) {
+  visit[x] = true;
+  step[x] = t;
+  for (size_t i = 0; i < vec[x].size(); ++i) {
+    if (!visit[vec[x][i]])
+      dfs(vec[x][i], t + 1);
+    else
+      ans = max(ans, step[vec[x][i]] - step[x] + 1);
+  }
 }
 
-int main(void)
-{
+int main(void) {
+  int n, m, ncase = 1, u, v;
+  scanf("%d", &ncase);
 
-  int n,m,ncase=1,u,v;
-  scanf("%d",&ncase);
+  while (~scanf("%d%d", &n, &m) && n) {
+    for (int i = 1; i <= n; ++i) vec[i].clear();
+    memset(visit, false, sizeof(visit));
+    memset(step, 0, sizeof(step));
+    ans = 0;
 
-  while(~scanf("%d%d",&n,&m) && n)
-  {
-    for(int i=1;i<=n;++i)
-      vec[i].clear();
-		memset(visit,false,sizeof(visit));
-		memset(step,0,sizeof(step));
-		ans=0;
+    while (m--) {
+      scanf("%d%d", &u, &v);
+      vec[u].push_back(v);
+      vec[v].push_back(u);
+    }
 
-		while(m--)
-		{
-			scanf("%d%d",&u,&v);
-			vec[u].push_back(v);
-			vec[v].push_back(u);
-		}
-
-		for(int i=1;i<=n;++i)
-			if(!visit[i])
-				dfs(i,0);
-		printf("%d\n",ans);
-
+    for (int i = 1; i <= n; ++i)
+      if (!visit[i]) dfs(i, 0);
+    printf("%d\n", ans);
   }
 
   return 0;
 }
-
-
