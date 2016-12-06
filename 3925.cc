723d4dcf4bece9f38dc8c9adf5da14865c8a705c
sdecoder <sde.work.inbox@gmail.com>
2016-12-06 18:15:30 -0500

    2016-12-06 18:15:30

--- 3925.cc
+++ 3925.cc
@@ -18,19 +18,19 @@ using namespace std;
 
 const int MAXN = 100;
 
-const int INF = 1<<30;
+const int INF = 1 << 30;
 
-#define CLR(x,y) memset(x,y,sizeof(x))
+#define CLR(x, y) memset(x, y, sizeof(x))
 
-#define MIN(m,v) (m)<(v)?(m):(v)
+#define MIN(m, v) (m) < (v) ? (m) : (v)
 
-#define MAX(m,v) (m)>(v)?(m):(v)
+#define MAX(m, v) (m) > (v) ? (m) : (v)
 
-#define ABS(x) ((x)>0?(x):-(x))
+#define ABS(x) ((x) > 0 ? (x) : -(x))
 
-#define rep(i,x,y) for(i=x;i<y;++i)
+#define rep(i, x, y) for (i = x; i < y; ++i)
 
-int n,m,k;
+int n, m, k;
 
 double ans = 0;
 
@@ -49,190 +49,151 @@ int ind[MAXN];
 double Prim()
 
 {
+  int i, j, tmp, mark_i;
 
-    int i,j,tmp,mark_i;
+  int mark_min;
 
-    int mark_min;
+  int sum_node = 0;
 
-    int sum_node = 0;
+  int sum_edge = 0;
 
-    int sum_edge = 0;
+  for (i = 0; i < n; ++i)
 
-    for(i = 0; i < n; ++i)
+  {
+    dist[i] = INF;
 
-    {
+    visit[i] = 0;
+  }
 
-        dist[i] = INF;
+  for (i = 0; i < n; ++i)
 
-        visit[i] = 0;
-
-    }
-
-    for(i = 0; i < n; ++i)
-
-        if(select[i]>0)
-
-        {
-
-            dist[i] = 0;
-
-            break;
-
-        }
-
-    int cnt = 0;
-
-    for(i = 0; i < n; ++i,++cnt)
+    if (select[i] > 0)
 
     {
+      dist[i] = 0;
 
-        if(cnt>=m) break;
-
-        mark_min = INF;
-
-        for(j  = 0; j < n; ++j)
-
-        {
-
-            if(select[j]>0 && !visit[j] && mark_min>dist[j])
-
-            {
-
-                mark_min = dist[j];
-
-                mark_i = j;
-
-            }
-
-        }
-
-        visit[mark_i] = 1;
-
-        sum_edge += dist[mark_i];
-
-        for( j = 0; j < n; ++j)
-
-        {
-
-            if(visit[j]==0 && select[j]>0 && dist[j] > g[mark_i][j])
-
-                dist[j] = g[mark_i][j];
-
-        }
-
+      break;
     }
 
-    for( i = 0; i < n; ++i)
+  int cnt = 0;
 
-        if(select[i] > 0 )
+  for (i = 0; i < n; ++i, ++cnt)
 
-            sum_node += val[i];
+  {
+    if (cnt >= m) break;
 
-    return double(sum_edge)/sum_node;
+    mark_min = INF;
 
+    for (j = 0; j < n; ++j)
+
+    {
+      if (select[j] > 0 && !visit[j] && mark_min > dist[j])
+
+      {
+        mark_min = dist[j];
+
+        mark_i = j;
+      }
+    }
+
+    visit[mark_i] = 1;
+
+    sum_edge += dist[mark_i];
+
+    for (j = 0; j < n; ++j)
+
+    {
+      if (visit[j] == 0 && select[j] > 0 && dist[j] > g[mark_i][j])
+
+        dist[j] = g[mark_i][j];
+    }
+  }
+
+  for (i = 0; i < n; ++i)
+
+    if (select[i] > 0) sum_node += val[i];
+
+  return double(sum_edge) / sum_node;
 }
 int DFS(int cur, int deep)
 
 {
+  double res = INF;
 
-    double res = INF;
+  select[cur] = 1;
 
-    select[cur] = 1;
+  if (deep < m)
 
-    if(deep < m)
-
-    {
-
-        for(int i = cur+1; i < n; ++i)
-
-        {
-
-            DFS(i,deep+1);
-
-        }
-
-    }
-
-    if(deep == m)
+  {
+    for (int i = cur + 1; i < n; ++i)
 
     {
-
-        res = Prim();
-
-        if(res < ans)
-
-        {
-
-            ans = res;
-
-            for(int i = 0; i < n; ++i)
-
-                ind[i] = select[i];
-
-        }
-
+      DFS(i, deep + 1);
     }
+  }
 
-    select[cur] = 0;
+  if (deep == m)
 
-    return 0;
+  {
+    res = Prim();
 
+    if (res < ans)
+
+    {
+      ans = res;
+
+      for (int i = 0; i < n; ++i) ind[i] = select[i];
+    }
+  }
+
+  select[cur] = 0;
+
+  return 0;
 }
 int Solve()
 
 {
+  while (scanf("%d%d", &n, &m) != EOF)
 
-    while(scanf("%d%d",&n,&m)!=EOF)
+  {
+    if (n == 0 && m == 0) break;
 
-    {
+    for (int i = 0; i < n; ++i) scanf("%d", &val[i]);
 
-        if(n == 0 && m == 0) break;
+    for (int i = 0; i < n; ++i)
 
-        for(int i = 0 ; i < n; ++i)
+      for (int j = 0; j < n; ++j) {
+        scanf("%d", &g[i][j]);
+      }
+    CLR(select, 0);
 
-            scanf("%d",&val[i]);
+    ans = INF;
 
-        for(int i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i) DFS(i, 1);
 
-            for(int j = 0; j < n; ++j)
-            {
-                scanf("%d",&g[i][j]);
-            }
-        CLR(select,0);
+    int tag = 0;
 
-        ans = INF;
+    for (int i = 0; i < n; ++i)
 
-        for(int i = 0 ; i < n; ++i)
+      if (ind[i] > 0)
 
-            DFS(i,1);
+        if (tag == 0)
 
-        int tag = 0;
+        {
+          printf("%d", i + 1);
 
-        for(int i = 0; i < n; ++i)
+          tag = 1;
 
-            if(ind[i] > 0)
+        }
 
-                if(tag == 0)
+        else
 
-                {
-
-                    printf("%d",i+1);
-
-                    tag = 1;
-
-                }
-
-                else
-
-                    printf(" %d",i+1);
-        printf("\n");
-    }
-    return 0;
+          printf(" %d", i + 1);
+    printf("\n");
+  }
+  return 0;
 }
-int main()
-{
-    Solve();
-    return 0;
+int main() {
+  Solve();
+  return 0;
 }
-
-
