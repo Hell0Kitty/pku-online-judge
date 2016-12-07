75a72adcc38c7594c8c3a4754bfd201aac73a52f
sdecoder <sde.work.inbox@gmail.com>
2016-12-06 19:50:25 -0500

    2016-12-06 19:50:25

--- 1191.cc
+++ 1191.cc
@@ -1,58 +1,89 @@
+#include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <string>
-#include <iostream>
-#include <algorithm>
 #include <cmath>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
-int n, gp[9][9], sum[9][9][9][9], dp[16][9][9][9][9];
-void init() {
-  for (int x = 0; x < 9; x++)
-    for (int y = 0; y < 9; y++)
-      for (int xx = x; xx < 9; xx++)
-        for (int yy = y; yy < 9; yy++) {
-          int ans = 0;
-          for (int i = x; i <= xx; i++)
-            for (int j = y; j <= yy; j++) {
-              ans += gp[i][j];
-            }
-          sum[x][y][xx][yy] = ans * ans;
-          sum[x][yy][xx][y] = ans * ans;
-          sum[xx][yy][x][y] = ans * ans;
-          sum[xx][y][x][yy] = ans * ans;
+
+const int N = 15;
+const double M = 100000000.0;
+double ans[N][10][10][10][10];
+double K;
+int sum[10][10][10][10];
+int a[10][10];
+int n;
+int dpsum(int x1, int y1, int x2, int y2) {
+  if (sum[x1][y1][x2][y2] != -1) return sum[x1][y1][x2][y2];
+  if (x1 == x2 && y1 == y2) {
+    sum[x1][y1][x2][y2] = a[x1][y1];
+  } else if (x1 < x2) {
+    sum[x1][y1][x2][y2] = dpsum(x1, y1, x1, y2) + dpsum(x1 + 1, y1, x2, y2);
+  } else if (y1 < y2) {
+    sum[x1][y1][x2][y2] = dpsum(x1, y1, x2, y1) + dpsum(x1, y1 + 1, x2, y2);
+  }
+  return sum[x1][y1][x2][y2];
+}
+double FFmin(double x, double y) { return (x < y) ? x : y; }
+double dpans(int k, int x1, int y1, int x2, int y2) {
+  if (ans[k][x1][y1][x2][y2] >= 0.0) {
+    return ans[k][x1][y1][x2][y2];
+  }
+  if (k == 0) {
+    ans[k][x1][y1][x2][y2] =
+        ((sum[x1][y1][x2][y2] - K) * (sum[x1][y1][x2][y2] - K));
+    return ans[k][x1][y1][x2][y2];
+  }
+  ans[k][x1][y1][x2][y2] = M;
+  for (int x = x1; x < x2; ++x) {
+    ans[k][x1][y1][x2][y2] =
+        FFmin(ans[k][x1][y1][x2][y2],
+              (dpans(k - 1, x1, y1, x, y2)) +
+                  (sum[x + 1][y1][x2][y2] - K) * (sum[x + 1][y1][x2][y2] - K));
+    ans[k][x1][y1][x2][y2] =
+        FFmin(ans[k][x1][y1][x2][y2],
+              (dpans(k - 1, x + 1, y1, x2, y2)) +
+                  (sum[x1][y1][x][y2] - K) * (sum[x1][y1][x][y2] - K));
+  }
+  for (int y = y1; y < y2; ++y) {
+    ans[k][x1][y1][x2][y2] =
+        FFmin(ans[k][x1][y1][x2][y2],
+              (dpans(k - 1, x1, y1, x2, y)) +
+                  (sum[x1][y + 1][x2][y2] - K) * (sum[x1][y + 1][x2][y2] - K));
+    ans[k][x1][y1][x2][y2] =
+        FFmin(ans[k][x1][y1][x2][y2],
+              (dpans(k - 1, x1, y + 1, x2, y2)) +
+                  (sum[x1][y1][x2][y] - K) * (sum[x1][y1][x2][y] - K));
+  }
+  return ans[k][x1][y1][x2][y2];
+}
+void begin(int n) {
+  for (int i = 0; i < n; ++i)
+    for (int l1 = 1; l1 <= 8; ++l1)
+      for (int l2 = 1; l2 <= 8; ++l2)
+        for (int l3 = 1; l3 <= 8; ++l3)
+          for (int l4 = 1; l4 <= 8; ++l4) {
+            ans[i][l1][l2][l3][l4] = -1.0;
+          }
+  for (int l1 = 1; l1 <= 8; ++l1)
+    for (int l2 = 1; l2 <= 8; ++l2)
+      for (int l3 = 1; l3 <= 8; ++l3)
+        for (int l4 = 1; l4 <= 8; ++l4) {
+          dpsum(l1, l2, l3, l4);
         }
 }
-int DP(int k, int x, int y, int xx, int yy) {
-  if (dp[k][x][y][xx][yy] >= 0) return dp[k][x][y][xx][yy];
-  if (k == n - 1) return sum[x][y][xx][yy];
-  int ans = 0;
-  dp[k][x][y][xx][yy] = 1 << 29;
-  for (int i = x; i < xx; i++) {
-    ans = min(DP(k + 1, x, y, i, yy) + sum[i + 1][y][xx][yy],
-              DP(k + 1, i + 1, y, xx, yy) + sum[x][y][i][yy]);
-    dp[k][x][y][xx][yy] = min(ans, dp[k][x][y][xx][yy]);
-  }
-  for (int i = y; i < yy; i++) {
-    ans = min(DP(k + 1, x, y, xx, i) + sum[x][i + 1][xx][yy],
-              DP(k + 1, x, i + 1, xx, yy) + sum[x][y][xx][i]);
-    dp[k][x][y][xx][yy] = min(ans, dp[k][x][y][xx][yy]);
-  }
-  return dp[k][x][y][xx][yy];
-}
 int main() {
-  int cnt = 0;
-  double ans;
-  scanf("%d", &n);
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      scanf("%d", &gp[i][j]);
-      cnt += gp[i][j];
+  while (scanf("%d", &n) != EOF) {
+    for (int i = 1; i <= 8; ++i) {
+      for (int j = 1; j <= 8; ++j) {
+        scanf("%d", &a[i][j]);
+      }
     }
+    memset(sum, -1, sizeof(sum));
+    begin(n);
+    K = 1.0 * sum[1][1][8][8] / n;
+    printf("%.3f\n", sqrt(1.0 * dpans(n - 1, 1, 1, 8, 8) / n));
   }
-  init();
-  memset(dp, -1, sizeof(dp));
-  DP(0, 0, 0, 7, 7);
-  ans = sqrt(dp[0][0][0][7][7] * 1.0 / n - ((cnt * 1.0) / n * (cnt * 1.0) / n));
-  printf("%.3f\n", ans);
   return 0;
 }
