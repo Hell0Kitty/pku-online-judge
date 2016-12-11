a18c7f2bf63a12a247fc38d01998b3643eca80af
sdecoder <sde.work.inbox@gmail.com>
2016-12-11 07:59:18 -0500

    2016-12-11 07:59:18

---
+++ 2894.cc
@@ -0,0 +1,31 @@
+# include <iostream>
+# include <cstring>
+using namespace std;
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+       int n;
+       cin>>n;
+       char l;
+       int s,e,c[1001],max=-1,min=0xfffffff;
+       memset(c,0,sizeof(c));
+       while(n--)
+       {
+          cin>>l>>s>>e;
+          for(int i=s;i<e;i++)
+            c[i]++;
+          if(e>max) max=e;
+          if(s<min) min=s;
+       }
+       for(int i=min;i<=max;i++)
+          if(c[i])
+            cout<<(char)(c[i]+64);
+       cout<<endl;
+    }
+    return 0;
+}
+
+
