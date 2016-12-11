c3e98fc97c42ebdbcbde5e82a08218761038adad
sdecoder <sde.work.inbox@gmail.com>
2016-12-11 01:20:33 -0500

    2016-12-11 01:20:33

--- 3494.cc
+++ 3494.cc
@@ -18,48 +18,41 @@ typedef long long ll;
 #define min_inf 0x80808080
 #define mod 1000000007
 
-int m , n , top;
-int q[2005] , lef[2005] , rig[2005];
+int m, n, top;
+int q[2005], lef[2005], rig[2005];
 
-int main()
-{
-	int h[2005];
-	int ans;
-	while (~scanf("%d%d",&m,&n))
-	{
-		int i , j;
-		ans = 0;
-		memset(h , 0 , sizeof h);
-		for (i = 0 ; i < m ; i++)
-		{
-			top = 0;
-			for (j = 0 ; j < n ; j++)
-			{
-				int x;
-				scanf("%d",&x);
-				h[j]++;
-				h[j] = x ? h[j] : 0;
-				while (top > 0 && h[q[top-1]] > h[j])
-				{
-					rig[q[top-1]] = j-1;
-					top--;
-				}
-				if (top <= 0)lef[j] = 0;
-				else lef[j] = q[top-1]+1;
-				q[top++] = j;
-			}
-			while (top)rig[q[top-1]] = n-1,top--;
+int main() {
+  int h[2005];
+  int ans;
+  while (~scanf("%d%d", &m, &n)) {
+    int i, j;
+    ans = 0;
+    memset(h, 0, sizeof h);
+    for (i = 0; i < m; i++) {
+      top = 0;
+      for (j = 0; j < n; j++) {
+        int x;
+        scanf("%d", &x);
+        h[j]++;
+        h[j] = x ? h[j] : 0;
+        while (top > 0 && h[q[top - 1]] > h[j]) {
+          rig[q[top - 1]] = j - 1;
+          top--;
+        }
+        if (top <= 0)
+          lef[j] = 0;
+        else
+          lef[j] = q[top - 1] + 1;
+        q[top++] = j;
+      }
+      while (top) rig[q[top - 1]] = n - 1, top--;
 
-			for (j = 0 ; j < n ; j++)
-			{
-				int temp = (rig[j]-lef[j]+1)*h[j];
-				ans = max(ans,temp);
-			}
-		}
-		printf("%d\n",ans);
-	}
-	return 0;
+      for (j = 0; j < n; j++) {
+        int temp = (rig[j] - lef[j] + 1) * h[j];
+        ans = max(ans, temp);
+      }
+    }
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
-
