f31b0f90e22ff0baa302c7f070642bb1a12207fb
sdecoder <sde.work.inbox@gmail.com>
2016-12-12 05:32:46 -0500

    2016-12-12 05:32:46

--- 2370.cc
+++ 2370.cc
@@ -1,30 +1,23 @@
-#include<iostream>
-#include<cstring>
-#include<cstdio>
-#include<algorithm>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 #define MAX 110
 int v[MAX];
-int main()
-{
-    int n;
-    while(scanf("%d",&n)!=EOF)
-    {
-        int i;
-        for(i=1;i<=n;i++)
-        {
-            scanf("%d",&v[i]);
-        }
-        sort(v+1,v+n+1);
-        int mid=n/2+1;
-        int sum=0;
-        for(i=1;i<=mid;i++)
-        {
-            sum+=v[i]/2+1;
-        }
-        printf("%d\n",sum);
+int main() {
+  int n;
+  while (scanf("%d", &n) != EOF) {
+    int i;
+    for (i = 1; i <= n; i++) {
+      scanf("%d", &v[i]);
     }
+    sort(v + 1, v + n + 1);
+    int mid = n / 2 + 1;
+    int sum = 0;
+    for (i = 1; i <= mid; i++) {
+      sum += v[i] / 2 + 1;
+    }
+    printf("%d\n", sum);
+  }
 }
-
-
-
