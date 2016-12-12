348ace1544db1c5a579edd39eb4dbc3b0a0b3e75
sdecoder <sde.work.inbox@gmail.com>
2016-12-11 23:55:15 -0500

    2016-12-11 23:55:15

--- 2262.cc
+++ 2262.cc
@@ -1,34 +1,29 @@
-#include<iostream>
-#include<cstdio>
-#include<cmath>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
 #define N 1000005
 using namespace std;
 int prime[N];
-void isprime(){
-    int i;
-    for(i=0;i<N;i++)
-        prime[i]=1;
-    for(i=3;i<1000;i+=2){
-        if(!prime[i]) continue;
-        for(int j=i;j<=N/i;j+=2)
-            prime[i*j]=0;
-    }
+void isprime() {
+  int i;
+  for (i = 0; i < N; i++) prime[i] = 1;
+  for (i = 3; i < 1000; i += 2) {
+    if (!prime[i]) continue;
+    for (int j = i; j <= N / i; j += 2) prime[i * j] = 0;
+  }
 }
-int main(){
-    isprime();
-    while(1){
-        int n;
-        scanf("%d",&n);
-        if(n==0)  break;
-        for(int i=3;i<n;i+=2){
-            if(prime[i]&&prime[n-i]){
-                printf("%d = %d + %d\n",n,i,n-i);
-                break;
-            }
-        }
+int main() {
+  isprime();
+  while (1) {
+    int n;
+    scanf("%d", &n);
+    if (n == 0) break;
+    for (int i = 3; i < n; i += 2) {
+      if (prime[i] && prime[n - i]) {
+        printf("%d = %d + %d\n", n, i, n - i);
+        break;
+      }
     }
-    return 0;
+  }
+  return 0;
 }
-
-
-
