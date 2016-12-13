1dfa2160b792c2b8c812e105a5bb843d0148ea7c
sdecoder <sde.work.inbox@gmail.com>
2016-12-13 18:42:01 -0500

    2016-12-13 18:42:01

---
+++ 3103.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x,y,z,n;
+	scanf("%d%d%d%d",&x,&y,&z,&n);
+	for(int i=0;i<n;++i)
+		printf("0 0 %.8lf %d %d %.8lf\n",(z*1.0/n)*i,x,y,(z*1.0/n)*(i+1));
+	return 0;	
+}
+
+
