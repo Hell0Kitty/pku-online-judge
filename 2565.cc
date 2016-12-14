caf7b168966f0f22a146ec0e9bf99174f595b6d4
sdecoder <sde.work.inbox@gmail.com>
2016-12-13 22:46:46 -0500

    2016-12-13 22:46:46

--- 2565.cc
+++ 2565.cc
@@ -1,143 +1,123 @@
 nclude<cstdio>
 
-#include<cstring>
+#include <cstring>
 
-#include<algorithm>
+#include <algorithm>
 
-using namespace std;
+    using namespace std;
 
-const double eps=1e-8;
+const double eps = 1e-8;
 
 struct data
 
-{
+    {
+  int h, m, s;
 
-    int h,m,s;
+  double tot, speed;
 
-    double tot,speed;
+  data(int _h, int _m, int _s)
 
-    data(int _h,int _m,int _s)
+  {
+    h = _h;
+    m = _m;
+    s = _s;
+
+    tot = _h * 60 + _m + _s / 60.0;
+
+    speed = 0;
+  }
+
+  data() {}
+
+  void print()
+
+  {
+    if (speed < eps)
+
+      printf("-\n");
+
+    else
 
     {
+      speed += 1 / 120.0;
 
-        h=_h;m=_m;s=_s;
+      int a = (int)speed, b = (int)(60.0 * (speed - (int)speed));
 
-        tot=_h*60+_m+_s/60.0;
+      printf("%d:", a);
 
-        speed=0;
+      if (b < 10)
 
+        printf("0%d min/km\n", b);
+
+      else
+
+        printf("%d min/km\n", b);
     }
+  }
 
-    data(){}
+  data operator+(const data &b) const
 
-    void print()
+  {
+    data a;
 
-    {
+    a.h = h + b.h;
 
-        if(speed<eps)
+    a.m = m + b.m;
 
-            printf("-\n");
+    a.s = s + b.s;
 
-        else
+    a.tot = a.h * 60.0 + a.m + a.s / 60.0;
 
-        {
+    a.speed = 0;
 
-            speed+=1/120.0;
-
-            int a=(int)speed,b=(int)(60.0*(speed-(int)speed));
-
-            printf("%d:",a);
-
-            if(b<10)
-
-                printf("0%d min/km\n",b);
-
-            else
-
-                printf("%d min/km\n",b);
-
-        }
-
-    }
-
-    data operator+(const data &b)const
-
-    {
-
-        data a;
-
-        a.h=h+b.h;
-
-        a.m=m+b.m;
-
-        a.s=s+b.s;
-
-        a.tot=a.h*60.0+a.m+a.s/60.0;
-
-        a.speed=0;
-
-        return a;
-
-    }
-
+    return a;
+  }
 };
 
 int main()
 
 {
+  int n, id;
 
-    int n,id;
+  double d;
 
-    double d;
+  scanf("%d%lf", &n, &d);
 
-    scanf("%d%lf",&n,&d);
+  while (scanf("%d", &id) != EOF)
 
-    while(scanf("%d",&id)!=EOF)
+  {
+    data a(0, 0, 0);
+
+    int h, m, s;
+
+    printf("%3d: ", id);
+
+    bool flag = false;
+
+    for (int i = 0; i < n; i++)
 
     {
+      char ss[30];
 
-        data a(0,0,0);
+      scanf("%s", ss);
 
-        int h,m,s;
+      if (ss[0] == '-' || flag)
 
-        printf("%3d: ",id);
+      {
+        flag = true;
 
-        bool flag=false;
+        continue;
+      }
 
-        for(int i=0;i<n;i++)
+      sscanf(ss, "%d:%d:%d", &h, &m, &s);
 
-        {
-
-            char ss[30];
-
-            scanf("%s",ss);
-
-            if(ss[0]=='-'||flag)
-
-            {
-
-                flag=true;
-
-                continue;
-
-            }
-
-            sscanf(ss,"%d:%d:%d",&h,&m,&s);
-
-            a=a+data(h,m,s);
-
-        }
-
-        if(!flag)
-
-            a.speed=a.tot/d;
-
-        a.print();
-
+      a = a + data(h, m, s);
     }
 
-    return 0;
+    if (!flag) a.speed = a.tot / d;
 
+    a.print();
+  }
+
+  return 0;
 }
-
-
