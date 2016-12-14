95f9535fbd099b9f3ce7b786dcfba9e8ad499544
sdecoder <sde.work.inbox@gmail.com>
2016-12-14 11:41:45 -0500

    2016-12-14 11:41:45

--- 3993.cc
+++ 3993.cc
@@ -1,38 +1,32 @@
-#include<cstdio>  
-#include<cmath>  
-#include<cstring>  
-using namespace std;  
-typedef long long ll;  
-const int N = 1000001;  
-int p[N], ans[N], vis[N], a, b;  
-void initPrime()  
-{  
-    int num = 0, m = sqrt (N + 0.5);  
-    for (int i = 2; i <= m; ++i)  
-        if (vis[i] == 0)  
-            for (int j = i * i; j <= N; j += i) vis[j] = 1;  
-    for (int i = 2; i <= N; ++i)  
-        if (vis[i] == 0) p[++num] = i;  
-}  
-
-int main()  
-{  
-    initPrime();  
-    int cas=0;  
-    while (scanf ("%d%d", &a, &b),a)  
-    {  
-        int cnt = 0, ans = 0, ca, cb;  
-        for (int i = 1; a > 1 || b > 1; ++i)  
-            if (a % p[i] == 0 || b % p[i] == 0)  
-            {  
-                ++cnt, ca = cb = 0;  
-                while (a % p[i] == 0) a /= p[i], ++ca;  
-                while (b % p[i] == 0) b /= p[i], ++cb;  
-                ans += (ca > cb ? ca - cb : cb - ca);  
-            }  
-        printf ("%d. %d:%d\n", ++cas,cnt, ans);  
-    }  
-    return 0;  
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+using namespace std;
+typedef long long ll;
+const int N = 1000001;
+int p[N], ans[N], vis[N], a, b;
+void initPrime() {
+  int num = 0, m = sqrt(N + 0.5);
+  for (int i = 2; i <= m; ++i)
+    if (vis[i] == 0)
+      for (int j = i * i; j <= N; j += i) vis[j] = 1;
+  for (int i = 2; i <= N; ++i)
+    if (vis[i] == 0) p[++num] = i;
 }
 
-
+int main() {
+  initPrime();
+  int cas = 0;
+  while (scanf("%d%d", &a, &b), a) {
+    int cnt = 0, ans = 0, ca, cb;
+    for (int i = 1; a > 1 || b > 1; ++i)
+      if (a % p[i] == 0 || b % p[i] == 0) {
+        ++cnt, ca = cb = 0;
+        while (a % p[i] == 0) a /= p[i], ++ca;
+        while (b % p[i] == 0) b /= p[i], ++cb;
+        ans += (ca > cb ? ca - cb : cb - ca);
+      }
+    printf("%d. %d:%d\n", ++cas, cnt, ans);
+  }
+  return 0;
+}
