e1136da36693f1f0c8e8721dc3e35c047353c2a3
sdecoder <sde.work.inbox@gmail.com>
2016-12-14 22:43:09 -0500

    2016-12-14 22:43:09

--- 1316.cc
+++ 1316.cc
@@ -8,30 +8,21 @@ using namespace std;
 
 bool is_self[maxn];
 
-int generate(int a)
-{
-    int ret = a;
-    while (a)
-    {
-        ret += a % 10;
-        a /= 10;
-    }
-    return ret;
+int generate(int a) {
+  int ret = a;
+  while (a) {
+    ret += a % 10;
+    a /= 10;
+  }
+  return ret;
 }
 
-int main()
-{
-    for (int i = 1; i < maxn; i++)
-        is_self[i] = true;
-    for (int i = 1; i < maxn; i++)
-    {
-        if (is_self[i])
-            printf("%d\n", i);
-        int temp = generate(i);
-        if (temp < maxn)
-            is_self[temp] = false;
-    }
-    return 0;
+int main() {
+  for (int i = 1; i < maxn; i++) is_self[i] = true;
+  for (int i = 1; i < maxn; i++) {
+    if (is_self[i]) printf("%d\n", i);
+    int temp = generate(i);
+    if (temp < maxn) is_self[temp] = false;
+  }
+  return 0;
 }
-
-
