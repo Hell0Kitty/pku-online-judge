306322d063bc36b6f4738f2010095254516469bb
sdecoder <sde.work.inbox@gmail.com>
2016-12-18 05:57:11 -0500

    2016-12-18 05:57:11

--- 2266.cc
+++ 2266.cc
@@ -22,21 +22,44 @@ typedef long long LL;
 typedef pair<int, int> PII;
 typedef pair<PII, int> PII2;
 
-template<class T1, class T2>
-ostream& operator << (ostream &out, pair<T1, T2> pair) {
-return out << "(" << pair.first << ", " << pair.second << ")"; }
+template <class T1, class T2>
+ostream& operator<<(ostream& out, pair<T1, T2> pair) {
+  return out << "(" << pair.first << ", " << pair.second << ")";
+}
 
 //#define DEBUGGER // show debugging message
-#ifdef DEBUGGER // require C11 (g++ -O2 -std=gnu++0x -Wall -Wshadow)
+#ifdef DEBUGGER  // require C11 (g++ -O2 -std=gnu++0x -Wall -Wshadow)
 template <class T>
-void _printcoll (T const& coll){ int first = 1; cout << "[";
-for (auto a : coll) { if (!first) printf(", "); cout << a; first = false; } cout << "]" << endl; }
+void _printcoll(T const& coll) {
+  int first = 1;
+  cout << "[";
+  for (auto a : coll) {
+    if (!first) printf(", ");
+    cout << a;
+    first = false;
+  }
+  cout << "]" << endl;
+}
 template <class T>
-void _printcoll (T const& coll, int len){ int first = 1; cout << "[";for (auto a : coll) {
-if (!first) printf(", "); cout << a; first = false; len--; if (!len) break; } cout << "]" << endl; }
-#define DBG(vari) cout<<#vari<<" = "<<(vari)<<endl;
-#define DBGC(vari) cout<<#vari<<" = ";_printcoll(vari);
-#define DBGCN(vari, len) cout<<#vari<<" = ";_printcoll(vari, len);
+void _printcoll(T const& coll, int len) {
+  int first = 1;
+  cout << "[";
+  for (auto a : coll) {
+    if (!first) printf(", ");
+    cout << a;
+    first = false;
+    len--;
+    if (!len) break;
+  }
+  cout << "]" << endl;
+}
+#define DBG(vari) cout << #vari << " = " << (vari) << endl;
+#define DBGC(vari)        \
+  cout << #vari << " = "; \
+  _printcoll(vari);
+#define DBGCN(vari, len)  \
+  cout << #vari << " = "; \
+  _printcoll(vari, len);
 #else
 #define DBG(vari)
 #define DBGC(vari)
@@ -51,50 +74,47 @@ string S;
 char board[MAXN][MAXN];
 int start;
 void solve(int ii, int jj, int len) {
-    if (len == 1) {
-        board[ii][jj] = S[start++];
-    } else if (S[start] != 'Q') {
-        for (int i = ii; i < ii + len; i++) {
-            for (int j = jj; j < jj + len; j++) {
-                board[i][j] = S[start];
-            }
-        }
-        start++;
-    } else {
-        start++;
-        solve(ii, jj, len / 2);
-        solve(ii, jj + len / 2, len / 2);
-        solve(ii + len / 2, jj, len / 2);
-        solve(ii + len / 2, jj + len / 2, len / 2);
+  if (len == 1) {
+    board[ii][jj] = S[start++];
+  } else if (S[start] != 'Q') {
+    for (int i = ii; i < ii + len; i++) {
+      for (int j = jj; j < jj + len; j++) {
+        board[i][j] = S[start];
+      }
     }
+    start++;
+  } else {
+    start++;
+    solve(ii, jj, len / 2);
+    solve(ii, jj + len / 2, len / 2);
+    solve(ii + len / 2, jj, len / 2);
+    solve(ii + len / 2, jj + len / 2, len / 2);
+  }
 }
 
 void print(int r, int c) {
-    int ret = 0;
-    for (int i = c + 7; i >= c; i--) {
-        ret <<= 1;
-        if (board[r][i] == 'B') ret += 1;
-    }
-    printf("0x%02x,", ret);
+  int ret = 0;
+  for (int i = c + 7; i >= c; i--) {
+    ret <<= 1;
+    if (board[r][i] == 'B') ret += 1;
+  }
+  printf("0x%02x,", ret);
 }
 
 int main() {
-    cin >> N >> S;
-    start = 0;
-    solve(0, 0, N);
+  cin >> N >> S;
+  start = 0;
+  solve(0, 0, N);
 
-    printf("#define quadtree_width %d\n", N);
-    printf("#define quadtree_height %d\n", N);
-    printf("static char quadtree_bits[] = {\n");
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N / 8; j++) {
-            print(i, j * 8);
-        }
-        printf("\n");
+  printf("#define quadtree_width %d\n", N);
+  printf("#define quadtree_height %d\n", N);
+  printf("static char quadtree_bits[] = {\n");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N / 8; j++) {
+      print(i, j * 8);
     }
-    printf("};");
-    return 0;
+    printf("\n");
+  }
+  printf("};");
+  return 0;
 }
-
-
-
