67e7233e9fcddd7ddfe36e40e4f39a324d2b8a1f
sdecoder <sde.work.inbox@gmail.com>
2016-12-19 16:38:18 -0500

    2016-12-19 16:38:18

--- 1477.cc
+++ 1477.cc
@@ -9,25 +9,18 @@ using namespace std;
 int n;
 int f[maxn];
 
-int main()
-{
-    int t = 0;
-    while (scanf("%d", &n), n)
-    {
-        for (int i = 0; i < n; i++)
-            scanf("%d", &f[i]);
-        int mean = 0;
-        for (int i = 0; i < n; i++)
-            mean += f[i];
-        mean /= n;
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-            ans += abs(f[i] - mean);
-        printf("Set #%d\n", ++t);
-        printf("The minimum number of moves is %d.\n", ans / 2);
-        puts("");
-    }
-    return 0;
+int main() {
+  int t = 0;
+  while (scanf("%d", &n), n) {
+    for (int i = 0; i < n; i++) scanf("%d", &f[i]);
+    int mean = 0;
+    for (int i = 0; i < n; i++) mean += f[i];
+    mean /= n;
+    int ans = 0;
+    for (int i = 0; i < n; i++) ans += abs(f[i] - mean);
+    printf("Set #%d\n", ++t);
+    printf("The minimum number of moves is %d.\n", ans / 2);
+    puts("");
+  }
+  return 0;
 }
-
-
