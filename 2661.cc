a0f6217cbb5aa2692a62b908fa0c35bb0b68d001
sdecoder <sde.work.inbox@gmail.com>
2016-12-19 11:19:06 -0500

    2016-12-19 11:19:06

--- 2661.cc
+++ 2661.cc
@@ -4,28 +4,21 @@
 using namespace std;
 const int Y = 1960;
 
-int main()
-{
-    int year;
+int main() {
+  int year;
 
-    double sum;
-    while(cin >> year)
-    {
-        if(year == 0)
-            break;
+  double sum;
+  while (cin >> year) {
+    if (year == 0) break;
 
-        int tmp = pow(2*1.0,(year - Y) / 10 + 2);
-        int n = 2;
-        sum = 0;
-        while(sum <= tmp)
-        {
-            sum += log(n*1.0) / log(2*1.0);
-            n++;
-        }
-        cout << n - 2 << endl;
+    int tmp = pow(2 * 1.0, (year - Y) / 10 + 2);
+    int n = 2;
+    sum = 0;
+    while (sum <= tmp) {
+      sum += log(n * 1.0) / log(2 * 1.0);
+      n++;
     }
-    return 0;
+    cout << n - 2 << endl;
+  }
+  return 0;
 }
-
-
-
