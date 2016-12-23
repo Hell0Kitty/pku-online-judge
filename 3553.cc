540e098f0a000c65515e55adb152378241149246
sdecoder <sde.work.inbox@gmail.com>
2016-12-23 01:56:54 -0500

    2016-12-23 01:56:54

--- 3553.cc
+++ 3553.cc
@@ -8,63 +8,46 @@ using namespace std;
 const int maxn = 50000 + 5;
 int n, m;
 
-struct node
-{
-    int p;
-    int d;
-    int id;
-}job[maxn];
+struct node {
+  int p;
+  int d;
+  int id;
+} job[maxn];
 
 vector<int> V[maxn];
 bool vis[maxn];
 
-bool cmp(node a, node b)
-{
-    return a.d < b.d;
+bool cmp(node a, node b) { return a.d < b.d; }
+
+void dfs(int cur) {
+  if (vis[cur]) return;
+  vis[cur] = true;
+  int k = V[cur].size();
+  for (int i = 0; i < k; i++) dfs(V[cur][i]);
+  printf("%d\n", cur);
 }
 
-void dfs(int cur)
-{
-    if(vis[cur])
-        return ;
-    vis[cur] = true;
-    int k = V[cur].size();
-    for(int i = 0; i < k; i++)
-        dfs(V[cur][i]);
-    printf("%d\n", cur);
+void init() {
+  memset(vis, false, sizeof vis);
+  for (int i = 1; i <= n; i++) V[i].clear();
 }
 
-void init()
-{
-    memset(vis, false, sizeof vis);
-    for(int i = 1; i <= n; i++)
-        V[i].clear();
-}
-
-int main()
-{
-    int a, b;
-    while(scanf("%d", &n) != EOF)
-    {
-        init();
-        for(int i = 1; i <= n; i++)
-        {
-            scanf("%d%d", &job[i].p, &job[i].d);
-            job[i].id = i;
-        }
-        sort(job+1, job+n+1, cmp);
-        scanf("%d", &m);
-        for(int i = 1; i <= m; i++)
-        {
-            scanf("%d%d", &a, &b);
-            V[b].push_back(a);
-        }
-        for(int i = 1; i <= n; i++)
-            if(!vis[job[i].id])
-                dfs(job[i].id);
+int main() {
+  int a, b;
+  while (scanf("%d", &n) != EOF) {
+    init();
+    for (int i = 1; i <= n; i++) {
+      scanf("%d%d", &job[i].p, &job[i].d);
+      job[i].id = i;
     }
-    return 0;
+    sort(job + 1, job + n + 1, cmp);
+    scanf("%d", &m);
+    for (int i = 1; i <= m; i++) {
+      scanf("%d%d", &a, &b);
+      V[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++)
+      if (!vis[job[i].id]) dfs(job[i].id);
+  }
+  return 0;
 }
-
-
-
