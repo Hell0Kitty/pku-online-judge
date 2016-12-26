6958c94d455f7ee8d98cc631a28f575268628806
sdecoder <sde.work.inbox@gmail.com>
2016-12-26 02:41:18 -0500

    2016-12-26 02:41:18

--- 3981.cc
+++ 3981.cc
@@ -1,47 +1,38 @@
-#include<stdio.h> 
+#include <stdio.h>
 
-char str[1002]; 
+char str[1002];
 
-int main() 
+int main()
 
-{ 
+{
+  int i;
 
-  int i; 
+  while (gets(str) != NULL)
 
-  while(gets(str)!=NULL) 
+  {
+    i = 0;
 
-  { 
+    for (i = 0; str[i] != '\0'; i++)
 
-    i=0;
+      if (str[i] == 'y' && str[i + 1] == 'o' && str[i + 2] == 'u')
 
-   for(i=0;str[i]!='\0';i++) 
+      {
+        printf("we");
 
-    if(str[i]=='y'&&str[i+1]=='o'&&str[i+2]=='u') 
+        i += 2;
 
-    { 
+      }
 
-      printf("we"); 
+      else
 
-      i+=2; 
+      {
+        printf("%c", str[i]);
+      }
 
-    } 
-
-    else
-
-     {   
-
-        printf("%c",str[i]); 
-
-     } 
-
-     printf("\0");
+    printf("\0");
 
     printf("\n");
+  }
 
-  } 
-
-  return 0; 
-
+  return 0;
 }
-
-
