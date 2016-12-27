92911525afa6e0f5da271402d2808e1bf96c47d2
sdecoder <sde.work.inbox@gmail.com>
2016-12-27 03:29:49 -0500

    2016-12-27 03:29:49

--- 1313.cc
+++ 1313.cc
@@ -9,49 +9,39 @@ using namespace std;
 int f[maxn][2];
 int n;
 
-void work()
-{
-    int page_num = (n + 3) / 4;
-    for (int i = 0; i < page_num * 2; i++)
-        f[i][(i & 1) ^ 1] = i + 1;
-    int j = page_num * 2 + 1;
-    for (int i = page_num * 2 - 1; i >= 0; i--, j++)
-        f[i][i & 1] = j;
+void work() {
+  int page_num = (n + 3) / 4;
+  for (int i = 0; i < page_num * 2; i++) f[i][(i & 1) ^ 1] = i + 1;
+  int j = page_num * 2 + 1;
+  for (int i = page_num * 2 - 1; i >= 0; i--, j++) f[i][i & 1] = j;
 }
 
-void output()
-{
-    int page_num = (n + 3) / 4;
-    for (int i = 0; i < page_num * 2; i++)
-    {
-        if (f[i][0] > n && f[i][1] > n)
-            continue;
-        printf("Sheet %d, ", i / 2 + 1);
-        if (i & 1)
-            printf("back :");
-        else
-            printf("front:");
-        if (f[i][0] <= n)
-            printf(" %d", f[i][0]);
-        else
-            printf(" Blank");
-        if (f[i][1] <= n)
-            printf(", %d", f[i][1]);
-        else
-            printf(", Blank");
-        puts("");
-    }
+void output() {
+  int page_num = (n + 3) / 4;
+  for (int i = 0; i < page_num * 2; i++) {
+    if (f[i][0] > n && f[i][1] > n) continue;
+    printf("Sheet %d, ", i / 2 + 1);
+    if (i & 1)
+      printf("back :");
+    else
+      printf("front:");
+    if (f[i][0] <= n)
+      printf(" %d", f[i][0]);
+    else
+      printf(" Blank");
+    if (f[i][1] <= n)
+      printf(", %d", f[i][1]);
+    else
+      printf(", Blank");
+    puts("");
+  }
 }
 
-int main()
-{
-    while (scanf("%d", &n), n)
-    {
-        printf("Printing order for %d pages:\n", n);
-        work();
-        output();
-    }
-    return 0;
+int main() {
+  while (scanf("%d", &n), n) {
+    printf("Printing order for %d pages:\n", n);
+    work();
+    output();
+  }
+  return 0;
 }
-
-
