d251bff87c30cd023500411d4e3d1b1e912e8016
sdecoder <sde.work.inbox@gmail.com>
2016-12-27 13:56:58 -0500

    2016-12-27 13:56:58

--- 3670.cc
+++ 3670.cc
@@ -10,53 +10,35 @@ int n;
 int cow[maxn];
 int f[maxn][5];
 
-void input()
-{
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &cow[i]);
+void input() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) scanf("%d", &cow[i]);
 }
 
-void work()
-{
-    memset(f, 0, sizeof(f));
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= 3; j++)
-        {
-            f[i][j] = f[i - 1][1];
-            for (int k = 2; k <= j; k++)
-                f[i][j] = min(f[i][j], f[i - 1][k]);
-            if (cow[i] != j)
-                f[i][j]++;
-        }
+void work() {
+  memset(f, 0, sizeof(f));
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= 3; j++) {
+      f[i][j] = f[i - 1][1];
+      for (int k = 2; k <= j; k++) f[i][j] = min(f[i][j], f[i - 1][k]);
+      if (cow[i] != j) f[i][j]++;
     }
-    int ans = n;
-    for (int i = 1; i <= 3; i++)
-        ans = min(ans, f[n][i]);
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= 3; j++)
-        {
-            f[i][j] = f[i - 1][3];
-            for (int k = 2; k >= j; k--)
-                f[i][j] = min(f[i][j], f[i - 1][k]);
-            if (cow[i] != j)
-                f[i][j]++;
-        }
+  }
+  int ans = n;
+  for (int i = 1; i <= 3; i++) ans = min(ans, f[n][i]);
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= 3; j++) {
+      f[i][j] = f[i - 1][3];
+      for (int k = 2; k >= j; k--) f[i][j] = min(f[i][j], f[i - 1][k]);
+      if (cow[i] != j) f[i][j]++;
     }
-    for (int i = 1; i <= 3; i++)
-        ans = min(ans, f[n][i]);
-    printf("%d\n", ans);
+  }
+  for (int i = 1; i <= 3; i++) ans = min(ans, f[n][i]);
+  printf("%d\n", ans);
 }
 
-int main()
-{
-    input();
-    work();
-    return 0;
+int main() {
+  input();
+  work();
+  return 0;
 }
-
-
-
-
