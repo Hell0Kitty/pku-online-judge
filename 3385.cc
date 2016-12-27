d6ffacda5629d60d59dd93414dbb1cc50cdf4f29
sdecoder <sde.work.inbox@gmail.com>
2016-12-27 06:57:21 -0500

    2016-12-27 06:57:21

--- 3385.cc
+++ 3385.cc
@@ -9,31 +9,24 @@ using namespace std;
 int n, d;
 int f[maxn];
 
-int cal(int a)
-{
-    int ret = 0;
-    while (a > d)
-    {
-        ret += a / d;
-        a = a / d + a % d;
-    }
-    return ret;
+int cal(int a) {
+  int ret = 0;
+  while (a > d) {
+    ret += a / d;
+    a = a / d + a % d;
+  }
+  return ret;
 }
 
-int main()
-{
-    scanf("%d%d", &n, &d);
-    for (int i = 0; i < n; i++)
-    {
-        int a;
-        scanf("%d", &a);
-        f[a]++;
-    }
-    int ans = 0;
-    for (int i = 0; i <= n; i++)
-        ans += cal(f[i]);
-    printf("%d\n", ans);
-    return 0;
+int main() {
+  scanf("%d%d", &n, &d);
+  for (int i = 0; i < n; i++) {
+    int a;
+    scanf("%d", &a);
+    f[a]++;
+  }
+  int ans = 0;
+  for (int i = 0; i <= n; i++) ans += cal(f[i]);
+  printf("%d\n", ans);
+  return 0;
 }
-
-
