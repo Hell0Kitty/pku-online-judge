7d2f74def3e0034c8e960e9d4b2987652be28c9c
sdecoder <sde.work.inbox@gmail.com>
2016-12-27 23:07:58 -0500

    2016-12-27 23:07:58

--- 2871.cc
+++ 2871.cc
@@ -1,20 +1,17 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<time.h>
-#include<iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
 using namespace std;
 int main() {
-	double a,b;
-	scanf("%lf",&a);
-	if(a!=999)
-	{
-		while(scanf("%lf",&b),b!=999) {
-			printf("%.2f\n",b-a);
-			a=b;
-		}
-	}
-	printf("End of Output\n");
-	return 0;
+  double a, b;
+  scanf("%lf", &a);
+  if (a != 999) {
+    while (scanf("%lf", &b), b != 999) {
+      printf("%.2f\n", b - a);
+      a = b;
+    }
+  }
+  printf("End of Output\n");
+  return 0;
 }
-
-
