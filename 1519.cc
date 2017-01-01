79b135096f4cf55c84e15a2d1f944ee540080953
sdecoder <sde.work.inbox@gmail.com>
2017-01-01 10:41:58 -0500

    2017-01-01 10:41:58

---
+++ 1519.cc
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+const int maxn = 1005;
+char a[ maxn ];
+int main(){
+    while( scanf("%s",a)!=EOF ){
+        if( strcmp(a,"0")==0 ) break;
+        int len;
+        len=strlen( a );
+        int ans=0;
+        for( int i=0;i<len;i++ ){
+            ans+=( a[ i ]-'0' );
+            if( ans>=10 ){
+                ans=ans/10+ans%10;
+            }
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}
+
+
