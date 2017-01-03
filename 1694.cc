6d4c8806d9a14a0efe0db32a8dd405f5d455d4b7
sdecoder <sde.work.inbox@gmail.com>
2017-01-03 16:03:41 -0500

    2017-01-03 16:03:41

--- 1694.cc
+++ 1694.cc
@@ -1,51 +1,36 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 #define N1 201
 int tree[N1][N1];
-bool cmp( const int a , const int b )
-{
-    return a>b;
-}
-int DFS( int node)
-{
-      int  num[N1], _max, i;
-      if(!tree[node][0])
-          return  1;
-      else
-      {
-          for(i=1;i<=tree[node][0];++i)
-              num[i-1] = DFS( tree[node][i] );
-          sort( num , num + tree[node][0] , cmp );
-          for( i = 0 , _max = -1 ; i < tree[node][0] ; ++i )
-          {
-              if( _max < num[i] + i )
-              { _max = num[i] + i; }
-          }
-          return _max;
+bool cmp(const int a, const int b) { return a > b; }
+int DFS(int node) {
+  int num[N1], _max, i;
+  if (!tree[node][0])
+    return 1;
+  else {
+    for (i = 1; i <= tree[node][0]; ++i) num[i - 1] = DFS(tree[node][i]);
+    sort(num, num + tree[node][0], cmp);
+    for (i = 0, _max = -1; i < tree[node][0]; ++i) {
+      if (_max < num[i] + i) {
+        _max = num[i] + i;
       }
-
-}
-int main()
-{
-    int i,j, M,N,p;
-    scanf("%d",&M);
-    while(M--)
-    {
-        scanf("%d",&N);
-        for(i=1;i<=N;i++)
-        {
-            scanf(" %d %d",&p,&tree[i][0]);
-                for(j=1;j<=tree[i][0];j++)
-                    scanf("%d",&tree[i][j]);
-        }
-            printf("%d\n",DFS(1));
-
-            memset(tree,0, sizeof(tree));
     }
-    return 0;
-
+    return _max;
+  }
 }
+int main() {
+  int i, j, M, N, p;
+  scanf("%d", &M);
+  while (M--) {
+    scanf("%d", &N);
+    for (i = 1; i <= N; i++) {
+      scanf(" %d %d", &p, &tree[i][0]);
+      for (j = 1; j <= tree[i][0]; j++) scanf("%d", &tree[i][j]);
+    }
+    printf("%d\n", DFS(1));
 
-
-
+    memset(tree, 0, sizeof(tree));
+  }
+  return 0;
+}
