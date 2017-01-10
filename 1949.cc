f94cac23e0674c0035ba70e78138184ce7c9c993
sdecoder <sde.work.inbox@gmail.com>
2017-01-10 11:31:03 -0500

    2017-01-10 11:31:03

--- 1949.cc
+++ 1949.cc
@@ -4,29 +4,22 @@
 #include <cstring>
 using namespace std;
 const int MAX = 10010;
-int			weight[ MAX ];
-int			n,stacki;
-int		main()
-{
-	int		mid,max,value,single;
-	max = -1;
-	scanf("%d",&n);
-	for( int i = 1 ; i <= n ; ++i )
-	{
-		scanf("%d%d",&value,&mid);
-		weight[i] = value;
-		for( int j = 0 ; j != mid ; ++j )
-		{
-			scanf("%d",&single);
-			if( weight[i] < weight[single] + value )
-				weight[i] = weight[single] + value;
-		}
-		if( weight[i] > max )
-			max = weight[i];
-	}
-	printf("%d\n",max);
-	return 0;
+int weight[MAX];
+int n, stacki;
+int main() {
+  int mid, max, value, single;
+  max = -1;
+  scanf("%d", &n);
+  for (int i = 1; i <= n; ++i) {
+    scanf("%d%d", &value, &mid);
+    weight[i] = value;
+    for (int j = 0; j != mid; ++j) {
+      scanf("%d", &single);
+      if (weight[i] < weight[single] + value)
+        weight[i] = weight[single] + value;
+    }
+    if (weight[i] > max) max = weight[i];
+  }
+  printf("%d\n", max);
+  return 0;
 }
-
-
-
