ab3fcc86f4e981779b1e8e0f4ca2602b1217a02d
sdecoder <sde.work.inbox@gmail.com>
2017-01-12 13:44:39 -0500

    2017-01-12 13:44:39

--- 1995.cc
+++ 1995.cc
@@ -1,39 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int quick_mod(int a,int b,int m)  
-{
-   a=a%m;
-   int ans=1;
-   while(b)
-   {
-     if(b&1)
-     {
-        ans=(ans*a)%m;
-     }
-     b>>=1;
-     a=(a*a)%m;
-   }
+int quick_mod(int a, int b, int m) {
+  a = a % m;
+  int ans = 1;
+  while (b) {
+    if (b & 1) {
+      ans = (ans * a) % m;
+    }
+    b >>= 1;
+    a = (a * a) % m;
+  }
 
-   return ans;
+  return ans;
 }
 
-int main()
-{
-   int size;
-   cin>>size;
-   int m, h, a, b;
-   while(cin>>m>>h)
-   {
-      int ans=0;
-        for(int i=0;i<h;i++)
-        {
-            cin>>a>>b;
-          ans=(ans+quick_mod(a,b,m))%m;
-        }
-      cout<<ans<<endl;
-   }
+int main() {
+  int size;
+  cin >> size;
+  int m, h, a, b;
+  while (cin >> m >> h) {
+    int ans = 0;
+    for (int i = 0; i < h; i++) {
+      cin >> a >> b;
+      ans = (ans + quick_mod(a, b, m)) % m;
+    }
+    cout << ans << endl;
+  }
 }
-
-
-
