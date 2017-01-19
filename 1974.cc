983e95a1a4ed2d5dd5b11b6766c3a066dc918dfc
sdecoder <sde.work.inbox@gmail.com>
2017-01-19 14:52:22 -0500

    2017-01-19 14:52:22

--- 1974.cc
+++ 1974.cc
@@ -7,88 +7,69 @@ using namespace std;
 
 #define maxp 140000
 
-struct Stone
-{
-    int x, y;
-}stone[maxp];
+struct Stone {
+  int x, y;
+} stone[maxp];
 
 int n, m, p;
 int ans;
 bool vis[maxp];
 
-bool operator < (const Stone &a, const Stone &b)
-{
-    if (a.x != b.x)
-        return a.x < b.x;
-    return a.y < b.y;
+bool operator<(const Stone &a, const Stone &b) {
+  if (a.x != b.x) return a.x < b.x;
+  return a.y < b.y;
 }
 
-int cal(int a)
-{
-    if (a > 1)
-        return 1;
-    return 0;
+int cal(int a) {
+  if (a > 1) return 1;
+  return 0;
 }
 
-void input()
-{
-    scanf("%d%d%d", &n, &m, &p);
-    for (int i = 0; i < p; i++)
-        scanf("%d%d", &stone[i].x, &stone[i].y);
+void input() {
+  scanf("%d%d%d", &n, &m, &p);
+  for (int i = 0; i < p; i++) scanf("%d%d", &stone[i].x, &stone[i].y);
 }
 
-void work()
-{
-    int pos = 0;
-    memset(vis, 0, sizeof(vis));
-    for (int i = 0; i < p - 1; i++)
-    {
-        ans += cal(stone[i].y - pos - 1);
-        pos = stone[i].y;
-        if (stone[i].x != stone[i + 1].x)
-        {
-            ans += cal(m - stone[i].y);
-            pos = 0;
-        }
-        vis[stone[i].x] = true;
+void work() {
+  int pos = 0;
+  memset(vis, 0, sizeof(vis));
+  for (int i = 0; i < p - 1; i++) {
+    ans += cal(stone[i].y - pos - 1);
+    pos = stone[i].y;
+    if (stone[i].x != stone[i + 1].x) {
+      ans += cal(m - stone[i].y);
+      pos = 0;
     }
-    ans += cal(stone[p - 1].y - pos - 1);
-    ans += cal(m - stone[p - 1].y);
-    vis[stone[p - 1].x] = true;
-    for (int i = 1; i <= n; i++)
-        if (!vis[i])
-            ans += cal(m);
+    vis[stone[i].x] = true;
+  }
+  ans += cal(stone[p - 1].y - pos - 1);
+  ans += cal(m - stone[p - 1].y);
+  vis[stone[p - 1].x] = true;
+  for (int i = 1; i <= n; i++)
+    if (!vis[i]) ans += cal(m);
 }
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        input();
-        if (n == 1 && m == 1)
-        {
-            printf("0\n");
-            continue;
-        }
-        if (p == 0)
-        {
-            printf("%d\n", cal(n) * m + cal(m) * n);
-            continue;
-        }
-        sort(stone, stone + p);
-        ans = 0;
-        work();
-        swap(n, m);
-        for (int i = 0; i < p; i++)
-            swap(stone[i].x, stone[i].y);
-        sort(stone, stone + p);
-        work();
-        printf("%d\n", ans);
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    input();
+    if (n == 1 && m == 1) {
+      printf("0\n");
+      continue;
     }
-    return 0;
+    if (p == 0) {
+      printf("%d\n", cal(n) * m + cal(m) * n);
+      continue;
+    }
+    sort(stone, stone + p);
+    ans = 0;
+    work();
+    swap(n, m);
+    for (int i = 0; i < p; i++) swap(stone[i].x, stone[i].y);
+    sort(stone, stone + p);
+    work();
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
-
