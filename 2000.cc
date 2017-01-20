853ee63a684f258c9b5ffda990792b29bf5e9bfb
sdecoder <sde.work.inbox@gmail.com>
2017-01-20 13:36:01 -0500

    2017-01-20 13:36:01

--- 2000.cc
+++ 2000.cc
@@ -1,29 +1,22 @@
 #include <iostream>
-#include<cstdio>
+#include <cstdio>
 using namespace std;
 int a[10001];
-int main()
-{
-    int n,j=1,i=1,temp=1,sum=0;
+int main() {
+  int n, j = 1, i = 1, temp = 1, sum = 0;
 
-    for(int i = 1;i<=10000;)
-    {
-        int k = j;
-        while(k>0)
-        {
-            sum += temp;
-            a[i++] = sum;
-            k--;
-        }
-        temp++;
-        j++;
+  for (int i = 1; i <= 10000;) {
+    int k = j;
+    while (k > 0) {
+      sum += temp;
+      a[i++] = sum;
+      k--;
     }
-    while(scanf("%d",&n)!=EOF&&n!=0)
-    {
-        printf("%d %d\n",n,a[n]);
-    }
-    return 0;
+    temp++;
+    j++;
+  }
+  while (scanf("%d", &n) != EOF && n != 0) {
+    printf("%d %d\n", n, a[n]);
+  }
+  return 0;
 }
-
-
-
