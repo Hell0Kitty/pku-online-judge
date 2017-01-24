5069c3176054388571c76871d51c382638b4385b
sdecoder <sde.work.inbox@gmail.com>
2017-01-24 16:47:03 -0500

    2017-01-24 16:47:03

--- 1806.cc
+++ 1806.cc
@@ -6,40 +6,31 @@ using namespace std;
 
 int n, size;
 
-void work()
-{
-    scanf("%d", &n);
-    size = n * 2 + 1;
-    for (int i = 0; i < size; i++)
-    {
-        printf("slice #%d:\n", i + 1);
-        for (int j = 0; j < size; j++)
-        {
-            for (int k = 0; k < size; k++)
-            {
-                int d = abs(j - n) + abs(k - n);
-                if (d + abs(i - n) > n)
-                    putchar('.');
-                else
-                    printf("%d", d + abs(i - n));
-            }
-            putchar('\n');
-        }
+void work() {
+  scanf("%d", &n);
+  size = n * 2 + 1;
+  for (int i = 0; i < size; i++) {
+    printf("slice #%d:\n", i + 1);
+    for (int j = 0; j < size; j++) {
+      for (int k = 0; k < size; k++) {
+        int d = abs(j - n) + abs(k - n);
+        if (d + abs(i - n) > n)
+          putchar('.');
+        else
+          printf("%d", d + abs(i - n));
+      }
+      putchar('\n');
     }
+  }
 }
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++)
-    {
-        printf("Scenario #%d:\n", i + 1);
-        work();
-        putchar('\n');
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    printf("Scenario #%d:\n", i + 1);
+    work();
+    putchar('\n');
+  }
+  return 0;
 }
-
-
-
