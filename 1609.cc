8d53e55a27aa12d58b43eaf6d3e5c025f6bf2b54
sdecoder <sde.work.inbox@gmail.com>
2017-02-10 22:18:25 -0500

    2017-02-10 22:18:25

--- 1609.cc
+++ 1609.cc
@@ -1,99 +1,81 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int n;
 
-struct e{
-
-    int s,t;
-
+struct e {
+  int s, t;
 };
 
 e a[10001];
 
-int cmp(const void *a,const void *b){
+int cmp(const void *a, const void *b) {
+  e *s = (e *)a;
 
-    e *s=(e*)a;
+  e *t = (e *)b;
 
-    e *t=(e*)b;
+  if (s->s != t->s)
 
-    if(s->s!=t->s)
+    return s->s - t->s;
 
-        return s->s-t->s;
+  else
 
-    else
-
-        return s->t-t->t;
-
+    return s->t - t->t;
 }
 
 int b[10001];
 
-int find(int s,int value){
+int find(int s, int value) {
+  int i = 0, k;
 
-    int i=0,k;
+  while (i <= s)
 
-    while(i<=s)
+  {
+    k = (i + s) >> 1;
+
+    if (b[k] > value)
+      s = k - 1;
+
+    else
+      i = k + 1;
+  }
+
+  return s;
+}
+
+void read() {
+  int i, j, k;
+
+  while (cin >> n) {
+    if (n == 0) break;
+
+    for (i = 0; i < n; i++) cin >> a[i].s >> a[i].t;
+
+    qsort(a, n, sizeof(e), cmp);
+
+    int m = 0;
+
+    b[0] = a[0].t;
+
+    for (i = 1; i < n; i++)
 
     {
+      j = find(m, a[i].t);
 
-        k=(i+s)>>1;
+      b[j + 1] = a[i].t;
 
-        if(b[k]>value) s=k-1;
-
-        else i=k+1;
-
+      if (j + 1 > m) m = j + 1;
     }
 
-    return s;
+    cout << m + 1 << endl;
+  }
 
+  cout << '*' << endl;
 }
 
-void read(){
+int main() {
+  read();
 
-    int i,j,k;
-
-    while(cin>>n){
-
-        if(n==0) break;
-
-    for(i=0;i<n;i++)
-
-        cin>>a[i].s>>a[i].t;
-
-    qsort(a,n,sizeof(e),cmp);
-
-      int  m=0;
-
-        b[0]=a[0].t;
-
-        for(i=1;i<n;i++)
-
-        {
-
-            j=find(m,a[i].t);
-
-            b[j+1]=a[i].t;
-
-            if(j+1>m) m=j+1;
-
-        }
-
-    cout<<m+1<<endl;
-
-    }
-
-    cout<<'*'<<endl;        
-
+  return 0;
 }
-
-int main(){
-
-    read();
-
-    return 0;
-
-}
-
-
