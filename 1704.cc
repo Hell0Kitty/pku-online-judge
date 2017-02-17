a7e15b36fe6832b9d2cd13ec43064b09402ab11a
sdecoder <sde.work.inbox@gmail.com>
2017-02-16 23:09:37 -0500

    2017-02-16 23:09:37

--- 1704.cc
+++ 1704.cc
@@ -10,38 +10,28 @@ using namespace std;
 int n;
 int f[maxn];
 
-void input()
-{
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &f[i]);
-    if (n & 1)
-        f[n++] = 0;
+void input() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d", &f[i]);
+  if (n & 1) f[n++] = 0;
 }
 
-void work()
-{
-    int ans = 0;
-    for (int i = n - 1; i > 0; i -= 2)
-        ans ^= f[i] - f[i - 1] - 1;
-    if (ans)
-        printf("Georgia will win\n");
-    else
-        printf("Bob will win\n");
+void work() {
+  int ans = 0;
+  for (int i = n - 1; i > 0; i -= 2) ans ^= f[i] - f[i - 1] - 1;
+  if (ans)
+    printf("Georgia will win\n");
+  else
+    printf("Bob will win\n");
 }
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        input();
-        sort(f, f + n);
-        work();
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    input();
+    sort(f, f + n);
+    work();
+  }
+  return 0;
 }
-
-
-
