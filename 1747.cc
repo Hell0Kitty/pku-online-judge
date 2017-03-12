52b5696c9685f7e7281b39e53a1a5dca5d832466
sdecoder <sde.work.inbox@gmail.com>
2017-03-12 19:19:30 -0400

    2017-03-12 19:19:30

--- 1747.cc
+++ 1747.cc
@@ -1,22 +1,19 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void solve(int n)
-{
-  if(n==1)
+void solve(int n) {
+  if (n == 1)
     printf("((A0|B0)|(A0|B0))");
-  else
-  {
-    printf("((A%d|B%d)|(",n-1,n-1);
-    solve(n-1);
-    printf("|((A%d|A%d)|(B%d|B%d))))",n-1,n-1,n-1,n-1);
+  else {
+    printf("((A%d|B%d)|(", n - 1, n - 1);
+    solve(n - 1);
+    printf("|((A%d|A%d)|(B%d|B%d))))", n - 1, n - 1, n - 1, n - 1);
   }
 }
 
-int main()
-{
+int main() {
   int n;
-  scanf("%d",&n);
+  scanf("%d", &n);
   solve(n);
   printf("\n");
   return 0;
