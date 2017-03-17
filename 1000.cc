81fdc5007442e7164673119dd57501f27d8cfbe8
Hell0Kitty <codeyui@outlook.com>
2017-03-17 22:03:29 +0800

    Update 1000.cc
    Some correct answers can be found at this blog:
    http://blog.sina.com.cn/s/blog_98cf2a6f01018tpt.html

--- 1000.cc
+++ 1000.cc
@@ -2,6 +2,6 @@
 int main() {
   int a, b;
   scanf("%d %d", &a, &b);
-  printf("%d\n", a + b);
+  printf("%d\n", a + b);     //how does this simple addition satisfiy the requirement of the question?
   return 0;
 }
