14d7934c39d55b392a0988586977dd25f4c11f58
sdecoder <sde.work.inbox@gmail.com>
2017-03-19 04:53:18 -0400

    2017-03-19 04:53:18

--- 1723.cc
+++ 1723.cc
@@ -10,22 +10,16 @@ using namespace std;
 int n;
 int x[maxn], y[maxn];
 
-int main()
-{
-       scanf("%d", &n);
-    for (int i =0; i < n; i++)
-        scanf("%d%d", &x[i], &y[i]);
-    sort(y, y + n);
-    sort(x, x + n);
-    for (int i =0; i < n; i++)
-        x[i] -= i;
-    sort(x, x + n);
-    int a = (n +1) /2-1;
-    int ans =0;
-    for (int i =0; i < n; i++)
-        ans += abs(y[a] - y[i]) + abs(x[a] - x[i]);
-    printf("%d\n", ans);
-    return 0;
+int main() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d%d", &x[i], &y[i]);
+  sort(y, y + n);
+  sort(x, x + n);
+  for (int i = 0; i < n; i++) x[i] -= i;
+  sort(x, x + n);
+  int a = (n + 1) / 2 - 1;
+  int ans = 0;
+  for (int i = 0; i < n; i++) ans += abs(y[a] - y[i]) + abs(x[a] - x[i]);
+  printf("%d\n", ans);
+  return 0;
 }
-
-
