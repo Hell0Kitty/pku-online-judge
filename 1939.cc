262d58dc13452659288d6c07fb93ba108c29c992
sdecoder <sde.work.inbox@gmail.com>
2017-04-05 06:47:22 -0400

    2017-04-05 06:47:22

--- 1939.cc
+++ 1939.cc
@@ -10,23 +10,19 @@
 using namespace std;
 
 const int MAX = 1010;
-struct point{ double x,y;};
+struct point {
+  double x, y;
+};
 point p[MAX];
-int main()
-{
-	int n;
-	while( ~scanf("%d",&n) )
-	{
-		for(int i=0; i<n; i++)
-			scanf("%lf %lf",&p[i].x,&p[i].y);
-		printf("%d",n);
-		for(int i=0; i<n; i++)
-			printf(" %.6lf %.6lf",(p[i].x + p[(i+1)%n].x)/2.0,
-								(p[i].y + p[(i+1)%n].y)/2.0);
-		printf("\n");
-	}
-return 0;
+int main() {
+  int n;
+  while (~scanf("%d", &n)) {
+    for (int i = 0; i < n; i++) scanf("%lf %lf", &p[i].x, &p[i].y);
+    printf("%d", n);
+    for (int i = 0; i < n; i++)
+      printf(" %.6lf %.6lf", (p[i].x + p[(i + 1) % n].x) / 2.0,
+             (p[i].y + p[(i + 1) % n].y) / 2.0);
+    printf("\n");
+  }
+  return 0;
 }
-
-
-
