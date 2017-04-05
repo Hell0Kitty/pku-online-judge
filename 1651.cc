7e9dfaf0c64cc146b72d0e0ea624afe313ce919d
sdecoder <sde.work.inbox@gmail.com>
2017-04-04 21:33:03 -0400

    2017-04-04 21:33:03

--- 1651.cc
+++ 1651.cc
@@ -6,65 +6,50 @@ using namespace std;
 
 int dp[max][max];
 
-int a[max]; 
+int a[max];
 
-int min(int a,int b)
+int min(int a, int b)
 
 {
-
-    return a>b?b:a;
-
+  return a > b ? b : a;
 }
 
 int main()
 
 {
+  int n;
 
-    int n;
+  cin >> n;
 
-    cin>>n;
+  int i, j, k;
 
-    int i,j,k;
+  for (i = 0; i < n; i++) cin >> a[i];
 
-    for(i=0;i<n;i++)
+  for (i = 0; i < n - 2; i++) dp[i][i + 2] = a[i] * a[i + 1] * a[i + 2];
 
-        cin>>a[i];
+  int len;
 
-    for(i=0;i<n-2;i++)
+  for (len = 3; len < n; len++)
 
-        dp[i][i+2]=a[i]*a[i+1]*a[i+2];
+    for (i = 0; i + len < n; i++)
 
-    int len;
+    {
+      j = i + len;
 
-    for(len=3;len<n;len++)
+      for (k = i + 1; k < j; k++)
 
-        for(i=0;i+len<n;i++)
+      {
+        if (dp[i][j] == 0)
 
-        {
+          dp[i][j] = dp[i][k] + dp[k][j] + a[i] * a[k] * a[j];
 
-            j=i+len;
+        else
 
-            for(k=i+1;k<j;k++)
+          dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + a[i] * a[j] * a[k]);
+      }
+    }
 
-            {
+  cout << dp[0][n - 1] << endl;
 
-                    if(dp[i][j]==0)
-
-                        dp[i][j]=dp[i][k]+dp[k][j]+a[i]*a[k]*a[j];
-
-                    else
-
-                        dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]+a[i]*a[j]*a[k]);
-
-            }
-
-        }
-
-        cout<<dp[0][n-1]<<endl;
-
-    return 0;
-
+  return 0;
 }
-
-
-
