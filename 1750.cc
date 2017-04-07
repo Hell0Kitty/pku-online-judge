b9f9ac68d49858d201d6dcab53c69ed991083d5e
sdecoder <sde.work.inbox@gmail.com>
2017-04-07 16:45:02 -0400

    2017-04-07 16:45:02

--- 1750.cc
+++ 1750.cc
@@ -8,27 +8,24 @@ using namespace std;
 
 char st[2][maxl];
 
-int main()
-{
-    int sum = 0;
-    int i = 0;
-    st[1][0] = '\0';
-    while (scanf("%s", st[i]) != EOF)
-    {
-        int same = 0;
-        for (int j = 0; j < sum + 1; j++)
-            if (st[i][j] == st[i ^ 1][j] && st[i][j] != 0)
-                same++;
-            else
-                break;
-        if (same == sum + 1)
-            sum++;
-        else
-            sum = same;
-        for (int j = 0; j < sum; j++)
-            putchar(' ');
-        printf("%s\n", st[i]);
-        i ^= 1;
-    }
-    return 0;
+int main() {
+  int sum = 0;
+  int i = 0;
+  st[1][0] = '\0';
+  while (scanf("%s", st[i]) != EOF) {
+    int same = 0;
+    for (int j = 0; j < sum + 1; j++)
+      if (st[i][j] == st[i ^ 1][j] && st[i][j] != 0)
+        same++;
+      else
+        break;
+    if (same == sum + 1)
+      sum++;
+    else
+      sum = same;
+    for (int j = 0; j < sum; j++) putchar(' ');
+    printf("%s\n", st[i]);
+    i ^= 1;
+  }
+  return 0;
 }
