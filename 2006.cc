10803f1b6a98998a26f866c111737dd59dd7f7c2
sdecoder <sde.work.inbox@gmail.com>
2017-04-08 04:03:14 -0400

    2017-04-08 04:03:14

--- 2006.cc
+++ 2006.cc
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
   double Ka;   //平衡常数
   double ori;  //原始酸浓度
-  int m;  // 1摩酸分子 完全溶解 分解出氢离子数
-  int n;  // 1摩酸分子 完全溶解 分解出酸根离子数
-  double H;  //最后计算Ph值的氢离子数
+  int m;       // 1摩酸分子 完全溶解 分解出氢离子数
+  int n;       // 1摩酸分子 完全溶解 分解出酸根离子数
+  double H;    //最后计算Ph值的氢离子数
 
   while (1) {
     cin >> Ka;
