447cedd36812d65b246ff1f423356fef1fb059a6
sdecoder <sde.work.inbox@gmail.com>
2017-04-12 07:26:33 -0400

    2017-04-12 07:26:33

--- 1947.cc
+++ 1947.cc
@@ -4,56 +4,48 @@
 
 const int INF = 10000000;
 const int N = 160;
-int n,p,root;
-int head[N],son[N],dp[N][N];
+int n, p, root;
+int head[N], son[N], dp[N][N];
 bool f[N];
 
-int min(int a, int b) {return  a < b ? a : b;}
+int min(int a, int b) { return a < b ? a : b; }
 
-void dfs(int v)
-{
-    int i,j,k;
-    for(i = 0; i <= p; i++) dp[v][i] = INF;
-    dp[v][1] = 0;
-    int s = son[v];
-    while(s)
-    {
-        dfs(s);
-        for(i = p; i >= 0; i--)
-        {
-            int tmp = dp[v][i]+1;
-            for(j = 1; j < i; j++)
-                tmp = min(tmp, dp[v][j]+dp[s][i-j]);
-            dp[v][i] = tmp;
-        }
-        s = head[s];
+void dfs(int v) {
+  int i, j, k;
+  for (i = 0; i <= p; i++) dp[v][i] = INF;
+  dp[v][1] = 0;
+  int s = son[v];
+  while (s) {
+    dfs(s);
+    for (i = p; i >= 0; i--) {
+      int tmp = dp[v][i] + 1;
+      for (j = 1; j < i; j++) tmp = min(tmp, dp[v][j] + dp[s][i - j]);
+      dp[v][i] = tmp;
     }
+    s = head[s];
+  }
 }
 
-int main()
-{
-    int i,j,k;
-    while(~scanf("%d%d", &n, &p))
-    {
-        memset(son, 0, sizeof(son));
-        memset(f, 1, sizeof(f));
-        for(i = 1; i < n; i++)
-        {
-            scanf("%d%d", &j, &k);
-            head[k] = son[j];
-            son[j] = k;
-            f[k] = 0;
-        }
-        for(i = 1; i <= n; i++)
-            if(f[i]) {root = i;break;}
-        dfs(root);
-        int ans = dp[root][p];
-        for(i = 1; i <= n; i++)
-            ans = min(ans, dp[i][p]+1);
-        printf("%d\n", ans);
+int main() {
+  int i, j, k;
+  while (~scanf("%d%d", &n, &p)) {
+    memset(son, 0, sizeof(son));
+    memset(f, 1, sizeof(f));
+    for (i = 1; i < n; i++) {
+      scanf("%d%d", &j, &k);
+      head[k] = son[j];
+      son[j] = k;
+      f[k] = 0;
     }
-    return 0;
+    for (i = 1; i <= n; i++)
+      if (f[i]) {
+        root = i;
+        break;
+      }
+    dfs(root);
+    int ans = dp[root][p];
+    for (i = 1; i <= n; i++) ans = min(ans, dp[i][p] + 1);
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
-
