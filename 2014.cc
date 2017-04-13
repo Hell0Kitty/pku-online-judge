d8083750f14a265dc8cc931080b5e6656fe857ff
sdecoder <sde.work.inbox@gmail.com>
2017-04-13 19:36:23 -0400

    2017-04-13 19:36:23

--- 2014.cc
+++ 2014.cc
@@ -1,65 +1,55 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int limitwidth;
+int main() {
+  int limitwidth;
 
-	int x;
-	int y;
+  int x;
+  int y;
 
-	int totalwidth;//for all
-	int totalheight;//for all
+  int totalwidth;  // for all
+  int totalheight;  // for all
 
-	cin>>limitwidth;
+  cin >> limitwidth;
 
-	while(limitwidth != 0)
-	{
-		totalwidth = 0;
-		totalheight = 0;
+  while (limitwidth != 0) {
+    totalwidth = 0;
+    totalheight = 0;
 
-		int sumwidthforeachline = 0;//for each line
-		int maxheightforeachline = 0;//for each line
+    int sumwidthforeachline = 0;  // for each line
+    int maxheightforeachline = 0;  // for each line
 
-		cin>>x>>y;
+    cin >> x >> y;
 
-		while(!((x == -1) && (y == -1)))
-		{
-			if(sumwidthforeachline + x <= limitwidth)//put it on the current line
-			{
-				sumwidthforeachline += x;
+    while (!((x == -1) && (y == -1))) {
+      if (sumwidthforeachline + x <= limitwidth)  // put it on the current line
+      {
+        sumwidthforeachline += x;
 
-			}
-			else//need to turn to next line
-			{
-				sumwidthforeachline = 0;
+      } else  // need to turn to next line
+      {
+        sumwidthforeachline = 0;
 
-				totalheight += maxheightforeachline;
+        totalheight += maxheightforeachline;
 
-				maxheightforeachline = 0;
+        maxheightforeachline = 0;
 
-				sumwidthforeachline += x;
+        sumwidthforeachline += x;
+      }
 
-			}
+      if (sumwidthforeachline > totalwidth) totalwidth = sumwidthforeachline;
 
-			if(sumwidthforeachline > totalwidth)
-					totalwidth = sumwidthforeachline;
+      if (y > maxheightforeachline) maxheightforeachline = y;
 
-			if(y > maxheightforeachline)
-					maxheightforeachline = y;
+      cin >> x >> y;
+    }  // end of while(!((x == -1) && (y == -1)))
 
-			cin>>x>>y;
-		}//end of while(!((x == -1) && (y == -1)))
+    totalheight += maxheightforeachline;
 
-		totalheight += maxheightforeachline;
+    cout << totalwidth << " x " << totalheight << endl;
 
-		cout<<totalwidth<<" x "<<totalheight<<endl;
+    cin >> limitwidth;
+  }
 
-		cin>>limitwidth;
-	}
-
-	return 0;
+  return 0;
 }
-
-
-
