2a58a680c94111b36bbdb0f47cbd8311316e5df6
sdecoder <sde.work.inbox@gmail.com>
2017-04-16 04:58:48 -0400

    2017-04-16 04:58:48

--- 1740.cc
+++ 1740.cc
@@ -6,27 +6,24 @@ using namespace std;
 
 bool f[1005];
 
-int main()
-{
-    int n;
-    while (scanf("%d", &n), n)
-    {
-        memset(f, 0, sizeof(f));
-        int ans = 0;
-        for (int i = 0; i < n; i++)
-        {
-            int a;
-            scanf("%d", &a);
-            if (f[a])
-                ans--;
-            else
-                ans++;
-            f[a] = !f[a];
-        }
-        if (ans)
-            printf("1\n");
-        else
-            printf("0\n");
+int main() {
+  int n;
+  while (scanf("%d", &n), n) {
+    memset(f, 0, sizeof(f));
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+      int a;
+      scanf("%d", &a);
+      if (f[a])
+        ans--;
+      else
+        ans++;
+      f[a] = !f[a];
     }
-    return 0;
+    if (ans)
+      printf("1\n");
+    else
+      printf("0\n");
+  }
+  return 0;
 }
