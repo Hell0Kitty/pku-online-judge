15f85c21c9c723e8aa603473c24825a2a4daf53c
sdecoder <sde.work.inbox@gmail.com>
2017-05-03 04:39:42 -0400

    2017-05-03 04:39:42

--- 1547.cc
+++ 1547.cc
@@ -7,39 +7,29 @@ using namespace std;
 
 #define maxn 15
 
-struct Student
-{
-    int v;
-    char name[10];
-}stu[maxn];
+struct Student {
+  int v;
+  char name[10];
+} stu[maxn];
 
 int n;
 
-bool operator < (const Student &a, const Student &b)
-{
-    return a.v < b.v;
+bool operator<(const Student &a, const Student &b) { return a.v < b.v; }
+
+void input() {
+  for (int i = 0; i < n; i++) {
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    stu[i].v = a * b * c;
+    scanf("%s", stu[i].name);
+  }
 }
 
-void input()
-{
-    for (int i = 0; i < n; i++)
-    {
-        int a, b, c;
-        scanf("%d%d%d", &a, &b, &c);
-        stu[i].v = a * b * c;
-        scanf("%s", stu[i].name);
-    }
+int main() {
+  while (scanf("%d", &n), ~n) {
+    input();
+    sort(stu, stu + n);
+    printf("%s took clay from %s.\n", stu[n - 1].name, stu[0].name);
+  }
+  return 0;
 }
-
-int main()
-{
-    while (scanf("%d", &n), ~n)
-    {
-        input();
-        sort(stu, stu + n);
-        printf("%s took clay from %s.\n", stu[n - 1].name, stu[0].name);
-    }
-    return 0;
-}
-
-
