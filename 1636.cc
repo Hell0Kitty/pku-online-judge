005de2bab565ce5ae5472332e9c77731943e82b2
sdecoder <sde.work.inbox@gmail.com>
2017-05-14 08:31:09 -0400

    2017-05-14 08:31:09

--- 1636.cc
+++ 1636.cc
@@ -1,168 +1,140 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int n,m;
+int n, m;
 
-int cnt=0;
+int cnt = 0;
 
-typedef struct e{
+typedef struct e {
+  int data;
 
-    int data;
+  e *next;
 
-    e *next;
-
-}e;
+} e;
 
 e edge[401];
 
 int v[401];
 
-int a[402],b[402];
+int a[402], b[402];
 
 int dp[402][402];
 
-void dfs(int s){
+void dfs(int s) {
+  v[s] = 1;
 
-    v[s]=1;
+  if (s <= n)
 
-    if(s<=n)
+    a[cnt]++;
 
-        a[cnt]++;
+  else
 
-    else
+    b[cnt]++;
 
-        b[cnt]++;
+  e *p = edge[s].next;
 
-    e *p=edge[s].next;
+  while (p) {
+    if (!v[p->data]) dfs(p->data);
 
-    while(p){
-
-        if(!v[p->data])
-
-            dfs(p->data);
-
-        p=p->next;
-
-    }
-
+    p = p->next;
+  }
 }
 
-void solve(){
+void solve() {
+  int i, j, k;
 
-    int i,j,k;
+  memset(dp, 0, sizeof(dp));
 
-    memset(dp,0,sizeof(dp));
+  dp[0][0] = 1;
 
-    dp[0][0]=1;
+  for (k = 1; k <= cnt; k++)
 
-    for(k=1;k<=cnt;k++)
+    for (i = n / 2; i >= a[k]; i--)
 
-        for(i=n/2;i>=a[k];i--)
+      for (j = n / 2; j >= b[k]; j--)
 
-            for(j=n/2;j>=b[k];j--)
+        dp[i][j] = dp[i][j] || dp[i - a[k]][j - b[k]];
 
-                dp[i][j]=dp[i][j]||dp[i-a[k]][j-b[k]];
+  for (i = n / 2; i >= 0; i--)
 
-    for(i=n/2;i>=0;i--)
+    if (dp[i][i]) break;
 
-        if(dp[i][i])
-
-            break;
-
-    cout<<i<<endl;
-
+  cout << i << endl;
 }
 
-void bfs(){
+void bfs() {
+  int i, j, k;
 
-    int i,j,k;
+  cnt = 0;
 
-    cnt=0;
+  memset(v, 0, sizeof(v));
 
-    memset(v,0,sizeof(v));
+  memset(a, 0, sizeof(a));
 
-    memset(a,0,sizeof(a));
+  memset(b, 0, sizeof(b));
 
-    memset(b,0,sizeof(b));
+  for (i = 1; i <= 2 * n; i++)
 
-    for(i=1;i<=2*n;i++)
-
-        if(v[i]==0)
-
-        {
-
-            cnt++;
-
-            dfs(i);
-
-        }
-
-}
-
-void read(){
-
-    int i,j,k,s,t;
-
-    int K;
-
-    cin>>K;
-
-    while(K--)
+    if (v[i] == 0)
 
     {
+      cnt++;
 
-        cin>>n>>m;
+      dfs(i);
+    }
+}
 
-        for(i=1;i<=2*n;i++)
+void read() {
+  int i, j, k, s, t;
 
-        {
+  int K;
 
-            edge[i].data=i;
+  cin >> K;
 
-            edge[i].next=0;
+  while (K--)
 
-        }
+  {
+    cin >> n >> m;
 
-        for(i=1;i<=m;i++)
+    for (i = 1; i <= 2 * n; i++)
 
-        {
+    {
+      edge[i].data = i;
 
-            cin>>j>>k;
-
-            e *p=new e;
-
-            p->data=k+n;
-
-            p->next=edge[j].next;
-
-            edge[j].next=p;
-
-            e *q=new e;
-
-            q->data=j;
-
-            q->next=edge[k+n].next;
-
-            edge[k+n].next=q;
-
-        }
-
-        bfs();
-
-        solve();
-
+      edge[i].next = 0;
     }
 
+    for (i = 1; i <= m; i++)
+
+    {
+      cin >> j >> k;
+
+      e *p = new e;
+
+      p->data = k + n;
+
+      p->next = edge[j].next;
+
+      edge[j].next = p;
+
+      e *q = new e;
+
+      q->data = j;
+
+      q->next = edge[k + n].next;
+
+      edge[k + n].next = q;
+    }
+
+    bfs();
+
+    solve();
+  }
 }
 
-int main(){
+int main() {
+  read();
 
-    read();
-
-    return 0;
-
+  return 0;
 }
-
-
-
