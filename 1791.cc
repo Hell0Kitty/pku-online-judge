7a137f87afc0b4f4ee793f2baacb5d8228e0816e
sdecoder <sde.work.inbox@gmail.com>
2017-05-24 16:28:02 -0400

    2017-05-24 16:28:02

--- 1791.cc
+++ 1791.cc
@@ -17,53 +17,50 @@ const double eps = 10e-9;
 const int maxn = 1000 + 20;
 
 int main() {
-	int a, b, c, d, e, f;
+  int a, b, c, d, e, f;
 
-    while(scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f) != EOF 
-            && a && b && c && d && e &&f) {
-      int ans = INF;
-      int ta, tb;
-      ta = a * c;
-       tb = b * d;
-      if(ta<=e && tb<=f) {
-           int tans = 0;
-          if(ta < e) tans += 1;
-          if(tb < f) tans += 1;
-          tans += a * b - 1;
-        ans = min(ans, tans);
-        }
-       if(ta<=f && tb<=e) {
-           int tans = 0;
-           if(ta < f) tans += 1;
-           if(tb < e) tans += 1;
-           tans += a * b - 1;
-           ans = min(ans, tans);
-        }
-       ta = a * d;
-       tb = b * c;
-       if(ta<=e && tb<=f) {
-           int tans = 0;
-           if(ta < e) tans += 1;
-           if(tb < f) tans += 1;
-           tans += a * b - 1;
-           ans = min(ans, tans);
-        }
-       if(ta<=f && tb<=e) {
-           int tans = 0;
-           if(ta < f) tans += 1;
-           if(tb < e) tans += 1;
-           tans += a * b - 1;
-           ans = min(ans, tans);
-        }
-       if(ans == INF) {
-           printf("The paper is too small.\n"); 
-        } else {
-           printf("The minimum number of cuts is %d.\n", ans);
-        }
-   }
+  while (scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f) != EOF && a && b && c &&
+         d && e && f) {
+    int ans = INF;
+    int ta, tb;
+    ta = a * c;
+    tb = b * d;
+    if (ta <= e && tb <= f) {
+      int tans = 0;
+      if (ta < e) tans += 1;
+      if (tb < f) tans += 1;
+      tans += a * b - 1;
+      ans = min(ans, tans);
+    }
+    if (ta <= f && tb <= e) {
+      int tans = 0;
+      if (ta < f) tans += 1;
+      if (tb < e) tans += 1;
+      tans += a * b - 1;
+      ans = min(ans, tans);
+    }
+    ta = a * d;
+    tb = b * c;
+    if (ta <= e && tb <= f) {
+      int tans = 0;
+      if (ta < e) tans += 1;
+      if (tb < f) tans += 1;
+      tans += a * b - 1;
+      ans = min(ans, tans);
+    }
+    if (ta <= f && tb <= e) {
+      int tans = 0;
+      if (ta < f) tans += 1;
+      if (tb < e) tans += 1;
+      tans += a * b - 1;
+      ans = min(ans, tans);
+    }
+    if (ans == INF) {
+      printf("The paper is too small.\n");
+    } else {
+      printf("The minimum number of cuts is %d.\n", ans);
+    }
+  }
 
-	return 0;
+  return 0;
 }
-
-
-
