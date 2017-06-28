085a1959186d075cc076863696d12e391ad34428
sdecoder <sde.work.inbox@gmail.com>
2017-06-27 22:56:46 -0400

    2017-06-27 22:56:46

--- 1837.cc
+++ 1837.cc
@@ -1,8 +1,8 @@
-nclude <iostream>
+nclude<iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-using namespace std;
+    using namespace std;
 
 #define maxn 25
 #define maxw 10005
@@ -11,23 +11,16 @@ int n, m;
 int hook[maxn], weight[maxn];
 int f[maxn][maxw];
 
-int main()
-{
-
-    scanf("%d%d", &n, &m);
-    for (int i =0; i < n; i++)
-        scanf("%d", &hook[i]);
-    for (int i =0; i < m; i++)
-        scanf("%d", &weight[i]);
-    f[0][5000] =1;
-    for (int i =0; i < m; i++)
-        for (int j =0; j < maxw; j++)
-            if (f[i][j])
-                for (int k =0; k < n; k++)
-                    f[i +1][j + hook[k] * weight[i]] += f[i][j];
-    printf("%d\n", f[m][5000]);
-    return 0;
+int main() {
+  scanf("%d%d", &n, &m);
+  for (int i = 0; i < n; i++) scanf("%d", &hook[i]);
+  for (int i = 0; i < m; i++) scanf("%d", &weight[i]);
+  f[0][5000] = 1;
+  for (int i = 0; i < m; i++)
+    for (int j = 0; j < maxw; j++)
+      if (f[i][j])
+        for (int k = 0; k < n; k++)
+          f[i + 1][j + hook[k] * weight[i]] += f[i][j];
+  printf("%d\n", f[m][5000]);
+  return 0;
 }
-
-
-
