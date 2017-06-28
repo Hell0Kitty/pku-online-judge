b3bb5727e6dbeb1b25d0b8d1d9b880e8361a99aa
sdecoder <sde.work.inbox@gmail.com>
2017-06-28 14:37:41 -0400

    2017-06-28 14:37:41

--- 1909.cc
+++ 1909.cc
@@ -7,69 +7,54 @@
 using namespace std;
 
 const int M = 10005;
-vector<int>que[M];
+vector<int> que[M];
 int num[M];
 int rd[M];
 int n, ans;
 
 void init() {
-
-    for(int i = 0; i <= n; i++){
-
-        que[i].clear();
-        num[i] = 0;
-        rd[i] = 0;
-    }
+  for (int i = 0; i <= n; i++) {
+    que[i].clear();
+    num[i] = 0;
+    rd[i] = 0;
+  }
 }
 
-void dfs(int u){
-
-     int v;
-    for(int i = 0; i < (int)que[u].size(); i++) {
-
-        v = que[u][i];
-        dfs(v);
-        num[u] += num[v];
-        ans += fabs(num[v]);
-    }
-    num[u] -= 1;
+void dfs(int u) {
+  int v;
+  for (int i = 0; i < (int)que[u].size(); i++) {
+    v = que[u][i];
+    dfs(v);
+    num[u] += num[v];
+    ans += fabs(num[v]);
+  }
+  num[u] -= 1;
 }
 
-int main()
-{
-    int a, b, q;
+int main() {
+  int a, b, q;
 
-    while(scanf("%d", &n) != EOF) {
-
-        if(!n)
-            break;
-         init();
-         ans = 0;
-        for(int i = 0; i< n; i++){
-
-            scanf("%d%d%d", &a, &b, &q);
-            num[a] = b;
-            for(int j = 0; j < q; j++){
-
-                scanf("%d", &b);
-                que[a].push_back(b);
-                rd[b]++;
-            }
-
-        }
-        for(int i = 1; i <= n; i++){
-
-            if(!rd[i]){
-
-                dfs(i);
-                break;
-            }
-        }
-        printf("%d\n", ans);
+  while (scanf("%d", &n) != EOF) {
+    if (!n) break;
+    init();
+    ans = 0;
+    for (int i = 0; i < n; i++) {
+      scanf("%d%d%d", &a, &b, &q);
+      num[a] = b;
+      for (int j = 0; j < q; j++) {
+        scanf("%d", &b);
+        que[a].push_back(b);
+        rd[b]++;
+      }
     }
+    for (int i = 1; i <= n; i++) {
+      if (!rd[i]) {
+        dfs(i);
+        break;
+      }
+    }
+    printf("%d\n", ans);
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
