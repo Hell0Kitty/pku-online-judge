e07e63c4fa411bd79d50ba8d919422fc4679a872
sdecoder <sde.work.inbox@gmail.com>
2017-07-02 10:37:58 -0400

    2017-07-02 10:37:58

--- 1770.cc
+++ 1770.cc
@@ -8,132 +8,107 @@
 
 #define N 205
 
-#define MAX(a,b) ((a)>(b)?(a):(b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 using namespace std;
 
-map<int,int>hash;
+map<int, int> hash;
 
-vector<int>gra[N],am,cm,tm;
+vector<int> gra[N], am, cm, tm;
 
-int vis[N],dp[N][2],n,m;
+int vis[N], dp[N][2], n, m;
 
 void dfs(int s)
 
 {
+  vis[s] = 1;
 
-    vis[s]=1;
+  for (int i = 0; i < gra[s].size(); i++)
 
-    for(int i=0;i<gra[s].size();i++)
+  {
+    int t = gra[s][i];
 
-    {
+    if (vis[t]) continue;
 
-        int t=gra[s][i];
+    dfs(t);
 
-        if(vis[t])continue;
+    dp[s][0] += MAX(dp[t][0], dp[t][1]);
 
-        dfs(t);
+    dp[s][1] += dp[t][0];
+  }
 
-        dp[s][0]+=MAX(dp[t][0],dp[t][1]);
-
-        dp[s][1]+=dp[t][0];
-
-    }
-
-    dp[s][1]+=am[s];
-
+  dp[s][1] += am[s];
 }
 
 void re(void)
 
 {
+  int a;
 
-    int a;
+  hash.clear();
 
-    hash.clear();
+  for (int i = 0; i <= n; i++) gra[i].clear();
 
-    for(int i=0;i<=n;i++)
+  am.clear();
+  cm.clear();
 
-        gra[i].clear();
+  for (int i = 1; i <= n; i++)
 
-    am.clear();cm.clear();
+  {
+    scanf("%d", &a);
 
-    for(int i=1;i<=n;i++)
+    hash[a] = i - 1;
 
-    {
+    am.push_back(a);
+  }
 
-        scanf("%d",&a);
-
-        hash[a]=i-1;
-
-        am.push_back(a);
-
-    }
-
-    for(int i=1;i<=m;i++)
-
-        scanf("%d",&a),cm.push_back(a);
-
+  for (int i = 1; i <= m; i++) scanf("%d", &a), cm.push_back(a);
 }
 
 void run(void)
 
 {
+  for (int i = 0; i < am.size(); i++)
 
-    for(int i=0;i<am.size();i++)
-
-        for(int j=0;j<cm.size();j++)
-
-        {
-
-            int a=am[i],c=cm[j];
-
-            if(hash.count(a+c))
-
-                gra[hash[a]].push_back(hash[a+c]);
-
-            if(hash.count(a-c))
-
-                gra[hash[a]].push_back(hash[a-c]);
-
-        }
-
-    memset(vis,0,sizeof(vis));
-
-    memset(dp,0,sizeof(dp));
-
-    int ans=0;
-
-    for(int i=0;i<am.size();i++)
+    for (int j = 0; j < cm.size(); j++)
 
     {
+      int a = am[i], c = cm[j];
 
-        int s=hash[am[i]];
+      if (hash.count(a + c)) gra[hash[a]].push_back(hash[a + c]);
 
-        if(vis[s]!=0)continue;
-
-        dfs(s);
-
-        ans+=MAX(dp[s][0],dp[s][1]);
-
+      if (hash.count(a - c)) gra[hash[a]].push_back(hash[a - c]);
     }
 
-    printf("%d\n",ans);
+  memset(vis, 0, sizeof(vis));
 
+  memset(dp, 0, sizeof(dp));
+
+  int ans = 0;
+
+  for (int i = 0; i < am.size(); i++)
+
+  {
+    int s = hash[am[i]];
+
+    if (vis[s] != 0) continue;
+
+    dfs(s);
+
+    ans += MAX(dp[s][0], dp[s][1]);
+  }
+
+  printf("%d\n", ans);
 }
 
 int main()
 
 {
+  while (scanf("%d%d", &n, &m), n + m)
 
-    while(scanf("%d%d",&n,&m),n+m)
+  {
+    re();
 
-    {
-
-        re();
-
-        run();
-
-    }
-
+    run();
+  }
 }
