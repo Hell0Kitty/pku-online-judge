1986513bbe1fa30aded3a11c88f5e290223b83cf
sdecoder <sde.work.inbox@gmail.com>
2017-07-02 03:38:52 -0400

    2017-07-02 03:38:52

--- 1555.cc
+++ 1555.cc
@@ -8,61 +8,44 @@ using namespace std;
 
 int f[maxn];
 
-void input()
-{
-    for (int i = 1; i < 9; i++)
-        scanf("%d", &f[i]);
+void input() {
+  for (int i = 1; i < 9; i++) scanf("%d", &f[i]);
 }
 
-void print()
-{
-    char ch[2];
-    ch[0] = '+';
-    ch[1] = '-';
-    int i = 0;
-    while (i < 9 && f[i] == 0)
-        i++;
-    if (i >= 9)
-    {
-        printf("0\n");
-        return;
+void print() {
+  char ch[2];
+  ch[0] = '+';
+  ch[1] = '-';
+  int i = 0;
+  while (i < 9 && f[i] == 0) i++;
+  if (i >= 9) {
+    printf("0\n");
+    return;
+  }
+  if (i == 8) {
+    printf("%d\n", f[8]);
+    return;
+  }
+  if (f[i] < 0) putchar('-');
+  if (abs(f[i]) > 1) printf("%d", abs(f[i]));
+  putchar('x');
+  if (8 - i > 1) printf("^%d", 8 - i);
+  i++;
+  for (; i < 8; i++)
+    if (f[i]) {
+      printf(" %c ", ch[f[i] < 0]);
+      if (abs(f[i]) > 1) printf("%d", abs(f[i]));
+      putchar('x');
+      if (8 - i > 1) printf("^%d", 8 - i);
     }
-    if (i == 8)
-    {
-        printf("%d\n", f[8]);
-        return;
-    }
-    if (f[i] < 0)
-        putchar('-');
-    if (abs(f[i]) > 1)
-        printf("%d", abs(f[i]));
-    putchar('x');
-    if (8 - i > 1)
-        printf("^%d", 8 - i);
-    i++;
-    for (; i < 8; i++)
-        if (f[i])
-        {
-            printf(" %c ", ch[f[i] < 0]);
-            if (abs(f[i]) > 1)
-                printf("%d", abs(f[i]));
-            putchar('x');
-            if (8 - i > 1)
-                printf("^%d", 8 - i);
-        }
-    if (f[i] != 0)
-        printf(" %c %d", ch[f[i] < 0], abs(f[i]));
-    putchar('\n');
+  if (f[i] != 0) printf(" %c %d", ch[f[i] < 0], abs(f[i]));
+  putchar('\n');
 }
 
-int main()
-{
-    while (scanf("%d", &f[0]) != EOF)
-    {
-        input();
-        print();
-    }
-    return 0;
+int main() {
+  while (scanf("%d", &f[0]) != EOF) {
+    input();
+    print();
+  }
+  return 0;
 }
-
-
