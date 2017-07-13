434c00c1710ac986868c7add408d3316959b4a38
sdecoder <sde.work.inbox@gmail.com>
2017-07-13 05:34:05 -0400

    2017-07-13 05:34:05

--- 1730.cc
+++ 1730.cc
@@ -9,30 +9,24 @@ using namespace std;
 
 long long n;
 
-void work()
-{
-    bool fu =false;
-    if (n <0)
-    {
-        fu =true;
-        n =-n;
+void work() {
+  bool fu = false;
+  if (n < 0) {
+    fu = true;
+    n = -n;
+  }
+  for (int i = 32; i >= 1; i--)
+    if (!fu || (fu && (i & 1))) {
+      double a = pow(double(n), 1.0 / i);
+      long long x = a;
+      if (abs(a - x) < eps || abs(a - x - 1) < eps) {
+        printf("%d\n", i);
+        return;
+      }
     }
-    for (int i =32; i >=1; i--)
-        if (!fu || (fu && (i &1)))
-        {
-            double a = pow(double(n), 1.0/ i);
-            long long x = a;
-            if (abs(a - x) < eps || abs(a - x -1) < eps)
-            {
-                printf("%d\n", i);
-                return;
-            }
-        }
 }
 
-int main()
-{
-    while (scanf("%lld", &n), n)
-        work();
-    return 0;
+int main() {
+  while (scanf("%lld", &n), n) work();
+  return 0;
 }
