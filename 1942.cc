1ce73823d97700d9d7c604972746a16dffa8990b
sdecoder <sde.work.inbox@gmail.com>
2017-07-14 12:22:30 -0400

    2017-07-14 12:22:30

--- 1942.cc
+++ 1942.cc
@@ -6,25 +6,17 @@ using namespace std;
 
 long long n, m;
 
-long long com(long long n, long long r)
-{
-    if (n - r < r)
-        r = n - r;
-    long long i, j, s = 1;
-    for (i = 0, j = 1; i < r; i++)
-    {
-        s *= (n - i);
-        for (; j <=r && s %j == 0; ++j) s /=j;
-    }
-    return s;
+long long com(long long n, long long r) {
+  if (n - r < r) r = n - r;
+  long long i, j, s = 1;
+  for (i = 0, j = 1; i < r; i++) {
+    s *= (n - i);
+    for (; j <= r && s % j == 0; ++j) s /= j;
+  }
+  return s;
 }
 
-int main()
-{
-    while (scanf("%lld%lld", &n, &m), n | m)
-        printf("%lld\n", com(n + m, n));
-    return 0;
+int main() {
+  while (scanf("%lld%lld", &n, &m), n | m) printf("%lld\n", com(n + m, n));
+  return 0;
 }
-
-
-
