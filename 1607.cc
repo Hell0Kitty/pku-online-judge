27596d4a892a6ac344c7483922dbf0b003fa3a28
sdecoder <sde.work.inbox@gmail.com>
2017-07-14 10:07:58 -0400

    2017-07-14 10:07:58

---
+++ 1607.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int n;
+
+int main()
+{
+    puts("Cards  Overhang");
+    while (~scanf("%d", &n))
+    {
+        double ans = 0;
+        for (int i = 1; i <= n; i++)
+            ans += 1.0 / (i * 2);
+        printf("%5d%10.3f\n", n, ans);    
+    }    
+    return 0;
+}
+
+
