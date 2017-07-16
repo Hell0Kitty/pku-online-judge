67cbf9b7a78a1be7a865ca4a6a1769613ab5af48
sdecoder <sde.work.inbox@gmail.com>
2017-07-16 05:56:22 -0400

    2017-07-16 05:56:22

--- 1859.cc
+++ 1859.cc
@@ -4,45 +4,38 @@
 using namespace std;
 
 struct Point {
-    double x, y;
+  double x, y;
 } P[20010], o;
 
 int cmp(Point a, Point b) {
-    if(a.x == b.x)
-        return a.y < b.y;
-    return a.x < b.x;
+  if (a.x == b.x) return a.y < b.y;
+  return a.x < b.x;
 }
 
-int main()
-{
-    int n;
-    while(~scanf("%d", &n) && n) {
-        bool flag = true;
-        for(int i = 1; i <= n; ++i) {
-            scanf("%lf%lf", &P[i].x, &P[i].y);
-        }
-        sort(P+1, P+n+1, cmp);
-        o.x = (P[1].x+P[n].x)/2;
-        o.y = (P[1].y+P[n].y)/2;
-        for(int i = 2; i <= n; ++i) {
-            double x = (P[i].x+P[n-i+1].x)/2;
-            double y = (P[i].y+P[n-i+1].y)/2;
-            if(x != o.x || y != o.y) {
-                flag = false;
-                                break;
-            }
-        }
-        if(flag) {
-            printf("V.I.P. should stay at (%.1lf,%.1lf).\n", o.x, o.y);
-        }
-        else {
-            printf("This is a dangerous situation!\n");
-        }
+int main() {
+  int n;
+  while (~scanf("%d", &n) && n) {
+    bool flag = true;
+    for (int i = 1; i <= n; ++i) {
+      scanf("%lf%lf", &P[i].x, &P[i].y);
     }
+    sort(P + 1, P + n + 1, cmp);
+    o.x = (P[1].x + P[n].x) / 2;
+    o.y = (P[1].y + P[n].y) / 2;
+    for (int i = 2; i <= n; ++i) {
+      double x = (P[i].x + P[n - i + 1].x) / 2;
+      double y = (P[i].y + P[n - i + 1].y) / 2;
+      if (x != o.x || y != o.y) {
+        flag = false;
+        break;
+      }
+    }
+    if (flag) {
+      printf("V.I.P. should stay at (%.1lf,%.1lf).\n", o.x, o.y);
+    } else {
+      printf("This is a dangerous situation!\n");
+    }
+  }
 
-    return 0;
+  return 0;
 }
-
-
-
-
