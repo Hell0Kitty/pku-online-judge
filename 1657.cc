ff3b64000b82115de1a9b9ea6bcd554b4b4932d3
sdecoder <sde.work.inbox@gmail.com>
2017-07-18 13:36:44 -0400

    2017-07-18 13:36:44

--- 1657.cc
+++ 1657.cc
@@ -5,48 +5,42 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
+int main() {
+  int t;
+  scanf("%d", &t);
+  getchar();
+  while (t--) {
+    int x1 = getchar() - 'a' + 1;
+    int y1 = getchar() - '0';
     getchar();
-    while (t--)
-    {
-        int x1 = getchar() - 'a' + 1;
-        int y1 = getchar() - '0';
-        getchar();
-        int x2 = getchar() - 'a' + 1;
-        int y2 = getchar() - '0';
-        int x = abs(x1 - x2);
-        int y = abs(y1 - y2);
-        getchar();
-        if (x == 0 && y == 0)
-        {
-            printf("0 0 0 0\n");
-            continue;
-        }
-
-        printf("%d ", max(x, y));
-
-        if (x == y || x == 0 || y == 0)
-            printf("1 ");
-        else
-            printf("2 ");
-
-        if (x == 0 || y == 0)
-            printf("1 ");
-        else
-            printf("2 ");
-
-        if ((x + y) & 1)
-            printf("Inf\n");
-        else if (x == y)
-            printf("1\n");
-        else
-            printf("2\n");
+    int x2 = getchar() - 'a' + 1;
+    int y2 = getchar() - '0';
+    int x = abs(x1 - x2);
+    int y = abs(y1 - y2);
+    getchar();
+    if (x == 0 && y == 0) {
+      printf("0 0 0 0\n");
+      continue;
     }
-    return 0;
+
+    printf("%d ", max(x, y));
+
+    if (x == y || x == 0 || y == 0)
+      printf("1 ");
+    else
+      printf("2 ");
+
+    if (x == 0 || y == 0)
+      printf("1 ");
+    else
+      printf("2 ");
+
+    if ((x + y) & 1)
+      printf("Inf\n");
+    else if (x == y)
+      printf("1\n");
+    else
+      printf("2\n");
+  }
+  return 0;
 }
-
-
-
