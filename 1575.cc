92d45ade99d4440e29ca2389a830b2cfed4f50bd
sdecoder <sde.work.inbox@gmail.com>
2017-08-03 18:40:01 -0400

    2017-08-03 18:40:01

--- 1575.cc
+++ 1575.cc
@@ -6,49 +6,38 @@ using namespace std;
 
 char st[50];
 
-bool v(char a)
-{
-    return a =='a' ||a =='e' ||a =='i' ||a =='o' ||a =='u';
+bool v(char a) {
+  return a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u';
 }
 
-bool accept()
-{
-    int n = strlen(st);
-    bool found = false;
-    for (int i = 0; i < n; i++)
-        if (v(st[i]))
-        {
-            found = true;
-            break;
-        }
-    if (!found)
-        return false;
-    int count = 1;
-    for (int i = 1; i < n; i++)
-    {
-        if (v(st[i]) == v(st[i - 1]))
-            count++;
-        else
-            count = 1;
-        if (count >= 3)
-            return false;
+bool accept() {
+  int n = strlen(st);
+  bool found = false;
+  for (int i = 0; i < n; i++)
+    if (v(st[i])) {
+      found = true;
+      break;
     }
-    for (int i = 1; i < n; i++)
-        if (st[i] == st[i - 1] && st[i] != 'e' && st[i] != 'o')
-            return false;
-    return true;
+  if (!found) return false;
+  int count = 1;
+  for (int i = 1; i < n; i++) {
+    if (v(st[i]) == v(st[i - 1]))
+      count++;
+    else
+      count = 1;
+    if (count >= 3) return false;
+  }
+  for (int i = 1; i < n; i++)
+    if (st[i] == st[i - 1] && st[i] != 'e' && st[i] != 'o') return false;
+  return true;
 }
 
-int main()
-{
-    while (scanf("%s", st), strcmp(st, "end"))
-    {
-        if (accept())
-            printf("<%s> is acceptable.\n", st);
-        else
-            printf("<%s> is not acceptable.\n", st);
-    }
-    return 0;
+int main() {
+  while (scanf("%s", st), strcmp(st, "end")) {
+    if (accept())
+      printf("<%s> is acceptable.\n", st);
+    else
+      printf("<%s> is not acceptable.\n", st);
+  }
+  return 0;
 }
-
-
