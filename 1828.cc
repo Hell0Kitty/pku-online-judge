49d1f403526ada062ce56e685c3c0a8d956cae70
sdecoder <sde.work.inbox@gmail.com>
2017-08-24 22:20:30 -0400

    2017-08-24 22:20:30

--- 1828.cc
+++ 1828.cc
@@ -1,64 +1,49 @@
 #include <iostream>
 #include <algorithm>
-#define F(i,a,b) for (int i=a;i<=b;i++)
-#define FD(i,a,b) for (int i=a;i>=b;i--)
+#define F(i, a, b) for (int i = a; i <= b; i++)
+#define FD(i, a, b) for (int i = a; i >= b; i--)
 
 using namespace std;
 
-struct P
-{
-	int x, y, id;
-	P()
-	{
-	}
-	P(int a, int b, int c)
-	{
-		x = a, y = b, id = c;
-	}
+struct P {
+  int x, y, id;
+  P() {}
+  P(int a, int b, int c) { x = a, y = b, id = c; }
 
-	bool operator < (const P& b) const
-	{
-		if (x==b.x)
-			return y < b.y;
-		return x < b.x;
-	}
+  bool operator<(const P& b) const {
+    if (x == b.x) return y < b.y;
+    return x < b.x;
+  }
 };
 
-P p[ 50001 ];
-int ax[ 50001 ], ay[ 50001 ];
+P p[50001];
+int ax[50001], ay[50001];
 
 int n;
 
-int main()
-{
-	int total, x, y;
+int main() {
+  int total, x, y;
 
-	while (scanf("%d",&n) && n != 0)
-	{
-		total = 0;
+  while (scanf("%d", &n) && n != 0) {
+    total = 0;
 
-		F ( i, 1, n )
-		{
-			scanf("%d%d",&x,&y);
-			p[ i ].x = x, p[ i ].y = y;
-		}
+    F(i, 1, n) {
+      scanf("%d%d", &x, &y);
+      p[i].x = x, p[i].y = y;
+    }
 
-		sort( & p[ 1 ], & p[ n + 1 ]);
+    sort(&p[1], &p[n + 1]);
 
-		total = 1;
-		int maxy = p[ n  ].y;
-		FD ( i, n-1 , 1 )
-		{
-			if ( p[ i  ].y > maxy )
-			{
-				maxy = p[ i ].y;
-				total++;
-			}
-		}
-        printf("%d\n", total) ;
-	}
+    total = 1;
+    int maxy = p[n].y;
+    FD(i, n - 1, 1) {
+      if (p[i].y > maxy) {
+        maxy = p[i].y;
+        total++;
+      }
+    }
+    printf("%d\n", total);
+  }
 
-	return 0;
+  return 0;
 }
-
-
