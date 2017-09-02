b3188980c1c712d79fc7c878d39a23f6ebfa5e56
sdecoder <sde.work.inbox@gmail.com>
2017-09-02 09:58:47 -0400

    2017-09-02 09:58:47

--- 1981.cc
+++ 1981.cc
@@ -4,64 +4,65 @@
 #include <algorithm>
 #define PI acos(-1.0)
 using namespace std;
-const int N=305;
+const int N = 305;
 int n;
-struct Point{double x,y;}p[N];
-struct Angle{double ang;bool is;}angle[N*2];
-int cmp(Angle M,Angle N){
-    return M.ang<N.ang;
+struct Point {
+  double x, y;
+} p[N];
+struct Angle {
+  double ang;
+  bool is;
+} angle[N * 2];
+int cmp(Angle M, Angle N) { return M.ang < N.ang; }
+double dist(Point a, Point b) {
+  return sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
 }
-double dist(Point a,Point b){
-    return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
+double k(Point a, Point b) {
+  double ki = atan(fabs((b.y - a.y) / (b.x - a.x)));
+  if (b.y - a.y > 0) {
+    if (b.x - a.x < 0) ki = PI - ki;
+  } else {
+    if (b.x < a.x)
+      ki += PI;
+    else
+      ki = 2 * PI - ki;
+  }
+  return ki;
 }
-double k(Point a,Point b){
-    double ki=atan(fabs((b.y-a.y)/(b.x-a.x)));
-    if(b.y-a.y>0){
-        if(b.x-a.x<0)
-        ki=PI-ki;
+void solve() {
+  int res = 1, top;
+  double dis, now, tmp;
+  for (int i = 0; i < n; i++) {
+    top = 0;
+    for (int j = 0; j < n; j++) {
+      if (i == j) continue;
+      dis = dist(p[i], p[j]);
+      if (dis > 2.0) continue;
+      now = k(p[i], p[j]);
+      tmp = acos(dis / 2.0);
+      angle[top].ang = now - tmp;
+      angle[top++].is = 1;
+      angle[top].ang = now + tmp;
+      angle[top++].is = 0;
     }
-    else{
-        if(b.x<a.x) ki+=PI;
-        else ki=2*PI-ki;
+    if (top < res) continue;
+    sort(angle, angle + top, cmp);
+    int count = 1;
+    for (int j = 0; j < top; j++) {
+      if (angle[j].is)
+        count++;
+      else
+        count--;
+      res = max(res, count);
     }
-    return ki;
+  }
+  printf("%d\n", res);
 }
-void solve(){
-    int res=1,top;
-    double dis,now,tmp;
-    for(int i=0;i<n;i++){
-        top=0;
-        for(int j=0;j<n;j++){
-            if(i==j) continue;
-            dis=dist(p[i],p[j]);
-            if(dis>2.0) continue;
-            now=k(p[i],p[j]);
-            tmp=acos(dis/2.0);
-            angle[top].ang=now-tmp;
-            angle[top++].is=1;
-            angle[top].ang=now+tmp;
-            angle[top++].is=0;
-        }
-        if(top<res) continue;
-        sort(angle,angle+top,cmp);
-        int count=1;
-        for(int j=0;j<top;j++){
-            if(angle[j].is) count++;
-            else count--;
-            res=max(res,count);
-        }
+int main() {
+  while (scanf("%d", &n) != EOF && n) {
+    for (int i = 0; i < n; i++) {
+      scanf("%lf%lf", &p[i].x, &p[i].y);
     }
-    printf("%d\n",res);
+    solve();
+  }
 }
-int main(){
-    while(scanf("%d",&n)!=EOF&&n){
-        for(int i=0;i<n;i++){
-            scanf("%lf%lf",&p[i].x,&p[i].y);
-        }
-        solve();
-    }
-}
-
-
-
-
