4022721436330fd1dd663ed9b1351e355bb7c9eb
sdecoder <sde.work.inbox@gmail.com>
2017-09-09 14:15:29 -0400

    2017-09-09 14:15:29

--- 2028.cc
+++ 2028.cc
@@ -9,46 +9,34 @@ using namespace std;
 int n, q;
 int f[maxd];
 
-void input()
-{
-    memset(f, 0, sizeof(f));
-    for (int i = 0; i < n; i++)
-    {
-        int num;
-        scanf("%d", &num);
-        for (int j = 0; j < num; j++)
-        {
-            int a;
-            scanf("%d", &a);
-            f[a]++;
-        }
+void input() {
+  memset(f, 0, sizeof(f));
+  for (int i = 0; i < n; i++) {
+    int num;
+    scanf("%d", &num);
+    for (int j = 0; j < num; j++) {
+      int a;
+      scanf("%d", &a);
+      f[a]++;
     }
+  }
 }
 
-void work()
-{
-    int temp = 0;
-    for (int i = 0; i <= 100; i++)
-    {
-        if (f[i] > f[temp])
-            temp = i;
-    }
-    if (f[temp] < q)
-        printf("0\n");
-    else
-        printf("%d\n", temp);
+void work() {
+  int temp = 0;
+  for (int i = 0; i <= 100; i++) {
+    if (f[i] > f[temp]) temp = i;
+  }
+  if (f[temp] < q)
+    printf("0\n");
+  else
+    printf("%d\n", temp);
 }
 
-int main()
-{
-
-    while (scanf("%d%d", &n, &q), n | q)
-    {
-        input();
-        work();
-    }
-    return 0;
+int main() {
+  while (scanf("%d%d", &n, &q), n | q) {
+    input();
+    work();
+  }
+  return 0;
 }
-
-
-
