3f101fd7944c20287c32bb8e47d16519af37c291
sdecoder <sde.work.inbox@gmail.com>
2017-09-20 15:53:52 -0400

    2017-09-20 15:53:52

---
+++ 1723.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define maxn 10004
+
+int n;
+int x[maxn], y[maxn];
+
+int main()
+{
+       scanf("%d", &n);
+    for (int i =0; i < n; i++)
+        scanf("%d%d", &x[i], &y[i]);
+    sort(y, y + n);
+    sort(x, x + n);
+    for (int i =0; i < n; i++)
+        x[i] -= i;
+    sort(x, x + n);
+    int a = (n +1) /2-1;
+    int ans =0;
+    for (int i =0; i < n; i++)
+        ans += abs(y[a] - y[i]) + abs(x[a] - x[i]);
+    printf("%d\n", ans);
+    return 0;
+}
+
+
