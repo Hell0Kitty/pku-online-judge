ff518cd7c35cfdc6d80b7dc4af6e8f0d798b5a0a
sdecoder <sde.work.inbox@gmail.com>
2017-10-02 02:49:37 -0400

    2017-10-02 02:49:37

--- 1666.cc
+++ 1666.cc
@@ -8,46 +8,33 @@ using namespace std;
 
 int f[maxn], g[maxn], n;
 
-void input()
-{
-    for (int i = 0; i < n; i++)
-        scanf("%d", &f[i]);
+void input() {
+  for (int i = 0; i < n; i++) scanf("%d", &f[i]);
 }
 
-void work()
-{
-    int ans = 0;
-    while (1)
-    {
-        ans++;
-        for (int i = 0; i < n; i++)
-            g[i] = f[i] / 2;
-        for (int i = 0; i < n - 1; i++)
-        {
-            f[i + 1] = f[i + 1] / 2 + g[i];
-            f[i + 1] += f[i + 1] & 1;
-        }
-        f[0] = g[n - 1] + f[0] / 2;
-        f[0] += f[0] & 1;
-        bool ok = true;
-        for (int i = 1; i < n; i++)
-            if (f[i] != f[0])
-                ok = false;
-        if (ok)
-            break;
+void work() {
+  int ans = 0;
+  while (1) {
+    ans++;
+    for (int i = 0; i < n; i++) g[i] = f[i] / 2;
+    for (int i = 0; i < n - 1; i++) {
+      f[i + 1] = f[i + 1] / 2 + g[i];
+      f[i + 1] += f[i + 1] & 1;
     }
-    printf("%d %d\n", ans, f[0]);
+    f[0] = g[n - 1] + f[0] / 2;
+    f[0] += f[0] & 1;
+    bool ok = true;
+    for (int i = 1; i < n; i++)
+      if (f[i] != f[0]) ok = false;
+    if (ok) break;
+  }
+  printf("%d %d\n", ans, f[0]);
 }
 
-int main()
-{
-    while (scanf("%d", &n), n)
-    {
-        input();
-        work();
-    }
-    return 0;
+int main() {
+  while (scanf("%d", &n), n) {
+    input();
+    work();
+  }
+  return 0;
 }
-
-
-
