0c9d637606285cac75f07b73d1b3ae0712fb78ea
sdecoder <sde.work.inbox@gmail.com>
2017-10-18 14:21:36 -0400

    2017-10-18 14:21:36

--- 1654.cc
+++ 1654.cc
@@ -5,67 +5,59 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    getchar();
-    while (t--)
-    {
-        long long ans = 0;
-        int x = 0;
-        int y = 0;
-        char ch;
-        while (ch = getchar(), ch != '5')
-        {
-            switch (ch)
-            {
-            case '1':
-                ans -= y * 2 - 1;
-                x--;
-                y--;
-                break;
-            case '2':
-                y--;
-                break;
-            case '3':
-                ans += y * 2 - 1;
-                x++;
-                y--;
-                break;
-            case '4':
-                ans -= y * 2;
-                x--;
-                break;
-            case '6':
-                ans += y * 2;
-                x++;
-                break;
-            case '7':
-                ans -= y * 2 + 1;
-                x--;
-                y++;
-                break;
-            case '8':
-                y++;
-                break;
-            case '9':
-                ans += y * 2 + 1;
-                x++;
-                y++;
-                break;
-            }
-        }
-        getchar();
-        if (ans < 0)
-            ans = -ans;
-        if (ans & 1)
-            cout << ans / 2 << ".5" << endl;
-        else
-            cout << ans / 2 << endl;
+int main() {
+  int t;
+  scanf("%d", &t);
+  getchar();
+  while (t--) {
+    long long ans = 0;
+    int x = 0;
+    int y = 0;
+    char ch;
+    while (ch = getchar(), ch != '5') {
+      switch (ch) {
+        case '1':
+          ans -= y * 2 - 1;
+          x--;
+          y--;
+          break;
+        case '2':
+          y--;
+          break;
+        case '3':
+          ans += y * 2 - 1;
+          x++;
+          y--;
+          break;
+        case '4':
+          ans -= y * 2;
+          x--;
+          break;
+        case '6':
+          ans += y * 2;
+          x++;
+          break;
+        case '7':
+          ans -= y * 2 + 1;
+          x--;
+          y++;
+          break;
+        case '8':
+          y++;
+          break;
+        case '9':
+          ans += y * 2 + 1;
+          x++;
+          y++;
+          break;
+      }
     }
-    return 0;
+    getchar();
+    if (ans < 0) ans = -ans;
+    if (ans & 1)
+      cout << ans / 2 << ".5" << endl;
+    else
+      cout << ans / 2 << endl;
+  }
+  return 0;
 }
-
-
-
