99265f9b5d84b941514a238893a300334a64cb95
sdecoder <sde.work.inbox@gmail.com>
2017-10-21 20:19:56 -0400

    2017-10-21 20:19:56

--- 1740.cc
+++ 1740.cc
@@ -1,35 +1,32 @@
-
 #include <iostream>
-#include <algorithm>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 using namespace std;
-#define MAXN 11
 
-long h[MAXN];
-long n;
+bool f[1005];
 
-bool Init() {
-  long k;
-  cin >> n;
-  for (k = 0; k < n; k++) cin >> h[k];
-  return n != 0;
-}
-
-bool GirlWin() {
-  long k;
-  if (n % 2 == 1) return true;
-  std::sort(h, h + n);
-  for (k = 0; k < n; k += 2)
-    if (h[k] != h[k + 1]) return true;
-  return false;
-}
-
-int main() {
-  while (Init()) {
-    if (GirlWin())
-      cout << 1 << endl;
-    else
-      cout << 0 << endl;
-  }
-  return 0;
+int main()
+{
+    int n;
+    while (scanf("%d", &n), n)
+    {
+        memset(f, 0, sizeof(f));
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a;
+            scanf("%d", &a);
+            if (f[a])
+                ans--;
+            else
+                ans++;
+            f[a] = !f[a];
+        }
+        if (ans)
+            printf("1\n");
+        else
+            printf("0\n");
+    }
+    return 0;
 }
