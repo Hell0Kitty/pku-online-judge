3365fb516b7721d0fe115ff29a38b06286343d30
sdecoder <sde.work.inbox@gmail.com>
2017-11-06 08:54:12 -0500

    2017-11-06 08:54:12

---
+++ 1547.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define maxn 15
+
+struct Student
+{
+    int v;
+    char name[10];
+}stu[maxn];
+
+int n;
+
+bool operator < (const Student &a, const Student &b)
+{
+    return a.v < b.v;
+}
+
+void input()
+{
+    for (int i = 0; i < n; i++)
+    {
+        int a, b, c;
+        scanf("%d%d%d", &a, &b, &c);
+        stu[i].v = a * b * c;
+        scanf("%s", stu[i].name);
+    }
+}
+
+int main()
+{
+    while (scanf("%d", &n), ~n)
+    {
+        input();
+        sort(stu, stu + n);
+        printf("%s took clay from %s.\n", stu[n - 1].name, stu[0].name);
+    }
+    return 0;
+}
+
+
