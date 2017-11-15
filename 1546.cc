981b9edd690382a994b5a99ae17d0dae0c6a09e8
sdecoder <sde.work.inbox@gmail.com>
2017-11-15 12:33:24 -0500

    2017-11-15 12:33:24

--- 1546.cc
+++ 1546.cc
@@ -1,91 +1,66 @@
-#include<iostream>
-#include<cmath>
-#include<string.h>
+#include <iostream>
+#include <cmath>
+#include <string.h>
 using namespace std;
-int n,m;
-char c[10],c1[10];
-void read(){
+int n, m;
+char c[10], c1[10];
+void read() {
+  int i, j, k;
+  while (cin >> c >> n >> m) {
+    j = 0;
 
-    int i,j,k;
-    while(cin>>c>>n>>m)
+    for (i = 0; i < strlen(c); i++)
+
     {
-        j=0;
+      if (c[i] >= '0' && c[i] <= '9') j = j * n + c[i] - '0';
 
-        for(i=0;i<strlen(c);i++)
-
-        {
-
-            if(c[i]>='0'&&c[i]<='9')
-
-                j=j*n+c[i]-'0';
-
-            if(c[i]>='A'&&c[i]<='Z')
-
-                j=j*n+c[i]-'A'+10;
-
-        }
-
-        k=0;
-
-        if(j>=pow(1.*m,7))
-
-        {
-
-            cout<<"  ERROR"<<endl;
-
-            continue;
-
-        }
-
-        if(j==0)
-
-        {
-
-            cout<<0<<endl;
-
-            continue;
-
-        }
-
-        while(j!=0)
-
-        {
-
-            int s=j%m;
-
-            if(s<=9)
-
-            c1[k++]=s+'0';
-
-            else
-
-                c1[k++]=s-10+'A';
-
-            j/=m;
-
-        }
-
-        for(i=6;i>=k;i--)
-
-            cout<<' ';
-
-        for(i=k-1;i>=0;i--)
-
-            cout<<c1[i];
-
-        cout<<endl;
-
+      if (c[i] >= 'A' && c[i] <= 'Z') j = j * n + c[i] - 'A' + 10;
     }
 
+    k = 0;
+
+    if (j >= pow(1. * m, 7))
+
+    {
+      cout << "  ERROR" << endl;
+
+      continue;
+    }
+
+    if (j == 0)
+
+    {
+      cout << 0 << endl;
+
+      continue;
+    }
+
+    while (j != 0)
+
+    {
+      int s = j % m;
+
+      if (s <= 9)
+
+        c1[k++] = s + '0';
+
+      else
+
+        c1[k++] = s - 10 + 'A';
+
+      j /= m;
+    }
+
+    for (i = 6; i >= k; i--) cout << ' ';
+
+    for (i = k - 1; i >= 0; i--) cout << c1[i];
+
+    cout << endl;
+  }
 }
 
-int main(){
+int main() {
+  read();
 
-    read();
-
-    return 0;
-
+  return 0;
 }
-
-
-
