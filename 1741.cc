0935edcb300e465265d6407240ab8cb98c808d9a
sdecoder <sde.work.inbox@gmail.com>
2017-11-27 04:55:59 -0500

    2017-11-27 04:55:59

--- 1741.cc
+++ 1741.cc
@@ -1,109 +1,93 @@
-
-#include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-const int maxn = 1e4 + 9;
-int head[maxn], lon;
-bool use[maxn];
-int a[maxn], d[maxn], top, son[maxn];
-int ans, n, m, mm;
-struct {
-  int next, to, w;
-} e[maxn << 1];
-
-void edgeini() {
-  memset(head, -1, sizeof(head));
-  lon = -1;
+#define N 10010
+#define INF 1000000000
+#define max(a,b) (a > b ? a : b)
+int first[N],f[N],size[N],can[N],list[N],d[N];
+int ans,cnt,tl,n,K,l1,l2,tot;
+struct edge{int y,next,w;}e[N << 1];
+inline void addedge(int x,int y,int z){
+   e[++ cnt].next = first[x];e[cnt].y = y;e[cnt].w = z;first[x] = cnt;
+   e[++ cnt].next = first[y];e[cnt].y = x;e[cnt].w = z;first[y] = cnt;
 }
-void edgemake(int from, int to, int w) {
-  e[++lon].to = to;
-  e[lon].w = w;
-  e[lon].next = head[from];
-  head[from] = lon;
+void clear(){
+   memset(first,0,sizeof first);
+   memset(can,-1,sizeof can);
+   cnt = 0;
+   ans = 0;
 }
-int maxson[maxn];
-int dp(int t, int from) {
-  int now, tmp = 1e5, ans;
-  son[t] = maxson[t] = 0;
-  for (int k = head[t]; k != -1; k = e[k].next) {
-    int u = e[k].to;
-    if (use[u] || u == from) continue;
-    now = dp(u, t);
-    if (maxson[now] < tmp) {
-      tmp = maxson[now];
-      ans = now;
-    }
-    son[t] += son[u];
-    maxson[t] = max(maxson[t], son[u]);
-  }
-  son[t]++;
-  maxson[t] = max(maxson[t], mm - son[t]);
-  if (maxson[t] < tmp) ans = t;
-  return ans;
+void dfs1(int x,int fa){
+   size[x] = 1;
+   list[++ tl] = x;
+   for (int i = first[x];i;i = e[i].next) if (e[i].y != fa && can[e[i].y]){
+       dfs1(e[i].y,x);
+       f[e[i].y] = x;
+       size[x] += size[e[i].y];
+   }
 }
-
-void dfs(int t, int from) {
-  a[++top] = d[t];
-  son[t] = 0;
-  for (int k = head[t], u; k != -1; k = e[k].next) {
-    u = e[k].to;
-    if (use[u] || u == from) continue;
-    d[u] = d[t] + e[k].w;
-    dfs(u, t);
-    son[t] += son[u];
-  }
-  son[t]++;
+int getroot(int x,int fa){
+   tl = 0;
+   dfs1(x,fa);
+   int pos,tmp = INF,d,y;
+   for (int i = 1;i <= tl;i ++){
+       d = 0;y = list[i];
+       for (int p = first[y];p;p = e[p].next)
+           if (e[p].y != f[y] && can[e[p].y]) d = max(d,size[e[p].y]);
+       if (y != x) d = max(d,size[x] - size[y]);
+       if (d < tmp) pos = y,tmp = d;
+   }
+   return pos;
 }
-
-int cal(int l, int r) {
-  int ret = 0, now = r;
-  for (int i = l; i <= now; i++) {
-    while (now > i && a[i] + a[now] > m) now--;
-    ret += now - i;
-  }
-  return ret;
+void dfs2(int x,int fa,int dis){
+   list[++ l1] = x;
+   d[x] = dis;
+   for (int i = first[x];i;i = e[i].next) if (e[i].y != fa && can[e[i].y]){
+       dfs2(e[i].y,x,dis + e[i].w);
+   }
 }
-
-void solve(int t) {
-  int now = dp(t, 0);
-  d[now] = top = 0;
-  use[now] = 1;
-  dfs(now, 0);
-  int ret = 0, tmp = 2;
-  for (int k = head[now]; k != -1; k = e[k].next) {
-    int u = e[k].to;
-    if (use[u]) continue;
-    sort(a + tmp, a + tmp + son[u]);
-    ret += cal(tmp, tmp + son[u] - 1);
-    tmp += son[u];
-  }
-  sort(a + 1, a + tmp);
-  ans += cal(1, tmp - 1) - ret;
-  //    printf("%d %d\n",now,ans);
-  for (int k = head[now]; k != -1; k = e[k].next) {
-    int u = e[k].to;
-    if (use[u] || son[u] == 1) continue;
-    mm = son[u];
-    solve(u);
-  }
+int getans(int *a,int l,int r){
+   int j = r,ret = 0;
+   for (int i = l;i <= r;i ++){
+       while (d[a[i]] + d[a[j]] > K && j > i) j --;
+       ret += j - i;
+       if (j == i) break;
+   }
+   return ret;
 }
-
-int main() {
-  //    freopen("in.txt","r",stdin);
-  while (scanf("%d %d", &n, &m), n && m) {
-    edgeini();
-    for (int i = 1, from, to, w; i < n; i++) {
-      scanf("%d %d %d", &from, &to, &w);
-      edgemake(from, to, w);
-      edgemake(to, from, w);
-    }
-    memset(use, 0, sizeof(use));
-    ans = 0;
-    mm = n;
-    solve(1);
-    cout << ans << endl;
-  }
-  return 0;
+inline int cmp(int i,int j){return d[i] < d[j];}
+void work(int x,int fa){
+   int root;
+   root = getroot(x,fa);
+   l1 = l2 = 0;
+   for (int i = first[root];i;i = e[i].next) if (can[e[i].y]){
+       l2 = l1;
+       dfs2(e[i].y,root,e[i].w);
+       sort(list + l2 + 1,list + l1 + 1,cmp);
+       ans -= getans(list,l2 + 1,l1);
+   }
+   list[++ l1] = root;d[root] = 0;
+   sort(list + 1,list + l1 + 1,cmp);
+   ans += getans(list,1,l1);
+   can[root] = 0;
+   for (int i = first[root];i;i = e[i].next)
+       if (can[e[i].y]) work(e[i].y,root);
+}
+char ch;
+void read(int &x){
+   while (ch = getchar(),ch < '0' || ch > '9');
+   x = ch - '0';
+   while (ch = getchar(),ch >= '0' && ch <= '9') x = x * 10 + ch - '0';
+}
+int main(){
+   while (scanf("%d%d",&n,&K),n || K){
+       clear();
+       int x,y,z;
+       for (int i = 1;i < n;i ++) read(x),read(y),read(z),addedge(x,y,z);
+       work(1,0);
+       printf("%d\n",ans);
+   }
+   return 0;
 }
