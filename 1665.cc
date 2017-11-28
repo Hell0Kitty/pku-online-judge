e2b38fdd29addc14322cd8214dfe376a72b0045f
sdecoder <sde.work.inbox@gmail.com>
2017-11-28 18:25:46 -0500

    2017-11-28 18:25:46

--- 1665.cc
+++ 1665.cc
@@ -9,19 +9,13 @@ using namespace std;
 int revolution;
 double travel_time, diameter;
 
-int main()
-{
-    int t = 0;
-    while (scanf("%lf%d%lf", &diameter, &revolution, &travel_time), revolution)
-    {
-        t++;
-        double distance = revolution * pi * diameter / 12 / 5280;    
-        double mph = distance * 3600 / travel_time;
-        printf("Trip #%d: %.2f %.2f\n", t, distance, mph);
-    }
-    return 0;
+int main() {
+  int t = 0;
+  while (scanf("%lf%d%lf", &diameter, &revolution, &travel_time), revolution) {
+    t++;
+    double distance = revolution * pi * diameter / 12 / 5280;
+    double mph = distance * 3600 / travel_time;
+    printf("Trip #%d: %.2f %.2f\n", t, distance, mph);
+  }
+  return 0;
 }
-
-
-
-
