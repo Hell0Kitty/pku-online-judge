7d32483ffaa01caa89a97adcc956d22d28f5d18a
sdecoder <sde.work.inbox@gmail.com>
2017-12-05 20:15:22 -0500

    2017-12-05 20:15:22

--- 1581.cc
+++ 1581.cc
@@ -2,38 +2,31 @@
 #include <string>
 using namespace std;
 
-int        n, winprob, winpoint, nowprob, nowpoint;
-string    winname, nowname;
+int n, winprob, winpoint, nowprob, nowpoint;
+string winname, nowname;
 
-int main()
-{
-    int        i, a, b, j;
+int main() {
+  int i, a, b, j;
 
-    cin >> n;
-    winprob = -1;
-    for (i = 0; i < n; i++)
-    {
-        cin >> nowname;
-        nowprob = 0;
-        nowpoint = 0;
-        for (j = 0; j < 4; j++)
-        {
-            cin >> a >> b;
-            if (b > 0)
-            {
-                nowprob++;
-                nowpoint = nowpoint + b + (a - 1) * 20;
-            }
-            if (nowprob > winprob || (nowprob == winprob && nowpoint < winpoint))
-            {
-                winname = nowname;
-                winprob = nowprob;
-                winpoint = nowpoint;
-            }
-        }
+  cin >> n;
+  winprob = -1;
+  for (i = 0; i < n; i++) {
+    cin >> nowname;
+    nowprob = 0;
+    nowpoint = 0;
+    for (j = 0; j < 4; j++) {
+      cin >> a >> b;
+      if (b > 0) {
+        nowprob++;
+        nowpoint = nowpoint + b + (a - 1) * 20;
+      }
+      if (nowprob > winprob || (nowprob == winprob && nowpoint < winpoint)) {
+        winname = nowname;
+        winprob = nowprob;
+        winpoint = nowpoint;
+      }
     }
-    cout << winname << " " << winprob << " " << winpoint << endl;
-    return 0;
+  }
+  cout << winname << " " << winprob << " " << winpoint << endl;
+  return 0;
 }
-
-
