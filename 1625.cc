5baef16dd6337180ab878baeb77c1b4a3c55c093
sdecoder <sde.work.inbox@gmail.com>
2017-12-07 07:46:37 -0500

    2017-12-07 07:46:37

--- 1625.cc
+++ 1625.cc
@@ -1,302 +1,235 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-struct e{
+struct e {
+  int p[51];
 
-    int p[51];
+  int end;
 
-    int end;
+} trie[501];
 
-}trie[501];
-
-int n,m,K;
+int n, m, K;
 
 int hash[600];
 
-int state=1;
+int state = 1;
 
 int f[501];
 
-void build(char c[]){
+void build(char c[]) {
+  int i, j = 0, k;
 
-    int i,j=0,k;
+  for (i = 1;;)
 
-    for(i=1;;)
+  {
+    if (trie[i].p[hash[c[j]]] == 0) trie[i].p[hash[c[j]]] = ++state;
 
-    {
+    i = trie[i].p[hash[c[j]]];
 
-        if(trie[i].p[hash[c[j]]]==0)
+    j++;
 
-            trie[i].p[hash[c[j]]]=++state;
+    if (j >= strlen(c)) break;
+  }
 
-        i=trie[i].p[hash[c[j]]];
-
-        j++;
-
-        if(j>=strlen(c))
-
-            break;
-
-    }
-
-    trie[i].end=1;
-
+  trie[i].end = 1;
 }
 
-void ac(){
+void ac() {
+  int q[501];
 
-    int q[501];
+  int l = 0, r = 0;
 
-    int l=0,r=0;
+  int i, j, k;
 
-    int i,j,k;
+  for (i = 0; i < n; i++)
 
-    for(i=0;i<n;i++)
+    if (trie[1].p[i])
 
-        if(trie[1].p[i])
+    {
+      q[++r] = trie[1].p[i];
 
-        {
-
-            q[++r]=trie[1].p[i];
-
-            f[trie[1].p[i]]=1;
-
-        }
-
-        else
-
-        {
-
-            trie[1].p[i]=1;
-
-        }
-
-    while(l<r){
-
-        i=q[++l];
-
-        for(j=0;j<n;j++)
-
-        {
-
-            if(trie[i].p[j])
-
-            {
-
-                q[++r]=trie[i].p[j];
-
-                k=f[i];
-
-                while(!trie[k].p[j])
-
-                    k=f[k];
-
-                f[trie[i].p[j]]=trie[k].p[j];
-
-                trie[trie[i].p[j]].end|=trie[trie[k].p[j]].end;
-
-            }
-
-        }
+      f[trie[1].p[i]] = 1;
 
     }
 
+    else
+
+    {
+      trie[1].p[i] = 1;
+    }
+
+  while (l < r) {
+    i = q[++l];
+
+    for (j = 0; j < n; j++)
+
+    {
+      if (trie[i].p[j])
+
+      {
+        q[++r] = trie[i].p[j];
+
+        k = f[i];
+
+        while (!trie[k].p[j]) k = f[k];
+
+        f[trie[i].p[j]] = trie[k].p[j];
+
+        trie[trie[i].p[j]].end |= trie[trie[k].p[j]].end;
+      }
+    }
+  }
 }
 
 int dp[51][501][100];
 
-void plus(int *a,int *b){
+void plus(int *a, int *b) {
+  int i, j = 0, k;
 
-    int i,j=0,k;
+  int c[101];
 
-    int c[101];
+  for (i = 0; i < 99; i++)
 
-    for(i=0;i<99;i++)
+  {
+    k = a[i] + b[i] + j;
 
-    {
+    c[i] = k % 10000;
 
-        k=a[i]+b[i]+j;
+    j = k / 10000;
+  }
 
-        c[i]=k%10000;
-
-        j=k/10000;
-
-    }
-
-    for(i=0;i<99;i++)
-
-        a[i]=c[i];
-
+  for (i = 0; i < 99; i++) a[i] = c[i];
 }
 
-void print(int *a){
+void print(int *a) {
+  int i, j, k;
 
-    int i,j,k;
+  j = 99;
 
-    j=99;
+  while (j >= 0 && a[j] == 0) j--;
 
-    while(j>=0&&a[j]==0)
+  if (j == -1)
 
-        j--;
+  {
+    cout << 0 << endl;
 
-    if(j==-1)
+    return;
+  }
 
-    {
+  cout << a[j];
+  cout.fill('0');
 
-        cout<<0<<endl;
+  for (i = j - 1; i >= 0; i--)
 
-        return;
+  {
+    cout.width(4);
 
-    }
+    cout << a[i];
+  }
 
-    cout<<a[j];cout.fill('0');
-
-    for(i=j-1;i>=0;i--)
-
-    {
-
-        cout.width(4);
-
-        cout<<a[i];
-
-    }
-
-    cout<<endl;
-
+  cout << endl;
 }
 
 void Pr(int *r)
 
 {
-
   int i;
 
-  for(i=99;i>0;i--)
+  for (i = 99; i > 0; i--)
 
-    if (r[i]!=0)
+    if (r[i] != 0) break;
 
-      break;
-
-  cout<<r[i];    
+  cout << r[i];
 
   cout.fill('0');
 
-  for(i--;i>=0;i--)
+  for (i--; i >= 0; i--)
 
-    {
-
+  {
     cout.width(4);
 
-    cout<<r[i];               
+    cout << r[i];
+  }
 
-    }
-
-  cout<<endl;  
-
+  cout << endl;
 }
 
-void loop(){
+void loop() {
+  int i, j, k, s;
 
-    int i,j,k,s;
+  for (i = 1; i <= state; i++)
 
-    for(i=1;i<=state;i++)
+    if (trie[i].end == 0) dp[0][i][0] = 1;
 
-        if(trie[i].end==0)
+  for (s = 0; s < m; s++)
 
-            dp[0][i][0]=1;
+    for (i = 1; i <= state; i++)
 
-    for(s=0;s<m;s++) 
+      if (trie[i].end)
 
-    for(i=1;i<=state;i++)
+        continue;
 
-        if(trie[i].end)
+      else
 
-            continue;
+      {
+        for (j = 0; j < n; j++)
 
-        else
+          if (trie[i].p[j])
 
-        {
+          {
+            k = trie[i].p[j];
 
-            for(j=0;j<n;j++)
+            if (trie[k].end == 0) plus(dp[s + 1][i], dp[s][k]);
 
-                if(trie[i].p[j])
+          }
 
-                {
+          else
 
-                    k=trie[i].p[j];
+          {
+            k = f[i];
 
-                    if(trie[k].end==0)
+            while (!trie[k].p[j]) k = f[k];
 
-                        plus(dp[s+1][i],dp[s][k]);
+            if (!trie[trie[k].p[j]].end)
 
-                }
+            {
+              k = trie[k].p[j];
 
-                else
-
-                {
-
-                    k=f[i];
-
-                    while(!trie[k].p[j])
-
-                        k=f[k];
-
-                    if(!trie[trie[k].p[j]].end)
-
-                    {
-
-                        k=trie[k].p[j];
-
-                        plus(dp[s+1][i],dp[s][k]);
-
-                    }
-
-                }
-
-        }
-
+              plus(dp[s + 1][i], dp[s][k]);
+            }
+          }
+      }
 }
 
-void read(){
+void read() {
+  int i, j, k;
 
-    int i,j,k;
+  char c[60];
 
-    char c[60];
+  cin >> n >> m >> K;
 
-    cin>>n>>m>>K;
+  cin >> c;
 
-    cin>>c;
+  for (i = 0; i < strlen(c); i++) hash[c[i]] = i;
 
-    for(i=0;i<strlen(c);i++)
+  for (i = 1; i <= K; i++)
 
-        hash[c[i]]=i;
+  {
+    cin >> c;
 
-    for(i=1;i<=K;i++)
+    build(c);
+  }
 
-    {
+  ac();
 
-        cin>>c;
+  loop();
 
-        build(c);
-
-    }
-
-    ac();
-
-    loop();
-
-    print(dp[m][1]);
-
+  print(dp[m][1]);
 }
 
-int main(){
+int main() {
+  read();
 
-    read();
-
-    return 0;
-
+  return 0;
 }
-
-
-
