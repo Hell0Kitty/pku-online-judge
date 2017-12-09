d5d2d231623a80eac4f4a43dc4d392dc8f6344e3
sdecoder <sde.work.inbox@gmail.com>
2017-12-09 03:36:53 -0500

    2017-12-09 03:36:53

--- 1799.cc
+++ 1799.cc
@@ -5,18 +5,15 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++)
-    {
-        double r;
-        int n;
-        scanf("%lf%d", &r, &n);
-        printf("Scenario #%d:\n%.3f\n\n", i + 1, (1 - 1 / (1 + sin(3.1415926 / n))) * r);
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    double r;
+    int n;
+    scanf("%lf%d", &r, &n);
+    printf("Scenario #%d:\n%.3f\n\n", i + 1,
+           (1 - 1 / (1 + sin(3.1415926 / n))) * r);
+  }
+  return 0;
 }
-
-
