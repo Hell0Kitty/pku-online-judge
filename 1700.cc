79683222673730704ef283e5f34e469e0cf82fd1
sdecoder <sde.work.inbox@gmail.com>
2017-12-10 11:53:12 -0500

    2017-12-10 11:53:12

--- 1700.cc
+++ 1700.cc
@@ -9,45 +9,36 @@ using namespace std;
 
 int n, f[maxn];
 
-int cal(int a, int b, int y, int z)
-{
-    return min(z + a + y + a, b + a + z + b);
+int cal(int a, int b, int y, int z) {
+  return min(z + a + y + a, b + a + z + b);
 }
 
-void work()
-{
-    int i = n -1;
-    int ans =0;
-    while (i >2)
-    {
-        ans += cal(f[0], f[1], f[i -1], f[i]);
-        i -=2;
+void work() {
+  int i = n - 1;
+  int ans = 0;
+  while (i > 2) {
+    ans += cal(f[0], f[1], f[i - 1], f[i]);
+    i -= 2;
+  }
+  if (i == 2)
+    ans += f[0] + f[1] + f[2];
+  else
+    ans += f[1];
+  printf("%d\n", ans);
+}
+
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) scanf("%d", &f[i]);
+    sort(f, f + n);
+    if (n == 1) {
+      printf("%d\n", f[0]);
+      continue;
     }
-    if (i ==2)
-        ans += f[0] + f[1] + f[2];
-    else
-        ans += f[1];
-    printf("%d\n", ans);
+    work();
+  }
+  return 0;
 }
-
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    while (t--)
-    {
-        scanf("%d", &n);
-        for (int i =0; i < n; i++)
-            scanf("%d", &f[i]);
-        sort(f, f + n);
-        if (n ==1)
-        {
-            printf("%d\n", f[0]);
-            continue;
-        }
-        work();
-    }
-    return 0;
-}
-
-
