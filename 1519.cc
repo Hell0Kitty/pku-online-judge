b4f1670cb0c9de2e4253b5fd80d9af5bcab87978
sdecoder <sde.work.inbox@gmail.com>
2017-12-14 18:44:39 -0500

    2017-12-14 18:44:39

--- 1519.cc
+++ 1519.cc
@@ -1,22 +1,20 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 const int maxn = 1005;
-char a[ maxn ];
-int main(){
-    while( scanf("%s",a)!=EOF ){
-        if( strcmp(a,"0")==0 ) break;
-        int len;
-        len=strlen( a );
-        int ans=0;
-        for( int i=0;i<len;i++ ){
-            ans+=( a[ i ]-'0' );
-            if( ans>=10 ){
-                ans=ans/10+ans%10;
-            }
-        }
-        printf("%d\n",ans);
+char a[maxn];
+int main() {
+  while (scanf("%s", a) != EOF) {
+    if (strcmp(a, "0") == 0) break;
+    int len;
+    len = strlen(a);
+    int ans = 0;
+    for (int i = 0; i < len; i++) {
+      ans += (a[i] - '0');
+      if (ans >= 10) {
+        ans = ans / 10 + ans % 10;
+      }
     }
-    return 0;
+    printf("%d\n", ans);
+  }
+  return 0;
 }
-
-
