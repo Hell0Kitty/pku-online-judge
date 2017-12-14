fd86da610b542c9892758ba920ffdd0ab270fe3d
sdecoder <sde.work.inbox@gmail.com>
2017-12-14 13:38:17 -0500

    2017-12-14 13:38:17

--- 1907.cc
+++ 1907.cc
@@ -1,118 +1,94 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int n,m,num;
+int n, m, num;
 
-struct e{
+struct e {
+  char c[20];
 
-    char c[20];
-
-    int sum;
-
+  int sum;
 };
 
 e a[101];
 
-int cmp(const void *a,const void *b){
+int cmp(const void *a, const void *b) {
+  e s, t;
 
-    e s,t;
+  s = *(e *)a;
+  t = *(e *)b;
 
-    s=*(e*)a;t=*(e*)b;
+  if (s.sum != t.sum)
 
-    if(s.sum!=t.sum)
+    return s.sum - t.sum;
 
-        return s.sum-t.sum;
+  else
 
-    else
-
-        return strcmp(s.c,t.c);
-
+    return strcmp(s.c, t.c);
 }
 
-void read(){
+void read() {
+  int i, j, k, s, t;
 
-    int i,j,k,s,t;
+  cin >> k;
 
-    cin>>k;
+  for (int cas = 1; cas <= k; cas++)
 
-    for(int cas=1;cas<=k;cas++)
+  {
+    printf("Case %d\n", cas);
+
+    cin >> n >> m >> num;
+
+    for (i = 0; i < num; i++)
 
     {
+      char c;
 
-        printf("Case %d\n",cas);
+      int top = 0;
 
-        cin>>n>>m>>num;
+      while (cin >> c)
 
-        for(i=0;i<num;i++)
+      {
+        if (c == ':') break;
 
-        {
+        a[i].c[top++] = c;
+      }
 
-            char c;
+      a[i].c[top] = '\0';
 
-            int top=0;
+      cin >> s >> c >> t;
 
-            while(cin>>c)
+      int n1 = n, m1 = m;
 
-            {
+      int ans = 0;
 
-                if(c==':')
+      while (n1 / 2 >= m)
 
-                    break;
+      {
+        ans += min(t, (n1 - n1 / 2) * s);
 
-                a[i].c[top++]=c;
+        n1 /= 2;
+      }
 
-            }
+      while (n1 != m)
 
-            a[i].c[top]='\0';
+      {
+        ans += s;
 
-            cin>>s>>c>>t;
+        n1--;
+      }
 
-            int n1=n,m1=m;
-
-            int ans=0;
-
-            while(n1/2>=m)
-
-            {
-
-                ans+=min(t,(n1-n1/2)*s);
-
-                n1/=2;
-
-            }
-
-            while(n1!=m)
-
-            {
-
-                ans+=s;
-
-                n1--;
-
-            }
-
-            a[i].sum=ans;
-
-        }
-
-        qsort(a,num,sizeof(e),cmp);
-
-        for(i=0;i<num;i++)
-
-            cout<<a[i].c<<' '<<a[i].sum<<endl;
-
+      a[i].sum = ans;
     }
 
+    qsort(a, num, sizeof(e), cmp);
+
+    for (i = 0; i < num; i++) cout << a[i].c << ' ' << a[i].sum << endl;
+  }
 }
 
-int main(){
+int main() {
+  read();
 
-    read();
-
-    return 0;
-
+  return 0;
 }
-
-
-
