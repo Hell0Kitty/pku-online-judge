8ed0bbfe04b9050c001d3cf8cd4c97f66c3b0f25
sdecoder <sde.work.inbox@gmail.com>
2017-12-17 10:59:46 -0500

    2017-12-17 10:59:46

--- 1804.cc
+++ 1804.cc
@@ -9,56 +9,41 @@ using namespace std;
 int n;
 int a[maxn], cnt, c[maxn];
 
-void input()
-{
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
+void input() {
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) scanf("%d", &a[i]);
 }
 
-void MergeSort(int l, int r)
-{
-    int mid, i, j, tmp;
-    if (r > l + 1)
-    {
-        mid = (l + r) / 2;
-        MergeSort(l, mid);
-        MergeSort(mid, r);
-        tmp = l;
-        for ( i = l, j = mid;  i < mid && j < r; )
-        {
-            if (a[i] > a[j])
-            {
-                c[tmp++] = a[j++];
-                cnt += mid - i;
-            }
-            else
-                c[tmp++] = a[i++];
-        }
-        if (j < r)
-            for (; j < r; ++j)
-                c[tmp++] = a[j];
-        else
-            for(; i < mid; ++i)
-                c[tmp++] = a[i];
-        for (i = l; i < r; ++i)
-            a[i] = c[i];
+void MergeSort(int l, int r) {
+  int mid, i, j, tmp;
+  if (r > l + 1) {
+    mid = (l + r) / 2;
+    MergeSort(l, mid);
+    MergeSort(mid, r);
+    tmp = l;
+    for (i = l, j = mid; i < mid && j < r;) {
+      if (a[i] > a[j]) {
+        c[tmp++] = a[j++];
+        cnt += mid - i;
+      } else
+        c[tmp++] = a[i++];
     }
+    if (j < r)
+      for (; j < r; ++j) c[tmp++] = a[j];
+    else
+      for (; i < mid; ++i) c[tmp++] = a[i];
+    for (i = l; i < r; ++i) a[i] = c[i];
+  }
 }
 
-int main()
-{
-    int t;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++)
-    {
-        input();
-        cnt = 0;
-        MergeSort(0, n);
-        printf("Scenario #%d:\n%d\n\n", i + 1, cnt);
-    }
-    return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    input();
+    cnt = 0;
+    MergeSort(0, n);
+    printf("Scenario #%d:\n%d\n\n", i + 1, cnt);
+  }
+  return 0;
 }
-
-
-
