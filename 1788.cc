6acb809e7a221ef958d67a57b4c7db7efb309612
sdecoder <sde.work.inbox@gmail.com>
2017-12-17 13:06:30 -0500

    2017-12-17 13:06:30

--- 1788.cc
+++ 1788.cc
@@ -1,37 +1,22 @@
-#include<cstdio>
-#include<cstring>
-#include<algorithm>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
 using namespace std;
-struct data
-{
-    int x,y;
-}po[101000];
-bool compx(data a,data b)
-{
-    return (a.x!=b.x)?a.x<b.x:a.y<b.y;
+struct data {
+  int x, y;
+} po[101000];
+bool compx(data a, data b) { return (a.x != b.x) ? a.x < b.x : a.y < b.y; }
+bool compy(data a, data b) { return (a.y != b.y) ? a.y < b.y : a.x < b.x; }
+int main() {
+  int n;
+  while (scanf("%d", &n), n) {
+    for (int i = 0; i < n; i++) scanf("%d%d", &po[i].x, &po[i].y);
+    int ans = 0;
+    sort(po, po + n, compx);
+    for (int i = 0; i < n - 1; i += 2) ans += po[i + 1].y - po[i].y;
+    sort(po, po + n, compy);
+    for (int i = 0; i < n - 1; i += 2) ans += po[i + 1].x - po[i].x;
+    printf("The length of the fence will be %d units.\n", ans);
+  }
+  return 0;
 }
-bool compy(data a,data b)
-{
-    return (a.y!=b.y)?a.y<b.y:a.x<b.x;
-}
-int main()
-{
-    int n;
-    while(scanf("%d",&n),n)
-    {
-        for(int i=0;i<n;i++)
-            scanf("%d%d",&po[i].x,&po[i].y);
-        int ans=0;
-        sort(po,po+n,compx);
-        for(int i=0;i<n-1;i+=2)
-            ans+=po[i+1].y-po[i].y;
-        sort(po,po+n,compy);
-        for(int i=0;i<n-1;i+=2)
-            ans+=po[i+1].x-po[i].x;
-        printf("The length of the fence will be %d units.\n",ans);
-    }
-    return 0;
-}
-
-
-
