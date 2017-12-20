698491c39ed54c39b8864e0838bb0a71871fe9a6
sdecoder <sde.work.inbox@gmail.com>
2017-12-20 13:22:51 -0500

    2017-12-20 13:22:51

--- 1680.cc
+++ 1680.cc
@@ -1,53 +1,42 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 int id, A, N, K, num;
 int ok, st;
-void work(int tag, int A, int loop)
-{
-	int pid = id;
-	if(ok == 1)
-		return;
-	for(int i=loop; i<N; i++)
-	{
-		if(tag != 0)
-		{
-			num++;
-			if(num == K)
-			{
-				printf("Loop %d: Process ID=%d\n", i, pid);
-				ok = 1;
-				return;
-			}
-			A += 7;
-			id++;
-			work(tag*0, A, i);
-		}
-		if(tag == 0)
-		{
-			num++;
-			if(num == K)
-			{
-				printf("Process ID=%d, A=%d\n", pid, A);
-				ok = 1;
-				return;
-			}
-		}
-		tag++;
-	} 
+void work(int tag, int A, int loop) {
+  int pid = id;
+  if (ok == 1) return;
+  for (int i = loop; i < N; i++) {
+    if (tag != 0) {
+      num++;
+      if (num == K) {
+        printf("Loop %d: Process ID=%d\n", i, pid);
+        ok = 1;
+        return;
+      }
+      A += 7;
+      id++;
+      work(tag * 0, A, i);
+    }
+    if (tag == 0) {
+      num++;
+      if (num == K) {
+        printf("Process ID=%d, A=%d\n", pid, A);
+        ok = 1;
+        return;
+      }
+    }
+    tag++;
+  }
 }
-int main()
-{
-	int t;
-	scanf("%d", &t);
-	while(t--)
-	{
-		scanf("%d%d", &N, &K);
-		num = ok = 0;
-		id = 1000;
-		work(1, 0, 0);
-	}
-	return 0;
+int main() {
+  int t;
+  scanf("%d", &t);
+  while (t--) {
+    scanf("%d%d", &N, &K);
+    num = ok = 0;
+    id = 1000;
+    work(1, 0, 0);
+  }
+  return 0;
 }
-
-
