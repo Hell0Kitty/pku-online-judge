d57bd12307fb47d565fe02b87bbb7e1cc88efe6e
sdecoder <sde.work.inbox@gmail.com>
2017-12-21 14:25:11 -0500

    2017-12-21 14:25:11

--- 1905.cc
+++ 1905.cc
@@ -9,45 +9,35 @@ using namespace std;
 
 double c, l1, n;
 
-double cal(double b)
-{
-    double a = l1 /2;
-    double d = sqrt(a * a + b * b);
-    double h = a * d / b;
-    double r = sqrt(h * h + d * d) /2;
-    return atan(b / (l1 /2)) *4* r;
+double cal(double b) {
+  double a = l1 / 2;
+  double d = sqrt(a * a + b * b);
+  double h = a * d / b;
+  double r = sqrt(h * h + d * d) / 2;
+  return atan(b / (l1 / 2)) * 4 * r;
 }
 
-int main()
-{
-   while (scanf("%lf%lf%lf", &l1, &n, &c) != EOF)
-    {
-        if (n <0&& l1 <0&& c <0)
-            break;
-        double l2 = l1 * (n * c +1);
-        if (l2 - l1 < eps)
-        {
-            printf("0.000\n");
-            continue;
-        }
-        double l = eps;
-        double r = l1 /2;
-        double mid;
-        while (abs(l - r) > eps)
-        {
-            mid = (l + r) /2;
-            double x = cal(mid);
-            if (abs(x - l2) < eps)
-                break;
-            if (x > l2)
-                r = mid - eps;
-            else
-                l = mid + eps;
-        }
-        printf("%.3f\n", mid);
+int main() {
+  while (scanf("%lf%lf%lf", &l1, &n, &c) != EOF) {
+    if (n < 0 && l1 < 0 && c < 0) break;
+    double l2 = l1 * (n * c + 1);
+    if (l2 - l1 < eps) {
+      printf("0.000\n");
+      continue;
     }
-    return 0;
+    double l = eps;
+    double r = l1 / 2;
+    double mid;
+    while (abs(l - r) > eps) {
+      mid = (l + r) / 2;
+      double x = cal(mid);
+      if (abs(x - l2) < eps) break;
+      if (x > l2)
+        r = mid - eps;
+      else
+        l = mid + eps;
+    }
+    printf("%.3f\n", mid);
+  }
+  return 0;
 }
-
-
-
