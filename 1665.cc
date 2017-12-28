aba3cda7f9794e2927db4ce53496e05d56ff48dc
sdecoder <sde.work.inbox@gmail.com>
2017-12-28 11:59:01 -0500

    2017-12-28 11:59:01

---
+++ 1665.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+
+#define pi 3.1415927
+
+int revolution;
+double travel_time, diameter;
+
+int main()
+{
+    int t = 0;
+    while (scanf("%lf%d%lf", &diameter, &revolution, &travel_time), revolution)
+    {
+        t++;
+        double distance = revolution * pi * diameter / 12 / 5280;    
+        double mph = distance * 3600 / travel_time;
+        printf("Trip #%d: %.2f %.2f\n", t, distance, mph);
+    }
+    return 0;
+}
+
+
+
+
